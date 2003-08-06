215808605393c5be80840eadc4676086e4a91042
Harry Moffat <hkmoffa@sandia.gov>
2003-08-06 17:32:02 +0000

    Added a simple test that calculate a rate of production for a surface mechanism

---
+++ test_problems/surfkin/Interface.h
@@ -0,0 +1,100 @@
+#ifndef CXX_INTERFACE
+#define CXX_INTERFACE
+
+#include <string>
+
+#include "kernel/SurfPhase.h"
+#include "kernel/InterfaceKinetics.h"
+#include "kernel/importCTML.h"
+
+namespace Cantera {
+
+    /**
+     * The class interface inherits from both SurfPhase and
+     * InterferFaceKinetics
+     */
+    class Interface : 
+        public SurfPhase, public InterfaceKinetics
+    {
+    public:
+	/**
+	 *
+	 * Constructor for the interface class:
+	 *
+	 *  infile = name of the file to get information about the
+	 *           surface.
+	 *  id = name of the surface phase.
+	 *  phases = Pointer to the list of volume phases that participate
+	 *           in the interface
+	 *
+	 */
+        Interface(string infile, string id, vector<ThermoPhase*> phases) 
+            : m_ok(false), m_r(0) {
+            string path = findInputFile(infile);
+            ifstream fin(path.c_str());
+            if (!fin) {
+                throw CanteraError("Interface","could not open "
+                    +path+" for reading.");
+            }
+   
+
+	    /*
+	     * Create a top level xml node
+	     */
+            m_r = new XML_Node("-");
+	    /*
+	     * Fill the XML_Node with all of the information in the
+	     * xml file
+	     */
+            m_r->build(fin);
+
+
+	    /*
+	     * Find the start of the surface phase data in the xml file
+	     * Store a pointer to the position in the tree.
+	     */
+            XML_Node* x = find_XML("", m_r, id, "", "");
+            if (!x) {
+	      throw CanteraError("Interface","error in find_XML");
+	    }
+
+	    /*
+	     * Import the values of the surface into the current object.
+	     * Note, since the current object inherits from SurfPhase
+	     * object, it contains all of the surfphase fields of the 
+	     * object. This operation fills all of those fields.
+	     */
+            importPhase(*x, this);
+            
+            phases.push_back(this);
+            importKinetics(*x, phases, this);
+            m_ok = true;
+        }
+
+	/**
+	 * Destructor for the Interface class. This is a virtual function, meaning
+	 * that after, it finishes with this class, it will call the destructor
+	 * functions for the two classes that Interface inherits from.
+	 */
+        virtual ~Interface() {
+	  /*
+	   * We created the XML data tree structure for the Interface object
+	   * in the constructor. Here, we delete the top level. of the structure.
+	   * I believe this delete all of the daughter elements as well.
+	   */
+	  delete m_r;
+	}
+
+        bool operator!() { return !m_ok;}
+        bool ready() { return m_ok; }
+
+    protected:
+        bool m_ok;
+        XML_Node* m_r;
+
+    private:
+    };
+}
+
+
+#endif
---
+++ test_problems/surfkin/surfdemo.cpp
@@ -0,0 +1,41 @@
+//
+//     Replace this sample main program with your program
+//
+//
+
+#include "Cantera.h"
+#include "IdealGasMix.h"
+#include "Interface.h"
+
+using namespace Cantera;
+
+int main() {
+
+    try {
+        IdealGasMix gas("gri30.xml");
+        gas.setState_TPX(1200.0, OneAtm, 
+            "H2:2, O2:1, OH:0.01, H:0.01, O:0.01");
+
+        vector<ThermoPhase*> phases;
+        phases.push_back(&gas);
+        Interface surf("surface.xml", "surface", phases);
+        vector_fp cov;
+        cov.push_back(0.8);
+        cov.push_back(0.2);
+        surf.setCoverages(cov.begin());
+        vector_fp wdot(gas.nSpecies() + surf.nSpecies());
+        surf.getNetProductionRates(wdot.begin());
+        int k;
+        for (k = 0; k < gas.nSpecies(); k++)
+            cout << gas.speciesName(k) << "   " << wdot[k] << endl;
+        for (k = 0; k < surf.nSpecies(); k++)
+            cout << surf.speciesName(k) << "   " 
+                 << wdot[k+gas.nSpecies()] << endl;
+        
+    }
+    catch (CanteraError) {
+        showErrors(cout);
+    }
+
+}
+      
