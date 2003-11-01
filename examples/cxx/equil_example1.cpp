f9fc73ac4ed74aee61b6d81e3cf3638946f4da91
Dave Goodwin <dgg@caltech.edu>
2003-11-01 10:08:09 +0000

    *** empty log message ***

--- examples/cxx/equil_example1.cpp
+++ examples/cxx/equil_example1.cpp
@@ -70,7 +70,7 @@ int equil_example1(int job) {
 
         // create a gas mixture, and set its state
 
-        IdealGasMix gas("silane.cti", "silane");
+        IdealGasMix gas("silane.xml", "silane");
         int nsp = gas.nSpecies();
 
         int ntemps = 50;   // number of temperatures
