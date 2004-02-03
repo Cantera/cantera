d86a6bf09799e98342bb5d8653fb04739d0c1752
Dave Goodwin <dgg@caltech.edu>
2004-02-03 03:33:42 +0000

    initial import

---
+++ Cantera/src/EdgePhase.h
@@ -0,0 +1,40 @@
+/**
+ *
+ *  @file EdgePhase.h
+ *
+ */
+
+/*  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright 2002 California Institute of Technology
+ *
+ */
+
+
+#ifndef CT_EDGEPHASE_H
+#define CT_EDGEPHASE_H
+
+#include "mix_defs.h"
+#include "ThermoPhase.h"
+
+namespace Cantera {
+
+    class EdgePhase : public SurfPhase  {
+
+    public:
+
+        EdgePhase(doublereal n0 = 0.0);
+        virtual ~EdgePhase() {}
+        virtual int eosType() const { return cEdge; }
+
+    };
+}
+        
+#endif
+
+
+
+
+
