11d73d12c1fd8e4d8a79c7d9a9c82034d6e6b81f
Harry Moffat <hkmoffa@sandia.gov>
2004-07-29 22:14:33 +0000

    Added depends target to makefiles

--- test_problems/silane_equil/silane_equil.cpp
+++ test_problems/silane_equil/silane_equil.cpp
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
   try {
     IdealGasMix g("silane.xml", "silane");
     g.setState_TPX(1500.0, 100.0, "SIH4:0.01, H2:0.99");
+    //g.setState_TPX(1500.0, 1.0132E5, "SIH4:0.01, H2:0.99");
     equilibrate(g, TP);
     cout << g;
     return 0;
