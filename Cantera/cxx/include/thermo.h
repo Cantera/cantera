218f2f43b14f42a3003b2b30138212f4db3a80a7
Harry Moffat <hkmoffa@sandia.gov>
2005-10-24 18:03:12 +0000

    Added a thermo.h file to the cxx directory. This file is analogous to the transport.h file.

---
+++ Cantera/cxx/include/thermo.h
@@ -0,0 +1,14 @@
+/**
+ * @file transport.h
+ *
+ * Support for thermo property calculation from C++ application programs.
+ * This header file includes several headers from the Cantera kernel needed
+ * to evaluate thermo properties.
+ */
+
+#ifndef CT_THERMO_INCL
+#define CT_THERMO_INCL
+
+#include "kernel/thermo/SingleSpeciesTP.h"
+
+#endif
