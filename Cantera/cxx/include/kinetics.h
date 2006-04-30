11b1d34752469bf6817cbf1d9aac2e2e031f8587
Harry Moffat <hkmoffa@sandia.gov>
2006-04-30 19:56:06 +0000

    Added a couple more include kinetics files.

--- Cantera/cxx/include/kinetics.h
+++ Cantera/cxx/include/kinetics.h
@@ -1,6 +1,11 @@
+/*
+ * $Id$
+ */
 #ifndef CXX_INCL_KINETICS
 #define CXX_INCL_KINETICS
 
 #include "kernel/Kinetics.h"
+#include "kernel/InterfaceKinetics.h"
+#include "kernel/KineticsFactory.h"
 
 #endif
