0b8e0297bf1af78720eca8fb407dd126be429ba0
Harry Moffat <hkmoffa@sandia.gov>
2006-06-23 21:04:30 +0000

    Added gri30.xml, so this example will work under the none python option.

--- examples/cxx/kinetics_example1.cpp
+++ examples/cxx/kinetics_example1.cpp
@@ -53,7 +53,7 @@ int kinetics_example1(int job) {
 
         // create an ideal gas mixture that corresponds to GRI-Mech
         // 3.0
-        IdealGasMix* gg = new IdealGasMix("gri30.cti", "gri30");
+        IdealGasMix* gg = new IdealGasMix("gri30.xml", "gri30");
         IdealGasMix& gas = *gg;
 
         // set the state
