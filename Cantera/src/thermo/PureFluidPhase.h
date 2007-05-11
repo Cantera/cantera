ea97be3421f80bdc6295be45c6ec13a15c5a3801
Harry Moffat <hkmoffa@sandia.gov>
2007-05-11 17:21:33 +0000

    Added a specific include config.h command, because doxygen wasn't recognizing it.

--- Cantera/src/thermo/PureFluidPhase.h
+++ Cantera/src/thermo/PureFluidPhase.h
@@ -21,10 +21,14 @@
 #define CT_EOS_TPX_H
 
 #include "ThermoPhase.h"
-
+/*
+ * For doxygen to recognize the ifdef below, it seems necessary to 
+ * include a specific include reference.
+ */
+#include "../../../config.h"
 /**
  * This object is only available if the WITH_PURE_FLUIDS optional compile
- * capability has been turned on in Cantera's makefile system. 
+ * capability has been turned on in Cantera's makefile system.  
  */
 #ifdef WITH_PURE_FLUIDS
 
