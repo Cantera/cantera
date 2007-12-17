dc94ba0decdf6a26a46e49184c96abfa5e841d0d
Dave Goodwin <dgg@caltech.edu>
2007-12-17 07:51:37 +0000

    *** empty log message ***

--- Cantera/src/spectra/rotor.h
+++ Cantera/src/spectra/rotor.h
@@ -1,3 +1,8 @@
+/**
+ * @file rotor.h
+ * Header file for class Rotor.
+ */
+
 #ifndef CT_ROTOR
 #define CT_ROTOR
 
@@ -5,6 +10,10 @@
 
 namespace Cantera {
 
+    /**
+     * Class Rotor represents a non-rigid quantum-mechanical rotor.
+     * @ingroup Spectroscopy
+     */
     class Rotor {
     public:
         Rotor() {}
@@ -16,14 +25,22 @@ namespace Cantera {
             doublereal Dv = 0.0, doublereal Hv = 0.0);
 
 
-        // energy in wavenumbers
+        /// energy in wavenumbers
         doublereal energy_w(int J);
 
-        doublereal degeneracy(int J);
+        /// degeneracy
+        int degeneracy(int J);
 
         doublereal partitionFunction(doublereal T, int cutoff=-1);
 
         doublereal frequency(int J_lower, int J_upper);
+
+        doublereal relPopulation(int J, doublereal T);
+
+        doublereal population(int J, doublereal T) {
+            return relPopulation(J,T)/partitionFunction(T);
+        }
+        doublereal intensity(int J_lower, int J_upper, doublereal T);
 
     protected:
 
--- test_problems/spectroscopy/spectratest.cpp
+++ test_problems/spectroscopy/spectratest.cpp
@@ -6,9 +6,20 @@ using namespace std;
 using namespace Cantera;
 
 int main() {
-
-    Rotor* r = new Rotor(1.0);
-    double w = 8065.0;
-    cout << "eV: " << wnum_to_eV(w) << endl;
-    delete r;
+    double B;
+    double T;
+    cout << "enter B, T: ";
+    cin >> B >> T;
+    Rotor* r = new Rotor(B);
+    double theta = wnum_to_J(B)/Boltzmann;
+    cout << "theta = " << theta << endl;
+    double pop, cpop = 0.0;
+    for (int j = 0; j < 50; j++) {
+        pop = r->population(j, T);
+        cpop += pop;
+        if (cpop > 0.999) break;
+        cout << j << ", " << r->energy_w(j) << ", " 
+             << r->frequency(j, j+1) << ", " 
+             << pop << ", " << cpop << ", " << r->partitionFunction(T) << ", " << T/theta << endl;
+    }
 }
