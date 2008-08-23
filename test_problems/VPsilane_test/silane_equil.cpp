88d1fa57c01028b81a5231354d2ea3800f92a896
Harry Moffat <hkmoffa@sandia.gov>
2008-08-23 01:00:53 +0000

    Added a test

---
+++ test_problems/VPsilane_test/silane_equil.cpp
@@ -0,0 +1,39 @@
+
+/*
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright 2002 California Institute of Technology
+ *
+ */
+
+#include "Cantera.h"
+#include "IdealGasMix.h"
+#include "equilibrium.h"
+#include "kernel/IdealSolnGasVPSS.h"
+#include "kernel/ThermoFactory.h"
+
+using namespace std;
+using namespace Cantera;
+
+int main(int argc, char **argv) {
+  try {
+    Cantera::IdealSolnGasVPSS gg("silane.xml", "silane");
+     ThermoPhase *g = &gg; 
+    //ThermoPhase *g = newPhase("silane.xml", "silane");
+    g->setState_TPX(1500.0, 100.0, "SIH4:0.01, H2:0.99");
+    //g.setState_TPX(1500.0, 1.0132E5, "SIH4:0.01, H2:0.99");
+    Cantera::ChemEquil_print_lvl = 40;
+    equilibrate(*g, "TP");
+    std::string r = Cantera::report(*g, true);
+    cout << r;
+    cout << endl;
+    return 0;
+  }
+  catch (CanteraError) {
+    showErrors(cerr);
+    cerr << "program terminating." << endl;
+    return -1;
+  }
+}
