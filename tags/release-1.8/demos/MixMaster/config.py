d58512c472f325e2364b062ea2d31f41cd3a256b
dggoodwin07 <dggoodwin07@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2009-03-12 15:51:07 +0000

    release 1.8

---
+++ tags/release-1.8/demos/MixMaster/config.py
@@ -0,0 +1,7 @@
+
+from Cantera import *
+
+# thermo parametrizations
+#from Cantera.Species.Thermo.NasaPolynomial import NasaPolynomial
+
+
---
+++ tags/release-1.8/include/core.h
@@ -0,0 +1,9 @@
+#ifndef CT_CORE_H
+#define CT_CORE_H
+
+#include "kernel/ThermoPhase.h"
+#include "kernel/Kinetics.h"
+#include "kernel/ctml.h"
+ 
+#endif
+
