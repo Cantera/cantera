509fb059c880e2870b0c29b3cfe67ff5354a3de3
Harry Moffat <hkmoffa@sandia.gov>
2009-03-19 00:51:21 +0000

    Updated the demo

--- Cantera/python/examples/transport/dustygas.py
+++ Cantera/python/examples/transport/dustygas.py
@@ -16,7 +16,7 @@ from Cantera.DustyGasTransport import *
 g = importPhase('h2o2.cti')
 
 # set the gas state
-g.set(T = 500.0, P = OneAtm, X = "OH:1, H:2, O2:3")
+g.set(T = 500.0, P = OneAtm, X = "OH:1, H:2, O2:3, O:1.0E-8, H2:1.0E-8, H2O:1.0E-8, H2O2:1.0E-8, HO2:1.0E-8, AR:1.0E-8")
 
 # create a Dusty Gas transport manager for this phase
 d = DustyGasTransport(g)
@@ -26,7 +26,7 @@ d.set(porosity = 0.2, tortuosity = 4.0,
       pore_radius = 1.5e-7, diameter = 1.5e-6)  # lengths in meters
 
 # print the multicomponent diffusion coefficients
-#print d.multiDiffCoeffs()
+print d.multiDiffCoeffs()
 
 # compute molar species fluxes
 state1 = g.saveState()
@@ -36,3 +36,4 @@ state2 = g.saveState()
 delta = 0.001
 
 print d.molarFluxes(state1, state1, delta)
+print d.molarFluxes(state1, state2, delta)
