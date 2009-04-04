0d32e517bda14596df0624b170ba51d588607c56
Harry Moffat <hkmoffa@sandia.gov>
2009-04-04 04:00:19 +0000

    Added in a conditional compilation of vcs_equilibrate().

--- Cantera/clib/src/ctmultiphase.cpp
+++ Cantera/clib/src/ctmultiphase.cpp
@@ -230,8 +230,15 @@ extern "C" {
 					    doublereal rtol, int maxsteps,
 					    int maxiter, int loglevel) { 
     try {
+#ifdef WITH_VCSNONIDEAL
       int retn = vcs_equilibrate(*_mix(i), XY, estimateEquil, printLvl, solver,
 				 rtol, maxsteps, maxiter, loglevel);
+#else
+      int retn = -1;
+      throw CanteraError("mix_vcs_equilibrate", 
+      "The VCS NonIdeal equilibrium solver isn't compiled in\n"
+      " To use this feature add export WITH_VCS_NONIDEAL='y' to the preconfig file");
+#endif
       return (double) retn;
     }
     catch (CanteraError) {
