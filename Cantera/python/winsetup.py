d7f9bb305efa6bf57c34affbcbc0aa2ad2848ab5
Harry Moffat <hkmoffa@sandia.gov>
2009-04-22 00:32:34 +0000

    Added a numpy directory

--- Cantera/python/winsetup.py
+++ Cantera/python/winsetup.py
@@ -2,6 +2,9 @@ from distutils.core import setup, Extension
 
 libdir = ['../../build/lib/i686-pc-win32']
     
+#  Below I switched to / from \ because python was interpreting \n in the
+#  string \numpy as a new line character
+#
 setup(name="Cantera",
           version="1.8.0",
           description="The Cantera Python Interface",
@@ -17,7 +20,8 @@ setup(name="Cantera",
           ext_modules=[ Extension("Cantera._cantera",
                                   ["src/pycantera.cpp"],
                                   include_dirs=["../../build/include",
-                                                "src", "../clib/src"],
+                                                "src", "../clib/src",
+                                                "c:\python26/lib/site-packages/numpy/core/include"],
                                   library_dirs = libdir,
                                   depends = [libdir[0]+'/clib.lib'],
                                   libraries = ["clib"])
