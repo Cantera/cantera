2bd7816b65c7375108f9ed0a03d582afdcd12047
Harry Moffat <hkmoffa@sandia.gov>
2009-09-16 02:13:43 +0000

    Added in a model for the thermal conductivity of pure water at all T and P. Added in a test for the new model.

--- Cantera/src/thermo/WaterProps.cpp
+++ Cantera/src/thermo/WaterProps.cpp
@@ -596,6 +596,117 @@ namespace Cantera {
     return mubar * muStar;
   }
 
+  //! Returns the thermal conductivity of water at the current conditions
+  //! (W/m/K)
+  /*!
+   *  This function calculates the value of the thermal conductivity of
+   *  water at the current T and P.
+   *
+   *  The formulas used are from the paper
+   *     J. V. Sengers, J. T. R. Watson, "Improved International
+   *     Formulations for the Viscosity and Thermal Conductivity of
+   *     Water Substance", J. Phys. Chem. Ref. Data, 15, 1291 (1986).
+   *
+   *  The formulation is accurate for all temperatures and pressures,
+   *  for steam and for water, even near the critical point.
+   *  Pressures above 500 MPa and temperature above 900 C are suspect.
+   */
+  double WaterProps::thermalConductivityWater() const {
+    static const double Tstar = 647.27;
+    static const double rhostar = 317.763;
+    static const double lambdastar = 0.4945;
+    static const double presstar = 22.115E6;
+    static const double L[4] = 
+      {
+	1.0000,
+	6.978267,
+	2.599096,
+	-0.998254
+      };
+    static const double Lji[6][5] = 
+      {
+	{ 1.3293046,    1.7018363,   5.2246158,   8.7127675, -1.8525999},
+	{-0.40452437,  -2.2156845, -10.124111,   -9.5000611,  0.93404690},
+	{ 0.24409490,   1.6511057,   4.9874687,   4.3786606,  0.0},
+	{ 0.018660751, -0.76736002, -0.27297694, -0.91783782, 0.0},
+	{-0.12961068,   0.37283344, -0.43083393,  0.0,        0.0},
+	{ 0.044809953, -0.11203160,  0.13333849,  0.0,        0.0},
+      };
+
+    double temp = m_waterIAPWS->temperature();
+    double dens = m_waterIAPWS->density();
+
+    double rhobar = dens/rhostar;
+    double tbar = temp / Tstar;
+    double tbar2 = tbar * tbar;
+    double tbar3 = tbar2 * tbar;
+    double lambda0bar = sqrt(tbar) / (L[0] + L[1]/tbar + L[2]/tbar2 + L[3]/tbar3);
+
+    //double lambdagas = lambda0bar * lambdastar * 1.0E3;
+
+    double tfac1 = 1.0 / tbar - 1.0;
+    double tfac2 = tfac1 * tfac1;
+    double tfac3 = tfac2 * tfac1;
+    double tfac4 = tfac3 * tfac1;
+
+    double rfac1 = rhobar - 1.0;
+    double rfac2 = rfac1 * rfac1;
+    double rfac3 = rfac2 * rfac1;
+    double rfac4 = rfac3 * rfac1;
+    double rfac5 = rfac4 * rfac1;
+
+    double sum = (Lji[0][0]       + Lji[0][1]*tfac1        + Lji[0][2]*tfac2       + Lji[0][3]*tfac3       + Lji[0][4]*tfac4       + 
+		  Lji[1][0]*rfac1 + Lji[1][1]*tfac1*rfac1  + Lji[1][2]*tfac2*rfac1 + Lji[1][3]*tfac3*rfac1 + Lji[1][4]*tfac4*rfac1 +
+		  Lji[2][0]*rfac2 + Lji[2][1]*tfac1*rfac2  + Lji[2][2]*tfac2*rfac2 + Lji[2][3]*tfac3*rfac2 +
+		  Lji[3][0]*rfac3 + Lji[3][1]*tfac1*rfac3  + Lji[3][2]*tfac2*rfac3 + Lji[3][3]*tfac3*rfac3 +
+		  Lji[4][0]*rfac4 + Lji[4][1]*tfac1*rfac4  + Lji[4][2]*tfac2*rfac4 +
+		  Lji[5][0]*rfac5 + Lji[5][1]*tfac1*rfac5  + Lji[5][2]*tfac2*rfac5 
+		  );
+    double  lambda1bar = exp(rhobar * sum);
+
+    double mu0bar = std::sqrt(tbar) / (H[0] + H[1]/tbar + H[2]/tbar2 + H[3]/tbar3);
+
+    double tfac5 = tfac4 * tfac1;    
+    double rfac6 = rfac5 * rfac1;
+
+    sum = (Hij[0][0]       + Hij[1][0]*tfac1       + Hij[4][0]*tfac4       + Hij[5][0]*tfac5 +
+	   Hij[0][1]*rfac1 + Hij[1][1]*tfac1*rfac1 + Hij[2][1]*tfac2*rfac1 + Hij[3][1]*tfac3*rfac1 +
+	   Hij[0][2]*rfac2 + Hij[1][2]*tfac1*rfac2 + Hij[2][2]*tfac2*rfac2 +
+	   Hij[0][3]*rfac3 + Hij[1][3]*tfac1*rfac3 + Hij[2][3]*tfac2*rfac3 + Hij[3][3]*tfac3*rfac3 +
+	   Hij[0][4]*rfac4 + Hij[3][4]*tfac3*rfac4 + 
+	   Hij[1][5]*tfac1*rfac5 + Hij[3][6]*tfac3*rfac6 
+	   );
+    double mu1bar = std::exp(rhobar * sum);
+
+    double t2r2 = tbar * tbar / (rhobar * rhobar);
+    double drhodp =  1.0 / m_waterIAPWS->dpdrho();
+    drhodp *= presStar / rhoStar;
+    double xsi = rhobar * drhodp;
+    double xsipow = std::pow(xsi, 0.4678);
+    double rho1 = rhobar - 1.;
+    double rho2 = rho1 * rho1;
+    double rho4 = rho2 * rho2;
+    double temp2 = (tbar - 1.0) * (tbar - 1.0);
+
+    /*
+     *     beta = M / (rho * Rgas) (d (pressure) / dT) at constant rho
+     *
+     *  Note for ideal gases this is equal to one.
+     *
+     *    beta = delta (phi0_d() + phiR_d())
+     *            - tau delta (phi0_dt() + phiR_dt())
+     */
+    double beta = m_waterIAPWS->coeffPresExp();
+
+    double dpdT_const_rho = beta * GasConstant * dens / 18.015268;
+    dpdT_const_rho *= Tstar /  presstar;
+
+    double  lambda2bar = 0.0013848 / (mu0bar * mu1bar) * t2r2 * dpdT_const_rho * dpdT_const_rho *
+      xsipow * sqrt(rhobar) * exp(-18.66*temp2 - rho4);
+
+    double lambda = ( lambda0bar * lambda1bar + lambda2bar) * lambdastar;
+    return lambda;
+  }
 
 
 }
