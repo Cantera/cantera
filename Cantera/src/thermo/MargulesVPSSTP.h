0d6d5642b17eaa8d192d5c450532979c3ec51486
Harry Moffat <hkmoffa@sandia.gov>
2009-11-09 22:55:34 +0000

    Added keywords property

--- Cantera/src/thermo/MargulesVPSSTP.h
+++ Cantera/src/thermo/MargulesVPSSTP.h
@@ -17,7 +17,7 @@
  * U.S. Government retains certain rights in this software.
  */
 /*
- *  $Id: MargulesVPSSTP.h,v 1.1 2009/03/03 21:08:31 hkmoffa Exp $
+ *  $Id$
  */
 
 #ifndef CT_MARGULESVPSSTP_H
