45ea2588b3d0758131c7b041ff942af39d731724
hkmoffa <hkmoffa@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2010-05-08 18:01:21 +0000

    Added VERSION control to this

--- cantera18/branches/Revision_1.8.0/winconfig.h
+++ cantera18/branches/Revision_1.8.0/winconfig.h
@@ -5,6 +5,17 @@
 #ifndef CT_CONFIG_H
 #define CT_CONFIG_H
 
+//---------------------------- Version Flags ------------------//
+// Cantera version -> this will be a double-quoted string value
+//                    refering to branch number within svn
+#define CANTERA_VERSION "1.8.0"
+
+// Integer for major number of Cantera
+#define CANTERA_VERSION_MAJORNUMBER  18
+// Flag indicating it's part of major version 18
+#define CANTERA_VERSION_18       1
+//  Flag indicating it's a production version 0
+#define CANTERA_VERSION_18_0   1
 
 //------------------------ Development flags ------------------//
 //
@@ -64,8 +75,6 @@ typedef  int          ftnlen;       // Fortran hidden string length type
 /* #undef DARWIN */
 #define HAS_SSTREAM 1
 
-// Cantera version
-#define CANTERA_VERSION "1.8.0"
 
 // Identify whether the operating system is cygwin's overlay of
 // windows, with gcc being used as the compiler.
