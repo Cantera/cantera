ae8a47c894919571d3e41aa1aed1815ac7362156
hkmoffa <hkmoffa@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2010-07-13 19:22:30 +0000

    Split LiquidTransportData from LTPspecies into separate files; they are separate concepts. Fixed pointers within LTPspecies and LiquidTransportData so that there aren't any more memory leaks. Everything now is based on deep copies.

--- cantera18/changes/liquidTransportDevelop/Cantera/src/transport/LiquidTransportData.cpp
+++ cantera18/changes/liquidTransportDevelop/Cantera/src/transport/LiquidTransportData.cpp
@@ -6,413 +6,110 @@
  *  $Author$
  *  $Date$
  *  $Revision$
- *
  */
 
 #include "LiquidTransportData.h"
 using namespace std;
 
-
+#ifndef SAFE_DELETE
+#define SAFE_DELETE(x)  if (x) { delete (x); x = 0; }
+#endif
 namespace Cantera {
 
-
-
-  /** 
-   * Exception thrown if an error is encountered while reading the 
-   * transport database.
-   */
-  class LTPError : public CanteraError {
-  public:
-    LTPError( std::string msg ) 
-      : CanteraError("LTPspecies",
-		     "error parsing transport data: " 
-		     + msg + "\n") {}
-  };
-
-
-  /**
-   * getArrhenius() parses the xml element called Arrhenius. 
-   * The Arrhenius expression is
-   * \f[        k =  A T^(b) exp (-E_a / RT). \f]
-   */
-  static void getArrhenius(const XML_Node& node, 
-			   doublereal& A, doublereal& b, doublereal& E) {
-    /* parse the children for the A, b, and E conponents.
-     */
-    A = getFloat(node, "A", "toSI");
-    b = getFloat(node, "b");
-    E = getFloat(node, "E", "actEnergy");
-    E /= GasConstant;
-  }                
-
+  //====================================================================================================================
   LiquidTransportData::LiquidTransportData() :
     speciesName("-"),   
     hydroRadius(0),
     viscosity(0),
     ionConductivity(0),
+    mobilityRatio(0),
+    selfDiffusion(0),
     thermalCond(0),
     electCond(0),
     speciesDiffusivity(0)
   {
 
   }
-
+  //====================================================================================================================
   // Copy constructor
   LiquidTransportData::LiquidTransportData(const LiquidTransportData &right) :
     speciesName("-"),   
     hydroRadius(0),
     viscosity(0),
     ionConductivity(0),
+    mobilityRatio(0),
+    selfDiffusion(0),
     thermalCond(0),
     electCond(0),
     speciesDiffusivity(0)
   {
     *this = right; //use assignment operator to do other work
   }
-  
+  //====================================================================================================================
   // Assignment operator
   LiquidTransportData& LiquidTransportData::operator=(const LiquidTransportData& right) 
   {
     if (&right != this) {
+      // These are all shallow pointer copies - yes, yes, yes horrible crime.
       speciesName        = right.speciesName;
-      hydroRadius        = right.hydroRadius;
-      viscosity          = right.viscosity;
-      ionConductivity    = right.ionConductivity;
-      mobilityRatio      = right.mobilityRatio;
-      selfDiffusion      = right.selfDiffusion;
-      thermalCond        = right.thermalCond;
-      electCond          = right.electCond;
-      speciesDiffusivity = right.speciesDiffusivity;
-    }
-    return *this; 
-  }
+      if (right.hydroRadius) {
+	hydroRadius = (right.hydroRadius)->duplMyselfAsLTPspecies();
+      }
+      if (right.viscosity) {
+	viscosity = (right.viscosity)->duplMyselfAsLTPspecies();
+      }
+      if (right.ionConductivity) {
+	ionConductivity = (right.ionConductivity)->duplMyselfAsLTPspecies();
+      }
 
+      mobilityRatio = right.mobilityRatio;
+      for (size_t k = 0; k < mobilityRatio.size(); k++) {
+	if (right.mobilityRatio[k]) {
+	  mobilityRatio[k] = (right.mobilityRatio[k])->duplMyselfAsLTPspecies();
+	}
+      }
 
+      selfDiffusion = right.selfDiffusion;
+      for (size_t k = 0; k < selfDiffusion.size(); k++) {
+	if (right.selfDiffusion[k]) {
+	  selfDiffusion[k] = (right.selfDiffusion[k])->duplMyselfAsLTPspecies();
+	}
+      }
 
-
-  // Copy constructor
-  LTPspecies::LTPspecies( const LTPspecies &right ) 
-  {
-    *this = right; //use assignment operator to do other work
-  }
-  
-  // Assignment operator
-  LTPspecies& LTPspecies::operator=(const LTPspecies& right ) 
-  {
-    if (&right != this) {
-      m_speciesName = right.m_speciesName;
-      m_property    = right.m_property;
-      m_model       = right.m_model;
-      m_coeffs      = right.m_coeffs;
-      m_thermo      = right.m_thermo;
-      m_mixWeight   = right.m_mixWeight;
-    }
-    return *this; 
-  }
-
-
-  // Construct an LTPspecies object for a liquid tranport property 
-  // expressed as a constant value.
-  /* The transport property is constructed from the XML node, 
-   *  \verbatim <propNode>, \endverbatim that is a child of the
-   *  \verbatim <transport> \endverbatim node and specifies a type of
-   *  transport property (like viscosity)
-   */ 
-  LTPspecies_Const::LTPspecies_Const( const XML_Node &propNode, 
-				      std::string name, 
-				      TransportPropertyList tp_ind, 
-				      thermo_t* thermo ) : 
-    LTPspecies( propNode, name, tp_ind, thermo) 
-  {
-    m_model = LTR_MODEL_CONSTANT;	
-    double A_k = getFloatCurrent(propNode, "toSI");
-    if (A_k > 0.0) {
-      m_coeffs.push_back(A_k);
-    } else throw LTPError("negative or zero " + propNode.name() );
-  }
-
-  // Copy constructor
-  LTPspecies_Const::LTPspecies_Const( const LTPspecies_Const &right ) 
-    : LTPspecies()
-  {
-    *this = right; //use assignment operator to do other work
-  }
-  
-  // Assignment operator
-  LTPspecies_Const& LTPspecies_Const::operator=(const LTPspecies_Const& right ) 
-  {
-    if (&right != this) {
-      //LTPspecies::operator=(right);
-      m_speciesName = right.m_speciesName;
-      m_property    = right.m_property;
-      m_model       = right.m_model;
-      m_coeffs      = right.m_coeffs;
-      m_thermo      = right.m_thermo;
-      m_mixWeight   = right.m_mixWeight;
-    }
-    return *this; 
-  }
-
-  // Return the (constant) value for this transport property
-  doublereal LTPspecies_Const::getSpeciesTransProp( ) {
-    return m_coeffs[0];
-  }
-
-  ///////////////////////////////////////////////////////////////
-
-  // Construct an LTPspecies object for a liquid tranport property 
-  // expressed in extended Arrhenius form.
-  /* The transport property is constructed from the XML node, 
-   *  \verbatim <propNode>, \endverbatim that is a child of the
-   *  \verbatim <transport> \endverbatim node and specifies a type of
-   *  transport property (like viscosity)
-   */ 
-  LTPspecies_Arrhenius::LTPspecies_Arrhenius( const XML_Node &propNode, 
-				      std::string name, 
-				      TransportPropertyList tp_ind, 
-				      thermo_t* thermo ) : 
-    LTPspecies( propNode, name, tp_ind, thermo) 
-  {
-    m_model = LTR_MODEL_ARRHENIUS;
-    m_temp = 0.0;
-    m_prop = 0.0;	
-
-    doublereal A_k, n_k, Tact_k;
-    getArrhenius(propNode, A_k, n_k, Tact_k);
-    if (A_k <= 0.0) {
-      throw LTPError("negative or zero " + propNode.name() );
-    }
-    m_coeffs.push_back( A_k );
-    m_coeffs.push_back( n_k );
-    m_coeffs.push_back( Tact_k );
-    m_coeffs.push_back( log( A_k ) );
-  }
-  
-  // Copy constructor
-  LTPspecies_Arrhenius::LTPspecies_Arrhenius( const LTPspecies_Arrhenius &right ) 
-    : LTPspecies()
-  {
-    *this = right; //use assignment operator to do other work
-  }
-  
-  // Assignment operator
-  LTPspecies_Arrhenius& LTPspecies_Arrhenius::operator=(const LTPspecies_Arrhenius& right )
-  {
-    if (&right != this) {
-      // LTPspecies::operator=(right);
-      m_speciesName = right.m_speciesName;
-      m_property    = right.m_property;
-      m_model       = right.m_model;
-      m_coeffs      = right.m_coeffs;
-      m_thermo      = right.m_thermo;
-      m_mixWeight   = right.m_mixWeight;
-
-      m_temp    = right.m_temp;
-      m_logt    = right.m_logt;
-      m_prop    = right.m_prop;
-      m_logProp = right.m_logProp;
-    }
-    return *this; 
-  }
-
-  // Return the pure species value for this transport property evaluated 
-  // from the Arrhenius expression
-  /*
-   * In general the Arrhenius expression is 
-   *
-   * \f[
-   *      \mu = A T^n \exp( - E / R T ).
-   * \f]
-   *
-   * Note that for viscosity, the convention is such that 
-   * a positive activation energy corresponds to the typical 
-   * case of a positive argument to the exponential so that 
-   * the Arrhenius expression is
-   *
-   * \f[
-   *      \mu = A T^n \exp( + E / R T ).
-   * \f]
-   *
-   * Any temperature and composition dependence will be 
-   *  adjusted internally according to the information provided.
-   */
-  doublereal LTPspecies_Arrhenius::getSpeciesTransProp( ) {
-
-    doublereal t = m_thermo->temperature();
-    //m_coeffs[0] holds A
-    //m_coeffs[1] holds n
-    //m_coeffs[2] holds Tact
-    //m_coeffs[3] holds log(A)
-    if (t != m_temp) {   
-      m_prop = 0;
-      m_logProp = 0;
-      m_temp = t;
-      m_logt = log(m_temp);
-      //For viscosity the sign convention on positive activation energy is swithced
-      if ( m_property == TP_VISCOSITY ) 
-	m_logProp = m_coeffs[3] + m_coeffs[1] * m_logt + m_coeffs[2] / m_temp ;
-      else
-	m_logProp = m_coeffs[3] + m_coeffs[1] * m_logt - m_coeffs[2] / m_temp ;
-      m_prop = exp( m_logProp );
-    }
-    return m_prop;
-  }
-
-
-
-
-
-
-  ///////////////////////////////////////////////////////////////
-
-  // Construct an LTPspecies object for a liquid tranport property 
-  // expressed as a polynomial in temperature.
-  /* The transport property is constructed from the XML node, 
-   *  \verbatim <propNode>, \endverbatim that is a child of the
-   *  \verbatim <transport> \endverbatim node and specifies a type of
-   *  transport property (like viscosity)
-   */ 
-  LTPspecies_Poly::LTPspecies_Poly( const XML_Node &propNode, 
-				    std::string name, 
-				    TransportPropertyList tp_ind, 
-				    thermo_t* thermo ) : 
-    LTPspecies( propNode, name, tp_ind, thermo) 
-  {
-    m_model = LTR_MODEL_POLY;
-    m_temp = 0.0;
-    m_prop = 0.0;	
-
-
-    getFloatArray(propNode, m_coeffs, "true", "toSI");
-
-    /*    if (m_coeffs[0] <= 0.0) {
-      throw LTPError("negative or zero " + propNode.name() );
-      }*/
-  }
-
-  // Copy constructor
-  LTPspecies_Poly::LTPspecies_Poly( const LTPspecies_Poly &right ) 
-    : LTPspecies()
-  {
-    *this = right; //use assignment operator to do other work
-  }
-  
-  // Assignment operator
-  LTPspecies_Poly& LTPspecies_Poly::operator=(const LTPspecies_Poly& right )
-  {
-    if (&right != this) {
-      //LTPspecies::operator=(right);
-      m_speciesName = right.m_speciesName;
-      m_property    = right.m_property;
-      m_model       = right.m_model;
-      m_coeffs      = right.m_coeffs;
-      m_thermo      = right.m_thermo;
-      m_mixWeight   = right.m_mixWeight;
-
-      m_temp    = right.m_temp;
-      m_prop    = right.m_prop;
-    }
-    return *this; 
-  }
-
-  // Return the value for this transport property evaluated 
-  // from the polynomial expression
-  doublereal LTPspecies_Poly::getSpeciesTransProp( ) {
-    
-    doublereal t = m_thermo->temperature();
-    if (t != m_temp) {  
-      m_prop = 0;
-      m_temp=t;
-      double tempN = 1.0;      
-      for (int i = 0; i < (int) m_coeffs.size() ; i++) {
-	m_prop += m_coeffs[i] * tempN;
-	//cout << "m_coeff = " <<m_coeffs[i] << ", tempN = " << tempN << ", m_prop = " << m_prop << endl;
-	tempN *= m_temp;
+      if (right.thermalCond) {
+	thermalCond  = (right.thermalCond)->duplMyselfAsLTPspecies();
+      }
+      if (right.electCond) {
+	electCond = (right.electCond)->duplMyselfAsLTPspecies();
+      }
+      if (right.speciesDiffusivity) {
+	speciesDiffusivity = (right.speciesDiffusivity)->duplMyselfAsLTPspecies();
       }
     }
-    //cout << "m_prop = " << m_prop << endl;
-    return m_prop;
-  }  
-
-
-
-///////////////////////////////////////////////////////////////
-
-  // Construct an LTPspecies object for a liquid tranport property 
-  // expressed as an exponential in temperature.
-  /* The transport property is constructed from the XML node, 
-   *  \verbatim <propNode>, \endverbatim that is a child of the
-   *  \verbatim <transport> \endverbatim node and specifies a type of
-   *  transport property (like viscosity)
-   */ 
-  LTPspecies_ExpT::LTPspecies_ExpT( const XML_Node &propNode, 
-				    std::string name, 
-				    TransportPropertyList tp_ind, 
-				    thermo_t* thermo ) : 
-    LTPspecies( propNode, name, tp_ind, thermo) 
-  {
-    m_model = LTR_MODEL_EXPT;
-    m_temp = 0.0;
-    m_prop = 0.0;	
-
-
-    getFloatArray(propNode, m_coeffs, "true", "toSI");
-
-    /*    if (m_coeffs[0] <= 0.0) {
-      throw LTPError("negative or zero " + propNode.name() );
-      }*/
+    return *this;
   }
+ //====================================================================================================================
+  LiquidTransportData::~LiquidTransportData() {
 
-  // Copy constructor
-  LTPspecies_ExpT::LTPspecies_ExpT( const LTPspecies_ExpT &right ) 
-    : LTPspecies()
-  {
-    *this = right; //use assignment operator to do other work
-  }
-  
-  // Assignment operator
-  LTPspecies_ExpT& LTPspecies_ExpT::operator=(const LTPspecies_ExpT& right )
-  {
-    if (&right != this) {
-      //LTPspecies::operator=(right);
-      m_speciesName = right.m_speciesName;
-      m_property    = right.m_property;
-      m_model       = right.m_model;
-      m_coeffs      = right.m_coeffs;
-      m_thermo      = right.m_thermo;
-      m_mixWeight   = right.m_mixWeight;
+    SAFE_DELETE(hydroRadius);
+    SAFE_DELETE(viscosity);
+    SAFE_DELETE(ionConductivity);
 
-      m_temp    = right.m_temp;
-      m_prop    = right.m_prop;
-    }
-    return *this; 
-  }
-
-  // Return the value for this transport property evaluated 
-  // from the exponential in temperature expression
-  doublereal LTPspecies_ExpT::getSpeciesTransProp( ) {
-    
-    doublereal t = m_thermo->temperature();
-    if (t != m_temp) {  
-      m_prop = 0;
-      m_temp=t;
-      m_prop=m_coeffs[0];
-      double tempN = 1.0;      
-      for (int i = 1; i < (int) m_coeffs.size() ; i++) {
-	tempN *= m_temp;
-	m_prop *= exp(m_coeffs[i] * tempN);
-	//cout << "m_coeff = " <<m_coeffs[i] << ", tempN = " << tempN << ", m_prop = " << m_prop << endl;
+    for (size_t k = 0; k < mobilityRatio.size(); k++) {
+      if (mobilityRatio[k]) {
+	SAFE_DELETE(mobilityRatio[k]);
       }
     }
-    //cout << "m_prop = " << m_prop << endl;
-    return m_prop;
+    for (size_t k = 0; k < selfDiffusion.size(); k++) {
+      if (selfDiffusion[k]) {
+	SAFE_DELETE(selfDiffusion[k]);
+      }
+    }
+
+    SAFE_DELETE(thermalCond);
+    SAFE_DELETE(electCond);
+    SAFE_DELETE(speciesDiffusivity);
+
   }
-
-
-
-
-
-
-
-
+ //====================================================================================================================
 }
