a737dd308f154c2f9dcfc4ab39f0b4ab5473b44a
Harry Moffat <hkmoffa@sandia.gov>
2010-07-21 23:35:42 +0000

    Doxygen update to DenseMatrix

--- Cantera/src/numerics/DenseMatrix.h
+++ Cantera/src/numerics/DenseMatrix.h
@@ -22,83 +22,88 @@
 
 namespace Cantera { 
 
-    /**
-     *  A class for full (non-sparse) matrices with Fortran-compatible
-     *  data storage. Adds matrix operations to class Array2D.
+  /**
+   *  A class for full (non-sparse) matrices with Fortran-compatible
+   *  data storage. Adds matrix operations to class Array2D.
+   */
+  class DenseMatrix : public Array2D {
+
+  public:
+
+    //! Default Constructor
+    DenseMatrix();
+
+    /** 
+     * Constructor. Create an \c n by \c m matrix, and initialize
+     * all elements to \c v.
      */
-    class DenseMatrix : public Array2D {
+    DenseMatrix(int n, int m, doublereal v = 0.0);
 
-    public:
+    //! copy constructor
+    /*!
+     *   @param y Object to be copied
+     */
+    DenseMatrix(const DenseMatrix& y);
 
-        DenseMatrix(){}
+    //! assignment operator
+    /*!
+     *   @param y Object to be copied
+     */
+    DenseMatrix& operator=(const DenseMatrix& y);
 
-        /** 
-         * Constructor. Create an \c n by \c m matrix, and initialize
-         * all elements to \c v.
-         */
-        DenseMatrix(int n, int m, doublereal v = 0.0) : Array2D(n,m,v) {
-            m_ipiv.resize( max(n, m) );
-        }
+    //! Destructor. Does nothing.
+    virtual ~DenseMatrix();
+   
 
-        /// copy constructor
-        DenseMatrix(const DenseMatrix& y) : Array2D(y) {
-            m_ipiv = y.ipiv();
-        }
+    void resize(int n, int m, doublereal v = 0.0);
 
-        /// assignment.
-        DenseMatrix& operator=(const DenseMatrix& y);
-
-        void resize(int n, int m, doublereal v = 0.0);
-
-        /// Destructor. Does nothing.
-        virtual ~DenseMatrix(){}
-
-
-        /**
-         * Multiply A*b and write result to \c prod.
-         */
-        virtual void mult(const double* b, double* prod) const;
-
-        /**
-         * Left-multiply the matrix by transpose(b), and write the
-         * result to prod.
-         */
-        virtual void leftMult(const double* b, double* prod) const;
-
-        vector_int& ipiv() { return m_ipiv; }
-        const vector_int& ipiv() const { return m_ipiv; }
-
-    protected:
-
-        vector_int     m_ipiv;
-    };
-
+ 
 
     /**
-     * Solve Ax = b. Array b is overwritten on exit with x.
+     * Multiply A*b and write result to \c prod.
      */
-     int solve(DenseMatrix& A, double* b);
+    virtual void mult(const double* b, double* prod) const;
 
-    /**  Solve Ax = b for multiple right-hand-side vectors. */
-     int solve(DenseMatrix& A, DenseMatrix& b);
+    /**
+     * Left-multiply the matrix by transpose(b), and write the
+     * result to prod.
+     */
+    virtual void leftMult(const double* b, double* prod) const;
+
+    vector_int& ipiv() { return m_ipiv; }
+    const vector_int& ipiv() const { return m_ipiv; }
+
+  protected:
+
+    vector_int     m_ipiv;
+  };
+
+
+  /**
+   * Solve Ax = b. Array b is overwritten on exit with x.
+   */
+  int solve(DenseMatrix& A, double* b);
+
+  /**  Solve Ax = b for multiple right-hand-side vectors. */
+  int solve(DenseMatrix& A, DenseMatrix& b);
 
 #ifdef INCL_LEAST_SQUARES
-    /** @todo fix lwork */
-     int leastSquares(DenseMatrix& A, double* b);
+  /** @todo fix lwork */
+  int leastSquares(DenseMatrix& A, double* b);
 #endif
-    /**
-     * Multiply \c A*b and return the result in \c prod. Uses BLAS
-     * routine DGEMV.
-     */
-     void multiply(const DenseMatrix& A, const double* b, double* prod);
+  /**
+   * Multiply \c A*b and return the result in \c prod. Uses BLAS
+   * routine DGEMV.
+   */
+  void multiply(const DenseMatrix& A, const double* b, double* prod);
 
-     void increment(const DenseMatrix& A,
-        const double* b, double* prod);
+  void increment(const DenseMatrix& A,
+		 const double* b, double* prod);
 
-    /**
-     * invert A. A is overwritten with A^-1.
-     */
-     int invert(DenseMatrix& A, int nn=-1);
+  /**
+   * invert A. A is overwritten with A^-1.
+   */
+  int invert(DenseMatrix& A, int nn=-1);
 
 }
 
