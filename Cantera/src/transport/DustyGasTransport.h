6d766926bdd1e1feb1dc7e5ef5dd42704976765e
Harry Moffat <hkmoffa@sandia.gov>
2010-08-17 22:48:59 +0000

    Updated this object    Put in the standard copy constructors and assignment operators    Started documenting it.

--- Cantera/src/transport/DustyGasTransport.h
+++ Cantera/src/transport/DustyGasTransport.h
@@ -1,10 +1,14 @@
-///
-///
-///  @file DustyGasTransport.h
-///  Interface for class DustyGasTransport
-///
-///
-
+/**
+ *  @file DustyGasTransport.h
+ *    Headers for the DustyGasTransport object, which models transport properties
+ *    in porous media using the dusty gas approximation
+ *    (see \ref tranprops and \link Cantera::DustyGasTransport DustyGasTransport \endlink) .
+ *
+ */
+/*
+ * $Revision$
+ * $Date$
+ */
 
 // Copyright 2003  California Institute of Technology
 
@@ -19,23 +23,52 @@
 
 namespace Cantera {
 
-  ///
-  /// Class DustyGasTransport implements the Dusty Gas model for
-  /// transport in porous media. As implemented here, only species
-  /// transport is handled. The viscosity, thermal conductivity, and
-  /// thermal diffusion coefficients are not implemented.
-  ///
+  //! Class DustyGasTransport implements the Dusty Gas model for transport in porous media.
+  /*!
+   *    As implemented here, only species transport is handled. The viscosity, thermal conductivity, and thermal 
+   *    diffusion coefficients are not implemented.
+   */
   class DustyGasTransport : public Transport {
 
   public:
 
-    /// default constructor
+    //! default constructor
+    /*!
+     *  @param thermo   Pointer to the %ThermoPhase object for this phase. Defaults to zero.
+     */
     DustyGasTransport(thermo_t* thermo=0);
+
+    //!   Copy Constructor for the %DustyGasTransport object.
+    /*!
+     *    @param right  %LiquidTransport to be copied
+     */
+    DustyGasTransport(const DustyGasTransport &right);
+
+    //! Assignment operator
+    /*!
+     *
+     *    Warning -> Shallow pointer copies are made of m_thermo and m_gastran.. gastran may not point to the correct 
+     *               object after this copy. The routine initialize() must be called after this
+     *               routine to complete the copy.
+     *
+     * @param right    Reference to %DustyGasTransport object to be copied 
+     *                 into the current one.
+     */
+    DustyGasTransport& operator=(const  DustyGasTransport& right);
         
-    /// Destructor. Does nothing, since class allocates no memory
-    /// on the heap.
-    virtual ~DustyGasTransport() {}
-        
+    //! Destructor.
+    virtual ~DustyGasTransport();
+
+    //! Duplication routine for objects which inherit from %Transport
+    /*!
+     *  This virtual routine can be used to duplicate %Transport objects
+     *  inherited from %Transport even if the application only has
+     *  a pointer to %Transport to work with.
+     *
+     *  These routines are basically wrappers around the derived copy
+     *  constructor.
+     */
+    virtual Transport *duplMyselfAsTransport() const;
         
     //---------------------------------------------------------
     // overloaded base class methods
@@ -87,14 +120,25 @@ namespace Cantera {
       m_diam = dbar;
     }
         
-    /// Set the permeability. If not set, the value for
-    /// close-packed spheres will be used by default. 
+    //! Set the permeability of the media
+    /*!
+     * If not set, the value for close-packed spheres will be used by default. 
+     *
+     *  The value for close-packed spheres is given below, where p is the porosity,
+     *  t is the tortuosity, and d is the diameter of the sphere
+     *
+     *  \f[
+     *      \kappa = \frac{p^3 d^2}{72 t (1 - p)^2}
+     *  \f]
+     *
+     * @param B  set the permeability of the media (units = m^2)
+     */
     void setPermeability(doublereal B) {
       m_perm = B;
     }
         
-    /// Return a reference to the transport manager used to compute the gas
-    /// binary diffusion coefficients and the visdcosity.
+    //! Return a reference to the transport manager used to compute the gas
+    //! binary diffusion coefficients and the visdcosity.
     Transport& gasTransport() { return *m_gastran; }
         
         
@@ -103,12 +147,26 @@ namespace Cantera {
         
   protected:
 
-    // called by TransportFactory
+    //!  Initialization routine called by TransportFactory
+    /*!
+     *  The DustyGas model is a subordinate model to the gas phase transport model. Here we 
+     *  set the gas phase models.
+     *
+     *  This is a protected routine, so that initialiation of the Model must occur within Cantera's setup
+     *
+     *   @param  phase           Pointer to the underlying ThermoPhase model for the gas phase
+     *   @param  gastr           Pointer to the underlying Transport model for transport in the gas phse.
+     */
     void initialize(ThermoPhase* phase, Transport* gastr);
         
         
   private:
 
+    //! Update temperature-dependent quantities within the object
+    /*!
+     *  The object keeps a value m_temp, which is the temperature at which quantities were last evaluated
+     *  at. If the temperature is changed, update Booleans are set false, triggering recomputation.
+     */
     void updateTransport_T();
     void updateTransport_C();
 
@@ -123,32 +181,31 @@ namespace Cantera {
     doublereal m_tmin, m_tmax;
     vector_fp  m_mw;
 
-    // property values
-
-    /// binary diffusion coefficients
+  
+    //! binary diffusion coefficients
     DenseMatrix                  m_d;
 
-    /// mole fractions
+    //! mole fractions
     vector_fp                    m_x;
 
-    /// Knudsen diffusion coefficients
+    //! Knudsen diffusion coefficients
     vector_fp                    m_dk;
 
-    /// temperature
+    //! temperature
     doublereal                   m_temp;
 
-    /// multicomponent diffusion coefficients
+    //! multicomponent diffusion coefficients
     DenseMatrix                  m_multidiff;
 
-    // work space
+    //!  work space of size m_nsp;
     vector_fp  m_spwork;
+    
+    //!  work space of size m_nsp;
     vector_fp  m_spwork2;
 
-    // concentration gradients
-    //vector_fp  m_gradConc; 
-    //vector_fp  m_conc;
-
-    doublereal m_gradP;   /// pressure gradient
+ 
+    //! Pressure Gradient
+    doublereal m_gradP;
 
     bool m_knudsen_ok;
     bool m_bulk_ok;
@@ -156,11 +213,35 @@ namespace Cantera {
     bool m_gradConc_set;
     bool m_gradP_set;
 
-    doublereal m_porosity;      /// porosity
-    doublereal m_tortuosity;    /// tortuosity
+    //! Porosity
+    doublereal m_porosity;   
+
+    //! Tortuosity
+    doublereal m_tortuosity;
     doublereal m_pore_radius;   /// pore radius (m)
-    doublereal m_diam;          /// particle diameter (m)
-    doublereal m_perm;          /// permeability
+
+    //! Particle diameter
+    /*!
+     *   The medium is assumed to consist of particles of size m_diam
+     *   units =  m
+     */
+    doublereal m_diam;
+
+    //! Permeability of the media
+    /*!
+     *  The permeability is the proportionality constant for Darcy's
+     *  law which relates discharge rate and viscosity to the applied
+     *  pressure gradient.
+     *
+     *   Below is Darcy's law, where \f$ \kappa \f$ is the permeability
+     *
+     *     \f[
+     *         v = \frac{\kappa}{\mu} \frac{\delta P}{\delta x}
+     *     \f]
+     *  
+     *  units are m2
+     */
+    doublereal m_perm; 
 
     Transport* m_gastran;       /// pointer to gas transport manager
 
