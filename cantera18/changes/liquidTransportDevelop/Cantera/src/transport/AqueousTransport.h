1b33247f7d926530c37676e9ea22f6d29107e283
hkmoffa <hkmoffa@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2011-03-28 20:18:37 +0000

    Took out namespaces from headers.

--- cantera18/changes/liquidTransportDevelop/Cantera/src/transport/AqueousTransport.h
+++ cantera18/changes/liquidTransportDevelop/Cantera/src/transport/AqueousTransport.h
@@ -13,7 +13,6 @@
 #ifndef CT_AQUEOUSTRAN_H
 #define CT_AQUEOUSTRAN_H
 
-using namespace std;
 
 // Cantera includes
 #include "TransportBase.h"
@@ -403,14 +402,14 @@ namespace Cantera {
      * These express the temperature dependendence of the pures
      * species viscosities.
      */
-    vector<vector_fp>            m_visccoeffs;
+    std::vector<vector_fp>            m_visccoeffs;
 
     //! Polynomial coefficients of the conductivities
     /*!
      * These express the temperature dependendence of the pures
      * species conductivities
      */
-    vector<vector_fp>            m_condcoeffs;
+    std::vector<vector_fp>            m_condcoeffs;
 
     //! Polynomial coefficients of the binary diffusion coefficients
     /*!
@@ -418,7 +417,7 @@ namespace Cantera {
      * binary diffusivities. An overall pressure dependence is then
      * added.
      */
-    vector<vector_fp>            m_diffcoeffs;
+    std::vector<vector_fp>            m_diffcoeffs;
  
 
     //! Internal value of the gradient of the mole fraction vector
