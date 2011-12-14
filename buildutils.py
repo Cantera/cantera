2c4623e3366667b4bb95ef352d21950ba555e54b
Ray Speth <speth@mit.edu>
2011-12-14 02:41:29 +0000

    SCons can now build the contents of the 'ext' directory

--- buildutils.py
+++ buildutils.py
@@ -1,3 +1,8 @@
+from glob import glob
+import os
+import shutil
+from os.path import join as pjoin
+
 class DefineDict(object):
     def __init__(self, data):
         self.data = data
@@ -11,6 +16,7 @@ class DefineDict(object):
             return '/* #undef %s */' % key
         else:
             return '#define %s %s' % (key, self.data[key])
+
 
 class ConfigBuilder(object):
     def __init__(self, defines):
@@ -34,5 +40,44 @@ class ConfigBuilder(object):
         for key in sorted(self.defines.undefined):
             print "    %-35s %s" % (key, '*undefined*')
 
+
+class CopyNoPrefix(object):
+    """
+    Copy a file, ignoring leading directories that are part
+    of 'prefix' (e.g. the variant directory)
+    """
+    def __init__(self, prefix):
+        self.prefix = prefix
+
+    def __call__(self, source, target, env):
+        sourcepath = psplit(str(source[0]))
+        targetpath = psplit(str(target[0]))
+
+        depth = 0
+        for a,b in zip(targetpath, psplit(self.prefix)):
+            if a == b:
+                depth += 1
+            else:
+                break
+        print str(source[0]), pjoin(*targetpath[depth:])
+        shutil.copyfile(str(source[0]), pjoin(*targetpath[depth:]))
+
+
 def quoted(s):
     return '"%s"' % s
+
+
+def mglob(env, subdir, *args):
+    """ each arg in args is assumed to be file extension """
+    return sum((env.Glob('%s/*.%s' % (subdir, ext)) for ext in args), [])
+
+
+def psplit(s):
+    head, tail = os.path.split(s)
+    path = [tail]
+    while head:
+        head, tail = os.path.split(head)
+        path.append(tail)
+
+    path.reverse()
+    return path
