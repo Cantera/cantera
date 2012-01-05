2e697b32232a6a5d289f3b4bbd582f090efe488d
Nicholas Malaya <nicholas.malaya@gmail.com>
2012-01-05 18:54:50 +0000

    [cantera]: few more modifications to trunk of our tools, to enable regression testing with make check

--- ext/math/printstring.c
+++ ext/math/printstring.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "config.h"
+
+typedef  int          ftnlen;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-void printstring_(char* s, ftnlen ls) {
+void printstring_(char* s, ftnlen ls) 
+{
     printf("%s",s);
 }
 #ifdef __cplusplus
