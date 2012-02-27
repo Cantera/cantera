0f023fc4821305693f081a899059fa957d0188e0
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-02-27 18:13:53 +0000

    Added tests for some methods of the Python Solution class

--- cantera/trunk/test/python/testThermo.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import Cantera as ct
-
-class ImportTest(unittest.TestCase):
-    def testCtiImport(self):
-        gas = ct.importPhase('test/data/air-no-reactions.cti', 'air')
-
-        self.assertAlmostEqual(gas.temperature(), 300)
