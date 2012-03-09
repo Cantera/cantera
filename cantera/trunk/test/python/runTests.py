328487c87623a62254f13c6deeb19a3e9e989b9f
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-03-09 22:54:21 +0000

    Fixed an environment issue with Python subprocesses

--- cantera/trunk/test/python/runTests.py
+++ cantera/trunk/test/python/runTests.py
@@ -4,6 +4,7 @@ Unit tests for the Cantera Python module.
 This script gathers all the tests defined in all of the test<foo>.py
 files, runs them, and prints a report.
 """
+import sys
 import unittest
 
 import Cantera
@@ -11,6 +12,7 @@ import Cantera
 if __name__ == '__main__':
     print '\n* INFO: using Cantera module found at this location:'
     print '*     ', repr(Cantera.__file__), '\n'
+    sys.stdout.flush()
 
     loader = unittest.TestLoader()
     runner = unittest.TextTestRunner(verbosity=2)
