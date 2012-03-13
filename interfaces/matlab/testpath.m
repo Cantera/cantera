bac65b26a1f0fc52053bb4824c937b192914a61b
Ray Speth <speth@mit.edu>
2012-03-13 17:32:10 +0000

    Added machinery for unit testing of the Matlab toolbox
    Unit tests are written using the Matlab xUnit Test Framework

--- interfaces/matlab/testpath.m
+++ interfaces/matlab/testpath.m
@@ -6,6 +6,9 @@
 %    cd ~/src/cantera
 %    run interfaces/matlab/testpath.m
 
+% Unload the mex file so copying the DLL will work
+clear ctmethods
+
 % get the list of directories on the Matlab path
 dirs = regexp(path, ['([^' pathsep ']*)'], 'match');
 
@@ -29,13 +32,18 @@ cantera_root = fullfile(pwd, '..', '..');
 % Copy the Cantera shared library from the build directory if necessary
 if strcmp(getenv('OS'), 'Windows_NT')
     copyfile(fullfile(cantera_root, 'build', 'lib', 'cantera_shared.dll'), ...
-             fullfile(pwd, 'toolbox'))
+        fullfile(pwd, 'toolbox'))
 end
 
 % Set path to Python module
 setenv('PYTHONPATH', fullfile(cantera_root, 'interfaces', 'python'))
 
 % A simple test to make sure that the ctmethods.mex file is present and working
-f = Func('polynomial', 3, [1,2,3,4])
+f = Func('polynomial', 3, [1,2,3,4]);
+if f(1) == 10
+    disp('Cantera MEX file successfully loaded.')
+else
+    disp('Something is wrong with the Cantera MEX file.')
+end
 
 adddir(fullfile(cantera_root, 'data', 'inputs'))
