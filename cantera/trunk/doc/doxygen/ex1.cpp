9131f9e7c4ba2c8ae82464648359cc321403d956
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-04-04 18:45:53 +0000

    Removed some obsolete documentation

--- cantera/trunk/doc/doxygen/ex1.cpp
+++
@@ -1,9 +0,0 @@
-#include "cantera/thermo.h"
-#include <iostream>
-
-int main(int argc, char** argv)
-{
-    Cantera::ThermoPhase* gas = Cantera::newPhase("h2o2.cti","ohmech");
-    std::cout << gas->temperature() << std::endl;
-    return 0;
-}
