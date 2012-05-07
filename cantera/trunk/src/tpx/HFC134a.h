26e33b261b5591215a1dee098c50f81197b7ec76
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-05-07 18:20:18 +0000

    Added reference for the HFC-134a equation of state

--- cantera/trunk/src/tpx/HFC134a.h
+++ cantera/trunk/src/tpx/HFC134a.h
@@ -5,6 +5,14 @@
 
 namespace tpx
 {
+//! Equation of state for HFC-134a.
+//!
+//! Implements the equation of state given in:
+//! R. Tillner-Roth and H.D. Baehr. "An International Standard Formulation for
+//! The Thermodynamic Properties of 1,1,1,2-Tetrafluoroethane (HFC-134a) for
+//! Temperatures From 170 K to 455 K and Pressures up to 70 MPa". J. Phys.
+//! Chem. Ref. Data, Vol. 23, No. 5, 1994. pp. 657--729.
+//! http://dx.doi.org/10.1063/1.555958
 class HFC134a : public Substance
 {
 public:
@@ -36,4 +44,3 @@ private:
 };
 }
 #endif // ! HFC134_H
-
