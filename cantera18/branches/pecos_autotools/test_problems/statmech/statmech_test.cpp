c23274acc6eda9fc213a52d4c2013db08cdc7e78
nicholas.malaya <nicholas.malaya@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-06-19 21:21:23 +0000

    [cantera]: adding statemech regression test

---
+++ cantera18/branches/pecos_autotools/test_problems/statmech/statmech_test.cpp
@@ -0,0 +1,39 @@
+/**
+ *  @file statmech
+ *       test problem for statistical mechanics in cantera
+ */
+
+//  Example 
+//
+// Test case for the statistical mechanics in cantera
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+/*****************************************************************/
+/*****************************************************************/
+
+#include "Cantera.h"
+#include "transport.h"
+#include "IdealGasMix.h"
+
+#include "TransportFactory.h"
+
+using namespace Cantera;
+using namespace Cantera_CXX;
+
+int main(int argc, char** argv) 
+{
+  int k;
+  IdealGasMix g("gri30.xml", "gri30_mix");
+  int nsp = g.nSpecies();
+  double pres = 1.0E5;
+  
+  
+}
