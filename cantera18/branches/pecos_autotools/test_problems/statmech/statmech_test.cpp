6e9b43d88e2410969df6c20bb34b5c9f6dd4238f
nicholas.malaya <nicholas.malaya@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-06-20 14:29:11 +0000

    [pecos]: making statmech test more meaningful

--- cantera18/branches/pecos_autotools/test_problems/statmech/statmech_test.cpp
+++ cantera18/branches/pecos_autotools/test_problems/statmech/statmech_test.cpp
@@ -31,14 +31,23 @@ using namespace Cantera_CXX;
 int main(int argc, char** argv) 
 {
   int k;
-  //IdealGasMix g("gri30.xml", "gri30_mix");
   IdealGasMix g("test.xml");
-  //int nsp = g.nSpecies();
-  //double pres = 1.0E5;
+  int nsp = g.nSpecies();
+  double pres = 1.0E5;
+
+  //vector_fp Xset(nsp, 0.0);
+  //Xset[0] =  0.5 ;
+  //Xset[1] =  0.5;
   
-  // transport
-  //int log_level = 0;
-  //Transport * tran = newTransportMgr("Mix", &g, log_level=0);
+  //g.setState_TPX(1500.0, pres, DATA_PTR(Xset));
+
+
+  vector_fp cp_R(nsp, 0.0);
+  g.getCp_R(DATA_PTR(cp_R));
   
-  
+  //vector_fp S_R(nsp, 0.0);
+  //g.getEntropy_R(DATA_PTR(S_R));
+  //printf("  S/R: %11.6g %11.6g %11.6g\n", S_R[0], S_R[1], S_R[2]);
+
+
 }
