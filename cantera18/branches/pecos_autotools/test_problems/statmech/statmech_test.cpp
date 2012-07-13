f93bac4269ef3e6482d942e21f07649149b32a83
nicholas.malaya <nicholas.malaya@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-07-13 20:57:23 +0000

    adding a final regression test: now have some confidence this is all correctly implemented

--- cantera18/branches/pecos_autotools/test_problems/statmech/statmech_test.cpp
+++ cantera18/branches/pecos_autotools/test_problems/statmech/statmech_test.cpp
@@ -66,18 +66,37 @@ int main(int argc, char** argv)
       // molecules. One species should suffice, lets try NO2, with
       // three vibrational modes:
       /// theta[0]: 1.07900e3
-      /// theta[1]: 1.9000003
+      /// theta[1]: 1.90000e3
       /// theta[2]: 2.32700e3
-      // at T = 2000
+      // at T = 1500
       //
-      // This is precisely: 6.655804161 (e.g. 5/2 + 1 + 3.1558..)
+      // This is precisely: 6.655804161 (e.g. 5/2 + 2 + 3.1558..)
       //
-      double sol = 6.655804161; 
-      double tol = 1e-10;
+      double theta[3];
+      theta[0] = 1.07900e3;
+      theta[1] = 1.90000e3;
+      theta[2] = 2.32700e3;
 
-      if(cp_R[3] - sol >= tol )
+      double T;
+      T = 1500.0;
+
+      double denom;
+      double ctr = 0.0;
+      double GasConstant = 1.0;
+
+      for(int i = 0; i < 3; i++)
 	{
-	  double diff = cp_R[3]-6.655804161;
+	  denom = exp(2*theta[i]/T) - 2* exp(theta[i]/T) + 1;
+	  ctr += GasConstant * theta[i] * (theta[i] * exp(theta[i]/T)/(T*T))/ (denom);
+	  //std::cout << "survey says: " << ctr << " and denom is: " << denom << std::endl;
+	}
+      //std::cout << "survey says: " << ctr << " and denom is: " << denom << std::endl;
+      double sol = ctr + 5/2 + 2;
+      double tol = 1e-9;
+
+      if(abs(cp_R[3] - sol) >= tol )
+	{
+	  double diff = cp_R[3]-sol;
 	  std::cout << "Error for Species NO2!\n";
 	  std::cout << "Diff was: " << diff << "\n";
 	  return 1;
