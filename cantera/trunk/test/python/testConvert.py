47d9c44a5d0bd30c45bcdee9e31a295242ae23e3
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-07-26 20:49:26 +0000

    Moved nasa9_reader test to Python

--- cantera/trunk/test/python/testConvert.py
+++ cantera/trunk/test/python/testConvert.py
@@ -3,9 +3,10 @@ import unittest
 import numpy as np
 import ck2cti
 
+import utilities
 import Cantera as ct
 
-class chemkinConverterTest(unittest.TestCase):
+class chemkinConverterTest(utilities.CanteraTest):
     def checkConversion(self, refFile, testFile):
         ref = ct.IdealGasMix(refFile)
         gas = ct.IdealGasMix(testFile)
@@ -55,3 +56,27 @@ class chemkinConverterTest(unittest.TestCase):
         self.assertRaises(ck2cti.InputParseError,
                           lambda: ck2cti.convertMech('../data/h2o2_missingThermo.inp',
                                                     quiet=True))
+
+    def test_nasa9(self):
+        if os.path.exists('nasa9_test.cti'):
+            os.remove('nasa9_test.cti')
+        ck2cti.convertMech('../data/nasa9-test.inp',
+                           thermoFile='../data/nasa9-test-therm.dat',
+                           outName='nasa9_test.cti', quiet=True)
+
+        self.checkConversion('../data/nasa9-test.xml', 'nasa9_test.cti')
+
+        ref = ct.IdealGasMix('../data/nasa9-test.xml')
+        gas = ct.IdealGasMix('nasa9_test.cti')
+
+        for T in [300, 500, 1200, 5000]:
+            ref_cp = ref.cp_R()
+            gas_cp = gas.cp_R()
+            ref_h = ref.enthalpies_RT()
+            gas_h = gas.enthalpies_RT()
+            ref_s = ref.entropies_R()
+            gas_s = gas.entropies_R()
+            for i in range(gas.nSpecies()):
+                self.assertNear(ref_cp[i], gas_cp[i], 1e-7)
+                self.assertNear(ref_h[i], gas_h[i], 1e-7)
+                self.assertNear(ref_s[i], gas_s[i], 1e-7)
