1603de490f2f65f0d7803ac280203e407616f72d
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-08-02 15:48:27 +0000

    Added copyString as a consistent interface for copying std::string to char*

--- cantera/trunk/src/base/stringUtils.cpp
+++ cantera/trunk/src/base/stringUtils.cpp
@@ -658,4 +658,14 @@ void tokenizeString(const std::string& oval,
 }
 //================================================================================================
 
+void copyString(const std::string& source, char* dest, size_t length)
+{
+    const char* c_src = source.c_str();
+    size_t N = std::min(length, source.length()+1);
+    std::copy(c_src, c_src + N, dest);
+    if (length != 0) {
+        dest[length-1] = '\0';
+    }
+}
+
 }
