91bcef65e4543f5c1fbed95e4c835f10175cd62b
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-09-06 19:56:58 +0000

    [Python] Outline of a strategy for wrapping classes which extend Transport

--- cantera/trunk/interfaces/cython/cantera/test/test_transport.py
+++ cantera/trunk/interfaces/cython/cantera/test/test_transport.py
@@ -9,3 +9,9 @@ class TestTransport(utilities.CanteraTest):
 
     def test_viscosity(self):
         self.assertTrue(self.phase.viscosity > 0.0)
+
+class TestDustyGas(utilities.CanteraTest):
+    def test_methods(self):
+        self.phase = ct.DustyGas('h2o2.xml')
+
+        self.phase.setPorosity(0.2)
--- cantera/trunk/interfaces/cython/cantera/transport.pyx
+++ cantera/trunk/interfaces/cython/cantera/transport.pyx
@@ -1,4 +1,17 @@
 cdef class Transport(_SolutionBase):
+    def __init__(self, *args, **kwargs):
+        if self.transport == NULL:
+            self.transport = newDefaultTransportMgr(self.thermo)
+        super().__init__(*args, **kwargs)
+
     property viscosity:
         def __get__(self):
             return self.transport.viscosity()
+
+cdef class DustyGasTransport(Transport):
+    def __init__(self, *args, **kwargs):
+        self.transport = newTransportMgr(stringify("DustyGas"), self.thermo)
+        super().__init__(*args, **kwargs)
+
+    def setPorosity(self, value):
+        (<CxxDustyGasTransport*>self.transport).setPorosity(value)
