e3ce74723c4246486bc229155494ab141a921103
Ray Speth <speth@mit.edu>
2012-09-06 19:59:18 +0000

    Expanded tests for Cython ThermoPhase class

--- interfaces/cython/cantera/test/utilities.py
+++ interfaces/cython/cantera/test/utilities.py
@@ -10,3 +10,10 @@ class CanteraTest(unittest.TestCase):
             if msg:
                 message = msg + '\n' + message
             self.fail(message)
+
+    def assertArrayNear(self, A, B, rtol=1e-8, atol=1e-12, msg=None):
+        if len(A) != len(B):
+            self.fail("Arrays are of different lengths ({0}, {0})".format(len(A), len(B)))
+
+        for a,b in zip(A,B):
+            self.assertNear(a,b, rtol, atol, msg)
