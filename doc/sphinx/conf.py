5a722f9608508ddbdf3dc85d62495fc842bfc349
Ray Speth <speth@mit.edu>
2012-09-06 19:58:50 +0000

    Added docstrings to Cython ThermoPhase class

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -95,7 +95,7 @@ if sys.version_info.major == 3:
 
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
+default_role = 'py:obj'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
