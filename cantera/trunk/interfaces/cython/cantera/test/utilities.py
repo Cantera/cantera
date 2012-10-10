f7c11944ccf45a883034cc8620fd13c51d7ca8d3
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-10-10 18:25:26 +0000

    [Cython] Fixed implementation of absolute error tolerances in unit tests

--- cantera/trunk/interfaces/cython/cantera/test/utilities.py
+++ cantera/trunk/interfaces/cython/cantera/test/utilities.py
@@ -1,9 +1,10 @@
 import numpy as np
 import unittest
 
+
 class CanteraTest(unittest.TestCase):
     def assertNear(self, a, b, rtol=1e-8, atol=1e-12, msg=None):
-        cmp = 2 * abs(a - b)/(abs(a) + abs(b) + atol)
+        cmp = 2 * abs(a - b)/(abs(a) + abs(b) + 2 * atol / rtol)
         if cmp > rtol:
             message = ('AssertNear: %.14g - %.14g = %.14g\n' % (a, b, a-b) +
                        'Relative error of %10e exceeds rtol = %10e' % (cmp, rtol))
