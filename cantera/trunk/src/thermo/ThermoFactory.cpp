81b1f36d33a0c9ac1cb63e232bb2ec2fea6319a1
hkmoffa <hkmoffa@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2012-11-07 23:51:30 +0000

    Added back constructPhaseFile() and constructPhaseXML(). All molten salt problems were broken and some brine problems were broken.

--- cantera/trunk/src/thermo/ThermoFactory.cpp
+++ cantera/trunk/src/thermo/ThermoFactory.cpp
@@ -247,7 +247,22 @@ ThermoPhase* newPhase(XML_Node& xmlphase)
     const XML_Node& th = xmlphase.child("thermo");
     string model = th["model"];
     ThermoPhase* t = newThermoPhase(model);
-    importPhase(xmlphase, t);
+    if (model == "singing cows") {
+      throw CanteraError("ThermoPhase::newPhase", "Cows don't sing");
+    }
+    else if (model == "HMW") {
+      HMWSoln* p = dynamic_cast<HMWSoln*>(t);
+      p->constructPhaseXML(xmlphase,"");
+    }
+    else if (model == "IonsFromNeutralMolecule") {
+      IonsFromNeutralVPSSTP* p = dynamic_cast<IonsFromNeutralVPSSTP*>(t);
+      p->constructPhaseXML(xmlphase,"");
+    }
+    else {
+      importPhase(xmlphase, t);
+    }
+    //return t;
+    //importPhase(xmlphase, t);
     return t;
 }
 
