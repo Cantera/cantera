cb8079bd9ee43286aed1ccf79bf450d85e8ed65b
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2013-02-01 23:40:18 +0000

    [Cython] Added 'add_module_directory' utility function

--- cantera/trunk/interfaces/cython/cantera/utils.py
+++ cantera/trunk/interfaces/cython/cantera/utils.py
@@ -1,4 +1,7 @@
-from . import Solution
+import os
+import inspect
+
+from . import Solution, add_directory
 
 
 def import_phases(filename, phase_names):
@@ -7,3 +10,11 @@ def import_phases(filename, phase_names):
     entered as a list of strings.
     """
     return [Solution(filename, p) for p in phase_names]
+
+
+def add_module_directory():
+    """
+    Add the directory containing the module from which this function is called
+    to the Cantera input file search path.
+    """
+    add_directory(os.path.dirname(os.path.abspath(inspect.stack()[1][1])))
