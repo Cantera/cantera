700a96713cd99a66134a1c76435a3ab46043c6af
hkmoffa <hkmoffa@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2013-02-11 23:03:38 +0000

    Added a compilation of a doubleFAD IdealGasPhase type that links successfully.

---
+++ extendedCXXTestSuite/trunk/IdealGasJacobian_tests/testIndVarSetup/testIndVarSetup.cpp
@@ -0,0 +1,55 @@
+/**
+ *  @file mixGasTransport.cpp
+ *       test problem for mixture transport
+ */
+
+//  Example
+//
+// Test case for mixture transport in a gas
+// The basic idea is to set up a gradient of some kind.
+// Then the resulting transport coefficients out.
+// Essentially all of the interface routines should be
+// exercised and the results dumped out.
+//
+// A blessed solution test will make sure that the actual
+// solution doesn't change as a function of time or
+// further development.
+
+// perhaps, later, an analytical solution could be added
+
+#include "cantera/transport.h"
+#include "cantera/IdealGasMix.h"
+#include "cantera/transport/TransportFactory.h"
+#include "cantera/thermo.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+using namespace Cantera;
+
+int main(int argc, char** argv)
+{
+#ifdef _MSC_VER
+    _set_output_format(_TWO_DIGIT_EXPONENT);
+#endif
+    string infile = "diamond.xml";
+
+    try {
+
+	IdealGasPhase<doubleFAD> * ig_fad = new IdealGasPhase<doubleFAD>("diamond.xml", "");
+
+	delete ig_fad;
+
+
+    } catch (CanteraError& err) {
+        std::cout << err.what() << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
+/***********************************************************/
