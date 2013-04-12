3f4d4f56197e33ab6035c9bf2e6dfc81bb84a46f
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2013-04-12 23:06:20 +0000

    Cleaned up Doxygen docs for KineticsFactory and ImportKinetics

--- cantera/trunk/include/cantera/kinetics/importKinetics.h
+++ cantera/trunk/include/cantera/kinetics/importKinetics.h
@@ -38,11 +38,9 @@ struct ReactionRules {
 //!This function returns a ratio if two reactions are duplicates of
 //!one another, and 0.0 otherwise.
 /*!
- * The input arguments are two
- * maps from species number to stoichiometric coefficient, one for
- * each reaction. The reactions are considered duplicates if their
- * stoichiometric coefficients have the same ratio for all
- * species.
+ * The input arguments are two maps from species number to stoichiometric
+ * coefficient, one for each reaction. The reactions are considered duplicates
+ * if their stoichiometric coefficients have the same ratio for all species.
  *
  * @param r1 map 1
  * @param r2 map 2
@@ -57,7 +55,6 @@ struct ReactionRules {
 doublereal isDuplicateReaction(std::map<int, doublereal>& r1,
                                std::map<int, doublereal>& r2);
 
-
 //! This function will check a specific reaction to see if the elements balance.
 /*!
  *   @param kin Kinetics object
@@ -71,36 +68,31 @@ void checkRxnElementBalance(Kinetics& kin,
                             doublereal errorTolerance = 1.0e-3);
 
 /**
- * Get the reactants or products of a reaction. The information
- * is returned in the spnum, stoich, and order vectors. The
- * length of the vectors is the number of different types of
- * reactants or products found for the reaction.
+ * Get the reactants or products of a reaction. The information is returned in
+ * the spnum, stoich, and order vectors. The length of the vectors is the
+ * number of different types of reactants or products found for the reaction.
  *
- * Input
- * --------
- *  rxn -> xml node pointing to the reaction element
- *         in the xml tree.
- *  kin -> Reference to the kinetics object to install
- *         the information into.
- *  rp = 1 -> Go get the reactants for a reaction
- *      -1 -> Go get the products for a reaction
- *  default_phase = String name for the default phase
- *          to loop up species in.
- *  Output
- * -----------
- *  spnum = vector of species numbers found.
- *          Length is number of reactants or products.
- *  stoich = stoichiometric coefficient of the reactant or product
- *          Length is number of reactants or products.
- *  order = Order of the reactant and product in the reaction rate expression
- *  @param rules If rules.skipUndeclaredSpecies is set and we fail to find a
- *      species we simply return false, allowing the calling routine to skip
- *      this reaction and continue. Otherwise, we will throw an error.
+ *  @param[in] rxn xml node pointing to the reaction element in the xml tree.
+ *  @param[in] kin Reference to the kinetics object to install the information
+ *                 into.
+ * @param[in] rp 1 -> Go get the reactants for a reaction; -1 -> Go get the
+ *               products for a reaction
+ * @param[in] default_phase Name for the default phase to loop up species in.
+ * @param[out] spnum vector of species numbers found. Length is number of
+ *                   reactants or products.
+ * @param[out] stoich stoichiometric coefficient of the reactant or product.
+ *                    Length is number of reactants or products.
+ * @param[out] order Order of the reactant and product in the reaction rate
+ *                   expression.
+ * @param[in] rules If rules.skipUndeclaredSpecies is set and we fail to find
+ *                  a species we simply return false, allowing the calling
+ *                  routine to skip this reaction and continue. Otherwise, we
+ *                  will throw an error.
  */
 bool getReagents(const XML_Node& rxn, Kinetics& kin, int rp,
                  std::string default_phase,
                  std::vector<size_t>& spnum, vector_fp& stoich,
-                 vector_fp& order, const ReactionRules& rule);
+                 vector_fp& order, const ReactionRules& rules);
 
 //! Read the rate coefficient data from the XML file.
 /*!
@@ -121,18 +113,12 @@ bool getReagents(const XML_Node& rxn, Kinetics& kin, int rp,
 void getRateCoefficient(const XML_Node& kf, Kinetics& kin, ReactionData& rdata,
                         const ReactionRules& rules);
 
-
-//! Create a new ThermoPhase object and initializes it according to the XML tree database.
-
-
 //!  Install information about reactions into the kinetics object, kin.
 /*!
  *  At this point, parent usually refers to the phase xml element.
  *  One of the children of this element is reactionArray,
  *  the element which determines where in the xml file to
  *  look up the reaction rate data.
- *
- * This is a wrapper routine around the static function installReaction()
  *
  *  @param p             parent XML phase element
  *  @param kin           Kinetics object to install reactions into
@@ -153,41 +139,33 @@ bool installReactionArrays(const XML_Node& p, Kinetics& kin,
 
 //! Import a reaction mechanism for a phase or an interface.
 /*!
- * This routine will import a reaction mechanism into a
- * kinetics object. The reaction
- * mechanism may either be homogeneous or heterogeneous,
- * involving multiple ThermoPhase objects.
- * The hosting phase should be included as the first argument.
- * For example, if phase I is an interface phase between bulk
- * phases A and B. Then, the XML_Node for phase I should be
- * the first argument.
- * The vector of %ThermoPhase objects should consist of pointers
- * to phases I, A, and B.
+ * This routine will import a reaction mechanism into a kinetics object. The
+ * reaction mechanism may either be homogeneous or heterogeneous, involving
+ * multiple ThermoPhase objects. The hosting phase should be included as the
+ * first argument. For example, if phase I is an interface phase between bulk
+ * phases A and B. Then, the XML_Node for phase I should be the first
+ * argument. The vector of ThermoPhase objects should consist of pointers to
+ * phases I, A, and B.
  *
- * @param phase This is an xml node containing a description
- *              of the owning phase for the kinetics object.
- *              Within the phase is a XML element
- *              called reactionArray containing the location
- *              of the description of the reactions that make
- *              up the kinetics object.
- *              Also within the phase is an XML element called
- *              phaseArray containing a listing of other phases
- *              that participate in the kinetics mechanism.
+ * @param phase This is an xml node containing a description of the owning
+ *              phase for the kinetics object. Within the phase is a XML
+ *              element called reactionArray containing the location of the
+ *              description of the reactions that make up the kinetics object.
+ *              Also within the phase is an XML element called phaseArray
+ *              containing a listing of other phases that participate in the
+ *              kinetics mechanism.
  *
- * @param th    This is a list of ThermoPhase pointers which must
- *              include all of
- *              the phases that participate in the kinetics
- *              operator. All of the phases must have already
- *              been initialized and formed within Cantera.
- *              However, their pointers should not have been
- *              added to the Kinetics object; this addition
- *              is carried out here. Additional phases may
- *              be include in the list; these have no effect.
+ * @param th    This is a list of ThermoPhase pointers which must include all
+ *              of the phases that participate in the kinetics operator. All
+ *              of the phases must have already been initialized and formed
+ *              within Cantera. However, their pointers should not have been
+ *              added to the Kinetics object; this addition is carried out
+ *              here. Additional phases may be include in the list; these have
+ *              no effect.
  *
- * @param kin   This is a pointer to a kinetics manager class
- *              that will be initialized with the kinetics
- *              mechanism. Inherited Kinetics classes may be
- *              used here.
+ * @param kin   This is a pointer to a kinetics manager class that will be
+ *              initialized with the kinetics mechanism. Inherited Kinetics
+ *              classes may be used here.
  *
  * @ingroup kineticsmgr
  *
@@ -230,11 +208,9 @@ bool buildSolutionFromXML(XML_Node& root, const std::string& id,
 
 //! Search an XML tree for species data.
 /*!
- *
- *   This utility routine will search the XML tree for the species
- *   named by the string, kname. It will return the XML_Node
- *   pointer.
- *   Failures of any kind return the null pointer.
+ *   This utility routine will search the XML tree for the species named by
+ *   the string, kname. It will return the XML_Node pointer. Failures of any
+ *   kind return the null pointer.
  *
  * @param kname species Name
  * @param phaseSpeciesData Pointer to the phase XML node pertaining to the
@@ -251,4 +227,3 @@ bool buildSolutionFromXML(XML_Node& root, const std::string& id,
 }
 
 #endif
-
