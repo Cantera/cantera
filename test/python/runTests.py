6ec7d048395682e4e86546e5e25161d4fe779b6c
Ray Speth <speth@mit.edu>
2013-12-09 01:34:36 +0000

    [Cython] Move pure fluid tests from old Python module to Cython module

--- test/python/runTests.py
+++ test/python/runTests.py
@@ -21,8 +21,7 @@ if __name__ == '__main__':
 
     loader = unittest.TestLoader()
     runner = unittest.TextTestRunner(verbosity=2)
-    suite = loader.loadTestsFromName('testPureFluid')
-    suite.addTests(loader.loadTestsFromName('testEquilibrium'))
+    suite = loader.loadTestsFromName('testEquilibrium')
     suite.addTests(loader.loadTestsFromName('testReactors'))
     suite.addTests(loader.loadTestsFromName('testConvert'))
 
