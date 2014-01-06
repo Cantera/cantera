0c3dacb0489c61cf6fd78ba2e83523a9cc49c8b4
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2014-01-06 18:46:30 +0000

    [Matlab] Allow nAtoms to take string inputs for species and elements
    Patch provided by Bryan Weber.
    
    Resolves Issue 159.

--- cantera/trunk/interfaces/matlab/toolbox/@ThermoPhase/nAtoms.m
+++ cantera/trunk/interfaces/matlab/toolbox/@ThermoPhase/nAtoms.m
@@ -1,6 +1,12 @@
 function n = nAtoms(a,k,m)
 % NATOMS - Number of atoms of element m in species k.
 if nargin == 3
+    if ischar(m)
+        m = elementIndex(a,m);
+    end
+    if ischar(k)
+        k = speciesIndex(a,k);
+    end
     n = phase_get(a.tp_id,14,k,m);
 else
     error('usage: nAtoms(phase, k, m)')
