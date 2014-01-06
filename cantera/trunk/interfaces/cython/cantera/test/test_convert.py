52f45edbf6bc7d5ec0f8a9d4451abc0d1e66a15c
yarmond@gmail.com <yarmond@gmail.com@02a645c2-efd0-11dd-984d-ab748d24aa7e>
2014-01-06 18:46:49 +0000

    [ck2cti] Improve handling of duplicate species
    Warn when a species is given multiple times in the input list. Do not add the
    duplicate species to speciesList.
    
    Based on patch provided by Bryan Weber.
    
    Resolves Issue 199.

--- cantera/trunk/interfaces/cython/cantera/test/test_convert.py
+++ cantera/trunk/interfaces/cython/cantera/test/test_convert.py
@@ -113,6 +113,19 @@ class chemkinConverterTest(utilities.CanteraTest):
         self.assertTrue(gas.n_species, 3)
         self.assertTrue(gas.n_reactions, 2)
 
+    def test_duplicate_species(self):
+        self.assertRaises(ck2cti.InputParseError,
+                          lambda: convertMech('../data/duplicate-species.inp',
+                                              outName='duplicate-species.cti',
+                                              quiet=True))
+
+        convertMech('../data/duplicate-species.inp',
+                    outName='duplicate-species.cti',
+                    quiet=True, permissive=True)
+
+        gas = ct.Solution('duplicate-species.cti')
+        self.assertEqual(gas.species_names, ['foo','bar','baz'])
+
     def test_pathologicalSpeciesNames(self):
         convertMech('../data/species-names.inp',
                     outName='species-names.cti', quiet=True)
