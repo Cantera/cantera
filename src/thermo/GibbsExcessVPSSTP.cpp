92a3d137527d9305dce247df75f79df789295df6
Victor Brunini <victorbrunini@gmail.com>
2014-02-12 21:30:09 +0000

    Prevent hidden overloaded virtual function compiler warning.

--- src/thermo/GibbsExcessVPSSTP.cpp
+++ src/thermo/GibbsExcessVPSSTP.cpp
@@ -126,7 +126,7 @@ void GibbsExcessVPSSTP::setPressure(doublereal p)
 
 void GibbsExcessVPSSTP::calcDensity()
 {
-    vector_fp vbar = getPartialMolarVolumes();
+    vector_fp vbar = getPartialMolarVolumesVector();
     //    double *vbar = &m_pp[0];
     //    getPartialMolarVolumes(&vbar[0]);
 
@@ -223,7 +223,7 @@ void GibbsExcessVPSSTP::getPartialMolarVolumes(doublereal* vbar) const
     getStandardVolumes(vbar);
 }
 
-const vector_fp& GibbsExcessVPSSTP::getPartialMolarVolumes() const
+const vector_fp& GibbsExcessVPSSTP::getPartialMolarVolumesVector() const
 {
     return getStandardVolumes();
 }
