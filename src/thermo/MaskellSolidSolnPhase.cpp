93fcf181b1ed4e186c74432915142022d0283e7e
Victor Brunini <victorbrunini@gmail.com>
2014-02-28 00:38:09 +0000

    Initial pass at implementing Maskell solid solution ThermoPhase.
    Based on Maskell, Shaw, and Tye, Electrochimica Acta 28(2) 225-230 1983.
    Includes unit tests checking calculation of the chemical potential values
    and parsing of the mixing enthalpy parameter from an XML file.

---
+++ src/thermo/MaskellSolidSolnPhase.cpp
@@ -0,0 +1,284 @@
+/**
+ *  @file MaskellSolidSolnPhase.cpp Implementation file for an ideal solid
+ *      solution model with incompressible thermodynamics (see \ref
+ *      thermoprops and \link Cantera::MaskellSolidSolnPhase
+ *      MaskellSolidSolnPhase\endlink).
+ */
+/*
+ * Copyright 2006 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000, with Sandia Corporation, the U.S. Government
+ * retains certain rights in this software.
+ */
+
+#include "cantera/thermo/MaskellSolidSolnPhase.h"
+
+#include "cantera/base/stringUtils.h"
+
+#include <cassert>
+#include <iostream>
+
+namespace Cantera
+{
+
+MaskellSolidSolnPhase::MaskellSolidSolnPhase() :
+    ThermoPhase(),
+    m_Pref(OneAtm),
+    m_Pcurrent(OneAtm),
+    m_tlast(-1),
+    m_h0_RT(2),
+    m_cp0_R(2),
+    m_g0_RT(2),
+    m_s0_R(2),
+    h_mixing(0.)
+{
+}
+
+MaskellSolidSolnPhase::MaskellSolidSolnPhase(const MaskellSolidSolnPhase& b)
+{
+    *this = b;
+}
+
+MaskellSolidSolnPhase& MaskellSolidSolnPhase::
+operator=(const MaskellSolidSolnPhase& b)
+{
+    if (this != &b) {
+        ThermoPhase::operator=(b);
+    }
+    return *this;
+}
+
+ThermoPhase* MaskellSolidSolnPhase::duplMyselfAsThermoPhase() const
+{
+    return new MaskellSolidSolnPhase(*this);
+}
+
+/********************************************************************
+ *            Molar Thermodynamic Properties of the Solution
+ ********************************************************************/
+
+doublereal MaskellSolidSolnPhase::
+enthalpy_mole() const
+{
+    _updateThermo();
+    const doublereal h0 = GasConstant * temperature() * mean_X(&m_h0_RT[0]);
+    const doublereal r = moleFraction(0);
+    const doublereal fmval = fm(r);
+    return h0 + r * fmval * h_mixing;
+}
+
+doublereal xlogx(doublereal x)
+{
+  return x * std::log(x);
+}
+
+doublereal MaskellSolidSolnPhase::entropy_mole() const
+{
+    _updateThermo();
+    const doublereal s0 = GasConstant * mean_X(&m_s0_R[0]);
+    const doublereal r = moleFraction(0);
+    const doublereal fmval = fm(r);
+    const doublereal rfm = r * fmval;
+    return s0 + GasConstant * (xlogx(1-rfm) - xlogx(rfm) - xlogx(1-r-rfm) - xlogx((1-fmval)*r) - xlogx(1-r) - xlogx(r));
+}
+
+/********************************************************************
+ *                  Mechanical Equation of State
+ ********************************************************************/
+
+void MaskellSolidSolnPhase::
+setDensity(const doublereal rho)
+{
+    /*
+     * Unless the input density is exactly equal to the density
+     * calculated and stored in the State object, we throw an
+     * exception. This is because the density is NOT an
+     * independent variable.
+     */
+    double dens = density();
+    if (rho != dens) {
+        throw CanteraError("MaskellSolidSolnPhase::setDensity",
+                           "Density is not an independent variable");
+    }
+}
+
+void MaskellSolidSolnPhase::setPressure(doublereal p)
+{
+    m_Pcurrent = p;
+}
+
+void MaskellSolidSolnPhase::setMolarDensity(const doublereal n)
+{
+    throw CanteraError("MaskellSolidSolnPhase::setMolarDensity",
+                       "Density is not an independent variable");
+}
+
+/********************************************************************
+ *        Chemical Potentials and Activities
+ ********************************************************************/
+
+void MaskellSolidSolnPhase::
+getActivityCoefficients(doublereal* ac) const
+{
+}
+
+void MaskellSolidSolnPhase::
+getChemPotentials(doublereal* mu) const
+{
+    _updateThermo();
+    const doublereal r = moleFraction(0);
+    const doublereal pval = p(r);
+    const doublereal fmval = fm(r);
+    const doublereal rfm = r * fmval;
+    const doublereal RT = GasConstant * temperature();
+    const doublereal muDelta = -pval * h_mixing - GasConstant * temperature()
+                       * std::log( (std::pow(1 - rfm, pval) * std::pow(rfm, pval) * std::pow(r - rfm, 1 - pval) * r)  /
+                                   (std::pow(1 - r - rfm, 1 + pval) * (1 - r)) );
+    mu[0] = RT * m_g0_RT[0] + muDelta;
+    mu[1] = RT * m_g0_RT[1] - muDelta;
+}
+
+void MaskellSolidSolnPhase::
+getChemPotentials_RT(doublereal* mu) const
+{
+  const doublereal invRT = 1.0 / (GasConstant * temperature());
+  getChemPotentials(mu);
+  for(unsigned sp=0; sp < m_kk; ++sp)
+  {
+    mu[sp] *= invRT;
+  }
+}
+
+/********************************************************************
+ *                    Partial Molar Properties
+ ********************************************************************/
+
+void MaskellSolidSolnPhase::getPartialMolarEnthalpies(doublereal* hbar) const
+{
+}
+
+void MaskellSolidSolnPhase::
+getPartialMolarEntropies(doublereal* sbar) const
+{
+}
+
+void MaskellSolidSolnPhase::
+getPartialMolarCp(doublereal* cpbar) const
+{
+}
+
+void MaskellSolidSolnPhase::
+getPartialMolarVolumes(doublereal* vbar) const
+{
+}
+
+void MaskellSolidSolnPhase::
+getPureGibbs(doublereal* gpure) const
+{
+    _updateThermo();
+    const doublereal RT = GasConstant * temperature();
+    for(unsigned sp=0; sp < m_kk; ++sp)
+    {
+        gpure[sp] = RT * m_g0_RT[sp];
+    }
+}
+
+void MaskellSolidSolnPhase::
+getStandardChemPotentials(doublereal* mu) const
+{
+  // What is the difference between this and getPureGibbs? IdealSolidSolnPhase gives the same for both
+  getPureGibbs(mu);
+}
+
+/*********************************************************************
+ *    Utility Functions
+ *********************************************************************/
+void MaskellSolidSolnPhase::initThermoXML(XML_Node& phaseNode, const std::string& id_)
+{
+    std::string subname = "MaskellSolidSolnPhase::initThermoXML";
+    if (id_.size() > 0) {
+        std::string idp = phaseNode.id();
+        if (idp != id_) {
+            throw CanteraError(subname.c_str(),
+                               "phasenode and Id are incompatible");
+        }
+    }
+
+    /*
+     * Check on the thermo field. Must have:
+     * <thermo model="MaskellSolidSolution" />
+     */
+    if (phaseNode.hasChild("thermo")) {
+        XML_Node& thNode = phaseNode.child("thermo");
+        std::string mStringa = thNode.attrib("model");
+        std::string mString = lowercase(mStringa);
+        if (mString != "maskellsolidsolnphase") {
+            throw CanteraError(subname.c_str(),
+                               "Unknown thermo model: " + mStringa);
+        }
+
+
+        /*
+         * Parse the enthalpy of mixing constant
+         */
+        if (thNode.hasChild("h_mix")) {
+            XML_Node& scNode = thNode.child("h_mix");
+            set_h_mix(fpValue(scNode.value()));
+        } else {
+            throw CanteraError(subname.c_str(),
+                               "Mixing enthalpy parameter not specified.");
+        }
+    } else {
+        throw CanteraError(subname.c_str(),
+                           "Unspecified thermo model");
+    }
+
+
+    // Confirm that the phase only contains 2 species
+    if( m_kk != 2 )
+    {
+      throw CanteraError( subname.c_str(), "MaskellSolidSolution model requires exactly 2 species.");
+    }
+
+    /*
+     * Call the base initThermo, which handles setting the initial
+     * state.
+     */
+    ThermoPhase::initThermoXML(phaseNode, id_);
+}
+
+void MaskellSolidSolnPhase::_updateThermo() const
+{
+    assert(m_kk == 2);
+    doublereal tnow = temperature();
+    if (m_tlast != tnow) {
+        /*
+         * Update the thermodynamic functions of the reference state.
+         */
+        m_spthermo->update(tnow, DATA_PTR(m_cp0_R), DATA_PTR(m_h0_RT),
+                           DATA_PTR(m_s0_R));
+        m_tlast = tnow;
+        for (size_t k = 0; k < m_kk; k++) {
+            m_g0_RT[k] = m_h0_RT[k] - m_s0_R[k];
+        }
+        m_tlast = tnow;
+    }
+}
+
+doublereal MaskellSolidSolnPhase::s() const
+{
+  return 1 + std::exp(h_mixing / (GasConstant * temperature()));
+}
+
+doublereal MaskellSolidSolnPhase::fm(const doublereal r) const
+{
+  const doublereal sval = s();
+  return (1 - std::sqrt(1 - 4*r*(1-r)/sval)) / (2*r);
+}
+
+doublereal MaskellSolidSolnPhase::p(const doublereal r) const
+{
+  const doublereal sval = s();
+  return (1 - 2*r) / std::sqrt(sval*sval - 4 * sval * r + 4 * sval * r * r);
+}
+
+}  // end namespace Cantera
