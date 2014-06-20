d5870c4e4ba660edc87abaae6f0ff0a6bb6ee1d7
Ray Speth <speth@mit.edu>
2014-06-20 18:45:22 +0000

    [Python] Allow Func1 to accept nearly-scalar types
    This includes lists, tuples, and numpy arrays with a single element

--- interfaces/cython/cantera/test/test_func1.py
+++ interfaces/cython/cantera/test/test_func1.py
@@ -33,6 +33,24 @@ class TestFunc1(utilities.CanteraTest):
         for t in [0.1, 0.7, 4.5]:
             self.assertNear(f(t), 5)
 
+    def test_sequence(self):
+        f = ct.Func1([5])
+        for t in [0.1, 0.7, 4.5]:
+            self.assertNear(f(t), 5)
+
+        with self.assertRaises(TypeError):
+            ct.Func1([3,4])
+
+    def test_numpy(self):
+        f = ct.Func1(np.array(5))
+        g = ct.Func1(np.array([[5]]))
+        for t in [0.1, 0.7, 4.5]:
+            self.assertNear(f(t), 5)
+            self.assertNear(g(t), 5)
+
+        with self.assertRaises(TypeError):
+            ct.Func1(np.array([3,4]))
+
     def test_failure(self):
         def fails(t):
             raise ValueError('bad')
