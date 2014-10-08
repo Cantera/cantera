b50ab993264b676eeb8711bdca9ba17224e78ebc
Ray Speth <speth@mit.edu>
2014-10-08 16:54:55 +0000

    [Test] Fix fileLog class used in DH_graph_1
    The class was missing the implementation of writeendl, which caused the
    base class method to be called, occasionally leading to confusing test failures.
    
    Not setting the mode of the output file caused it not to be automatically created,
    so the log file usually wasn't even being written.

--- test_problems/shared/fileLog.h
+++ test_problems/shared/fileLog.h
@@ -7,11 +7,15 @@ class fileLog: public Cantera::Logger
 public:
     explicit fileLog(const std::string& fName) {
         m_fName = fName;
-        m_fs.open(fName.c_str());
+        m_fs.open(fName.c_str(), std::ios::out);
     }
 
     virtual void write(const std::string& msg) {
-        m_fs << msg << std::endl;
+        m_fs << msg;
+    }
+
+    virtual void writeendl() {
+        m_fs << std::endl;
     }
 
     virtual ~fileLog() {
