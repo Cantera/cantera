00020a988964ce0ceab3b94f18c2cb1e62ad27cf
Bryan W. Weber <bryan.w.weber@gmail.com>
2015-06-13 23:24:53 -0400

    Rename setDensityPressure to setState_RP
    Also remove redundant checks on the validity of the inputs

--- interfaces/matlab/toolbox/@ThermoPhase/setState_RP.m
+++ interfaces/matlab/toolbox/@ThermoPhase/setState_RP.m
@@ -1,6 +1,6 @@
-function setDensityPressure(tp, density, p)
-% SETDENSITYPRESSURE  Set the density and pressure.
-% setDensityPressure(tp,density,p)
+function setState_RP(tp, density, p)
+% SETSTATE_RP  Set the density and pressure.
+% setState_RP(tp,density,p)
 % The density is set first, then the pressure is set by
 % changing the temperature holding the density and
 % chemical composition fixed.
@@ -13,13 +13,5 @@ function setDensityPressure(tp, density, p)
 % :param p:
 %     Pressure. Units: Pa
 %
-
-if p <= 0.0:
-    error('The pressure must be positive.')
-end
-
-if density <= 0.0:
-    error('The pressure must be positive.')
-end
 
 thermo_set(tp.tp_id, 26, density, p);
