cf49649dd86a6c68cc8291e73c13a77eac3d1069
Bryan W. Weber <bryan@bryanwweber.com>
2015-12-26 14:49:47 -0500

    [Matlab] Move lowercase air.m to uppercase Air.m to be consistent with the other PureFluid functions

--- interfaces/matlab/toolbox/Air.m
+++ interfaces/matlab/toolbox/Air.m
@@ -1,12 +1,12 @@
-function gas = air
+function gas = Air()
 % AIR  Create an object representing air.
-% gas = air
+% gas = Air()
 % Air is modeled as an ideal gas mixture. The specification is taken
-% from file air.cti. Several reactions among oxygen and nitrogen are
+% from file ``air.xml``. Several reactions among oxygen and nitrogen are
 % defined.
 %
 % :return:
 %     Instance of class :mat:func:`Solution`
 %
 
-gas = Solution('air.cti', 'air');
+gas = Solution('air.xml', 'air');
