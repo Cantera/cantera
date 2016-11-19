dc5c2793214cc5d2f25ac94932189a193ad15b0b
Ray Speth <speth@mit.edu>
2016-11-19 15:45:27 -0500

    Allow colons in species names in CTI phase definitions

--- interfaces/cython/cantera/ctml_writer.py
+++ interfaces/cython/cantera/ctml_writer.py
@@ -1767,28 +1767,7 @@ class phase(object):
         # dictionary of species names
         self._spmap = {}
 
-        # for each species string, check whether or not the species
-        # are imported or defined locally. If imported, the string
-        # contains a colon (:)
-        for sp in self._species:
-            icolon = sp.find(':')
-            if icolon > 0:
-                #datasrc, spnames = sp.split(':')
-                datasrc = sp[:icolon].strip()
-                spnames = sp[icolon+1:]
-                self._sp.append((datasrc+'.xml', spnames))
-            else:
-                spnames = sp
-                self._sp.append(('', spnames))
-
-            for s in spnames.split():
-                self._spmap[s] = self._dim
-
         self._rxns = reactions
-
-        # check that species have been declared
-        if len(self._spmap) == 0:
-            raise CTI_Error('No species declared for phase '+self._name)
 
         # and that only one species is declared if it is a pure phase
         if self.is_pure() and len(self._spmap) > 1:
@@ -1818,7 +1797,6 @@ class phase(object):
         """Concentration dimensions. Used in computing the units for reaction
         rate coefficients."""
         return (1, -self._dim)
-
 
     def buildrxns(self, p):
 
@@ -1865,6 +1843,34 @@ class phase(object):
 
 
     def build(self, p):
+        # for each species string, check whether or not the species
+        # are imported or defined locally. If imported, the string
+        # contains a colon (:)
+        for sp in self._species:
+            foundColon = False
+            allLocal = True
+            for token in sp.split():
+                if ':' in sp:
+                    foundColon = True
+                if token not in _speciesnames:
+                    allLocal = False
+
+            if foundColon and not allLocal:
+                icolon = sp.find(':')
+                datasrc = sp[:icolon].strip()
+                spnames = sp[icolon+1:]
+                self._sp.append((datasrc+'.xml', spnames))
+            else:
+                spnames = sp
+                self._sp.append(('', spnames))
+
+            for s in spnames.split():
+                self._spmap[s] = self._dim
+
+        # check that species have been declared
+        if len(self._spmap) == 0:
+            raise CTI_Error('No species declared for phase '+self._name)
+
         p.addComment('    phase '+self._name+'     ')
         ph = p.addChild('phase')
         ph['id'] = self._name
