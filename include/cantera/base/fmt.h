ea69a014a54ac0fb5199c63b4b673af0b3eff23d
Ray Speth <speth@mit.edu>
2017-08-14 17:02:42 -0400

    Add compatibility with fmt 4.0.0

--- include/cantera/base/fmt.h
+++ include/cantera/base/fmt.h
@@ -3,8 +3,14 @@
 
 #if CT_USE_SYSTEM_FMT
   #include "fmt/format.h"
+  #if defined(FMT_VERSION) && FMT_VERSION >= 40000
+    #include "fmt/printf.h"
+  #endif
   #include "fmt/ostream.h"
 #else
   #include "cantera/ext/fmt/format.h"
+  #if defined(FMT_VERSION) && FMT_VERSION >= 40000
+    #include "cantera/ext/fmt/printf.h"
+  #endif
   #include "cantera/ext/fmt/ostream.h"
 #endif
