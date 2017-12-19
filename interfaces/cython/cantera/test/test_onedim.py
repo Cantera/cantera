24940aeef774e91156cbaa15e8e219b4c35287ad
Ray Speth <speth@mit.edu>
2017-12-18 21:26:51 -0500

    [1D] Fix occasional test failures with IonFlame

--- interfaces/cython/cantera/test/test_onedim.py
+++ interfaces/cython/cantera/test/test_onedim.py
@@ -900,6 +900,8 @@ class TestIonFlame(utilities.CanteraTest):
         self.gas.TPX = Tin, p, reactants
         self.sim = ct.IonFlame(self.gas, width=width)
         self.sim.set_refine_criteria(ratio=4, slope=0.8, curve=1.0)
+        # Ionized species may require tighter absolute tolerances
+        self.sim.flame.set_steady_tolerances(Y=(1e-4, 1e-12))
 
         # stage one
         self.sim.solve(loglevel=0, auto=True)
