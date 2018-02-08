03f18ae0004729f6e922b64eca1160e2099c22de
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-02-08 16:35:01 -0500

    [Doc] Add Donation link to sidebar

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -130,6 +130,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'cttheme'
+html_sidebars = {'**': ['localtoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html', 'numfocus.html']}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
