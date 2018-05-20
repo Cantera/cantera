1889484796b84068843622c1f7d043b657e973a1
axbriones <alejandro.briones@udri.udayton.edu>
2018-05-20 10:13:37 -0400

    adding comments to Stflow.h

--- include/cantera/oneD/StFlow.h
+++ include/cantera/oneD/StFlow.h
@@ -463,15 +463,15 @@ protected:
     virtual void updateTransport(doublereal* x, size_t j0, size_t j1);
 
     // support continuation procedure	
-    static bool m_strainRateEq;
-    bool m_UnityLewisNumber;
-    static bool m_onePointControl;
-    static bool m_twoPointControl; 
-    doublereal m_Tfuel;
-    int m_Tfuel_j;
-    doublereal m_Toxid; 
-    int m_Toxid_j;
-    static bool m_reactions;
+    bool m_strainRateEq;        // active strain rate eigenvalue equations
+    bool m_UnityLewisNumber;    // enable unity Lewis number
+    bool m_onePointControl;     // enable one point continuation
+    bool m_twoPointControl;     // enable two point continuation
+    doublereal m_Tfuel;         // fixed temperature at fuel side
+    int m_Tfuel_j;              // fixed temperature location at fuel side
+    doublereal m_Toxid;         // fixed temperature at oxidizer side
+    int m_Toxid_j;              // fixed temperature location at oxidizer side
+    bool m_reactions;           // enable reactions
     
 private:
     vector_fp m_ybar;
