a3a5bc2b7e16331d9fb49fe2d6f8ab759625f795
axbriones <alejandro.briones@udri.udayton.edu>
2018-06-01 00:59:08 -0400

    corrections to continuation test

--- interfaces/cython/cantera/test/test_onedim.py
+++ interfaces/cython/cantera/test/test_onedim.py
@@ -943,7 +943,7 @@ class TestIonFlame(utilities.CanteraTest):
         # Regression test
         self.assertNear(min(self.sim.E) / max(self.sim.E), -5.0765, 1e-3)
 
-class OneDimNumCont(utilities.CanteraTest):
+class TestOneDimNumCont(utilities.CanteraTest):
     def get_C():
         #start the clock
         timestart = time.clock()
@@ -1129,7 +1129,7 @@ class OneDimNumCont(utilities.CanteraTest):
                 file.write("%f %f\n" % (Zfuel, 0.0)) 
 
         file.close()  
-    def DiffusionFlame(self):
+    def test_diffusion_flame_cont(self):
 
         # start of main, set plot mode to interactive
         init_plot = True
