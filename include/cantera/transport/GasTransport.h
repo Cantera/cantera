QuentinMale/canteraBS

[Transport] add dispersion coefficient and quadrupole polarizability

include/cantera/transport/GasTransport.h
@@ -478,6 +478,12 @@ protected:
      */
     vector_fp m_w_ac;
 
+    //! Dispersion coefficient
+    vector_fp m_disp;
+
+    //! Quadrupole polarizability
+    vector_fp m_qua_polar;
+
     //! Level of verbose printing during initialization
     int m_log_level;
 };

interfaces/cython/cantera/transport.pyx
@@ -30,11 +30,13 @@ cdef class GasTransportData:
     """
     def __cinit__(self, geometry='', diameter=-1, well_depth=-1,
                   dipole=0.0, polarizability=0.0, rotational_relaxation=0.0,
-                  acentric_factor=0.0, *, init=True):
+                  acentric_factor=0.0, dispersion_coefficient=0.0,
+                  quadrupole_polarizability=0.0, *, init=True):
         if init:
             self._data.reset(new CxxGasTransportData(stringify(geometry),
                 diameter, well_depth, dipole, polarizability,
-                rotational_relaxation, acentric_factor))
+                rotational_relaxation, acentric_factor,
+                dispersion_coefficient, quadrupole_polarizability))
             self.data = <CxxGasTransportData*?>self._data.get()
 
     cdef _assign(self, shared_ptr[CxxTransportData] other):
@@ -43,14 +45,16 @@ cdef class GasTransportData:
 
     def set_customary_units(self, geometry, diameter, well_depth, dipole=0.0,
                             polarizability=0.0, rotational_relaxation=0.0,
-                            acentric_factor=0.0):
+                            acentric_factor=0.0, dispersion_coefficient=0.0,
+                            quadrupole_polarizability=0.0):
         """
         Set the parameters using "customary" units: diameter in Angstroms, well
         depth in Kelvin, dipole in Debye, and polarizability in Angstroms^3.
         These are the units used in in CK-style input files.
         """
         self.data.setCustomaryUnits(stringify(geometry), diameter, well_depth,
-            dipole, polarizability, rotational_relaxation, acentric_factor)
+            dipole, polarizability, rotational_relaxation, acentric_factor,
+            dispersion_coefficient, quadrupole_polarizability)
 
     property geometry:
         """
@@ -107,6 +111,20 @@ cdef class GasTransportData:
             return self.data.acentric_factor
         def __set__(self, acentric_factor):
             self.data.acentric_factor = acentric_factor
+
+    property dispersion_coefficient:
+        """ Get/Set dispersion coefficient. [A^5] """
+        def __get__(self):
+            return self.data.dispersion_coefficient
+        def __set__(self, dispersion_coefficient):
+            self.data.dispersion_coefficient = dispersion_coefficient
+
+    property quadrupole_polarizability:
+        """ Get/Set quadrupole polarizability. [A^5] """
+        def __get__(self):
+            return self.data.quadrupole_polarizability
+        def __set__(self, quadrupole_polarizability):
+            self.data.quadrupole_polarizability = quadrupole_polarizability
 
 
 cdef class Transport(_SolutionBase):

src/transport/TransportData.cpp
@@ -18,13 +18,16 @@ GasTransportData::GasTransportData()
     , polarizability(0.0)
     , rotational_relaxation(0.0)
     , acentric_factor(0.0)
+    , dispersion_coefficient(0.0)
+    , quadrupole_polarizability(0.0)
 {
 }
 
 GasTransportData::GasTransportData(
         const std::string& geometry_,
         double diameter_, double well_depth_, double dipole_,
-        double polarizability_, double rot_relax, double acentric)
+        double polarizability_, double rot_relax, double acentric,
+        double dispersion_, double qua_polar_)
     : geometry(geometry_)
     , diameter(diameter_)
     , well_depth(well_depth_)
@@ -32,13 +35,16 @@ GasTransportData::GasTransportData(
     , polarizability(polarizability_)
     , rotational_relaxation(rot_relax)
     , acentric_factor(acentric)
+    , dispersion_coefficient(dispersion_)
+    , quadrupole_polarizability(qua_polar_)
 {
 }
 
 void GasTransportData::setCustomaryUnits(
         const std::string& geometry_,
         double diameter_, double well_depth_, double dipole_,
-        double polarizability_, double rot_relax, double acentric)
+        double polarizability_, double rot_relax, double acentric,
+        double dispersion_, double qua_polar_)
 {
     geometry = geometry_;
     diameter = 1e-10 * diameter_; // convert from Angstroms to m
@@ -47,6 +53,8 @@ void GasTransportData::setCustomaryUnits(
     polarizability = 1e-30 * polarizability_; // convert from Angstroms^3 to m^3
     rotational_relaxation = rot_relax; // pure number
     acentric_factor = acentric; // dimensionless
+    dispersion_coefficient = 1e-50 * dispersion_; // convert from Angstroms^5 to m^5
+    quadrupole_polarizability = 1e-50 * qua_polar_; // convert from Angstroms^5 to m^5
 }
 
 void GasTransportData::validate(const Species& sp)
@@ -105,6 +113,16 @@ void GasTransportData::validate(const Species& sp)
         throw CanteraError("GasTransportData::validate",
             "negative rotation relaxation number for species '{}'.", sp.name);
     }
+
+    if (dispersion_coefficient < 0.0) {
+        throw CanteraError("GasTransportData::validate",
+            "negative dispersion coefficient for species '{}'.", sp.name);
+    }
+
+    if (quadrupole_polarizability < 0.0) {
+        throw CanteraError("GasTransportData::validate",
+            "negative quadrupole polarizability for species '{}'.", sp.name);
+    }
 }
 
 void setupGasTransportData(GasTransportData& tr, const XML_Node& tr_node)
@@ -126,8 +144,14 @@ void setupGasTransportData(GasTransportData& tr, const XML_Node& tr_node)
     double acentric = 0.0;
     getOptionalFloat(tr_node, "acentric_factor", acentric);
 
+    double dispersion = 0.0;
+    getOptionalFloat(tr_node, "dispersion_coefficient", dispersion);
+
+    double qua = 0.0;
+    getOptionalFloat(tr_node, "quadrupole_polarizability", qua);
+
     tr.setCustomaryUnits(geometry, diam, welldepth, dipole, polar,
-                         rot, acentric);
+                         rot, acentric, dispersion, qua);
 }
 
 shared_ptr<TransportData> newTransportData(const XML_Node& transport_node)

Cantera/cantera

include/cantera/transport/GasTransport.h
@@ -478,6 +478,12 @@ protected:
      */
     vector_fp m_w_ac;
 
+    //! Dispersion coefficient
+    vector_fp m_disp;
+
+    //! Quadrupole polarizability
+    vector_fp m_quad_polar;
+
     //! Level of verbose printing during initialization
     int m_log_level;
 };

interfaces/cython/cantera/transport.pyx
@@ -30,11 +30,13 @@ cdef class GasTransportData:
     """
     def __cinit__(self, geometry='', diameter=-1, well_depth=-1,
                   dipole=0.0, polarizability=0.0, rotational_relaxation=0.0,
-                  acentric_factor=0.0, *, init=True):
+                  acentric_factor=0.0, dispersion_coefficient=0.0,
+                  quadrupole_polarizability=0.0, *, init=True):
         if init:
             self._data.reset(new CxxGasTransportData(stringify(geometry),
                 diameter, well_depth, dipole, polarizability,
-                rotational_relaxation, acentric_factor))
+                rotational_relaxation, acentric_factor,
+                dispersion_coefficient, quadrupole_polarizability))
             self.data = <CxxGasTransportData*?>self._data.get()
 
     cdef _assign(self, shared_ptr[CxxTransportData] other):
@@ -43,14 +45,16 @@ cdef class GasTransportData:
 
     def set_customary_units(self, geometry, diameter, well_depth, dipole=0.0,
                             polarizability=0.0, rotational_relaxation=0.0,
-                            acentric_factor=0.0):
+                            acentric_factor=0.0, dispersion_coefficient=0.0,
+                            quadrupole_polarizability=0.0):
         """
         Set the parameters using "customary" units: diameter in Angstroms, well
         depth in Kelvin, dipole in Debye, and polarizability in Angstroms^3.
         These are the units used in in CK-style input files.
         """
         self.data.setCustomaryUnits(stringify(geometry), diameter, well_depth,
-            dipole, polarizability, rotational_relaxation, acentric_factor)
+            dipole, polarizability, rotational_relaxation, acentric_factor,
+            dispersion_coefficient, quadrupole_polarizability)
 
     property geometry:
         """
@@ -107,6 +111,20 @@ cdef class GasTransportData:
             return self.data.acentric_factor
         def __set__(self, acentric_factor):
             self.data.acentric_factor = acentric_factor
+
+    property dispersion_coefficient:
+        """ Get/Set dispersion coefficient. [m^5] """
+        def __get__(self):
+            return self.data.dispersion_coefficient
+        def __set__(self, dispersion_coefficient):
+            self.data.dispersion_coefficient = dispersion_coefficient
+
+    property quadrupole_polarizability:
+        """ Get/Set quadrupole polarizability. [m^5] """
+        def __get__(self):
+            return self.data.quadrupole_polarizability
+        def __set__(self, quadrupole_polarizability):
+            self.data.quadrupole_polarizability = quadrupole_polarizability
 
 
 cdef class Transport(_SolutionBase):

src/transport/TransportData.cpp
@@ -18,13 +18,16 @@ GasTransportData::GasTransportData()
     , polarizability(0.0)
     , rotational_relaxation(0.0)
     , acentric_factor(0.0)
+    , dispersion_coefficient(0.0)
+    , quadrupole_polarizability(0.0)
 {
 }
 
 GasTransportData::GasTransportData(
         const std::string& geometry_,
         double diameter_, double well_depth_, double dipole_,
-        double polarizability_, double rot_relax, double acentric)
+        double polarizability_, double rot_relax, double acentric,
+        double dispersion, double quad_polar)
     : geometry(geometry_)
     , diameter(diameter_)
     , well_depth(well_depth_)
@@ -32,13 +35,16 @@ GasTransportData::GasTransportData(
     , polarizability(polarizability_)
     , rotational_relaxation(rot_relax)
     , acentric_factor(acentric)
+    , dispersion_coefficient(dispersion)
+    , quadrupole_polarizability(quad_polar)
 {
 }
 
 void GasTransportData::setCustomaryUnits(
         const std::string& geometry_,
         double diameter_, double well_depth_, double dipole_,
-        double polarizability_, double rot_relax, double acentric)
+        double polarizability_, double rot_relax, double acentric,
+        double dispersion, double quad_polar)
 {
     geometry = geometry_;
     diameter = 1e-10 * diameter_; // convert from Angstroms to m
@@ -47,6 +53,8 @@ void GasTransportData::setCustomaryUnits(
     polarizability = 1e-30 * polarizability_; // convert from Angstroms^3 to m^3
     rotational_relaxation = rot_relax; // pure number
     acentric_factor = acentric; // dimensionless
+    dispersion_coefficient = 1e-50 * dispersion; // convert from Angstroms^5 to m^5
+    quadrupole_polarizability = 1e-50 * quad_polar; // convert from Angstroms^5 to m^5
 }
 
 void GasTransportData::validate(const Species& sp)
@@ -105,6 +113,16 @@ void GasTransportData::validate(const Species& sp)
         throw CanteraError("GasTransportData::validate",
             "negative rotation relaxation number for species '{}'.", sp.name);
     }
+
+    if (dispersion_coefficient < 0.0) {
+        throw CanteraError("GasTransportData::validate",
+            "negative dispersion coefficient for species '{}'.", sp.name);
+    }
+
+    if (quadrupole_polarizability < 0.0) {
+        throw CanteraError("GasTransportData::validate",
+            "negative quadrupole polarizability for species '{}'.", sp.name);
+    }
 }
 
 void setupGasTransportData(GasTransportData& tr, const XML_Node& tr_node)
@@ -126,8 +144,14 @@ void setupGasTransportData(GasTransportData& tr, const XML_Node& tr_node)
     double acentric = 0.0;
     getOptionalFloat(tr_node, "acentric_factor", acentric);
 
+    double dispersion = 0.0;
+    getOptionalFloat(tr_node, "dispersion_coefficient", dispersion);
+
+    double quad = 0.0;
+    getOptionalFloat(tr_node, "quadrupole_polarizability", quad);
+
     tr.setCustomaryUnits(geometry, diam, welldepth, dipole, polar,
-                         rot, acentric);
+                         rot, acentric, dispersion, quad);
 }
 
 shared_ptr<TransportData> newTransportData(const XML_Node& transport_node)