97b0b2bd3a35cac6a542fdd960142b94c6d44312
Ray Speth <speth@mit.edu>
2018-08-30 10:32:12 -0400

    [Python] Make transport properties accessible with the Water() function

--- interfaces/cython/cantera/liquidvapor.py
+++ interfaces/cython/cantera/liquidvapor.py
@@ -1,11 +1,19 @@
 # This file is part of Cantera. See License.txt in the top-level directory or
 # at http://www.cantera.org/license.txt for license and copyright information.
 
-from . import PureFluid
+from . import PureFluid, _cantera
+
+
 
 def Water():
-    """Create a `PureFluid` object using the equation of state for water."""
-    return PureFluid('liquidvapor.xml','water')
+    """
+    Create a `PureFluid` object using the equation of state for water and the
+    `WaterTransport` class for viscosity and thermal conductivity."""
+    class WaterWithTransport(PureFluid, _cantera.Transport):
+        __slots__ = ()
+
+
+    return WaterWithTransport('liquidvapor.xml', 'water', transport_model='Water')
 
 def Nitrogen():
     """Create a `PureFluid` object using the equation of state for nitrogen."""
