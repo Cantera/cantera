420616398f310cd14e68d27239aee9296c3ffbeb
Ray Speth <speth@mit.edu>
2018-11-18 23:22:01 -0500

    UnitSystem class supports default source units for conversions

--- test/general/test_units.cpp
+++ test/general/test_units.cpp
@@ -38,3 +38,14 @@ TEST(Units, prefixes) {
     EXPECT_DOUBLE_EQ(U.convert(1.0, "m^2", "cm^2"), 1e4);
     EXPECT_DOUBLE_EQ(U.convert(1.0, "m/s", "km/hr"), 3.6);
 }
+
+TEST(Units, with_defaults) {
+    UnitSystem U({"cm", "g", "mol", "atm"});
+    EXPECT_DOUBLE_EQ(U.convert(1.0, "m"), 0.01);
+    EXPECT_DOUBLE_EQ(U.convert(1.0, "kmol/m^3"), 1000);
+    EXPECT_DOUBLE_EQ(U.convert(1.0, "kg/kmol"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convert(1.0, "cm^2"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convert(1.0, "Pa"), 101325);
+    EXPECT_DOUBLE_EQ(U.convert(1.0, "hPa"), 1013.25);
+    EXPECT_DOUBLE_EQ(U.convert(1.0, "Pa*m^6/kmol"), 101325*1e-12*1000);
+}
