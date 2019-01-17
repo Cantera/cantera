7d964dae47bc77f15dfc2270385d06b5fe3e059a
Ray Speth <speth@mit.edu>
2019-01-17 18:24:50 -0500

    [Input] Create MaskellSolidSoln objects from YAML definitions

--- src/thermo/MaskellSolidSolnPhase.cpp
+++ src/thermo/MaskellSolidSolnPhase.cpp
@@ -175,6 +175,18 @@ void MaskellSolidSolnPhase::getStandardChemPotentials(doublereal* mu) const
 
 // Utility Functions
 
+void MaskellSolidSolnPhase::initThermo()
+{
+    if (m_input.hasKey("excess-enthalpy")) {
+        set_h_mix(m_input.convert("excess-enthalpy", "J/kmol"));
+    }
+    if (m_input.hasKey("product-species")) {
+        setProductSpecies(m_input["product-species"].asString());
+    }
+    VPStandardStateTP::initThermo();
+}
+
+
 void MaskellSolidSolnPhase::initThermoXML(XML_Node& phaseNode, const std::string& id_)
 {
     if (id_.size() > 0 && phaseNode.id() != id_) {
