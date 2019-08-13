7964cf33495414fbe81f88a83afac203ceebdcf8
Ingmar Schoegl <ischoegl@lsu.edu>
2019-08-13 10:33:02 -0500

    [Input] Implement pickle serialization for Solution objects

--- interfaces/cython/cantera/base.pyx
+++ interfaces/cython/cantera/base.pyx
@@ -9,6 +9,18 @@ cdef class _SolutionBase:
                   source=None, yaml=None, thermo=None, species=(),
                   kinetics=None, reactions=(), **kwargs):
 
+        # run instantiation only if valid sources are specified
+        if origin or infile or source or yaml or (thermo and species):
+
+            self._cinit(infile=infile, name=name, adjacent=adjacent,
+                        origin=origin, source=source, yaml=yaml,
+                        thermo=thermo, species=species, kinetics=kinetics,
+                        reactions=reactions, **kwargs)
+
+    def _cinit(self, infile='', name='', adjacent=(), origin=None,
+                  source=None, yaml=None, thermo=None, species=(),
+                  kinetics=None, reactions=(), **kwargs):
+
         if 'phaseid' in kwargs:
             if name is not '':
                 raise AttributeError('duplicate specification of phase name')
@@ -351,8 +363,18 @@ cdef class _SolutionBase:
             for i,spec in enumerate(species):
                 self._selected_species[i] = self.species_index(spec)
 
-    def __reduce__(self):
-        raise NotImplementedError('Solution object is not picklable')
+    def __getstate__(self):
+        """Save complete information of the current phase for pickling."""
+        if self.kinetics.nTotalSpecies() > self.thermo.nSpecies():
+            raise NotImplementedError(
+                "Pickling of Interface objects is not implemented.")
+        return self.write_yaml(), self.state
+
+    def __setstate__(self, pkl):
+        """Restore Solution from pickled information."""
+        yml, state = pkl
+        self._cinit(yaml=yml)
+        self.state = state
 
     def __copy__(self):
         raise NotImplementedError('Solution object is not copyable')
