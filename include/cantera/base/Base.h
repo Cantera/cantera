f71954ac96d80508cec96d8d3d31d646b62b19b7
Ingmar Schoegl <ischoegl@lgmail.com>
2019-08-16 19:43:40 -0500

    [Base] associate phase ID with SolutionBase object

--- include/cantera/base/Base.h
+++ include/cantera/base/Base.h
@@ -31,29 +31,42 @@ public:
 
     //! String indicating the type of the SolutionBase object. Corresponds
     //! to the type of phase originally instantiated
-    std::string type() const {
-        return m_type;
-    }
+    std::string type() const;
 
     //! Set the type of this SolutionBase object
-    void setType(const std::string& type){
-        m_type = type;
-    }
+    void setType(const std::string& type);
+
+    /*! Name and ID
+     * Class SolutionBase references two strings that identify a SolutionBase.
+     * The ID is the value of the ID attribute of the XML/YAML node that is used
+     * to initialize a phase when it is read. The name field is also initialized
+     * to the value of the ID attribute of the XML/YAML node.
+     *
+     * However, the name field may be changed to another value during the course
+     * of a calculation. For example, if a SolutionBase is located in two places,
+     * but has the same constitutive input, the IDs of the two SolutionBases
+     * will be the same, but the names of the two SOlutionBases  may be different.
+     *
+     * It is an error to have two phases in a single problem with the same name
+     * and ID (or the name from one phase being the same as the id of another
+     * SolutionBase). Thus, it is expected that there is a 1-1 correspondence
+     * between names and unique SolutionBase objects within a Cantera problem.
+     */
+
+    //! Return the string id for the SolutionBase.
+    std::string id() const;
+
+    //! Set the string id for the SolutionBase.
+    void setID(const std::string& id);
 
     //! Return the name of this SolutionBase object
-    std::string name() const {
-        return m_name;
-    }
+    std::string name() const;
 
     //! Set the name of this SolutionBase object
-    void setName(const std::string& name){
-        m_name = name;
-    }
+    void setName(const std::string& name);
 
     //! Generate self-documenting YAML string
-    virtual std::string toYAML() const {
-        throw NotImplementedError("SolutionBase::toYAML");
-    }
+    virtual std::string toYAML() const;
 
     //! Set the ThermoPhase object
     void setThermoPhase(shared_ptr<ThermoPhase> thermo);
@@ -70,7 +83,6 @@ protected:
     shared_ptr<Transport> m_transport;  //! Transport manager
 
     std::string m_type;  //! type of SolutionBase object
-    std::string m_name;  //! name of SolutionBase object
 };
 
 }
--- interfaces/cython/cantera/thermo.pyx
+++ interfaces/cython/cantera/thermo.pyx
@@ -285,15 +285,6 @@ cdef class ThermoPhase(_SolutionBase):
     def __call__(self, *args, **kwargs):
         print(self.report(*args, **kwargs))
 
-    property ID:
-        """
-        The ID of the phase. The default is taken from the CTI/XML input file.
-        """
-        def __get__(self):
-            return pystr(self.thermo.id())
-        def __set__(self, id_):
-            self.thermo.setID(stringify(id_))
-
     property basis:
         """
         Determines whether intensive thermodynamic properties are treated on a
