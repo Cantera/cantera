ede585c4823e9e311e434f408b5046587e6a9276
Yi Wang <wangyi_x11@outlook.com>
2019-10-25 17:26:59 +0800

    correct the oxidizer velocity signal

--- src/oneD/Boundary1D.cpp
+++ src/oneD/Boundary1D.cpp
@@ -224,7 +224,7 @@ void Inlet1D::eval(size_t jg, double* xg, double* rg,
             rb[c_offset_T] -= m_temp; // T
         }
         if (m_flow->onePointControlEnabled() || m_flow->twoPointControlEnabled()) {
-            m_mdot = m_flow->density(last_index) * xb[c_offset_Uo];
+            m_mdot = -(m_flow->density(last_index)) * xb[c_offset_Uo];
         } 
             
         rb[c_offset_U] += m_mdot; // u      
