b1b6470c5e2c4eb170034f8edc2bb3d132c70b45
bangshiuh <bangshiuh.chen@gmail.com>
2019-12-16 07:38:07 -0500

    [numerics] add comment

--- include/cantera/numerics/funcs.h
+++ include/cantera/numerics/funcs.h
@@ -30,6 +30,12 @@ namespace Cantera
 doublereal linearInterp(doublereal x, const vector_fp& xpts,
                         const vector_fp& fpts);
 
+//! Calculate the quadrature of a discrete function by simpson's 1/3 rule.
+/*!
+ * @param x coordinate vector
+ * @param y function vector
+ * @returns the value of the quadrature.
+ */
 double simpsonQuadrature(const vector_fp& x, const vector_fp& y);
 }
 
