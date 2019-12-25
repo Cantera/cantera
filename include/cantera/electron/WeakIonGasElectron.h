b8929f5c42140bbc722edd1a8b024180c6866b31
bangshiuh <bangshiuh.chen@gmail.com>
2019-12-25 09:25:44 -0500

    [electron] update WeakIonGasElectron

--- include/cantera/electron/WeakIonGasElectron.h
+++ include/cantera/electron/WeakIonGasElectron.h
@@ -12,15 +12,59 @@
 #include "cantera/electron/Electron.h"
 #include <Eigen/Sparse>
 
-typedef Eigen::SparseMatrix<double> SpMat;
-typedef Eigen::Triplet<double> T;
-
 namespace Cantera
 {
+typedef Eigen::SparseMatrix<double> SparseMat;
+
 /**
  * This class calculates the properties of electron in a weakly ionized gas.
  * Only electron-neutral collisions are considered for calculating the
- * electron energy distribution function (EEDF).
+ * electron energy distribution function (EEDF). Equation of EEDF becomes,
+ * \f[
+ *   \frac{d}{d \epsilon}\left(\tilde{W} F_0 - \tilde{D} \frac{d F_0}{d \epsilon}\right)
+ *   = \tilde{S}
+ * \f]
+ * where
+ * \f[
+ *     \tilde{W} = -\gamma\epsilon^2\sigma_{\epsilon},
+ * \f]
+ * \f[
+ *     \tilde{D} = \frac{\gamma}{3} \left(\frac{E}{N} \right)^2 \frac{\epsilon}{\tilde{\sigma}_m} +
+ *                 \frac{\gamma k_B T}{e} \epsilon^2 \sigma_{\epsilon},
+ * \f]
+ * \f[
+ *     \tilde{S} = \sum_{k=inelastic} \tilde{C}_{0,k} + G
+ * \f]
+ * where \f$ \gamma = (\frac{2 e}{m})^{1/2} \f$, \f$ \epsilon \f$ is the electron energy,
+ * \f$ \sigma_{\epsilon} \f$ is the total elastic collision cross section,
+ * \f$ E \f$ is electric field strength, \f$ N \f$ is gas number density,
+ * \f$ \tilde{\sigma}_m \f$ is the effective total momentum-transfer cross section,
+ * \f$ k_B \f$ is Boltzmann constant, \f$ e \f$ is the elementary charge,
+ * \f$ \tilde{C}_{0,k} \f$ represents the rate of change in EEDF due to collisions,
+ * and \f$ F_0 \f$ is the normalized EEDF.
+ * 
+ * The collision terms are,
+ * \f[
+ *     \tilde{C}_{0,k=elastic} = \gamma X_k \frac{2 m}{M_k} \frac{d}{d \epsilon}
+ *         \left[\epsilon^2 \sigma_k \left( F_0 + \frac{k_B T}{e} \frac{d F_0}{\epsilon} \right) \right],
+ * \f]
+ * \f[
+ *     \tilde{C}_{0,k=inelastic} = -\gamma X_k \epsilon \sigma_k F_0
+ *                                 \big|^{\epsilon=\epsilon}_{\epsilon=\epsilon + u_k}
+ * \f]
+ * \f[
+ *     \tilde{C}_{0,k=ionization} = -\gamma X_k \epsilon \sigma_k F_0
+ *                                  \big|^{\epsilon=\epsilon}_{\epsilon=2\epsilon + u_k}
+ * \f]
+ * \f[
+ *     \tilde{C}_{0,k=attachment} = -\gamma X_k \epsilon \sigma_k F_0
+ * \f]
+ * For exponential temporal growth model,
+ * \f[
+ *     G = \left[ \int_0^\infty \left(\sum_{k=ionization} X_k \sigma_k - \sum_{k=attachment} X_k \sigma_k \right)
+ *         \epsilon F_0 d \epsilon \right] \epsilon^{1/2} F_0
+ * \f]
+ * where \f$ X_k \f$ is the mole fraction of the target species, and \f$ \sigma_k \f$ is the cross section.
  *
  * Reference:
  * [1] G. J. M. Hagelaar and L. C. Pitchford
@@ -56,7 +100,7 @@ public:
     double realMobility();
 
     //! electron temperature
-    //! If the reduced electric field is set, electron tempeature is calculated
+    //! If the reduced electric field is set, electron temperature is calculated
     //! from EEDF.
     virtual double electronTemperature();
 
@@ -67,52 +111,92 @@ public:
     }
 
 protected:
-    //! Calculate distribution function
+    //! Calculate distribution function by solving Boltzmann equation
+    //! with two-term approximate method.
     void calculateDistributionFunction();
 
-    //! Calculate total cross section
+    //! Calculate total cross section. The total cross section is defined as the summation
+    //! of weighted (by mole fractions) cross sections of all coliision processes.
     void calculateTotalCrossSection();
 
-    //! Calculate total elastic cross section
+    //! Calculate total elastic cross section \f$ \sigma_{\epsilon} \f$.
+    /**
+     * \f[
+     *     \sigma_{\epsilon} = \sum_{k=elastic} \frac{2 m}{M_k} X_k \sigma_k,
+     * \f]
+     * where \f$ m \f$ is the mass of electron, \f$ M_k \f$ is the mass of the target species,
+     * \f$ X_k \f$ is the mole fraction of the target species, \f$ \sigma_k \f$ is the cross section.
+     */
     void calculateTotalElasticCrossSection();
 
-    //! The integral in [a, b] of x * u(x) exp[g * (x0 - x)]
+    //! The integral in [a, b] of \f$x u(x) \exp[g (x_0 - x)]\f$
     //! assuming that u is linear with u(a) = u0 and u(b) = u1
     double integralPQ(double a, double b, double u0, double u1,
                        double g, double x0);
 
-    //! Norm of electron energy distribution function
-    double norm(Eigen::VectorXd f);
-
     //! Vector g is used by matrix_PQ.
-    vector_fp vector_g(Eigen::VectorXd f0);
+    /**
+     * \f[
+     * g_i = \frac{1}{\epsilon_{i+1} - \epsilon_{i-1}} \ln(\frac{F_{0, i+1}}{F_{0, i-1}})
+     * \f]
+     */
+    vector_fp vector_g(Eigen::VectorXd& f0);
 
-    //! The matrix of scattering-out and scattering-in
-    SpMat matrix_PQ(vector_fp g, size_t k);
-
-    //! The matrix of scattering-out
-    SpMat matrix_P(vector_fp g, size_t k);
+    //! The matrix of scattering-out.
+    /**
+     * \f[
+     * P_i = \sum_{k=inelastic} \gamma X_k \int_{\epsilon_i - 1/2}^{\epsilon_i + 1/2}
+     * \epsilon \sigma_k exp[(\epsilon_i - \epsilon)g_i] d \epsilon
+     * \f]
+     */
+    SparseMat matrix_P(vector_fp& g, size_t k);
 
     //! The matrix of scattering-in
-    SpMat matrix_Q(vector_fp g, size_t k);
+    /**
+     * \f[
+     * Q_{i,j} = \sum_{k=inelastic} \gamma X_k \int_{\epsilon_1}^{\epsilon_2}
+     * \epsilon \sigma_k exp[(\epsilon_j - \epsilon)g_j] d \epsilon
+     * \f]
+     */
+    //! where the interval \f$[\epsilon_1, \epsilon_2]\f$ is the overlap of cell j,
+    //! and cell i shifted by the threshold energy u_k:
+    /**
+     * \f[
+     * \epsilon_1 = \min(\max(\epsilon_{i-1/2}+u_k, \epsilon_{j-1/2}),\epsilon_{j+1/2}),
+     * \f]
+     * \f[
+     * \epsilon_2 = \min(\max(\epsilon_{i+1/2}+u_k, \epsilon_{j-1/2}),\epsilon_{j+1/2})
+     * \f]
+     */
+    SparseMat matrix_Q(vector_fp& g, size_t k);
 
-    //! matrix A represents equation (45) of ref. [1]
-    SpMat matrix_A(Eigen::VectorXd f0);
+    //! Matrix A (Ax = b) of the equation of EEDF, which is discretized by the exponential scheme
+    //! of Scharfetter and Gummel,
+    /**
+     * \f[
+     *     \left[ \tilde{W} F_0 - \tilde{D} \frac{d F_0}{\epsilon} \right]_{i+1/2} =
+     *     \frac{\tilde{W}_{i+1/2} F_{0,i}}{1 - \exp[-z_{i+1/2}]} +
+     *     \frac{\tilde{W}_{i+1/2} F_{0,i+1}}{1 - \exp[z_{i+1/2}]}
+     * \f]
+     * where \f$ z_{i+1/2} = \tilde{w}_{i+1/2} / \tilde{D}_{i+1/2} \f$ (Peclet number).
+     */
+    SparseMat matrix_A(Eigen::VectorXd& f0);
 
     //! An iteration of solving electron energy distribution function
-    Eigen::VectorXd iterate(Eigen::VectorXd f0, double delta = 1e14);
+    Eigen::VectorXd iterate(Eigen::VectorXd& f0, double delta = 1e14);
 
     //! Iterate until convergence and obtain EEDF
-    Eigen::VectorXd converge(Eigen::VectorXd f0);
+    Eigen::VectorXd converge(Eigen::VectorXd& f0);
 
     //! Reduced net production frequency. Equation (10) of ref. [1]
     //! divided by N.
     //! @param f0 EEDF
     //! @param chem_rate Chemionization rate. This is an optional source of
     //!        electron.
-    double netProductionFreq(Eigen::VectorXd f0);
+    double netProductionFreq(Eigen::VectorXd& f0);
 
-    //! electron temperature. For internal use only.
+    //! electron temperature. For internal use only. This function is used to evaluate EEDF by
+    //! comparing the resulting electron temeprature to gas temperature.
     double electronTemperature(Eigen::VectorXd f0);
 
     //! bi-Maxwellian Boltzmann factor. Assume that the excitation
@@ -120,11 +204,11 @@ protected:
     double biMaxwellFraction(size_t k);
 
     //! Total electron cross section on the cell center of energy grid
-    vector_fp m_totalCrossSectionC;
+    vector_fp m_totalCrossSectionCenter;
 
     //! Total electron cross section on the cell boundary (i-1/2) of
     //! energy grid
-    vector_fp m_totalCrossSectionB;
+    vector_fp m_totalCrossSectionEdge;
 
     //! vector of total elastic cross section weighted with mass ratio
     vector_fp m_sigmaElastic;
--- src/electron/WeakIonGasElectron.cpp
+++ src/electron/WeakIonGasElectron.cpp
@@ -9,6 +9,18 @@
 
 namespace Cantera {
 
+typedef Eigen::Triplet<double> Triplet;
+
+//! Calculate the norm of EEDF
+double norm(const Eigen::VectorXd& f, const vector_fp& grid)
+{
+    vector_fp p(f.size());
+    for (int i = 0; i < f.size(); i++) {
+        p[i] = f(i) * pow(grid[i], 0.5);
+    }
+    return simpsonQuadrature(grid, p);
+}
+
 WeakIonGasElectron::WeakIonGasElectron()
     : m_chemionScatRate(0.0)
 {
@@ -16,18 +28,16 @@ WeakIonGasElectron::WeakIonGasElectron()
 
 void WeakIonGasElectron::calculateTotalCrossSection()
 {
-    m_totalCrossSectionC.clear();
-    m_totalCrossSectionC.resize(m_points, 0.0);
-    m_totalCrossSectionB.clear();
-    m_totalCrossSectionB.resize(m_points + 1, 0.0);
+    m_totalCrossSectionCenter.assign(m_points, 0.0);
+    m_totalCrossSectionEdge.assign(m_points + 1, 0.0);
     for (size_t k = 0; k < m_ncs; k++) {
-        vector_fp x = m_crossSections[k][0];
-        vector_fp y = m_crossSections[k][1];
+        vector_fp& x = m_crossSections[k][0];
+        vector_fp& y = m_crossSections[k][1];
         for (size_t i = 0; i < m_points; i++) {
-            m_totalCrossSectionC[i] += m_moleFractions[k] * linearInterp(m_gridC[i], x, y);
+            m_totalCrossSectionCenter[i] += m_moleFractions[k] * linearInterp(m_gridCenter[i], x, y);
         }
         for (size_t i = 0; i < m_points + 1; i++) {
-            m_totalCrossSectionB[i] += m_moleFractions[k] * linearInterp(m_gridB[i], x, y);
+            m_totalCrossSectionEdge[i] += m_moleFractions[k] * linearInterp(m_gridEdge[i], x, y);
         }
     }
 }
@@ -37,11 +47,12 @@ void WeakIonGasElectron::calculateTotalElasticCrossSection()
     m_sigmaElastic.clear();
     m_sigmaElastic.resize(m_points, 0.0);
     for (size_t k : m_kElastic) {
-        vector_fp x = m_crossSections[k][0];
-        vector_fp y = m_crossSections[k][1];
+        vector_fp& x = m_crossSections[k][0];
+        vector_fp& y = m_crossSections[k][1];
         for (size_t i = 0; i < m_points; i++) {
-            m_sigmaElastic[i] += 2.0 * m_massRatios[k] * m_moleFractions[k] *
-                                 linearInterp(m_gridB[i], x, y);
+            double mass_ratio = ElectronMass / (Dalton * m_thermo->molecularWeight(m_kTargets[k]));
+            m_sigmaElastic[i] += 2.0 * mass_ratio * m_moleFractions[k] *
+                                 linearInterp(m_gridEdge[i], x, y);
         }
     }
 }
@@ -51,7 +62,7 @@ void WeakIonGasElectron::calculateDistributionFunction()
     // check if T or C is changed
     update_T();
     update_C();
-    if (m_f0_ok == true) {
+    if (m_f0_ok) {
         return;
     }
 
@@ -65,29 +76,22 @@ void WeakIonGasElectron::calculateDistributionFunction()
 
     for (size_t j = 0; j < m_points; j++) {
         m_f0(j) = 2.0 * pow(1.0/Pi, 0.5) * pow(kT, -3./2.) *
-                  std::exp(-m_gridC[j]/kT);
+                  std::exp(-m_gridCenter[j]/kT);
     }
 
     if (m_E != 0.0) {
         m_f0 = converge(m_f0);
         double Te = electronTemperature(m_f0);
+        // Evaluate the EEDF by comparing electron temperature to gas temperature,
+        // and replace the EEDF with a Maxwellian distribution at gas temperature.
         if (Te < m_thermo->temperature()) {
             for (size_t j = 0; j < m_points; j++) {
                 m_f0(j) = 2.0 * pow(1.0/Pi, 0.5) * pow(m_kT, -3./2.) *
-                          std::exp(-m_gridC[j]/m_kT);
+                          std::exp(-m_gridCenter[j]/m_kT);
             }
         }
     }
     m_f0_ok = true;
-}
-
-double WeakIonGasElectron::norm(Eigen::VectorXd f)
-{
-    vector_fp p(f.size());
-    for (int i = 0; i < f.size(); i++) {
-        p[i] = f(i) * pow(m_gridC[i], 0.5);
-    }
-    return simpsonQuadrature(m_gridC, p);
 }
 
 double WeakIonGasElectron::integralPQ(double a, double b, double u0, double u1,
@@ -117,60 +121,39 @@ double WeakIonGasElectron::integralPQ(double a, double b, double u0, double u1,
     return c0 * A1 + c1 * A2;
 }
 
-vector_fp WeakIonGasElectron::vector_g(Eigen::VectorXd f0)
+vector_fp WeakIonGasElectron::vector_g(Eigen::VectorXd& f0)
 {
     vector_fp g(m_points, 0.0);
-    g[0] = std::log(f0(1)/f0(0)) / (m_gridC[1] - m_gridC[0]);
+    g[0] = std::log(f0(1)/f0(0)) / (m_gridCenter[1] - m_gridCenter[0]);
     double N = m_points - 1;
-    g[N] = std::log(f0(N)/f0(N-1)) / (m_gridC[N] - m_gridC[N-1]);
+    g[N] = std::log(f0(N)/f0(N-1)) / (m_gridCenter[N] - m_gridCenter[N-1]);
     for (size_t i = 1; i < m_points - 1; i++) {
-        g[i] = std::log(f0(i+1)/f0(i-1)) / (m_gridC[i+1] - m_gridC[i-1]);
+        g[i] = std::log(f0(i+1)/f0(i-1)) / (m_gridCenter[i+1] - m_gridCenter[i-1]);
     }
     return g;
 }
 
-SpMat WeakIonGasElectron::matrix_PQ(vector_fp g, size_t k)
+SparseMat WeakIonGasElectron::matrix_P(vector_fp& g, size_t k)
 {
-    std::vector<T> tripletList;
-    for (size_t n = 0; n < m_eps[k].size(); n++) {
-        double eps_a = m_eps[k][n][0];
-        double eps_b = m_eps[k][n][1];
-        double sigma_a = m_sigma[k][n][0];
-        double sigma_b = m_sigma[k][n][1];
-        double i = m_i[k][n];
-        double j = m_j[k][n];
-        double r = integralPQ(eps_a, eps_b, sigma_a, sigma_b, g[j], m_gridC[j]);
-        double q = m_inFactor[k] * m_gamma * m_moleFractions[k] * r;
-        double p = - m_gamma * m_moleFractions[k] * r;
-        tripletList.push_back(T(i, j, q));
-        tripletList.push_back(T(j, j, p));
-    }
-    SpMat PQ(m_points, m_points);
-    PQ.setFromTriplets(tripletList.begin(), tripletList.end());
-    return PQ;
-}
-
-SpMat WeakIonGasElectron::matrix_P(vector_fp g, size_t k)
-{
-    std::vector<T> tripletList;
+    std::vector<Triplet> tripletList;
     for (size_t n = 0; n < m_eps[k].size(); n++) {
         double eps_a = m_eps[k][n][0];
         double eps_b = m_eps[k][n][1];
         double sigma_a = m_sigma[k][n][0];
         double sigma_b = m_sigma[k][n][1];
         double j = m_j[k][n];
-        double r = integralPQ(eps_a, eps_b, sigma_a, sigma_b, g[j], m_gridC[j]);
+        double r = integralPQ(eps_a, eps_b, sigma_a, sigma_b, g[j], m_gridCenter[j]);
         double p = m_gamma * r;
-        tripletList.push_back(T(j, j, p));
+        tripletList.push_back(Triplet(j, j, p));
     }
-    SpMat P(m_points, m_points);
+    SparseMat P(m_points, m_points);
     P.setFromTriplets(tripletList.begin(), tripletList.end());
     return P;
 }
 
-SpMat WeakIonGasElectron::matrix_Q(vector_fp g, size_t k)
+SparseMat WeakIonGasElectron::matrix_Q(vector_fp& g, size_t k)
 {
-    std::vector<T> tripletList;
+    std::vector<Triplet> tripletList;
     for (size_t n = 0; n < m_eps[k].size(); n++) {
         double eps_a = m_eps[k][n][0];
         double eps_b = m_eps[k][n][1];
@@ -178,16 +161,16 @@ SpMat WeakIonGasElectron::matrix_Q(vector_fp g, size_t k)
         double sigma_b = m_sigma[k][n][1];
         double i = m_i[k][n];
         double j = m_j[k][n];
-        double r = integralPQ(eps_a, eps_b, sigma_a, sigma_b, g[j], m_gridC[j]);
+        double r = integralPQ(eps_a, eps_b, sigma_a, sigma_b, g[j], m_gridCenter[j]);
         double q = m_inFactor[k] * m_gamma * r;
-        tripletList.push_back(T(i, j, q));
+        tripletList.push_back(Triplet(i, j, q));
     }
-    SpMat Q(m_points, m_points);
+    SparseMat Q(m_points, m_points);
     Q.setFromTriplets(tripletList.begin(), tripletList.end());
     return Q;
 }
 
-SpMat WeakIonGasElectron::matrix_A(Eigen::VectorXd f0)
+SparseMat WeakIonGasElectron::matrix_A(Eigen::VectorXd& f0)
 {
     vector_fp a0(m_points + 1);
     vector_fp a1(m_points + 1);
@@ -199,61 +182,61 @@ SpMat WeakIonGasElectron::matrix_A(Eigen::VectorXd f0)
     a0[N+1] = NAN;
     a1[N+1] = NAN;
     for (size_t j = 1; j < m_points; j++) {
-        double sigma_tilde = m_totalCrossSectionB[j] + nu / pow(m_gridB[j], 0.5) / m_gamma;
+        double sigma_tilde = m_totalCrossSectionEdge[j] + nu / pow(m_gridEdge[j], 0.5) / m_gamma;
         double omega = 2 * Pi * m_F;
-        double q = omega / (m_N * m_gamma * pow(m_gridB[j], 0.5));
-        double W = -m_gamma * m_gridB[j] * m_gridB[j] * m_sigmaElastic[j];
+        double q = omega / (m_N * m_gamma * pow(m_gridEdge[j], 0.5));
+        double W = -m_gamma * m_gridEdge[j] * m_gridEdge[j] * m_sigmaElastic[j];
         double F = sigma_tilde * sigma_tilde / (sigma_tilde * sigma_tilde + q * q);
-        double DA = m_gamma / 3.0 * pow(m_E / m_N, 2.0) * m_gridB[j];
-        double DB = m_gamma * m_kT * m_gridB[j] * m_gridB[j] * m_sigmaElastic[j];
+        double DA = m_gamma / 3.0 * pow(m_E / m_N, 2.0) * m_gridEdge[j];
+        double DB = m_gamma * m_kT * m_gridEdge[j] * m_gridEdge[j] * m_sigmaElastic[j];
         double D = DA / sigma_tilde * F + DB;
-        double z = W * (m_gridC[j] - m_gridC[j-1]) / D;
+        double z = W * (m_gridCenter[j] - m_gridCenter[j-1]) / D;
         a0[j] = W / (1 - std::exp(-z));
         a1[j] = W / (1 - std::exp(z));
     }
 
-    std::vector<T> tripletList;
+    std::vector<Triplet> tripletList;
     // center diagonal
     // zero flux b.c. at energy = 0
-    tripletList.push_back(T(0, 0, a0[1]));
+    tripletList.push_back(Triplet(0, 0, a0[1]));
     for (size_t j = 1; j < m_points - 1; j++) {
-        tripletList.push_back(T(j, j, a0[j+1] - a1[j]));
+        tripletList.push_back(Triplet(j, j, a0[j+1] - a1[j]));
     }
 
     // upper diagonal
     for (size_t j = 0; j < m_points - 1; j++) {
-        tripletList.push_back(T(j, j+1, a1[j+1]));
+        tripletList.push_back(Triplet(j, j+1, a1[j+1]));
     }
 
     // lower diagonal
     for (size_t j = 1; j < m_points; j++) {
-        tripletList.push_back(T(j, j-1, -a0[j]));
+        tripletList.push_back(Triplet(j, j-1, -a0[j]));
     }
 
     // zero flux b.c.
-    tripletList.push_back(T(N, N, -a1[N]));
+    tripletList.push_back(Triplet(N, N, -a1[N]));
 
-    SpMat A(m_points, m_points);
+    SparseMat A(m_points, m_points);
     A.setFromTriplets(tripletList.begin(), tripletList.end());
 
     //plus G
-    SpMat G(m_points, m_points);
+    SparseMat G(m_points, m_points);
     for (size_t i = 0; i < m_points; i++) {
-        G.insert(i,i) = 2.0 / 3.0 * (pow(m_gridB[i+1], 1.5) - pow(m_gridB[i], 1.5)) * nu;
+        G.insert(i,i) = 2.0 / 3.0 * (pow(m_gridEdge[i+1], 1.5) - pow(m_gridEdge[i], 1.5)) * nu;
     }
     return A + G;
 }
 
-Eigen::VectorXd WeakIonGasElectron::iterate(Eigen::VectorXd f0, double delta)
+Eigen::VectorXd WeakIonGasElectron::iterate(Eigen::VectorXd& f0, double delta)
 {
-    SpMat PQ(m_points, m_points);
+    SparseMat PQ(m_points, m_points);
     vector_fp g = vector_g(f0);
     for (size_t k : m_kInelastic) {
-        PQ += matrix_PQ(g, k);
+        PQ += (matrix_Q(g, k) - matrix_P(g, k)) * m_moleFractions[k];
     }
 
-    SpMat A = matrix_A(f0);
-    SpMat I(m_points, m_points);
+    SparseMat A = matrix_A(f0);
+    SparseMat I(m_points, m_points);
     for (size_t i = 0; i < m_points; i++) {
         I.insert(i,i) = 1.0;
     }
@@ -265,14 +248,14 @@ Eigen::VectorXd WeakIonGasElectron::iterate(Eigen::VectorXd f0, double delta)
     f0(0) += m_chemionScatRate;
 
     // solve f0
-    Eigen::SparseLU<SpMat> solver(A);
+    Eigen::SparseLU<SparseMat> solver(A);
     Eigen::VectorXd f1 = solver.solve(f0);
 
-    f1 *= 1.0 / norm(f1);
+    f1 *= 1.0 / norm(f1, m_gridCenter);
     return f1;
 }
 
-Eigen::VectorXd WeakIonGasElectron::converge(Eigen::VectorXd f0)
+Eigen::VectorXd WeakIonGasElectron::converge(Eigen::VectorXd& f0)
 {
     double err0 = 0.0;
     double err1 = 0.0;
@@ -288,7 +271,7 @@ Eigen::VectorXd WeakIonGasElectron::converge(Eigen::VectorXd f0)
         for (size_t i = 0; i < m_points; i++) {
             Df0(i) = std::abs(f0(i) - f1(i));
         }
-        err1 = norm(Df0);
+        err1 = norm(Df0, m_gridCenter);
         if (err1 < m_rtol) {
             return f1;
         }
@@ -297,7 +280,7 @@ Eigen::VectorXd WeakIonGasElectron::converge(Eigen::VectorXd f0)
     throw CanteraError("WeakIonGasElectron::converge", "Convergence failed");
 }
 
-double WeakIonGasElectron::netProductionFreq(Eigen::VectorXd f0)
+double WeakIonGasElectron::netProductionFreq(Eigen::VectorXd& f0)
 {
     double nu = m_chemionScatRate;
     vector_fp g = vector_g(f0);
@@ -305,7 +288,7 @@ double WeakIonGasElectron::netProductionFreq(Eigen::VectorXd f0)
     for (size_t k = 0; k < m_ncs; k++) {
         if (m_kinds[k] == "IONIZATION" ||
             m_kinds[k] == "ATTACHMENT") {
-            SpMat PQ = matrix_PQ(g, k);
+            SparseMat PQ = (matrix_Q(g, k) - matrix_P(g, k)) * m_moleFractions[k];
             Eigen::VectorXd s = PQ * f0;
             for (size_t i = 0; i < m_points; i++) {
                 nu += s[i];
@@ -321,12 +304,12 @@ double WeakIonGasElectron::electronDiffusivity()
     vector_fp y(m_points, 0.0);
     double nu = netProductionFreq(m_f0);
     for (size_t i = 0; i < m_points; i++) {
-        if (m_gridC[i] != 0.0) {
-            y[i] = m_gridC[i] * m_f0(i) /
-                   (m_totalCrossSectionC[i] + nu / m_gamma / pow(m_gridC[i], 0.5));
+        if (m_gridCenter[i] != 0.0) {
+            y[i] = m_gridCenter[i] * m_f0(i) /
+                   (m_totalCrossSectionCenter[i] + nu / m_gamma / pow(m_gridCenter[i], 0.5));
         }
     }
-    return 1./3. * m_gamma * simpsonQuadrature(m_gridC, y) / m_N;
+    return 1./3. * m_gamma * simpsonQuadrature(m_gridCenter, y) / m_N;
 }
 
 double WeakIonGasElectron::electronMobility()
@@ -336,13 +319,13 @@ double WeakIonGasElectron::electronMobility()
     vector_fp y(m_points + 1, 0.0);
     for (size_t i = 1; i < m_points; i++) {
         // calculate df0 at i-1/2
-        double df0 = (m_f0(i) - m_f0(i-1)) / (m_gridC[i] - m_gridC[i-1]);
-        if (m_gridB[i] != 0.0) {
-            y[i] = m_gridB[i] * df0 /
-                   (m_totalCrossSectionB[i] + nu / m_gamma / pow(m_gridB[i], 0.5));
+        double df0 = (m_f0(i) - m_f0(i-1)) / (m_gridCenter[i] - m_gridCenter[i-1]);
+        if (m_gridEdge[i] != 0.0) {
+            y[i] = m_gridEdge[i] * df0 /
+                   (m_totalCrossSectionEdge[i] + nu / m_gamma / pow(m_gridEdge[i], 0.5));
         }
     }
-    return -1./3. * m_gamma * simpsonQuadrature(m_gridB, y) / m_N;
+    return -1./3. * m_gamma * simpsonQuadrature(m_gridEdge, y) / m_N;
 }
 
 double WeakIonGasElectron::realMobility()
@@ -352,14 +335,14 @@ double WeakIonGasElectron::realMobility()
     vector_fp y(m_points + 1, 0.0);
     for (size_t i = 1; i < m_points; i++) {
         // calculate df0 at i-1/2
-        double df0 = (m_f0(i) - m_f0(i-1)) / (m_gridC[i] - m_gridC[i-1]);
-        if (m_gridB[i] != 0.0) {
-            double Q = m_totalCrossSectionB[i] + nu / m_gamma / pow(m_gridB[i], 0.5);
-            double q = 2.0 * Pi * m_F / (m_N * m_gamma * pow(m_gridB[i], 0.5));
-            y[i] = m_gridB[i] * Q / (Q * Q + q * q) * df0;
+        double df0 = (m_f0(i) - m_f0(i-1)) / (m_gridCenter[i] - m_gridCenter[i-1]);
+        if (m_gridEdge[i] != 0.0) {
+            double Q = m_totalCrossSectionEdge[i] + nu / m_gamma / pow(m_gridEdge[i], 0.5);
+            double q = 2.0 * Pi * m_F / (m_N * m_gamma * pow(m_gridEdge[i], 0.5));
+            y[i] = m_gridEdge[i] * Q / (Q * Q + q * q) * df0;
         }
     }
-    return -1./3. * m_gamma * simpsonQuadrature(m_gridB, y) / m_N;
+    return -1./3. * m_gamma * simpsonQuadrature(m_gridEdge, y) / m_N;
 }
 
 double WeakIonGasElectron::powerGain()
@@ -378,11 +361,11 @@ double WeakIonGasElectron::elasticPowerLoss()
     vector_fp y(m_points + 1, 0.0);
     for (size_t i = 1; i < m_points; i++) {
         // calculate df0 at i-1/2
-        double df0 = (m_f0(i) - m_f0(i-1)) / (m_gridC[i] - m_gridC[i-1]);
+        double df0 = (m_f0(i) - m_f0(i-1)) / (m_gridCenter[i] - m_gridCenter[i-1]);
         double f0 = 0.5 * (m_f0(i-1) + m_f0(i));
-        y[i] = m_sigmaElastic[i] * (m_gridB[i] * m_gridB[i] * f0 + m_kT *  df0);
+        y[i] = m_sigmaElastic[i] * (m_gridEdge[i] * m_gridEdge[i] * f0 + m_kT *  df0);
     }
-    sum += m_gamma * simpsonQuadrature(m_gridB, y);
+    sum += m_gamma * simpsonQuadrature(m_gridEdge, y);
     return sum * m_N;
 }
 
@@ -397,10 +380,7 @@ double WeakIonGasElectron::totalCollisionFreq()
 
 double WeakIonGasElectron::biMaxwellFraction(size_t k)
 {
-    double y0 = 1.0;
-    double y1 = std::exp(-m_thresholds[k] / m_kT);
-    y0 *= 1.0 / (y0 + y1);
-    return y0;
+    return 1.0 / (1 + exp(-m_thresholds[k] / m_kT));
 }
 
 double WeakIonGasElectron::inelasticPowerLoss()
@@ -423,7 +403,7 @@ double WeakIonGasElectron::rateCoefficient(size_t k)
 {
     calculateDistributionFunction();
     vector_fp g = vector_g(m_f0);
-    SpMat P = matrix_P(g, k);
+    SparseMat P = matrix_P(g, k);
     Eigen::VectorXd s = P * m_f0;
     double sum = 0.0;
     for (size_t i = 0; i < m_points; i++) {
@@ -436,7 +416,7 @@ double WeakIonGasElectron::reverseRateCoefficient(size_t k)
 {
     calculateDistributionFunction();
     vector_fp g = vector_g(m_f0);
-    SpMat Q = matrix_Q(g, k);
+    SparseMat Q = matrix_Q(g, k);
     Eigen::VectorXd s = Q * m_f0;
     double sum = 0.0;
     for (size_t i = 0; i < m_points; i++) {
@@ -450,7 +430,7 @@ double WeakIonGasElectron::meanElectronEnergy()
     calculateDistributionFunction();
     double sum = 0;
     for (size_t i = 0; i < m_points - 1; i++) {
-        sum += (pow(m_gridB[i+1], 2.5) -  pow(m_gridB[i], 2.5)) * m_f0(i);
+        sum += (pow(m_gridEdge[i+1], 2.5) -  pow(m_gridEdge[i], 2.5)) * m_f0(i);
     }
     return 0.4 * sum;
 }
@@ -469,7 +449,7 @@ double WeakIonGasElectron::electronTemperature(Eigen::VectorXd f0)
 {
     double sum = 0;
     for (size_t i = 0; i < m_points - 1; i++) {
-        sum += (pow(m_gridB[i+1], 2.5) -  pow(m_gridB[i], 2.5)) * f0(i);
+        sum += (pow(m_gridEdge[i+1], 2.5) -  pow(m_gridEdge[i], 2.5)) * f0(i);
     }
     return 2./3. * 0.4 * sum / Boltzmann * ElectronCharge;
 }
