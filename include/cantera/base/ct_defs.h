2d9a88d44da976372801d290af6f4315745d0b9f
Ray Speth <speth@mit.edu>
2020-01-09 18:47:43 -0500

    Fix Doxygen warnings generated in ct_defs.h

--- include/cantera/base/ct_defs.h
+++ include/cantera/base/ct_defs.h
@@ -59,19 +59,19 @@ const double Pi = 3.14159265358979323846;
  */
 //@{
 
-//! Avogadro's Number \f$ N_{\mathrm{A}} $\f [number/kmol]
+//! Avogadro's Number \f$ N_{\mathrm{A}} \f$ [number/kmol]
 const double Avogadro = 6.02214076e26;
 
-//! Boltzmann constant \f$ k $\f [J/K]
+//! Boltzmann constant \f$ k \f$ [J/K]
 const double Boltzmann = 1.380649e-23;
 
-//! Planck constant \f$ h $\f [J-s]
+//! Planck constant \f$ h \f$ [J-s]
 const double Planck = 6.62607015e-34;
 
-//! Elementary charge \f$ e $\f [C]
+//! Elementary charge \f$ e \f$ [C]
 const double ElectronCharge = 1.602176634e-19;
 
-/// Speed of Light in a vacuum \f$ c $\f [m/s]
+/// Speed of Light in a vacuum \f$ c \f$ [m/s]
 const double lightSpeed = 299792458.0;
 
 //! One atmosphere [Pa]
@@ -88,10 +88,10 @@ const double OneBar = 1.0E5;
  */
 //@{
 
-//! Fine structure constant \f$ \alpha $\f []
+//! Fine structure constant \f$ \alpha \f$ []
 const double fineStructureConstant = 7.2973525693e-3;
 
-//! Electron Mass \f$ m_e $\f [kg]
+//! Electron Mass \f$ m_e \f$ [kg]
 const double ElectronMass = 9.1093837015e-31;
 
 //@}
@@ -102,10 +102,10 @@ const double ElectronMass = 9.1093837015e-31;
  */
 //@{
 
-//! Reduced Planck constant \f$ \hbar $\f [m2-kg/s]
+//! Reduced Planck constant \f$ \hbar \f$ [m2-kg/s]
 const double Planck_bar = Planck / (2 * Pi);
 
-//! Universal Gas Constant \f$ R_u $\f [J/kmol/K]
+//! Universal Gas Constant \f$ R_u \f$ [J/kmol/K]
 const double GasConstant = Avogadro * Boltzmann;
 
 const double logGasConstant = std::log(GasConstant);
@@ -116,16 +116,16 @@ const double GasConst_cal_mol_K = GasConstant / 4184.0;
 //! log(k_b/h)
 const double logBoltz_Planck = std::log(Boltzmann / Planck);
 
-//! Stefan-Boltzmann constant \f$ \sigma $\f [W/m2/K4]
+//! Stefan-Boltzmann constant \f$ \sigma \f$ [W/m2/K4]
 const double StefanBoltz = Pi * Pi * std::pow(Boltzmann, 4.0) / (60.0 * std::pow(Planck_bar, 3.0) * lightSpeed * lightSpeed); // 5.670374419e-8
 
-//! Faraday constant \f$ F $\f [C/kmol]
+//! Faraday constant \f$ F \f$ [C/kmol]
 const double Faraday = ElectronCharge * Avogadro;
 
-//! Permeability of free space \f$ \mu_0 $\f [N/A2]
+//! Permeability of free space \f$ \mu_0 \f$ [N/A2]
 const double permeability_0 = 2 * fineStructureConstant * Planck / (ElectronCharge * ElectronCharge * lightSpeed);
 
-//! Permittivity of free space \f$ \varepsilon_0 $\f [F/m]
+//! Permittivity of free space \f$ \varepsilon_0 \f$ [F/m]
 const double epsilon_0 = 1.0 / (lightSpeed * lightSpeed * permeability_0);
 
 //@}
