1f6e690b0ab2f47152992d91e6bb53a7670aa7d1
Ray Speth <speth@mit.edu>
2020-01-22 18:51:04 -0500

    Add functions for conversion from given units into a UnitSystem

--- test/general/test_units.cpp
+++ test/general/test_units.cpp
@@ -42,20 +42,22 @@ TEST(Units, prefixes) {
 
 TEST(Units, with_defaults1) {
     UnitSystem U({"cm", "g", "mol", "atm", "kcal"});
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "m"), 0.01);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "kmol/m^3"), 1000);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "kg/kmol"), 1.0);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "cm^2"), 1.0);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "Pa"), 101325);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "hPa"), 1013.25);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "Pa*m^6/kmol"), 101325*1e-12*1000);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "J"), 4184);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "m"), 0.01);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "kmol/m^3"), 1000);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "kg/kmol"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "cm^2"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "Pa"), 101325);
+    EXPECT_DOUBLE_EQ(U.convertFrom(101325, "Pa"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "hPa"), 1013.25);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "Pa*m^6/kmol"), 101325*1e-12*1000);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "J"), 4184);
 }
 
 TEST(Units, with_defaults2) {
     UnitSystem U({"dyn/cm^2"});
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "Pa"), 0.1);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "N/m^2"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "Pa"), 0.1);
+    EXPECT_DOUBLE_EQ(U.convertFrom(1.0, "Pa"), 10);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "N/m^2"), 1.0);
 }
 
 TEST(Units, with_defaults_map) {
@@ -65,14 +67,14 @@ TEST(Units, with_defaults_map) {
     };
     UnitSystem U;
     U.setDefaults(defaults);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "m"), 0.01);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "kmol/m^3"), 1000);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "kg/kmol"), 1.0);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "cm^2"), 1.0);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "Pa"), 101325);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "hPa"), 1013.25);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "Pa*m^6/kmol"), 101325*1e-12*1000);
-    EXPECT_DOUBLE_EQ(U.convert(1.0, "J/cm^3"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convertFrom(0.01, "m"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "kmol/m^3"), 1000);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "kg/kmol"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "cm^2"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "Pa"), 101325);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "hPa"), 1013.25);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "Pa*m^6/kmol"), 101325*1e-12*1000);
+    EXPECT_DOUBLE_EQ(U.convertTo(1.0, "J/cm^3"), 1.0);
 }
 
 TEST(Units, bad_defaults) {
@@ -96,23 +98,25 @@ TEST(Units, activation_energies2) {
     UnitSystem U;
     U.setDefaultActivationEnergy("cal/mol");
     U.setDefaults({"cm", "g", "J"});
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(1000, "cal/mol"), 1000);
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(1000, "J/kmol"), 4184e3);
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(1000, "K"), 4184e3 / GasConstant);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(1000, "cal/mol"), 1000);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(1000, "J/kmol"), 4184e3);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyFrom(4184e3, "J/kmol"), 1000);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(1000, "K"), 4184e3 / GasConstant);
 }
 
 TEST(Units, activation_energies3) {
     UnitSystem U({"cal", "mol"});
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(1000, "cal/mol"), 1000);
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(1000, "J/kmol"), 4184e3);
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(1000, "K"), 4184e3 / GasConstant);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(1000, "cal/mol"), 1000);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(1000, "J/kmol"), 4184e3);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(1000, "K"), 4184e3 / GasConstant);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyFrom(4184e3 / GasConstant, "K"), 1000);
 }
 
 TEST(Units, activation_energies4) {
     UnitSystem U;
     U.setDefaultActivationEnergy("K");
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(2000, "K"), 2000);
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(2000, "J/kmol"), 2000 * GasConstant);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(2000, "K"), 2000);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(2000, "J/kmol"), 2000 * GasConstant);
 }
 
 TEST(Units, activation_energies5) {
@@ -121,8 +125,8 @@ TEST(Units, activation_energies5) {
         {"quantity", "mol"}, {"energy", "cal"}, {"activation-energy", "K"}
     };
     U.setDefaults(defaults);
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(2000, "K"), 2000);
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(2000, "J/kmol"), 2000 * GasConstant);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(2000, "K"), 2000);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(2000, "J/kmol"), 2000 * GasConstant);
 }
 
 TEST(Units, activation_energies6) {
@@ -131,8 +135,8 @@ TEST(Units, activation_energies6) {
         {"activation-energy", "eV"}
     };
     U.setDefaults(defaults);
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(1, "J/kmol"), ElectronCharge * Avogadro);
-    EXPECT_DOUBLE_EQ(U.convertActivationEnergy(1, "eV"), 1.0);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(1, "J/kmol"), ElectronCharge * Avogadro);
+    EXPECT_DOUBLE_EQ(U.convertActivationEnergyTo(1, "eV"), 1.0);
 }
 
 TEST(Units, from_anymap) {
@@ -172,10 +176,10 @@ TEST(Units, from_yaml) {
     );
 
     EXPECT_FALSE(m.hasKey("units"));
-    EXPECT_DOUBLE_EQ(m.units().convert(1, "m"), 1000);
+    EXPECT_DOUBLE_EQ(m.units().convertTo(1, "m"), 1000);
     auto& foo = m["foo"].asVector<AnyMap>();
-    EXPECT_DOUBLE_EQ(foo[0].units().convert(1, "m"), 0.01);
-    EXPECT_DOUBLE_EQ(foo[1].units().convert(1, "m"), 0.001);
+    EXPECT_DOUBLE_EQ(foo[0].units().convertTo(1, "m"), 0.01);
+    EXPECT_DOUBLE_EQ(foo[1].units().convertTo(1, "m"), 0.001);
     EXPECT_DOUBLE_EQ(foo[0].convert("bar", "m"), 0.006);
     auto& spam = m["spam"].asVector<AnyMap>();
     EXPECT_DOUBLE_EQ(spam[0].convert("eggs", "m"), 3000);
