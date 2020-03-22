fa19f0826ba62b5db943b23b63114e6c8a39e9ba
Arsh Mahajan <05arshmahajan@gmail.com>
2020-03-23 01:35:09 +0530

    Created ThermoPhase.pyx
    Implemented a sample wrapper using Pint UnitRegistry().wrap().

--- interfaces/cython/cantera/sample_wrapper/ThermoPhase.pyx
+++ interfaces/cython/cantera/sample_wrapper/ThermoPhase.pyx
@@ -1,6 +1,10 @@
 # This is a sample cython code that contains a sample wrapper for the function "setState_PX" in class "ThermoPhase" in namespace "Cantera".
 # I have made this wrapper assuming we would pass a third parameter (datatype = string) in the function containing which units we want, which by default would be SI unit(Pascal(Pa) in this case).
-# Right now I have added support for only atm and Pa and will extend to other Methods, Classes and Units once this sample is approved
+# Right now I have added support for only for one Method/Function. I will extend to other Methods, Classes and Units once this sample is approved
+
+from pint import UnitRegistry
+
+ureg = UnitRegistry()
 
 cdef extern from "ThermoPhase.h" namespace "Cantera":
     cdef cppclass ThermoPhase:
@@ -14,7 +18,4 @@ cdef class PyThermoPhase:
     def __dealloc__(self):
         del self.thisptr
     def setState_PX(self, doublereal p, doublereal* x, string units)
-        if units == "atm":    # if the user entered his input in atm(atmosphere)
-            return self.thisptr.setState_PX(p*101325,x)
-        else                  # in all other cases we assume it to be Pa(Pascal)
-            return self.thisptr.setState_PX(p,x)
+        return self.thisptr.setState_PX(p.to(ureg.Pa),x)    #converts the input from any unit to SI unit for pressure(Pascal(Pa)) to make it compatible with current Cantera library design!
