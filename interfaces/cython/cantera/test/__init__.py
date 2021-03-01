da0cbf9771b01bbb863ef53f12f79f4d53d477ad
Ingmar Schoegl <ischoegl@lsu.edu>
2021-03-01 14:53:44 -0600

    [UnitTest] include new test in __init__

--- interfaces/cython/cantera/test/__init__.py
+++ interfaces/cython/cantera/test/__init__.py
@@ -1,17 +1,18 @@
 import os
 import cantera
 
-from .test_thermo import *
-from .test_purefluid import *
-from .test_equilibrium import *
-from .test_kinetics import *
-from .test_transport import *
-from .test_mixture import *
-from .test_func1 import *
-from .test_reactor import *
-from .test_onedim import *
-from .test_convert import *
 from .test_composite import *
+from .test_convert import *
+from .test_equilibrium import *
+from .test_func1 import *
+from .test_kinetics import *
+from .test_mixture import *
+from .test_onedim import *
+from .test_purefluid import *
+from .test_reaction import *
+from .test_reactor import *
+from .test_thermo import *
+from .test_transport import *
 
 cantera.add_directory(os.path.join(os.path.dirname(__file__), 'data'))
 cantera.add_directory(os.path.join(os.path.dirname(__file__), '..', 'examples', 'surface_chemistry'))
