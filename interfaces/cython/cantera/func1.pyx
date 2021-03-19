435ae43a2dab8eb6ac0b92ce8289edd124652d37
Ray Speth <speth@mit.edu>
2021-03-19 17:46:38 -0400

    [Reactor] Add delegates for remaining core Reactor functions

--- interfaces/cython/cantera/func1.pyx
+++ interfaces/cython/cantera/func1.pyx
@@ -22,9 +22,38 @@ cdef double func_callback(double t, void* obj, void** err):
         return 0.0
 
 
+cdef void callback_v(PyFuncInfo& funcInfo):
+    try:
+        (<object>funcInfo.func())()
+    except BaseException as e:
+        exc_type, exc_value = sys.exc_info()[:2]
+        funcInfo.setExceptionType(<PyObject*>exc_type)
+        funcInfo.setExceptionValue(<PyObject*>exc_value)
+
+
 cdef void callback_v_d(PyFuncInfo& funcInfo, double arg):
     try:
         (<object>funcInfo.func())(arg)
+    except BaseException as e:
+        exc_type, exc_value = sys.exc_info()[:2]
+        funcInfo.setExceptionType(<PyObject*>exc_type)
+        funcInfo.setExceptionValue(<PyObject*>exc_value)
+
+
+cdef void callback_v_b(PyFuncInfo& funcInfo, cbool arg):
+    try:
+        (<object>funcInfo.func())(arg)
+    except BaseException as e:
+        exc_type, exc_value = sys.exc_info()[:2]
+        funcInfo.setExceptionType(<PyObject*>exc_type)
+        funcInfo.setExceptionValue(<PyObject*>exc_value)
+
+
+cdef void callback_v_dp(PyFuncInfo& funcInfo, size_array1 sizes, double* arg):
+    cdef double[:] view = <double[:sizes[0]]>arg if sizes[0] else None
+
+    try:
+        (<object>funcInfo.func())(view)
     except BaseException as e:
         exc_type, exc_value = sys.exc_info()[:2]
         funcInfo.setExceptionType(<PyObject*>exc_type)
@@ -45,6 +74,23 @@ cdef void callback_v_d_dp_dp_dp(PyFuncInfo& funcInfo, size_array3 sizes, double 
         funcInfo.setExceptionValue(<PyObject*>exc_value)
 
 
+cdef int callback_i_dr_d_dp(PyFuncInfo& funcInfo, double& out,
+                            size_array1 sizes, double arg1, double* arg2):
+    cdef double[:] view = <double[:sizes[0]]>arg2 if sizes[0] else None
+    try:
+        ret = (<object>funcInfo.func())(arg1, view)
+        if ret is None:
+            return 0
+        else:
+            (&out)[0] = ret
+            return 1
+    except BaseException as e:
+        exc_type, exc_value = sys.exc_info()[:2]
+        funcInfo.setExceptionType(<PyObject*>exc_type)
+        funcInfo.setExceptionValue(<PyObject*>exc_value)
+    return -1
+
+
 cdef int callback_i_sr_z(PyFuncInfo& funcInfo, string& out, size_t arg):
     try:
         ret = (<object>funcInfo.func())(arg)
@@ -52,6 +98,20 @@ cdef int callback_i_sr_z(PyFuncInfo& funcInfo, string& out, size_t arg):
             return 0
         else:
             (&out)[0] = stringify(ret)
+            return 1
+    except BaseException as e:
+        exc_type, exc_value = sys.exc_info()[:2]
+        funcInfo.setExceptionType(<PyObject*>exc_type)
+        funcInfo.setExceptionValue(<PyObject*>exc_value)
+    return -1
+
+cdef int callback_i_zr_csr(PyFuncInfo& funcInfo, size_t& out, const string& arg):
+    try:
+        ret = (<object>funcInfo.func())(pystr(arg))
+        if ret is None:
+            return 0
+        else:
+            (&out)[0] = ret
             return 1
     except BaseException as e:
         exc_type, exc_value = sys.exc_info()[:2]
