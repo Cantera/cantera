eb31985a2e242579f73749fa0fd308046aaf5e98
Ray Speth <speth@mit.edu>
2021-04-30 13:59:19 -0400

    [Test] Replace some uses of frac.cti and ptcombust.cti
    Explicit tests of parsing/conversion should remain until removal of
    CTI/XML formats.

--- test/kinetics/rates.cpp
+++ test/kinetics/rates.cpp
@@ -4,6 +4,7 @@
 #include "cantera/thermo/IdealGasPhase.h"
 #include "cantera/thermo/SurfPhase.h"
 #include "cantera/kinetics/GasKinetics.h"
+#include "cantera/base/Solution.h"
 
 namespace Cantera
 {
@@ -37,10 +38,10 @@ class FracCoeffTest : public testing::Test
 {
 public:
     FracCoeffTest() :
-        therm("../data/frac.xml", "gas")
+        therm("frac.yaml", "gas")
     {
         std::vector<ThermoPhase*> phases { &therm };
-        importKinetics(therm.xml(), phases, &kin);
+        kin = newKinetics(phases, "frac.yaml", "gas");
         therm.setState_TPX(2000, 4*OneAtm,
                             "H2O:0.5, OH:.05, H:0.1, O2:0.15, H2:0.2");
         kH2O = therm.speciesIndex("H2O");
@@ -50,29 +51,29 @@ public:
         kH2 = therm.speciesIndex("H2");
     }
     IdealGasPhase therm;
-    GasKinetics kin;
+    unique_ptr<Kinetics> kin;
     size_t kH2O, kH, kOH, kO2, kH2;
 };
 
 TEST_F(FracCoeffTest, StoichCoeffs)
 {
-    EXPECT_DOUBLE_EQ(1.0, kin.reactantStoichCoeff(kH2O, 0));
-    EXPECT_DOUBLE_EQ(1.4, kin.productStoichCoeff(kH, 0));
-    EXPECT_DOUBLE_EQ(0.6, kin.productStoichCoeff(kOH, 0));
-    EXPECT_DOUBLE_EQ(0.2, kin.productStoichCoeff(kO2, 0));
+    EXPECT_DOUBLE_EQ(1.0, kin->reactantStoichCoeff(kH2O, 0));
+    EXPECT_DOUBLE_EQ(1.4, kin->productStoichCoeff(kH, 0));
+    EXPECT_DOUBLE_EQ(0.6, kin->productStoichCoeff(kOH, 0));
+    EXPECT_DOUBLE_EQ(0.2, kin->productStoichCoeff(kO2, 0));
 
-    EXPECT_DOUBLE_EQ(0.7, kin.reactantStoichCoeff(kH2, 1));
-    EXPECT_DOUBLE_EQ(0.6, kin.reactantStoichCoeff(kOH, 1));
-    EXPECT_DOUBLE_EQ(0.2, kin.reactantStoichCoeff(kO2, 1));
-    EXPECT_DOUBLE_EQ(1.0, kin.productStoichCoeff(kH2O, 1));
+    EXPECT_DOUBLE_EQ(0.7, kin->reactantStoichCoeff(kH2, 1));
+    EXPECT_DOUBLE_EQ(0.6, kin->reactantStoichCoeff(kOH, 1));
+    EXPECT_DOUBLE_EQ(0.2, kin->reactantStoichCoeff(kO2, 1));
+    EXPECT_DOUBLE_EQ(1.0, kin->productStoichCoeff(kH2O, 1));
 }
 
 TEST_F(FracCoeffTest, RateConstants)
 {
-    vector_fp kf(kin.nReactions(), 0.0);
-    vector_fp kr(kin.nReactions(), 0.0);
-    kin.getFwdRateConstants(&kf[0]);
-    kin.getRevRateConstants(&kr[0]);
+    vector_fp kf(kin->nReactions(), 0.0);
+    vector_fp kr(kin->nReactions(), 0.0);
+    kin->getFwdRateConstants(&kf[0]);
+    kin->getRevRateConstants(&kr[0]);
 
     // sum of reaction orders is 1.0; kf has units of 1/s
     EXPECT_DOUBLE_EQ(1e13, kf[0]);
@@ -88,12 +89,12 @@ TEST_F(FracCoeffTest, RateConstants)
 
 TEST_F(FracCoeffTest, RatesOfProgress)
 {
-    vector_fp kf(kin.nReactions(), 0.0);
+    vector_fp kf(kin->nReactions(), 0.0);
     vector_fp conc(therm.nSpecies(), 0.0);
-    vector_fp ropf(kin.nReactions(), 0.0);
+    vector_fp ropf(kin->nReactions(), 0.0);
     therm.getConcentrations(&conc[0]);
-    kin.getFwdRateConstants(&kf[0]);
-    kin.getFwdRatesOfProgress(&ropf[0]);
+    kin->getFwdRateConstants(&kf[0]);
+    kin->getFwdRatesOfProgress(&ropf[0]);
 
     EXPECT_DOUBLE_EQ(conc[kH2O]*kf[0], ropf[0]);
     EXPECT_DOUBLE_EQ(pow(conc[kH2], 0.8)*conc[kO2]*pow(conc[kOH],2)*kf[1],
@@ -102,12 +103,12 @@ TEST_F(FracCoeffTest, RatesOfProgress)
 
 TEST_F(FracCoeffTest, CreationDestructionRates)
 {
-    vector_fp ropf(kin.nReactions(), 0.0);
+    vector_fp ropf(kin->nReactions(), 0.0);
     vector_fp cdot(therm.nSpecies(), 0.0);
     vector_fp ddot(therm.nSpecies(), 0.0);
-    kin.getFwdRatesOfProgress(&ropf[0]);
-    kin.getCreationRates(&cdot[0]);
-    kin.getDestructionRates(&ddot[0]);
+    kin->getFwdRatesOfProgress(&ropf[0]);
+    kin->getCreationRates(&cdot[0]);
+    kin->getDestructionRates(&ddot[0]);
 
     EXPECT_DOUBLE_EQ(ropf[0], ddot[kH2O]);
     EXPECT_DOUBLE_EQ(1.4*ropf[0], cdot[kH]);
@@ -125,10 +126,10 @@ TEST_F(FracCoeffTest, CreationDestructionRates)
 
 TEST_F(FracCoeffTest, EquilibriumConstants)
 {
-    vector_fp Kc(kin.nReactions(), 0.0);
+    vector_fp Kc(kin->nReactions(), 0.0);
     vector_fp mu0(therm.nSpecies(), 0.0);
 
-    kin.getEquilibriumConstants(&Kc[0]);
+    kin->getEquilibriumConstants(&Kc[0]);
     therm.getGibbs_ref(&mu0[0]); // at pRef
 
     double deltaG0_0 = 1.4 * mu0[kH] + 0.6 * mu0[kOH] + 0.2 * mu0[kO2] - mu0[kH2O];
@@ -147,38 +148,38 @@ class NegativePreexponentialFactor : public testing::Test
 public:
     NegativePreexponentialFactor() {}
     void setup(const std::string& infile) {
-        therm.reset(newPhase(infile));
-        std::vector<ThermoPhase*> phases { therm.get() };
-        importKinetics(therm->xml(), phases, &kin);
-        therm->setState_TPX(2000, OneAtm,
-                            "H2O:1.0, H:0.2, O2:0.3, NH:0.05, NO:0.05, N2O:0.05");
+        soln = newSolution(infile);
+        soln->thermo()->setState_TPX(2000, OneAtm,
+            "H2O:1.0, H:0.2, O2:0.3, NH:0.05, NO:0.05, N2O:0.05");
+        nSpec = soln->thermo()->nSpecies();
+        nRxn = soln->kinetics()->nReactions();
     }
 
     void testNetProductionRates() {
         const double wdot_ref[] = {0.44705, -0.0021443, 0, -279.36, 0.0021432, 278.92, 0.4449, -279.36, 279.36, 0, 0, 0};
-        ASSERT_EQ(12, (int) therm->nSpecies());
-        ASSERT_EQ(12, (int) kin.nReactions());
-        vector_fp wdot(therm->nSpecies());
-        kin.getNetProductionRates(&wdot[0]);
-        for (size_t i = 0; i < therm->nSpecies(); i++) {
+        ASSERT_EQ(12, (int) nSpec);
+        ASSERT_EQ(12, (int) nRxn);
+        vector_fp wdot(nSpec);
+        soln->kinetics()->getNetProductionRates(&wdot[0]);
+        for (size_t i = 0; i < nSpec; i++) {
             EXPECT_NEAR(wdot_ref[i], wdot[i], std::abs(wdot_ref[i])*2e-5 + 1e-9);
         }
 
         const double ropf_ref[] = {479.305, -128.202, 0, -0, 0, 0, 0, 0, 0, 0.4449, 0, 0};
         const double ropr_ref[] = {97.94, -26.1964, 0, -0, 1.10334e-06, 0, 0, 0, 6.58592e-06, 0, 0, 0.00214319};
 
-        vector_fp ropf(kin.nReactions());
-        vector_fp ropr(kin.nReactions());
-        kin.getFwdRatesOfProgress(&ropf[0]);
-        kin.getRevRatesOfProgress(&ropr[0]);
-        for (size_t i = 0; i < kin.nReactions(); i++) {
+        vector_fp ropf(nRxn);
+        vector_fp ropr(nRxn);
+        soln->kinetics()->getFwdRatesOfProgress(&ropf[0]);
+        soln->kinetics()->getRevRatesOfProgress(&ropr[0]);
+        for (size_t i = 0; i < nRxn; i++) {
             EXPECT_NEAR(ropf_ref[i], ropf[i], std::abs(ropf_ref[i])*2e-5 + 1e-9);
             EXPECT_NEAR(ropr_ref[i], ropr[i], std::abs(ropr_ref[i])*2e-5 + 1e-9);
         }
     }
 
-    shared_ptr<ThermoPhase> therm;
-    GasKinetics kin;
+    shared_ptr<Solution> soln;
+    size_t nRxn, nSpec;
 };
 
 TEST_F(NegativePreexponentialFactor, fromCti)
@@ -187,17 +188,17 @@ TEST_F(NegativePreexponentialFactor, fromCti)
     testNetProductionRates();
 }
 
-TEST_F(NegativePreexponentialFactor, fromXml)
+TEST_F(NegativePreexponentialFactor, fromYaml)
 {
-    setup("../data/noxNeg.xml");
+    setup("noxNeg.yaml");
     testNetProductionRates();
 }
 
 TEST(InterfaceReaction, CoverageDependency) {
-    IdealGasPhase gas("ptcombust.cti", "gas");
-    SurfPhase surf("ptcombust.cti", "Pt_surf");
-    std::vector<ThermoPhase*> phases { &gas, &surf };
-    shared_ptr<Kinetics> kin(newKineticsMgr(surf.xml(), phases));
+    IdealGasPhase gas("ptcombust.yaml", "gas");
+    SurfPhase surf("ptcombust.yaml", "Pt_surf");
+    std::vector<ThermoPhase*> phases { &surf, &gas };
+    shared_ptr<Kinetics> kin(newKinetics(phases, "ptcombust.yaml", "Pt_surf"));
     ASSERT_EQ(kin->nReactions(), (size_t) 24);
 
     double T = 500;
