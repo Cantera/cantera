5aeeb8b3dce14b14dea04daea8530094e8941679
Ray Speth <speth@mit.edu>
2021-05-15 12:52:08 -0400

    [Test] Update runDiamond test to use YAML
    Retain runs with the existing CTI and XML input files until those
    formats are removed.

--- test_problems/diamondSurf/runDiamond.cpp
+++ test_problems/diamondSurf/runDiamond.cpp
@@ -1,14 +1,6 @@
 /**
  *  @file runDiamond.cpp
- *
  */
-
-//  Example
-//
-// Note that this example needs updating. It works fine, but is
-// written in a way that is less than transparent or
-// user-friendly. This could be rewritten using class Interface to
-// make things simpler.
 
 #include "cantera/thermo.h"
 #include "cantera/kinetics.h"
@@ -34,36 +26,30 @@ int main(int argc, char** argv)
 #endif
     if (argc != 2) {
         cout << "Error: no input file specified.\n"
-             "Choose either 'diamond.cti' or 'diamond_blessed.xml" << endl;
+             "Choose 'diamond.yaml', 'diamond.cti', or 'diamond.xml'" << endl;
         exit(-1);
     }
     std::string infile(argv[1]);
     int i, k;
 
     try {
-        XML_Node* xc = get_XML_File(infile);
-
-        XML_Node* const xg = xc->findNameID("phase", "gas");
-        ThermoPhase* gasTP = newPhase(*xg);
-        size_t nsp = gasTP->nSpecies();
+        shared_ptr<ThermoPhase> gas(newPhase(infile, "gas"));
+        size_t nsp = gas->nSpecies();
         cout.precision(4);
         cout << "Number of species = " << nsp << endl;
 
-        XML_Node* const xd = xc->findNameID("phase", "diamond");
-        ThermoPhase* diamondTP = newPhase(*xd);
-        size_t nsp_diamond = diamondTP->nSpecies();
+        shared_ptr<ThermoPhase> diamond(newPhase(infile, "diamond"));
+        size_t nsp_diamond = diamond->nSpecies();
         cout << "Number of species in diamond = " << nsp_diamond << endl;
 
-
-        XML_Node* const xs = xc->findNameID("phase", "diamond_100");
-        ThermoPhase* diamond100TP = newPhase(*xs);
-        size_t nsp_d100 = diamond100TP->nSpecies();
+        shared_ptr<ThermoPhase> diamond100(newPhase(infile, "diamond_100"));
+        size_t nsp_d100 = diamond100->nSpecies();
         cout << "Number of species in diamond_100 = " << nsp_d100 << endl;
 
-        vector<ThermoPhase*> phaseList { gasTP, diamondTP, diamond100TP };
-        InterfaceKinetics* iKin_ptr = new InterfaceKinetics();
-        importKinetics(*xs, phaseList, iKin_ptr);
-        size_t nr = iKin_ptr->nReactions();
+        auto kin = newKinetics({gas.get(), diamond.get(), diamond100.get()},
+                               infile, "diamond_100");
+        InterfaceKinetics& ikin = dynamic_cast<InterfaceKinetics&>(*kin);
+        size_t nr = kin->nReactions();
         cout << "Number of reactions = " << nr << endl;
 
         double x[20];
@@ -76,24 +62,24 @@ int main(int argc, char** argv)
         x[3] = 0.0100;
         double p = 20.0*OneAtm/760.0;
 
-        gasTP->setState_TPX(1200., p, x);
+        gas->setState_TPX(1200., p, x);
 
         for (i = 0; i < 20; i++) {
             x[i] = 0.0;
         }
-        size_t i0 = diamond100TP->speciesIndex("c6H*");
+        size_t i0 = diamond100->speciesIndex("c6H*");
         x[i0] = 0.1;
-        size_t i1 = diamond100TP->speciesIndex("c6HH");
+        size_t i1 = diamond100->speciesIndex("c6HH");
         x[i1] = 0.9;
-        diamond100TP->setState_TX(1200., x);
+        diamond100->setState_TX(1200., x);
 
         for (i = 0; i < 20; i++) {
             x[i] = 0.0;
         }
         x[0] = 1.0;
-        diamondTP->setState_TP(1200., p);
+        diamond100->setState_TP(1200., p);
 
-        iKin_ptr->advanceCoverages(100.);
+        ikin.advanceCoverages(100.);
 
         // Throw some asserts in here to test that they compile
         AssertTrace(p == p);
@@ -104,39 +90,37 @@ int main(int argc, char** argv)
         for (i = 0; i < 20; i++) {
             src[i] = 0.0;
         }
-        iKin_ptr->getNetProductionRates(src);
+        kin->getNetProductionRates(src);
         double sum = 0.0;
         double naH = 0.0;
         for (k = 0; k < 13; k++) {
             if (k < 4) {
-                naH = gasTP->nAtoms(k, 0);
+                naH = gas->nAtoms(k, 0);
             } else if (k == 4) {
                 naH = 0;
             } else if (k > 4) {
                 int itp = k - 5;
-                naH = diamond100TP->nAtoms(itp, 0);
+                naH = diamond100->nAtoms(itp, 0);
             }
             cout << k << "  " << naH << "  " ;
             printDbl(src[k]);
             cout << endl;
             sum += naH * src[k];
-
         }
 
         cout << "sum = ";
         printDbl(sum);
         cout << endl;
-        double mwd = diamondTP->molecularWeight(0);
-        double dens = diamondTP->density();
+        double mwd = diamond->molecularWeight(0);
+        double dens = diamond->density();
         double gr = src[4] * mwd / dens;
         gr *= 1.0E6 * 3600.;
         cout << "growth rate = " << gr << " microns per hour" << endl;
 
-
-        diamond100TP->getMoleFractions(x);
+        diamond100->getMoleFractions(x);
         cout << "Coverages:" << endl;
         for (k = 0; k < 8; k++) {
-            cout << k << "   " << diamond100TP->speciesName(k)
+            cout << k << "   " << diamond100->speciesName(k)
                  << "   "
                  << x[k] << endl;
         }
@@ -146,4 +130,3 @@ int main(int argc, char** argv)
 
     return 0;
 }
-/***********************************************************/
