f024fb08ee5a5e6a899ef39e6c2e875362c73a8c
Ingmar Schoegl <ischoegl@lsu.edu>
2021-05-18 07:26:31 -0500

    [Kinetics] Use numbered *Reaction2 labels for old Python framework

--- interfaces/cython/cantera/test/test_convert.py
+++ interfaces/cython/cantera/test/test_convert.py
@@ -604,8 +604,8 @@ class cti2yamlTest(utilities.CanteraTest):
             self.assertEqual(C.composition, Y.composition)
 
         for C, Y in zip(ctiPhase.reactions(), yamlPhase.reactions()):
-            if Y.__class__.__name__.endswith('3'):
-                self.assertEqual(C.__class__.__name__, Y.__class__.__name__[:-1])
+            if C.__class__.__name__.endswith('2'):
+                self.assertEqual(C.__class__.__name__[:-1], Y.__class__.__name__)
             else:
                 self.assertEqual(C.__class__, Y.__class__)
             self.assertEqual(C.reactants, Y.reactants)
@@ -854,8 +854,8 @@ class ctml2yamlTest(utilities.CanteraTest):
             self.assertEqual(C.composition, Y.composition)
 
         for C, Y in zip(ctmlPhase.reactions(), yamlPhase.reactions()):
-            if Y.__class__.__name__.endswith('3'):
-                self.assertEqual(C.__class__.__name__, Y.__class__.__name__[:-1])
+            if C.__class__.__name__.endswith('2'):
+                self.assertEqual(C.__class__.__name__[:-1], Y.__class__.__name__)
             else:
                 self.assertEqual(C.__class__, Y.__class__)
             self.assertEqual(C.reactants, Y.reactants)
