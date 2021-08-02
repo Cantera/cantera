fa86de5895d5f69348bd237ae7ef3c2a4b22b276
Ingmar Schoegl <ischoegl@lsu.edu>
2021-08-01 22:44:47 -0500

    [Numerics] Update eigen_dense.h and introduce eigen_sparse.h

--- include/cantera/numerics/eigen_dense.h
+++ include/cantera/numerics/eigen_dense.h
@@ -1,12 +1,25 @@
-#include "cantera/base/ct_defs.h"
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at https://cantera.org/license.txt for license and copyright information.
+
+#ifndef CT_EIGEN_DENSE_H
+#define CT_EIGEN_DENSE_H
+
+#include "cantera/base/config.h"
 #if CT_USE_SYSTEM_EIGEN
 #include <Eigen/Dense>
 #else
 #include "cantera/ext/Eigen/Dense"
 #endif
 
-namespace Cantera {
-    typedef Eigen::Map<Eigen::MatrixXd> MappedMatrix;
-    typedef Eigen::Map<Eigen::VectorXd> MappedVector;
-    typedef Eigen::Map<const Eigen::VectorXd> ConstMappedVector;
+namespace Cantera
+{
+
+typedef Eigen::Map<Eigen::MatrixXd> MappedMatrix;
+typedef Eigen::Map<const Eigen::MatrixXd> ConstMappedMatrix;
+typedef Eigen::Map<Eigen::VectorXd> MappedVector;
+typedef Eigen::Map<const Eigen::VectorXd> ConstMappedVector;
+typedef Eigen::Map<Eigen::RowVectorXd> MappedRowVector;
+typedef Eigen::Map<const Eigen::RowVectorXd> ConstMappedRowVector;
 }
+
+#endif
