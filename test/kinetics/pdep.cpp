1f22be60a486352adab503aeaf94f9967c53beee
Ingmar Schoegl <ischoegl@lsu.edu>
2021-08-14 08:38:05 -0500

    [Unittest] Do not use legacy rate constant definition for test suite

--- test/kinetics/pdep.cpp
+++ test/kinetics/pdep.cpp
@@ -225,6 +225,7 @@ int main(int argc, char** argv)
 {
     printf("Running main() from pdep.cpp\n");
     Cantera::make_deprecation_warnings_fatal();
+    Cantera::use_legacy_rate_constants(false);
     testing::InitGoogleTest(&argc, argv);
     int result = RUN_ALL_TESTS();
     Cantera::appdelete();
