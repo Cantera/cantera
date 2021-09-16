Cantera/cantera

commit c7a5d1885e64f6c3f391c2a5d2693a301557c716

Replace stripDrive with pathlib
Remove unused functions

SConstruct
@@ -1482,16 +1482,23 @@ addInstallActions = ('install' in COMMAND_LINE_TARGETS or
 
 # Directories where things will be staged for package creation. These
 # variables should always be used by the Install(...) targets
-if env['stage_dir']:
-    instRoot = pjoin(os.getcwd(), env['stage_dir'],
-                     stripDrive(env['prefix']).strip('/\\'))
-    if env['python_prefix']:
-        env['python_prefix'] = pjoin(os.getcwd(), env['stage_dir'],
-                       stripDrive(env['python_prefix']).strip('/\\'))
+if env["stage_dir"]:
+    stage_prefix = Path(env["prefix"])
+    # Strip the root off the prefix if it's absolute
+    if stage_prefix.is_absolute():
+        stage_prefix = Path(*stage_prefix.parts[1:])
+
+    instRoot = Path.cwd().joinpath(env["stage_dir"], stage_prefix)
+
+    if env["python_prefix"]:
+        stage_py_prefix = Path(env["python_prefix"])
+        if stage_py_prefix.is_absolute():
+            stage_py_prefix = Path(*stage_py_prefix.parts[1:])
+        env["python_prefix"] = Path.cwd().joinpath(env["stage_dir"], stage_py_prefix)
     else:
-        env['python_prefix'] = pjoin(os.getcwd(), env['stage_dir'])
+        env["python_prefix"] = Path.cwd().joinpath(env["stage_dir"])
 else:
-    instRoot = env['prefix']
+    instRoot = env["prefix"]
 
 # Prevent setting Cantera installation path to source directory
 if os.path.abspath(instRoot) == Dir('.').abspath:

site_scons/buildutils.py
@@ -537,39 +537,6 @@ def mglob(env, subdir, *args):
     return matches
 
 
-def psplit(s):
-    """
-    Split a path given as a string into a list.
-    This is the inverse of os.path.join.
-    """
-    s = s.strip('/\\')
-    head, tail = os.path.split(s)
-    path = [tail]
-    while head:
-        head, tail = os.path.split(head)
-        path.append(tail)
-
-    path.reverse()
-    return path
-
-
-def subdirs(path):
-    """ Get the subdirectories of a specified directory """
-    for subdir in os.listdir(path):
-        dirpath = pjoin(path, subdir)
-        if os.path.isdir(dirpath):
-            yield subdir
-
-def stripDrive(s):
-    """
-    Remove a Windows drive letter specification from a path.
-    """
-    if len(s) > 1 and s[1] == ':':
-        return s[2:]
-    else:
-        return s
-
-
 def which(program):
     """ Replicates the functionality of the 'which' shell command """
     def is_exe(fpath):

commit ade8f88ac7854592f751d2199def0bf5031e88f9

[SCons] Fix installation of Python package into stage directory
In the default case where the 'python_prefix' variable is '$prefix',
the Python module would get installed to the literal '$prefix'
subdirectory of the staging directory, instead of actually expanding
the '$prefix' variable.

Fixes a regression introduced in c7a5d1885e6.

SConstruct
@@ -1504,7 +1504,7 @@ if env["stage_dir"]:
     instRoot = Path.cwd().joinpath(env["stage_dir"], stage_prefix)
 
     if env["python_prefix"]:
-        stage_py_prefix = Path(env["python_prefix"])
+        stage_py_prefix = Path(env.subst("$python_prefix"))
         if stage_py_prefix.is_absolute():
             stage_py_prefix = Path(*stage_py_prefix.parts[1:])
         env["python_prefix"] = Path.cwd().joinpath(env["stage_dir"], stage_py_prefix)