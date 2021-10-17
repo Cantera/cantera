feddb766ab5165301fc41fbf243f456224fc0072
Ray Speth <speth@mit.edu>
2021-10-16 21:41:22 -0400

    [Test] Add workaround for Plog rate setting test
    Using chained getter/setter notation to set the rate of a Plog
    reaction doesn't work because the 'rate' getter always returns a copy
    of the PlogRate object, and modifying this object doesn't update the
    parent Reaction object.

--- interfaces/cython/cantera/test/test_kinetics.py
+++ interfaces/cython/cantera/test/test_kinetics.py
@@ -1121,12 +1121,15 @@ class TestReaction(utilities.CanteraTest):
         r = ct.PlogReaction()
         r.reactants = {'R1A':1, 'R1B':1}
         r.products = {'P1':1, 'H':1}
-        r.rate.rates = [
+        # @todo: Fix so chained setter works, i.e. r.rate.rates = ...
+        rate = r.rate
+        rate.rates = [
             (0.01*ct.one_atm, ct.Arrhenius(1.2124e13, -0.5779, 10872.7*4184)),
             (1.0*ct.one_atm, ct.Arrhenius(4.9108e28, -4.8507, 24772.8*4184)),
             (10.0*ct.one_atm, ct.Arrhenius(1.2866e44, -9.0246, 39796.5*4184)),
             (100.0*ct.one_atm, ct.Arrhenius(5.9632e53, -11.529, 52599.6*4184))
         ]
+        r.rate = rate
 
         gas2 = ct.Solution(thermo='IdealGas', kinetics='GasKinetics',
                            species=species, reactions=[r])
