bae0d8ce0b9bf0871f2a60885deed517b00a4983
Ingmar Schoegl <ischoegl@lsu.edu>
2021-12-09 09:45:13 -0600

    [UnitTests] Add test_jacobian.py to Python test suite

--- interfaces/cython/cantera/test/__init__.py
+++ interfaces/cython/cantera/test/__init__.py
@@ -5,6 +5,7 @@ from .test_composite import *
 from .test_convert import *
 from .test_equilibrium import *
 from .test_func1 import *
+from .test_jacobian import *
 from .test_kinetics import *
 from .test_mixture import *
 from .test_onedim import *
