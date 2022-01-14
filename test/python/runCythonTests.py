97f69b423f956bf4bb9b61d45d7397f039d0d0ae
Ingmar Schoegl <ischoegl@lsu.edu>
2022-01-14 15:00:43 -0600

    [CI] Remove unittest TestRunner support

--- test/python/runCythonTests.py
+++ test/python/runCythonTests.py
@@ -3,7 +3,7 @@ Unit tests for Cantera's Cython-based Python module.
 
 This script gathers all the tests defined 'cantera.test' module, runs them,
 and prints a report. Extra command line arguments can be used to run subsets
-of the test suite, e.g.:
+of the test suite, for example:
 
 all tests from 'test_thermo.py' and 'test_kinetics.py':
 
@@ -25,40 +25,9 @@ from pathlib import Path
 cantera_root = os.path.relpath(__file__).split(os.sep)[:-1] + ['..', '..']
 os.chdir(os.sep.join(cantera_root + ['test', 'work']))
 
-import unittest
-try:
-    import pytest
-except ImportError:
-    pytest = None
+import pytest
 import cantera
 import cantera.test
-
-class TestResult(unittest.TextTestResult):
-    def __init__(self, *args, **kwargs):
-        unittest.TextTestResult.__init__(self, *args, **kwargs)
-        self.outName = 'python-results.txt'
-        with open(self.outName, 'w') as f:
-            pass # just create an empty output file
-
-    def reformat(self, test_string):
-        name, cls = test_string.split()
-        cls = cls.replace('(cantera.test.', '').replace(')','')
-        return '%s.%s' % (cls, name)
-
-    def addSuccess(self, test):
-        with open(self.outName, 'a') as f:
-            f.write('PASS: %s\n' % self.reformat(str(test)))
-        unittest.TextTestResult.addSuccess(self, test)
-
-    def addFailure(self, test, err):
-        with open(self.outName, 'a') as f:
-            f.write('FAIL: %s\n' % self.reformat(str(test)))
-        unittest.TextTestResult.addFailure(self, test, err)
-
-    def addError(self, test, err):
-        with open(self.outName, 'a') as f:
-            f.write('ERROR: %s\n' % self.reformat(str(test)))
-        unittest.TextTestResult.addFailure(self, test, err)
 
 
 if __name__ == '__main__':
@@ -82,41 +51,23 @@ if __name__ == '__main__':
         verbose = True
         subset_start += 1
 
-    if pytest is not None:
-        base = Path(cantera.__file__).parent.joinpath('test')
-        subsets = []
-        for name in sys.argv[subset_start:]:
-            subsets.append(str(base.joinpath(f"test_{name}.py")))
+    base = Path(cantera.__file__).parent.joinpath('test')
+    subsets = []
+    for name in sys.argv[subset_start:]:
+        subsets.append(str(base.joinpath(f"test_{name}.py")))
 
-        if not subsets:
-            subsets.append(str(base))
+    if not subsets:
+        subsets.append(str(base))
 
-        pytest_args = ["-raP", "--junitxml=pytest.xml"]
-        if show_long:
-            pytest_args += ["--durations=50"]
-        if fast_fail:
-            pytest_args.insert(0, "-x")
-        if verbose:
-            pytest_args.insert(0, "-v")
-        else:
-            pytest_args.append("--log-level=ERROR")
-
-        ret_code = pytest.main(pytest_args + subsets)
-        sys.exit(ret_code)
+    pytest_args = ["-raP", "--junitxml=pytest.xml"]
+    if show_long:
+        pytest_args += ["--durations=50"]
+    if fast_fail:
+        pytest_args.insert(0, "-x")
+    if verbose:
+        pytest_args.insert(0, "-v")
     else:
-        loader = unittest.TestLoader()
-        runner = unittest.TextTestRunner(
-            verbosity=2, resultclass=TestResult, failfast=fast_fail
-        )
-        suite = unittest.TestSuite()
-        subsets = []
-        for name in sys.argv[subset_start:]:
-            subsets.append('cantera.test.test_' + name)
+        pytest_args.append("--log-level=ERROR")
 
-        if not subsets:
-            subsets.append('cantera.test')
-
-        suite = loader.loadTestsFromNames(subsets)
-
-        results = runner.run(suite)
-        sys.exit(len(results.errors) + len(results.failures))
+    ret_code = pytest.main(pytest_args + subsets)
+    sys.exit(ret_code)
