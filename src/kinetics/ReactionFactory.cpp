9b60e0b9ea89abe29c315eb6cbfe14b70fe307af
Ingmar Schoegl <ischoegl@lsu.edu>
2022-01-17 13:37:24 -0600

    [Kinetics] Use C++ static_cast rather than C cast

--- src/kinetics/ReactionFactory.cpp
+++ src/kinetics/ReactionFactory.cpp
@@ -253,7 +253,7 @@ bool isThreeBody(const Reaction& R)
        if (trunc(reac.second) != reac.second) {
            return false;
        }
-       nreac += (size_t)reac.second;
+       nreac += static_cast<size_t>(reac.second);
     }
 
     // ensure that all products have integer stoichiometric coefficients
@@ -262,7 +262,7 @@ bool isThreeBody(const Reaction& R)
        if (trunc(prod.second) != prod.second) {
            return false;
        }
-       nprod += (size_t)prod.second;
+       nprod += static_cast<size_t>(prod.second);
     }
 
     // either reactant or product side involves exactly three species
