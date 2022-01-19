ee19de29c89e7e6962241eb6b93901c4f1c76c43
Ray Speth <speth@mit.edu>
2022-01-18 23:50:38 -0500

    [Thermo] Write critical parameters for Redlich-Kwong species

--- include/cantera/thermo/RedlichKwongMFTP.h
+++ include/cantera/thermo/RedlichKwongMFTP.h
@@ -279,9 +279,9 @@ protected:
     //! Explicitly-specified binary interaction parameters
     std::map<std::string, std::map<std::string, std::pair<double, double>>> m_binaryParameters;
 
-    //! For each species, true if the a and b coefficients were determined from
-    //! the critical properties database
-    std::vector<bool> m_coeffs_from_db;
+    enum class CoeffSource { EoS, CritProps, Database };
+    //! For each species, specifies the source of the a and b coefficients
+    std::vector<CoeffSource> m_coeffSource;
 
     int NSolns_;
 
