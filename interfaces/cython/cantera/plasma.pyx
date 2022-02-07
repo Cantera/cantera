61899f71a3ef18d6075c26f44d4e7f9b77d44de5
bangshiuh <bangshiuh.chen@gmail.com>
2022-02-07 14:11:02 -0500

    [plasma] add test_set_electron_energy_distribution

--- interfaces/cython/cantera/plasma.pyx
+++ interfaces/cython/cantera/plasma.pyx
@@ -11,6 +11,22 @@ cdef class PlasmaPhase(ThermoPhase):
             raise TypeError('Underlying ThermoPhase object is of the wrong type.')
         self.plasma = <CxxPlasmaPhase*>(self.thermo)
 
+    def set_electron_energy_distribution(self, grid, distrb):
+        """ Set electron energy distribution. When this method is used, electron
+        temeprature is calculated from the distribution.
+        :param grid:
+            vector of electron energy grid [eV]
+        :param distrb:
+            vector of distribution
+        """
+        cdef vector[double] cxxdata_grid
+        cdef vector[double] cxxdata_distrb
+        for value in grid:
+            cxxdata_grid.push_back(value)
+        for value in distrb:
+            cxxdata_distrb.push_back(value)
+        self.plasma.setElectronEnergyDistrb(cxxdata_grid, cxxdata_distrb)
+
     property electron_energy_grid:
         """ Electron energy grid [eV]"""
         def __get__(self):
@@ -22,3 +38,10 @@ cdef class PlasmaPhase(ThermoPhase):
             for value in grid:
                 cxxdata.push_back(value)
             self.plasma.setElectronEnergyGrid(cxxdata)
+
+    property electron_energy_distribution:
+        """ Electron energy distribution """
+        def __get__(self):
+            cdef vector[double] cxxdata
+            self.plasma.getElectronEnergyDistrb(cxxdata)
+            return np.fromiter(cxxdata, np.double)
