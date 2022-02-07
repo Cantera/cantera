cd39a0f179f94f5275921facd237b236f2a6e920
bangshiuh <bangshiuh.chen@gmail.com>
2022-02-07 13:45:50 -0500

    [plasma] add get/setElectronEnergyDistrb

--- src/thermo/PlasmaPhase.cpp
+++ src/thermo/PlasmaPhase.cpp
@@ -17,9 +17,9 @@ PlasmaPhase::PlasmaPhase(const std::string& inputFile, const std::string& id_)
     initThermoFile(inputFile, id_);
 
     // initial grid
-    m_EE_grid.resize(m_nPoints);
+    m_electronEnergyGrid.resize(m_nPoints);
     for (size_t i = 0; i <= m_nPoints - 1; i++) {
-        m_EE_grid(i) = 0.001 + 0.001 * i;
+        m_electronEnergyGrid(i) = 0.001 + 0.001 * i;
     }
     // initial electron temperature
     setElectronTemperature(temperature());
@@ -27,14 +27,14 @@ PlasmaPhase::PlasmaPhase(const std::string& inputFile, const std::string& id_)
 
 void PlasmaPhase::updateIsotropicElectronEnergyDistrb()
 {
-    m_EE_distrb_vec.resize(m_nPoints);
-    m_EE_mean = 3.0 / 2.0 * electronTemperature() * GasConstant / (Avogadro * ElectronCharge);
+    m_electronEnergyDistrb.resize(m_nPoints);
+    m_meanElectronEnergy = 3.0 / 2.0 * electronTemperature() * GasConstant / (Avogadro * ElectronCharge);
     double gamma1 = boost::math::tgamma(3.0 / 2.0 * m_x);
     double gamma2 = boost::math::tgamma(5.0 / 2.0 * m_x);
     double c1 = m_x * std::pow(gamma2, 1.5) / std::pow(gamma1, 2.5);
     double c2 = m_x * std::pow(gamma2 / gamma1, m_x);
-    m_EE_distrb_vec = c1 * m_EE_grid.array().sqrt() / std::pow(m_EE_mean, 1.5) *
-                      (-c2 * (m_EE_grid.array() / m_EE_mean).pow(m_x)).exp();
+    m_electronEnergyDistrb = c1 * m_electronEnergyGrid.array().sqrt() / std::pow(m_meanElectronEnergy, 1.5) *
+                      (-c2 * (m_electronEnergyGrid.array() / m_meanElectronEnergy).pow(m_x)).exp();
 }
 
 void PlasmaPhase::setElectronTemperature(const double Te) {
@@ -63,9 +63,9 @@ void PlasmaPhase::setElectronEnergyDistrbMethod(std::string method)
 void PlasmaPhase::setElectronEnergyGrid(const vector_fp& grid)
 {
     m_nPoints = grid.size();
-    m_EE_grid.resize(m_nPoints);
+    m_electronEnergyGrid.resize(m_nPoints);
     for (size_t i = 0; i < m_nPoints; i++) {
-        m_EE_grid(i) = grid[i];
+        m_electronEnergyGrid(i) = grid[i];
     }
 }
 
@@ -73,7 +73,30 @@ void PlasmaPhase::getElectronEnergyGrid(vector_fp& grid) const
 {
     grid.resize(m_nPoints);
     for (size_t i = 0; i < m_nPoints; i++) {
-        grid[i] = m_EE_grid(i);
+        grid[i] = m_electronEnergyGrid(i);
+    }
+}
+
+void PlasmaPhase::setElectronEnergyDistrb(const vector_fp& grid,
+                                          const vector_fp& distrb)
+{
+    if (grid.size() != distrb.size()) {
+        throw CanteraError("PlasmaPhase::setElectronEnergyDistrb",
+                           "Vector lengths need to be the same.");
+    }
+    setElectronEnergyGrid(grid);
+    m_nPoints = grid.size();
+    m_electronEnergyDistrb.resize(m_nPoints);
+    for (size_t i = 0; i < m_nPoints; i++) {
+        m_electronEnergyDistrb(i) = distrb[i];
+    }
+}
+
+void PlasmaPhase::getElectronEnergyDistrb(vector_fp& distrb) const
+{
+    distrb.resize(m_nPoints);
+    for (size_t i = 0; i < m_nPoints; i++) {
+        distrb[i] = m_electronEnergyDistrb(i);
     }
 }
 
