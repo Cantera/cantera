d825ab191ae7c3a7d26cb08998e4b054d943e17d
Ingmar Schoegl <ischoegl@lsu.edu>
2022-02-11 10:17:05 -0600

    [Kinetics] Implement InterfaceRate<> class template

--- include/cantera/kinetics/Coverage.h
+++ include/cantera/kinetics/Coverage.h
@@ -100,5 +100,101 @@ protected:
 };
 
 
+//! A class template for interface reaction rate specifications
+template <class RateType, class DataType>
+class InterfaceRate : public RateType, public Coverage
+{
+public:
+    InterfaceRate() = default;
+    using RateType::RateType; // inherit constructors
+
+    //! Constructor based on AnyMap content
+    InterfaceRate(const AnyMap& node, const UnitStack& rate_units={}) {
+        setParameters(node, rate_units);
+    }
+
+    unique_ptr<MultiRateBase> newMultiRate() const override {
+        return unique_ptr<MultiRateBase>(
+            new MultiRate<InterfaceRate<RateType, DataType>, DataType>);
+    }
+
+    //! Identifier of reaction rate type
+    virtual const std::string type() const override {
+        return RateType::type() + "-interface";
+    }
+
+    virtual void setParameters(
+        const AnyMap& node, const UnitStack& rate_units) override
+    {
+        if (node.hasKey("coverage-dependencies")) {
+            setCoverageDependencies(
+                node["coverage-dependencies"].as<AnyMap>(), node.units());
+        }
+        RateType::m_negativeA_ok = node.getBool("negative-A", false);
+        if (!node.hasKey("rate-constant")) {
+            RateType::setRateParameters(AnyValue(), node.units(), rate_units);
+            return;
+        }
+        RateType::setRateParameters(node["rate-constant"], node.units(), rate_units);
+    }
+
+    virtual void getParameters(AnyMap& node) const override {
+        node["type"] = type();
+        if (RateType::m_negativeA_ok) {
+            node["negative-A"] = true;
+        }
+        AnyMap rateNode;
+        RateType::getRateParameters(rateNode);
+        if (!rateNode.empty()) {
+            // RateType object is configured
+            node["rate-constant"] = std::move(rateNode);
+        }
+        if (!m_cov.empty()) {
+            AnyMap deps;
+            getCoverageDependencies(deps);
+            node["coverage-dependencies"] = std::move(deps);
+        }
+    }
+
+    virtual void setContext(const Reaction& rxn, const Kinetics& kin) override {
+        RateType::setContext(rxn, kin);
+        setSpecies(kin);
+    }
+
+    //! Evaluate reaction rate
+    //! @param shared_data  data shared by all reactions of a given type
+    double evalFromStruct(const DataType& shared_data) const {
+        return RateType::evalRate(shared_data.logT, shared_data.recipT) *
+            std::exp(std::log(10.0) * m_acov - m_ecov * shared_data.recipT + m_mcov);
+    }
+
+    //! Evaluate derivative of reaction rate with respect to temperature
+    //! divided by reaction rate
+    //! @param shared_data  data shared by all reactions of a given type
+    double ddTScaledFromStruct(const DataType& shared_data) const {
+        throw NotImplementedError("InterfaceRate<>::ddTScaledFromStruct");
+    }
+
+    //! Return the effective pre-exponential factor *A* (in m, kmol, s to powers
+    //! depending on the reaction order) accounting for coverage dependent terms.
+    double effectivePreExponentialFactor() const {
+        return RateType::preExponentialFactor() *
+            std::exp(std::log(10.0) * m_acov + m_mcov);
+    }
+
+    //! Return the effective activation energy *Ea* [J/kmol]
+    double effectiveActivationEnergy() const {
+        return RateType::activationEnergy() + m_ecov * GasConstant;
+    }
+
+    //! Return the effective activation temperature (energy divided by the gas
+    //! constant) [K]
+    double effectiveActivationEnergy_R() const {
+        return RateType::activationEnergy_R() + m_ecov;
+    }
+};
+
+using ArrheniusInterfaceRate = InterfaceRate<Arrhenius3, CoverageData>;
+
 }
 #endif
