cca7239a4eb3646dbedbb5cf686e33416116612e
Ingmar Schoegl <ischoegl@lsu.edu>
2022-03-15 10:19:07 -0500

    [Kinetics] Implement layout for ThreeBodyRate<> template

--- include/cantera/kinetics/BulkRate.h
+++ include/cantera/kinetics/BulkRate.h
@@ -36,9 +36,138 @@ public:
     using RateType::getParameters;
 };
 
-typedef BulkRate<Arrhenius3, ArrheniusData> ArrheniusRate;
-typedef BulkRate<TwoTempPlasma, TwoTempPlasmaData> TwoTempPlasmaRate;
-typedef BulkRate<BlowersMasel, BlowersMaselData> BlowersMaselRate;
+
+using ArrheniusRate = BulkRate<Arrhenius3, ArrheniusData>;
+using TwoTempPlasmaRate = BulkRate<TwoTempPlasma, TwoTempPlasmaData>;
+using BlowersMaselRate = BulkRate<BlowersMasel, BlowersMaselData>;
+
+
+class ThreeBodyBase
+{
+public:
+    ThreeBodyBase();
+
+    //! Set third-body collision efficiency parameters
+    //! @param node  Coverage dependencies
+    void setEfficiencies(const AnyMap& node);
+
+    //! Get third-body collision efficiency parameters
+    //! @param node  AnyMap receiving coverage information
+    void getEfficiencies(AnyMap& node) const;
+
+    //! Get the third-body efficiency for species *k*
+    double efficiency(const std::string& k) const;
+
+    //! Build rate-specific parameters based on Reaction and Kinetics context
+    //! @param rxn  Associated reaction object
+    //! @param kin  Kinetics object holding the rate evaluator
+    void setContext(const Reaction& rxn, const Kinetics& kin);
+
+    //! Update reaction rate parameters
+    //! @param shared_data  data shared by all reactions of a given type
+    void updateFromStruct(const ReactionData& shared_data) {}
+
+    //! Apply correction
+    double applyCorrection(double value) {
+        if (m_massAction) {
+            return value * m_threeBodyConc;
+        }
+        return value;
+    }
+
+protected:
+    double m_threeBodyConc; //!< Effective third-body concentration
+
+    //! The default third body efficiency for species not listed in
+    double m_defaultEfficiency;
+
+    //! Input explicitly specifies collision partner
+    bool m_specifiedCollisionPartner;
+
+    //! Third body is used by law of mass action
+    //! (`true` for three-body reactions, `false` for falloff reactions)
+    bool m_massAction;
+
+    Composition m_efficiencyMap; //!< Map of species to third body efficiency
+
+private:
+    //! Vector of pairs containing indices and efficiencies
+    std::vector<std::pair<size_t, double>> m_efficiencies;
+};
+
+
+template <class RateType, class DataType>
+class ThreeBodyRate : public RateType, public ThreeBodyBase
+{
+    CT_DEFINE_HAS_MEMBER(has_update, updateFromStruct)
+
+public:
+    ThreeBodyRate() = default;
+    using RateType::RateType; // inherit constructors
+
+    //! Constructor based on AnyMap content
+    ThreeBodyRate(const AnyMap& node, const UnitStack& rate_units={}) {
+        setParameters(node, rate_units);
+    }
+
+    unique_ptr<MultiRateBase> newMultiRate() const override {
+        return unique_ptr<MultiRateBase>(
+            new MultiRate<ThreeBodyRate<RateType, DataType>, DataType>);
+    }
+
+    virtual const std::string type() const override {
+        return "three-body-" + RateType::type();
+    }
+
+    virtual void setParameters(
+        const AnyMap& node, const UnitStack& rate_units) override
+    {
+        RateType::setParameters(node, rate_units);
+        ThreeBodyBase::setEfficiencies(node);
+    }
+
+    virtual void getParameters(AnyMap& node) const override {
+        RateType::getParameters(node);
+        node["type"] = type();
+        ThreeBodyBase::getEfficiencies(node);
+    }
+
+    virtual void setContext(const Reaction& rxn, const Kinetics& kin) override {
+        RateType::setContext(rxn, kin);
+        ThreeBodyBase::setContext(rxn, kin);
+    }
+
+    //! Update reaction rate parameters
+    //! @param shared_data  data shared by all reactions of a given type
+    void updateFromStruct(const DataType& shared_data) {
+        _update(shared_data);
+        ThreeBodyBase::updateFromStruct(shared_data);
+    }
+
+    //! Evaluate reaction rate
+    //! @param shared_data  data shared by all reactions of a given type
+    double evalFromStruct(const DataType& shared_data) const {
+        return applyCorrection(RateType::evalFromStruct(shared_data));
+    }
+
+protected:
+    //! Helper function to process updates for rate types that implement the
+    //! `updateFromStruct` method.
+    template <typename T=RateType,
+        typename std::enable_if<has_update<T>::value, bool>::type = true>
+    void _update(const DataType& shared_data) {
+        T::updateFromStruct(shared_data);
+    }
+
+    //! Helper function for rate types that do not implement `updateFromStruct`.
+    //! Does nothing, but exists to allow generic implementations of update().
+    template <typename T=RateType,
+        typename std::enable_if<!has_update<T>::value, bool>::type = true>
+    void _update(const DataType& shared_data) {
+    }
+};
+
+using ThreeBodyArrheniusRate = ThreeBodyRate<Arrhenius3, ArrheniusData>;
 
 }
 
---
+++ src/kinetics/BulkRate.cpp
@@ -0,0 +1,59 @@
+//! @file BulkRate.cpp
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at https://cantera.org/license.txt for license and copyright information.
+
+#include "cantera/kinetics/BulkRate.h"
+#include "cantera/kinetics/Kinetics.h"
+// #include "cantera/thermo/ThermoPhase.h"
+// #include "cantera/thermo/SurfPhase.h"
+#include "cantera/base/AnyMap.h"
+
+namespace Cantera
+{
+
+ThreeBodyBase::ThreeBodyBase()
+    : m_threeBodyConc(NAN)
+    , m_defaultEfficiency(1.)
+    , m_specifiedCollisionPartner(false)
+    , m_massAction(false)
+{}
+
+void ThreeBodyBase::setEfficiencies(const AnyMap& node)
+{
+    m_defaultEfficiency = node.getDouble("default-efficiency", 1.0);
+    if (node.hasKey("efficiencies")) {
+        m_efficiencyMap = node["efficiencies"].asMap<double>();
+    }
+}
+
+void ThreeBodyBase::getEfficiencies(AnyMap& node) const
+{
+    if (!m_specifiedCollisionPartner) {
+        node["efficiencies"] = m_efficiencies;
+        node["efficiencies"].setFlowStyle();
+        if (m_defaultEfficiency != 1.0) {
+            node["default-efficiency"] = m_defaultEfficiency;
+        }
+    }
+}
+
+double ThreeBodyBase::efficiency(const std::string& k) const {
+    return getValue(m_efficiencyMap, k, m_defaultEfficiency);
+}
+
+void ThreeBodyBase::setContext(const Reaction& rxn, const Kinetics& kin)
+{
+    for (const auto& eff : m_efficiencyMap) {
+        size_t k = kin.kineticsSpeciesIndex(eff.first);
+        if (k != npos) {
+            m_efficiencies.emplace_back(k, eff.second);
+        } else if (!kin.skipUndeclaredThirdBodies()) {
+            throw CanteraError("ThreeBodyBase::setContext",
+                "Found third-body efficiency for undeclared species '{}' "
+                "while adding reaction '{}'", eff.first, rxn.equation());
+        }
+    }
+}
+
+}
