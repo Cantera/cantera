18ae9ccd358859edabd7e492eb0d01f7e58e1404
Ray Speth <speth@mit.edu>
2022-04-01 22:14:08 -0400

    Add keywords to Fortran examples

--- samples/f90/demo.f90
+++ samples/f90/demo.f90
@@ -2,6 +2,8 @@
 !
 ! This program illustrates using Cantera in Fortran 90 to compute
 ! thermodynamic, kinetic, and transport properties of a gas mixture.
+!
+! Keywords: tutorial, equilibrium, thermodynamics, kinetics, transport
 
 ! This file is part of Cantera. See License.txt in the top-level directory or
 ! at https://cantera.org/license.txt for license and copyright information.
