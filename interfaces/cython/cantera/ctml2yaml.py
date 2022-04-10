aed4f606aa1ececf1cb3cb1ed3586b97f6539034
Ray Speth <speth@mit.edu>
2022-04-10 10:00:22 -0400

    Bump version to 2.6.0b2

--- interfaces/cython/cantera/ctml2yaml.py
+++ interfaces/cython/cantera/ctml2yaml.py
@@ -2641,7 +2641,7 @@ def convert(
     metadata = BlockMap(
         {
             "generator": "ctml2yaml",
-            "cantera-version": "2.6.0b1",
+            "cantera-version": "2.6.0b2",
             "date": formatdate(localtime=True),
         }
     )
