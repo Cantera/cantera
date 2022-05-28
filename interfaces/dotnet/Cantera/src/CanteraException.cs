2591213b582add76c7bd14ec872713dac64c7764
Sammo Gabay <sammolito@gmail.com>
2022-05-27 23:17:08 -0400

    [.NET] Generate C# interop code automatically
    Uses an MSBuild task and a Python script to generate C# P/Invoke calls and SafeHandles from the Cantera CLib header files.
    The script is written in Python because it's already part of the build environment.

--- interfaces/dotnet/Cantera/src/CanteraException.cs
+++ interfaces/dotnet/Cantera/src/CanteraException.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using Cantera.Interop;
 
 namespace Cantera;
 
@@ -7,17 +8,11 @@ namespace Cantera;
 /// </summary>
 public class CanteraException : ExternalException
 {
-    static class LibCantera
-    {
-        [DllImport(Interop.LIBCANTERA)]
-        unsafe public static extern int ct_getCanteraError(int buflen, byte* buf);
-    }
-
     private CanteraException(string message) : base(message) { }
 
     unsafe internal static void ThrowLatest()
     {
-        var errorMessage = Interop.GetString(500, LibCantera.ct_getCanteraError);
+        var errorMessage = InteropUtil.GetString(500, LibCantera.ct_getCanteraError);
         throw new CanteraException(errorMessage);
     }
 }
