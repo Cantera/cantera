ff2e87e2925e4cc778ae9efdcbb5403ef41e11cd
Sammo Gabay <sammolito@gmail.com>
2022-06-02 21:31:30 -0400

    [.NET] Stub out platform-sniffing with compilation switches

--- interfaces/dotnet/Cantera/src/Interop/LibCantera.cs
+++ interfaces/dotnet/Cantera/src/Interop/LibCantera.cs
@@ -2,5 +2,11 @@
 
 static partial class LibCantera
 {
-    const string LibFile = "cantera.2.6.0";
+#if IS_WINDOWS
+    const string LibFile = "cantera.2.6.0.dll";
+#elif IS_MACOS
+    const string LibFile = "cantera.2.6.0.dylib";
+#elif IS_LINUX
+    const string LibFile = "cantera.2.6.0.so";
+#endif
 }
