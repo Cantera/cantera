bd1f047eb5ecd835ecc1519cea4dc1401ed46bbc
ssun30 <sun.su@northeastern.edu>
2022-06-30 20:56:51 -0400

    Tutorials/Sconscript/Docs

---
+++ samples/matlab_experimental/test_examples.m
@@ -0,0 +1,12 @@
+% runs selected examples without pausing
+equil();
+isentropic();
+reactor1();
+reactor2();
+surfreactor;
+periodic_cstr;
+rankine(300.0, 2.0*oneatm, 0.8, 0.7);
+prandtl1();
+flame1;
+catcomb;
+exit;
