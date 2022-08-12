0d3baceb1fbd09aecada3dc0d7f60d9486559300
Sammo Gabay <sammolito@gmail.com>
2022-08-12 02:53:21 -0400

    [sourcegen] Tweak generate_source for better error handling

--- interfaces/sourcegen/sourcegen/_orchestrate.py
+++ interfaces/sourcegen/sourcegen/_orchestrate.py
@@ -1,6 +1,7 @@
 # This file is part of Cantera. See License.txt in the top-level directory or
 # at https://cantera.org/license.txt for license and copyright information.
 
+from distutils.command.config import LANG_EXT
 import importlib
 import inspect
 from pathlib import Path
@@ -10,28 +11,31 @@ from ._dataclasses import HeaderFile
 from ._SourceGenerator import SourceGenerator
 
 
+_clib_path = Path(__file__).parent.joinpath("../../../include/cantera/clib").resolve()
+_clib_defs_path = _clib_path.joinpath("clib_defs.h")
+
 def generate_source(lang: str, out_dir: str):
     print("Generating source files...")
 
-    my_path = Path(__file__).parent
-    clib_path = my_path.joinpath("../../../include/cantera/clib").resolve()
-    config_path = my_path.joinpath(lang, "config.yaml").resolve()
+    module = importlib.import_module(__package__ + "." + lang)
+    config_path = Path(module.__file__).parent.joinpath("config.yaml")
 
-    with config_path.open() as config_file:
-        config = ruamel.yaml.safe_load(config_file)
+    config = {}
+    if config_path.exists():
+        with config_path.open() as config_file:
+            config = ruamel.yaml.safe_load(config_file)
+
+    ignore_files: list[str] = config.get("ignore_files", [])
+    ignore_functions: dict[str, list[str]] = config.get("ignore_funcs", {})
+
+    files = (HeaderFile.parse(f, ignore_functions.get(f.name, []))
+        for f in _clib_path.glob("*.h") if f.name not in ignore_files)
+    # removes instances where HeaderFile.parse() returned None
+    files = list(filter(lambda p: p is not None and p.path != _clib_defs_path, files))
 
     # find and instantiate the language-specific SourceGenerator
-    module = importlib.import_module(__package__ + "." + lang)
     _, scaffolder_type = inspect.getmembers(module,
         lambda m: inspect.isclass(m) and issubclass(m, SourceGenerator))[0]
     scaffolder: SourceGenerator = scaffolder_type(Path(out_dir), config)
-
-    ignore_files: list[str] = config.get("ignore_files", [])
-    ignore_functions: dict[str, list[str]] = config.get("ignore_functions", {})
-
-    files = (HeaderFile.parse(f, ignore_functions.get(f.name, []))
-        for f in clib_path.glob("*.h") if f.name not in ignore_files)
-    # removes instances where HeaderFile.parse() returned None
-    files = list(filter(None, files))
 
     scaffolder.generate_source(files)
