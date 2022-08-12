ba4d10fde9ba19751a00920538d364861a1a7852
ssun30 <sun.su@northeastern.edu>
2022-08-12 13:44:19 -0400

    Changed how strings are passed between Matlab and Clib

--- interfaces/matlab_experimental/Utility/geterr.m
+++ interfaces/matlab_experimental/Utility/geterr.m
@@ -4,9 +4,11 @@ function e = geterr()
     checklib;
     try
         buflen = calllib(ct, 'ct_getCanteraError', 0, '');
-        aa = char(zeros(1, buflen));
-        [~, aa] = calllib(ct, 'ct_getCanteraError', buflen, aa);
-        e = aa;
+        aa = char([zeros(1, buflen, 'int8')]);
+        ptr = libpointer('cstring', aa);
+        [~, bb] = calllib(ct, 'ct_getCanteraError', buflen, ptr);
+        e = bb;
+        clear aa bb ptr
     catch ME
         e = getReport(ME);
     end
