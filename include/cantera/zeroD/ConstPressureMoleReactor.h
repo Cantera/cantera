bcd812487d183f66693c739755220d6b04eac3bd
Anthony Walker <walkerant@kairospower.com>
2022-09-09 13:18:49 -0400

    Version documentation for MoleReactors

--- include/cantera/zeroD/ConstPressureMoleReactor.h
+++ include/cantera/zeroD/ConstPressureMoleReactor.h
@@ -14,7 +14,7 @@ namespace Cantera
 /*!
  * ConstPressureMoleReactor is a class for constant-pressure reactors
  * which use a state of moles.
- * @SInCE New in Cantera 3.0
+ * @since New in Cantera 3.0
  */
 class ConstPressureMoleReactor : public MoleReactor
 {
