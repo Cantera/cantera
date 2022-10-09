49f6d701533befd5a69139d3230a6613ec96db18
Corey R. Randall <corey.r.randall@gmail.com>
2022-10-09 14:37:08 -0400

    Update test/python/test_kinetics.py
    Co-authored-by: Ingmar Schoegl <ischoegl@lsu.edu>

--- test/python/test_kinetics.py
+++ test/python/test_kinetics.py
@@ -1087,9 +1087,9 @@ class TestLithiumIonBatteryKinetics(utilities.CanteraTest):
             charges = p.charges
 
             method = anode_int.interface_current(p)
-            manual = sum(net_prod_rates*charges)*ct.faraday
+            manual = sum(net_prod_rates * charges) * ct.faraday
 
-            self.assertEqual(method,manual)
+            self.assertEqual(method, manual)
 
 
 class TestDuplicateReactions(utilities.CanteraTest):
