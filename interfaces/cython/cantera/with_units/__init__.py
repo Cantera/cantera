dfec96f2214bdc3266af7cb9b81b56ccbe7fd6b2
Bryan Weber <bryan.w.weber@gmail.com>
2022-10-11 22:27:41 -0400

    Missing a license header

--- interfaces/cython/cantera/with_units/__init__.py
+++ interfaces/cython/cantera/with_units/__init__.py
@@ -1,3 +1,6 @@
+# This file is part of Cantera. See License.txt in the top-level directory or
+# at https://cantera.org/license.txt for license and copyright information.
+
 # This code to set the application registry has to come before any code that wants to
 # use the registry is imported. In particular, it has to come before any of our code
 # that uses units!
