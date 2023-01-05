4363c810596a1a0440b416a41b5502522e6b9eef
Cory Kinney <cory.kinney@ucf.edu>
2023-01-05 13:08:42 -0500

    Add derivatives

--- include/cantera/thermo/SoaveRedlichKwong.h
+++ include/cantera/thermo/SoaveRedlichKwong.h
@@ -26,9 +26,67 @@ public:
         return "Soave-Redlich-Kwong";
     }
 
+protected:
+
+    virtual double dpdVCalc(double T, double molarVol, double& presCalc) const;
+
+    // Special functions not inherited from MixtureFugacityTP
+
+    //! Calculate temperature derivative \f$d(a \alpha)/dT\f$
+    /*!
+     *  These are stored internally.
+     */
+    double daAlpha_dT() const;
+
+    //! Calculate second derivative \f$d^2(a \alpha)/dT^2\f$
+    /*!
+     *  These are stored internally.
+     */
+    double d2aAlpha_dT2() const;
+
 public:
-    int SoaveRedlichKwong::solveCubic(double T, double pres, double a, double b, double aAlpha,
-                                  double Vroot[3]) const;
+    //! Calculate \f$dp/dV\f$ and \f$dp/dT\f$ at the current conditions
+    /*!
+     *  These are stored internally.
+     */
+    void calculatePressureDerivatives() const;
+
+    //! Prepare variables and call the function to solve the cubic equation of state
+    int solveCubic(double T, double pres, double a, double b, double aAlpha,
+                   double Vroot[3]) const;
+
+protected:
+    //! Value of \f$b\f$ in the equation of state
+    /*!
+     *  `m_b` is a function of the mole fractions and species-specific b values.
+     */
+    double m_b;
+
+    //! Value of \f$a\f$ in the equation of state
+    /*!
+     *  `m_a` depends only on the mole fractions.
+     */
+    double m_a;
+
+    //! Value of \f$\alpha\f$ in the equation of state
+    /*!
+     *  `m_aAlpha_mix` is a function of the temperature and the mole fractions.
+     */
+    double m_aAlpha_mix;
+
+    //! The derivative of the pressure with respect to the volume
+    /*!
+     * Calculated at the current conditions. temperature and mole number kept
+     * constant
+     */
+    mutable double m_dpdV;
+
+    //! The derivative of the pressure with respect to the temperature
+    /*!
+     *  Calculated at the current conditions. Total volume and mole number kept
+     *  constant
+     */
+    mutable double m_dpdT;
 
 private:
     static const double omega_a;
--- src/thermo/SoaveRedlichKwong.cpp
+++ src/thermo/SoaveRedlichKwong.cpp
@@ -20,6 +20,80 @@ const double SoaveRedlichKwong::omega_vc = 3.33333333333333E-01;
 
 
 
+double SoaveRedlichKwong::dpdVCalc(double T, double molarVol, double& presCalc) const
+{
+    double denom = molarVol * (molarVol + m_b);
+    double vmb = molarVol - m_b;
+    return -GasConstant * T / (vmb * vmb) + m_aAlpha_mix * (2 * molarVol + m_b) / (denom * denom);
+}
+
+void SoaveRedlichKwong::calculatePressureDerivatives() const
+{
+    double T = temperature();
+    double mv = molarVolume();
+    double pres;
+
+    m_dpdV = dpdVCalc(T, mv, pres);
+    m_dpdT = GasConstant / (mv - m_b) - daAlpha_dT() / (mv * (mv + m_b));
+}
+
+double SoaveRedlichKwong::daAlpha_dT() const
+{
+    // Same as PengRobinson::daAlpha_dT()
+    double daAlphadT = 0.0, k, Tc, sqtTr, coeff1, coeff2;
+    for (size_t i = 0; i < m_kk; i++) {
+        // Calculate first derivative of alpha for individual species
+        Tc = speciesCritTemperature(m_a_coeffs(i,i), m_b_coeffs[i]);
+        sqtTr = sqrt(temperature() / Tc);
+        coeff1 = 1 / (Tc*sqtTr);
+        coeff2 = sqtTr - 1;
+        k = m_kappa[i];
+        m_dalphadT[i] = coeff1 * (k*k*coeff2 - k);
+    }
+    // Calculate mixture derivative
+    for (size_t i = 0; i < m_kk; i++) {
+        for (size_t j = 0; j < m_kk; j++) {
+            daAlphadT += moleFractions_[i] * moleFractions_[j] * 0.5
+                         * m_aAlpha_binary(i, j)
+                         * (m_dalphadT[i] / m_alpha[i] + m_dalphadT[j] / m_alpha[j]);
+        }
+    }
+    return daAlphadT;
+}
+
+double SoaveRedlichKwong::d2aAlpha_dT2() const
+{
+    // Same as PengRobinson::d2aAlpha_dT2()
+    for (size_t i = 0; i < m_kk; i++) {
+        double Tcrit_i = speciesCritTemperature(m_a_coeffs(i, i), m_b_coeffs[i]);
+        double sqt_Tr = sqrt(temperature() / Tcrit_i);
+        double coeff1 = 1 / (Tcrit_i*sqt_Tr);
+        double coeff2 = sqt_Tr - 1;
+        // Calculate first and second derivatives of alpha for individual species
+        double k = m_kappa[i];
+        m_dalphadT[i] = coeff1 * (k*k*coeff2 - k);
+        m_d2alphadT2[i] = (k*k + k) * coeff1 / (2*sqt_Tr*sqt_Tr*Tcrit_i);
+    }
+
+    // Calculate mixture derivative
+    double d2aAlphadT2 = 0.0;
+    for (size_t i = 0; i < m_kk; i++) {
+        double alphai = m_alpha[i];
+        for (size_t j = 0; j < m_kk; j++) {
+            double alphaj = m_alpha[j];
+            double alphaij = alphai * alphaj;
+            double term1 = m_d2alphadT2[i] / alphai + m_d2alphadT2[j] / alphaj;
+            double term2 = 2 * m_dalphadT[i] * m_dalphadT[j] / alphaij;
+            double term3 = m_dalphadT[i] / alphai + m_dalphadT[j] / alphaj;
+            d2aAlphadT2 += 0.5 * moleFractions_[i] * moleFractions_[j]
+                           * m_aAlpha_binary(i, j)
+                           * (term1 + term2 - 0.5 * term3 * term3);
+        }
+    }
+    return d2aAlphadT2;
+}
+
+
 int SoaveRedlichKwong::solveCubic(double T, double pres, double a, double b, double aAlpha,
                                   double Vroot[3]) const
 {
@@ -30,7 +104,7 @@ int SoaveRedlichKwong::solveCubic(double T, double pres, double a, double b, dou
 
     double tc = a * omega_b / (b * omega_a * GasConstant);
     double pc = omega_b * R * tc / b;
-    double vc = omega_vc * GasConstant * tc / pc
+    double vc = omega_vc * GasConstant * tc / pc;
 
     return MixtureFugacityTP::solveCubic(T, pres, a, b, aAlpha, Vroot,
                                          an, bn, cn, dn, tc, vc);
