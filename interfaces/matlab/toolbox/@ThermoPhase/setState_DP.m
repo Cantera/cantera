68ade2d2b7677edfd29d67446c953147fa8acce4
Ingmar Schoegl <ischoegl@lsu.edu>
2023-02-09 10:34:43 -0600

    [matlab] Redirect RP setter to thermo_set_DP

--- interfaces/matlab/toolbox/@ThermoPhase/setState_DP.m
+++ interfaces/matlab/toolbox/@ThermoPhase/setState_DP.m
@@ -1,6 +1,6 @@
-function setState_RP(tp, rp)
-% SETSTATE_RP  Set the density and pressure.
-% setState_RP(tp, [density,p])
+function setState_DP(tp, dp)
+% SETSTATE_DP  Set the density and pressure.
+% setState_DP(tp, [density,p])
 % The density is set first, then the pressure is set by
 % changing the temperature holding the density and
 % chemical composition fixed.
@@ -8,9 +8,9 @@ function setState_RP(tp, rp)
 % :param tp:
 %     Instance of class :mat:func:`ThermoPhase` (or another
 %     class derived from ThermoPhase)
-% :param rp:
+% :param dp:
 %     Vector of length 2 containing the desired values for the density (kg/m^3)
 %     and pressure (Pa)
 %
-warning('To be removed after Cantera 3.0. Renamed to setState_DP.')
-thermo_set(tp.tp_id, 26, rp);
+
+thermo_set(tp.tp_id, 26, dp);
