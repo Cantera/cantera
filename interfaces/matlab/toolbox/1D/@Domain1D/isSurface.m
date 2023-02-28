065afc5ea2cb4707dadf733db674073e55fb3586
Ingmar Schoegl <ischoegl@lsu.edu>
2023-02-28 09:59:37 -0600

    [matlab] Replace magic number domain types

--- interfaces/matlab/toolbox/1D/@Domain1D/isSurface.m
+++ interfaces/matlab/toolbox/1D/@Domain1D/isSurface.m
@@ -7,9 +7,5 @@ function a = isSurface(d)
 %     1 if the domain is a surface, and 0 otherwise.
 %
 
-t = domainType(d);
-if t == 102
-    a = 1;
-else
-    a = 0;
+a = int8(strcmp(domainType(d), 'surface'));
 end
