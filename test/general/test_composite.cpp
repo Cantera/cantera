608fdd03ae9334cf9abd34fb232befd0f2f665f8
Ingmar Schoegl <ischoegl@lsu.edu>
2023-03-05 07:56:55 -0600

    [unittest] Add tests for templated Solution getters

--- test/general/test_composite.cpp
+++ test/general/test_composite.cpp
@@ -1,7 +1,30 @@
 #include "gtest/gtest.h"
-#include "cantera/base/Interface.h"
+#include "cantera/core.h"
+#include "cantera/thermo/IdealGasPhase.h"
+#include "cantera/kinetics/GasKinetics.h"
+#include "cantera/transport/MixTransport.h"
 
 using namespace Cantera;
+
+TEST(Solution, valid_cast)
+{
+    auto soln = newSolution("h2o2.yaml", "ohmech");
+    ASSERT_TRUE(soln->thermo<IdealGasPhase>());
+    auto hRT_ref = soln->thermo<IdealGasPhase>()->enthalpy_RT_ref();
+    ASSERT_EQ(hRT_ref.size(), 10);
+    ASSERT_TRUE(soln->kinetics<GasKinetics>());
+    soln->kinetics<GasKinetics>()->updateROP();
+    ASSERT_EQ(soln->kinetics<GasKinetics>()->nReactions(), 29);
+    ASSERT_TRUE(soln->transport<MixTransport>());
+}
+
+TEST(Solution, invalid_cast)
+{
+    auto soln = newSolution("h2o2.yaml", "ohmech", "none");
+    ASSERT_THROW(soln->thermo<SurfPhase>(), CanteraError);
+    ASSERT_THROW(soln->kinetics<InterfaceKinetics>(), CanteraError);
+    ASSERT_THROW(soln->transport<MixTransport>(), CanteraError);
+}
 
 TEST(Interface, incompatible_phase)
 {
