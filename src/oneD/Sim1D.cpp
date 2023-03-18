6d84d7fb038931998a8a5a16201b9a39a84d25c0
Ingmar Schoegl <ischoegl@lsu.edu>
2023-03-18 14:06:58 -0500

    [1D] Use StFlow::fixed_mdot when applicable

--- src/oneD/Sim1D.cpp
+++ src/oneD/Sim1D.cpp
@@ -625,7 +625,7 @@ int Sim1D::setFixedTemperature(double t)
         StFlow* d_free = dynamic_cast<StFlow*>(&domain(n));
         size_t npnow = d.nPoints();
         size_t nstart = znew.size();
-        if (d_free && d_free->isFree()) {
+        if (d_free && !d_free->fixed_mdot()) {
             for (size_t m = 0; m < npnow - 1; m++) {
                 bool fixedpt = false;
                 double t1 = value(n, 2, m);
@@ -703,7 +703,7 @@ double Sim1D::fixedTemperature()
     double t_fixed = std::numeric_limits<double>::quiet_NaN();
     for (size_t n = 0; n < nDomains(); n++) {
         StFlow* d = dynamic_cast<StFlow*>(&domain(n));
-        if (d && d->isFree() && d->m_tfixed > 0) {
+        if (d && !d->fixed_mdot() && d->m_tfixed > 0) {
             t_fixed = d->m_tfixed;
             break;
         }
@@ -716,7 +716,7 @@ double Sim1D::fixedTemperatureLocation()
     double z_fixed = std::numeric_limits<double>::quiet_NaN();
     for (size_t n = 0; n < nDomains(); n++) {
         StFlow* d = dynamic_cast<StFlow*>(&domain(n));
-        if (d && d->isFree() && d->m_tfixed > 0) {
+        if (d && !d->fixed_mdot() && d->m_tfixed > 0) {
             z_fixed = d->m_zfixed;
             break;
         }
