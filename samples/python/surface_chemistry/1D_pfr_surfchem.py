ae84930285717e43a41f6b47859e3ceb34c2bc8e
Ray Speth <speth@mit.edu>
2023-05-08 13:58:03 -0400

    [Examples] Clean up SiF4 surface PFR example

--- samples/python/surface_chemistry/1D_pfr_surfchem.py
+++ samples/python/surface_chemistry/1D_pfr_surfchem.py
@@ -1,116 +1,104 @@
 """
-Solution of a 1-D steady state plug-flow reactor with surface chemistry.
-Assumes:
-    - adiabatic
-    - frictionless
-    - constant area, cylindrical reactor
+A 1-D steady state plug-flow reactor with surface chemistry.
 
-Based off the jupyter notebook created by Yuanjie Jiang
+Assumes an adiabatic, frictionless, constant area, cylindrical reactor.
+
+Based off the jupyter notebook created by Yuanjie Jiang.
+
+Requires: cantera >= 3.0, matplotlib >= 2.0
+Keywords: catalysis, plug flow reactor, reactor network, surface chemistry
 """
-
 
 import numpy as np
 import cantera as ct
 import matplotlib.pyplot as plt
 
-
 mech = 'SiF4_NH3_mec.yaml'
-# import the models for gas and bulk
-gas, bulk_si, bulk_n = ct.import_phases(mech, ['gas', 'SiBulk', 'NBulk'])
-# import the model for gas-Si-N interface
-gas_si_n_interface = ct.Interface(mech, 'SI3N4', [gas, bulk_si, bulk_n])
+
+# import the model for gas-Si-N interface and adjacent bulk phases
+gas_si_n_interface = ct.Interface(mech, 'SI3N4')
+gas = gas_si_n_interface.adjacent['gas']
 
 # Set the initial conditions
-T0 = 1713  # K
-p0 = 2 * ct.one_atm / 760.0  # Pa ~2Torr
-gas.TPX = T0, p0, "NH3:6, SiF4:1"
-bulk_si.TP = T0, p0
-bulk_n.TP = T0, p0
+T0 = 1750  # K
+p0 = 5000 * ct.one_atm / 760.0  # Pa ~2Torr
+gas.TPX = T0, p0, "NH3:1, SiF4:1, N2:4, H2:3"
 gas_si_n_interface.TP = T0, p0
 D = 5.08e-2  # diameter of the tube [m]
 Ac = np.pi * D**2 / 4  # cross section of the tube [m]
-
-# calculate the site fractions of surface species at the entrance of the tube
-# at steady state
 u0 = 11.53  # m/s initial velocity of the flow
 
-
 reactor = ct.FlowReactor(gas, area=Ac)
-# set the mass flow-rate
 reactor.mass_flow_rate = gas.density * u0 * Ac
 
-gas_si_n_interface.advance_coverages(2.0)
 rsurf = ct.ReactorSurface(gas_si_n_interface, reactor)
 net = ct.ReactorNet([reactor])
-soln = ct.SolutionArray(gas, extra=['t', 'speed', 'surf_coverages'])
+soln = ct.SolutionArray(gas, extra=['x', 'speed', 'surf_coverages'])
 
-while net.time < 0.7:
+# Integrate the reactor network. Note that "time" is really the coordinate along
+# the length of the plug flow reactor
+while net.time < 1.2:
     print(net.time, rsurf.coverages)
     net.step()
-    soln.append(T=reactor.thermo.T,
-                P=reactor.thermo.P,
-                X=reactor.thermo.X,
-                t=net.time,
+    soln.append(TDY=reactor.thermo.TDY,
+                x=net.time,
                 speed=reactor.speed,
                 surf_coverages=rsurf.coverages)
 
-f, ax = plt.subplots(4, 2, figsize=(9, 16), dpi=96)
+# Plot the results
+plt.rcParams['figure.constrained_layout.use'] = True
+f, ax = plt.subplots(2, 3, figsize=(9,6))
+
+# plot the temperature profile along the flow direction
+ax[0, 0].plot(soln.x, soln.T[:], color='C3')
+ax[0, 0].set(xlabel='Distance (m)', ylabel='Temperature (K)', title='Bulk properties')
+
+# plot the pressure of the gas along the flow direction
+ax_p = ax[0,0].twinx()
+ax_p.plot(soln.x, soln.P, color='C2')
+ax_p.set(ylabel='Pressure (Pa)')
+ax_p.ticklabel_format(axis='y', style='sci',
+                          scilimits=(-2, 2))  # scientific notation
+
 # plot gas velocity along the flow direction
-ax[0, 0].plot(soln.t, soln.speed, color='C0')
-ax[0, 0].set_xlabel('Distance (m)')
-ax[0, 0].set_ylabel('Velocity (m/s)')
+h_vel = ax[1, 0].plot(soln.x, soln.speed, color='C0', label='velocity')
+ax[1, 0].set(xlabel='Distance (m)', ylabel='Velocity (m/s)')
 
 # plot gas density along the flow direction
-ax[0, 1].plot(soln.t, soln.density, color='C1')
-ax[0, 1].set_xlabel('Distance (m)')
-ax[0, 1].set_ylabel(r'Density ($\mathregular{kg/m^3}$)')
-ax[0, 1].ticklabel_format(axis='y', style='sci',
-                          scilimits=(-2, 2))  # scientific notation
+ax_rho = ax[1,0].twinx()
+h_rho = ax_rho.plot(soln.x, soln.density, color='C1', label='density')
+ax_rho.set(ylabel=r'Density ($\mathregular{kg/m^3}$)')
+ax_rho.legend(handles=h_vel+h_rho)
 
 # plot major and minor gas species separately
 minor_idx = []
 major_idx = []
 for i, name in enumerate(gas.species_names):
     mean = np.mean(soln(name).Y)
-    if mean <= 0.01:
-        minor_idx.append(i)
-    else:
+    if mean >= 0.001:
         major_idx.append(i)
-
-# plot minor gas species along the flow direction
-for i in minor_idx:
-    style = '-' if i < 10 else '--'
-    ax[1, 0].plot(soln.t, soln(gas.species_names[i]).Y,
-                  label=gas.species_names[i], linestyle=style)
-ax[1, 0].legend(fontsize=7.5, loc='best')
-ax[1, 0].set_xlabel('Distance (m)')
-ax[1, 0].set_ylabel('Mass Fraction')
-ax[1, 0].ticklabel_format(axis='y', style='sci',
-                          scilimits=(-2, 2))  # scientific notation
+    elif mean >= 1e-10:
+        minor_idx.append(i)
 
 # plot major gas species along the flow direction
 for j in major_idx:
-    ax[1, 1].plot(soln.t, soln(gas.species_names[j]).Y,
-                  label=gas.species_names[j])
-ax[1, 1].legend(fontsize=8, loc='best')
-ax[1, 1].set_xlabel('Distance (m)')
-ax[1, 1].set_ylabel('Mass Fraction')
+    ax[0, 1].plot(soln.x, soln.Y[:,j], label=gas.species_name(j))
+ax[0, 1].legend(fontsize=8, loc='best')
+ax[0, 1].set(xlabel='Distance (m)', ylabel='Mass Fraction',
+             title='Gas phase major species')
 
-# plot the pressure of the gas along the flow direction
-ax[2, 0].plot(soln.t, soln.P, color='C2')
-ax[2, 0].set_xlabel('Distance (m)')
-ax[2, 0].set_ylabel('Pressure (Pa)')
+# plot minor gas species along the flow direction
+for k, i in enumerate(minor_idx):
+    style = '-' if k < 10 else '--'
+    ax[1, 1].plot(soln.x, soln.Y[:,i], label=gas.species_name(i), linestyle=style)
+ax[1, 1].legend(fontsize=7.5, loc='best')
+ax[1, 1].set(xlabel='Distance (m)', ylabel='Mass Fraction',
+             title='Gas phase minor species')
 
 # plot the site fraction of the surface species along the flow direction
 for i, name in enumerate(gas_si_n_interface.species_names):
-    ax[2, 1].plot(soln.t, soln.surf_coverages[:, i], label=name)
-ax[2, 1].legend(fontsize=8)
-ax[2, 1].set_xlabel('Distance (m)')
-ax[2, 1].set_ylabel('Site Fraction')
+    ax[0, 2].plot(soln.x, soln.surf_coverages[:, i], label=name)
+ax[0, 2].legend(fontsize=8)
+ax[0, 2].set(xlabel='Distance (m)', ylabel='Site Fraction', title='Surface species')
 
-# plot the temperature profile along the flow direction
-ax[3, 0].plot(soln.t, soln.T[:], color='C3')
-ax[3, 0].set_xlabel('Distance (m)')
-ax[3, 0].set_ylabel('Temperature (K)')
-f.tight_layout(pad=0.5)
 plt.show()
