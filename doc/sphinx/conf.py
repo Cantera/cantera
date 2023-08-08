bfc26cfd1d0402a2cb0ea2720c4a37fe3b263688
Ray Speth <speth@mit.edu>
2023-08-07 20:40:32 -0400

    Fix alignment of equations in Sphinx docs
    Co-authored-by: Sammo Gabay <sammolito@gmail.com>

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -222,6 +222,10 @@ html_logo = '_static/images/cantera-logo.png'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    'custom.css',
+]
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
