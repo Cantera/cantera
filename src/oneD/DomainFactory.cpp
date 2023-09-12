44d6b03f73a65b652b114c741cbee1ab8f852b75
Ingmar Schoegl <ischoegl@lsu.edu>
2023-09-11 19:48:56 -0500

    [oneD] Reintroduce StFlow

--- src/oneD/DomainFactory.cpp
+++ src/oneD/DomainFactory.cpp
@@ -7,6 +7,7 @@
 #include "cantera/oneD/Boundary1D.h"
 #include "cantera/oneD/Flow1D.h"
 #include "cantera/oneD/IonFlow.h"
+#include "cantera/oneD/StFlow.h"
 #include "cantera/transport/Transport.h"
 
 namespace Cantera
@@ -40,6 +41,9 @@ DomainFactory::DomainFactory()
     });
     reg("gas-flow", [](shared_ptr<Solution> solution, const string& id) {
         return new Flow1D(solution, id);
+    });
+    reg("legacy-flow", [](shared_ptr<Solution> solution, const string& id) {
+        return new StFlow(solution, id);
     });
     reg("ion-flow", [](shared_ptr<Solution> solution, const string& id) {
         return new IonFlow(solution, id);
