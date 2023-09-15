14f8b8cb3e7306e775572a4dc3503d194f1421c0
Christopher Neal <chrisneal@snumerics.com>
2023-09-15 18:14:16 -0400

    two-point flame control c++ additions

--- src/oneD/StFlow.cpp
+++ src/oneD/StFlow.cpp
@@ -61,11 +61,11 @@ StFlow::StFlow(ThermoPhase* ph, size_t nsp, size_t points) :
 
     //-------------- default solution bounds --------------------
     setBounds(c_offset_U, -1e20, 1e20); // no bounds on u
-    setBounds(c_offset_V, -1e20, 1e20); // V
+    setBounds(c_offset_V, -1e20, 1e20); // no bounds on V
     setBounds(c_offset_T, 200.0, 2*m_thermo->maxTemp()); // temperature bounds
     setBounds(c_offset_L, -1e20, 1e20); // lambda should be negative
-    setBounds(c_offset_E, -1e20, 1e20); // no bounds for inactive component
-
+    setBounds(c_offset_E, -1e20, 1e20); // no bounds on electric field
+    setBounds(c_offset_Uo, -1e20, 1e20); // no bounds on Uo
     // mass fraction bounds
     for (size_t k = 0; k < m_nsp; k++) {
         setBounds(c_offset_Y+k, -1.0e-7, 1.0e5);
@@ -76,6 +76,7 @@ StFlow::StFlow(ThermoPhase* ph, size_t nsp, size_t points) :
     m_refiner->setActive(c_offset_V, false);
     m_refiner->setActive(c_offset_T, false);
     m_refiner->setActive(c_offset_L, false);
+    m_refiner->setActive(c_offset_Uo, false);
 
     vector<double> gr;
     for (size_t ng = 0; ng < m_points; ng++) {
@@ -339,6 +340,7 @@ void StFlow::eval(size_t jGlobal, double* xGlobal, double* rsdGlobal,
     evalEnergy(x, rsd, diag, rdt, jmin, jmax);
     evalLambda(x, rsd, diag, rdt, jmin, jmax);
     evalElectricField(x, rsd, diag, rdt, jmin, jmax);
+    evalUo(x, rsd, diag, rdt, jmin, jmax);
     evalSpecies(x, rsd, diag, rdt, jmin, jmax);
 }
 
@@ -512,9 +514,13 @@ void StFlow::evalLambda(double* x, double* rsd, int* diag,
         }
         return;
     }
-
+ 
     if (jmin == 0) { // left boundary
-        rsd[index(c_offset_L, jmin)] = -rho_u(x, jmin);
+        if (m_twoPointControl) {
+            rsd[index(c_offset_L, jmin)] = lambda(x,1) - lambda(x,0);
+        } else {
+            rsd[index(c_offset_L, jmin)] = -rho_u(x, jmin);
+        }
     }
 
     if (jmax == m_points - 1) { // right boundary
@@ -525,8 +531,19 @@ void StFlow::evalLambda(double* x, double* rsd, int* diag,
     // j0 and j1 are constrained to only interior points
     size_t j0 = std::max<size_t>(jmin, 1);
     size_t j1 = std::min(jmax, m_points - 2);
+    double epsilon = 1e-5; // Precision threshold for being 'equal' to a coordinate
     for (size_t j = j0; j <= j1; j++) { // interior points
-        rsd[index(c_offset_L, j)] = lambda(x, j) - lambda(x, j - 1);
+        if (m_twoPointControl) {
+            if (std::abs(grid(j) - m_zLeft) < epsilon ) {
+                rsd[index(c_offset_L, j)] = T(x,j) - m_tLeft;
+            } else if (grid(j) > m_zLeft) {
+                rsd[index(c_offset_L, j)] = lambda(x,j) - lambda(x,j-1);
+            } else if (grid(j) < m_zLeft) {
+                rsd[index(c_offset_L, j)] = lambda(x,j+1) - lambda(x,j);
+            }
+        } else {
+            rsd[index(c_offset_L, j)] = lambda(x,j) - lambda(x,j-1);
+        }
     }
 }
 
@@ -565,6 +582,50 @@ void StFlow::evalEnergy(double* x, double* rsd, int* diag,
             rsd[index(c_offset_T, j)] = T(x,j) - T_fixed(j);
             diag[index(c_offset_T, j)] = 0;
         }
+    }
+}
+
+void StFlow::evalUo(double* x, double* rsd, int* diag,
+                    double rdt, size_t jmin, size_t jmax)
+{
+    if (!m_twoPointControl) { // disable this equation
+        for (size_t j = jmin; j <= jmax; j++) {
+            rsd[index(c_offset_Uo, j)] = Uo(x, j);
+            diag[index(c_offset_Uo, j)] = 0;
+        }
+        return;
+    }
+
+    if (jmin == 0) { // left boundary
+        rsd[index(c_offset_Uo,jmin)] = Uo(x,jmin+1) - Uo(x,jmin);
+    }
+
+    if (jmax == m_points - 1) { // right boundary
+        if(m_twoPointControl) {
+            rsd[index(c_offset_Uo, jmax)] = Uo(x,jmax) - Uo(x,jmax-1);
+        } else {
+            rsd[index(c_offset_Uo, jmax)] = Uo(x,jmax);
+        }
+        diag[index(c_offset_Uo, jmax)] = 0;
+    }
+
+    // j0 and j1 are constrained to only interior points
+    size_t j0 = std::max<size_t>(jmin, 1);
+    size_t j1 = std::min(jmax, m_points - 2);
+    double epsilon = 1e-5; // Precision threshold for being 'equal' to a coordinate
+    for (size_t j = j0; j <= j1; j++) { // interior points
+        if (m_twoPointControl) {
+            if (std::abs(grid(j) - m_zRight) < epsilon) {
+                rsd[index(c_offset_Uo, j)] = T(x,j) - m_tRight;
+            } else if (grid(j) > m_zRight) {
+                rsd[index(c_offset_Uo, j)] = Uo(x,j) - Uo(x,j-1);
+            } else if (grid(j) < m_zRight) {
+                rsd[index(c_offset_Uo, j)] = Uo(x,j+1) - Uo(x,j);
+            }
+        } else {
+            rsd[index(c_offset_Uo, j)] = Uo(x,j+1) - Uo(x,j);
+        }
+        diag[index(c_offset_Uo, j)] = 0;
     }
 }
 
@@ -740,6 +801,8 @@ string StFlow::componentName(size_t n) const
         return "lambda";
     case c_offset_E:
         return "eField";
+    case c_offset_Uo:
+        return "Uo";
     default:
         if (n >= c_offset_Y && n < (c_offset_Y + m_nsp)) {
             return m_thermo->speciesName(n - c_offset_Y);
@@ -761,6 +824,8 @@ size_t StFlow::componentIndex(const string& name) const
         return c_offset_L;
     } else if (name == "eField") {
         return c_offset_E;
+    } else if (name == "Uo") {
+        return c_offset_Uo;
     } else {
         for (size_t n=c_offset_Y; n<m_nsp+c_offset_Y; n++) {
             if (componentName(n)==name) {
@@ -833,6 +898,15 @@ AnyMap StFlow::getMeta() const
         state["fixed-point"]["location"] = m_zfixed;
         state["fixed-point"]["temperature"] = m_tfixed;
     }
+
+    // // One and two-point control meta data
+    // if (m_twoPointControl) {
+    //     state["point-control"]["type"] = "two-point";
+    //     state["point-control"]["left-location"] = m_zLeft;
+    //     state["point-control"]["right-location"] = m_zRight;
+    //     state["point-control"]["left-temperature"] = m_tLeft;
+    //     state["point-control"]["right-temperature"] = m_tRight;
+    // }
 
     return state;
 }
@@ -960,6 +1034,18 @@ void StFlow::setMeta(const AnyMap& state)
         m_zfixed = state["fixed-point"]["location"].asDouble();
         m_tfixed = state["fixed-point"]["temperature"].asDouble();
     }
+
+    /* // Two-point control meta data
+    if (state.hasKey("point-control")) {
+        const AnyMap& pc = state["point-control"].as<AnyMap>();
+        if (pc["type"] == "two-point") {
+            m_twoPointControl = true;
+            m_zLeft = pc["left-location"].asDouble();
+            m_zRight = pc["right-location"].asDouble();
+            m_tLeft = pc["left-temperature"].asDouble();
+            m_tRight = pc["right-temperature"].asDouble();
+        }
+    } */
 }
 
 void StFlow::solveEnergyEqn(size_t j)
