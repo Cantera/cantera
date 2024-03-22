5077786b02dd1713160873b32e0415d22a49c0c4
Ray Speth <speth@mit.edu>
2024-03-22 13:43:12 -0400

    Revise twin flame example based on Jupyter
    Re-draw figure from Jupyter example as an SVG.
    
    Co-authored-by: Santosh Shanbhogue <santosh.shanbhogue@gmail.com>

--- samples/python/onedim/premixed_counterflow_twin_flame.py
+++ samples/python/onedim/premixed_counterflow_twin_flame.py
@@ -2,14 +2,22 @@
 Symmetric premixed twin flame
 =============================
 
-Simulate two counter-flow jets of reactants shooting into each other. This simulation
-differs from the similar :doc:`premixed_counterflow_flame.py
+Simulate two counter-flow jets of premixed reactants shooting into each other. This
+simulation differs from the similar :doc:`premixed_counterflow_flame.py
 <premixed_counterflow_flame>` example as the latter simulates a jet of reactants
 shooting into products.
 
-Requires: cantera >= 3.0
+Requires: cantera >= 3.0, matplotlib >= 2.0
 
 .. tags:: Python, combustion, 1D flow, premixed flame, strained flame, plotting
+
+An illustration of this configuration is shown in the figure below:
+
+.. image:: /_static/images/samples/twin-premixed-flame.svg
+   :width: 90%
+   :alt: Twin flame established by two opposed jets of fuel + oxidizer mixtures
+   :align: center
+
 """
 
 import sys
@@ -32,58 +40,61 @@ def derivative(x, y):
     return dydx
 
 
-def computeStrainRates(oppFlame):
+def compute_strain_rates(opposed_flame):
     # Compute the derivative of axial velocity to obtain normal strain rate
-    strainRates = derivative(oppFlame.grid, oppFlame.velocity)
+    strain_rates = derivative(opposed_flame.grid, opposed_flame.velocity)
 
     # Obtain the location of the max. strain rate upstream of the pre-heat zone.
     # This is the characteristic strain rate
-    maxStrLocation = abs(strainRates).argmax()
-    minVelocityPoint = oppFlame.velocity[:maxStrLocation].argmin()
+    max_strain_location = abs(strain_rates).argmax()
+    min_velocity_point = opposed_flame.velocity[:max_strain_location].argmin()
 
     # Characteristic Strain Rate = K
-    strainRatePoint = abs(strainRates[:minVelocityPoint]).argmax()
-    K = abs(strainRates[strainRatePoint])
+    strain_rate_point = abs(strain_rates[:min_velocity_point]).argmax()
+    K = abs(strain_rates[strain_rate_point])
 
-    return strainRates, strainRatePoint, K
+    return strain_rates, strain_rate_point, K
 
 
-def computeConsumptionSpeed(oppFlame):
+def compute_consumption_speed(opposed_flame):
+    Tb = max(opposed_flame.T)
+    Tu = min(opposed_flame.T)
+    rho_u = max(opposed_flame.density)
 
-    Tb = max(oppFlame.T)
-    Tu = min(oppFlame.T)
-    rho_u = max(oppFlame.density)
+    integrand = opposed_flame.heat_release_rate / opposed_flame.cp
 
-    integrand = oppFlame.heat_release_rate/oppFlame.cp
-
-    total_heat_release = np.trapz(integrand, oppFlame.grid)
-    Sc = total_heat_release/(Tb - Tu)/rho_u
+    total_heat_release = np.trapz(integrand, opposed_flame.grid)
+    Sc = total_heat_release / (Tb - Tu) / rho_u
 
     return Sc
 
 
 # This function is called to run the solver
-def solveOpposedFlame(oppFlame, massFlux=0.12, loglevel=1,
-                      ratio=2, slope=0.3, curve=0.3, prune=0.05):
+def solve_opposed_flame(opposed_flame, mass_flux=0.12, loglevel=1,
+                        ratio=2, slope=0.3, curve=0.3, prune=0.05):
     """
-    Execute this function to run the Opposed Flow Simulation This function
+    Execute this function to run the Opposed Flow Simulation. This function
     takes a CounterFlowTwinPremixedFlame object as the first argument
     """
 
-    oppFlame.reactants.mdot = massFlux
-    oppFlame.set_refine_criteria(ratio=ratio, slope=slope, curve=curve, prune=prune)
+    opposed_flame.reactants.mdot = mass_flux
+    opposed_flame.set_refine_criteria(ratio=ratio, slope=slope,
+                                      curve=curve, prune=prune)
 
-    oppFlame.show()
-    oppFlame.solve(loglevel, auto=True)
+    opposed_flame.show()
+    opposed_flame.solve(loglevel, auto=True)
 
     # Compute the strain rate, just before the flame. This is not necessarily
     # the maximum We use the max. strain rate just upstream of the pre-heat zone
     # as this is the strain rate that computations compare against, like when
     # plotting Su vs. K
-    strainRates, strainRatePoint, K = computeStrainRates(oppFlame)
+    strain_rates, strain_rate_point, K = compute_strain_rates(opposed_flame)
 
-    return np.max(oppFlame.T), K, strainRatePoint
+    return np.max(opposed_flame.T), K, strain_rate_point
 
+# %%
+# Define parameters
+# -----------------
 
 # Select the reaction mechanism
 gas = ct.Solution('gri30.yaml')
@@ -99,27 +110,39 @@ axial_velocity = 2.0  # in m/s
 # Domain half-width of 2.5 cm, meaning the whole domain is 5 cm wide
 width = 0.025
 
-# Done with initial conditions
+# %%
+# Set up the simulation
+# ---------------------
+
 # Compute the mass flux, as this is what the Flame object requires
-massFlux = gas.density * axial_velocity  # units kg/m2/s
+mass_flux = gas.density * axial_velocity  # units kg/m2/s
 
 # Create the flame object
-oppFlame = ct.CounterflowTwinPremixedFlame(gas, width=width)
+opposed_flame = ct.CounterflowTwinPremixedFlame(gas, width=width)
 
 # Uncomment the following line to use a Multi-component formulation. Default is
 # mixture-averaged
-# oppFlame.transport_model = 'multicomponent'
+# opposed_flame.transport_model = 'multicomponent'
 
-# Now run the solver. The solver returns the peak temperature, strain rate and
-# the point which we ascribe to the characteristic strain rate.
+# %%
+# Run the solver
+# --------------
+#
+# The solver returns the peak temperature, strain rate and the point which we ascribe to
+# the characteristic strain rate.
 
-(T, K, strainRatePoint) = solveOpposedFlame(oppFlame, massFlux, loglevel=1)
+T, K, strain_rate_point = solve_opposed_flame(opposed_flame, mass_flux, loglevel=1)
 
-# You can plot/see all state space variables by calling oppFlame.foo where foo
-# is T, Y[i], etc. The spatial variable (distance in meters) is in oppFlame.grid
-# Thus to plot temperature vs distance, use oppFlame.grid and oppFlame.T
+# %%
+# Compute flame properties
+# ------------------------
+#
+# You can plot/see all state space variables by calling `opposed_flame.foo`, where
+# `foo` is `T`, `Y[i]`, and so forth. The spatial variable (distance in meters) is in
+# `opposed_flame.grid`. Thus to plot temperature vs distance, use `opposed_flame.grid``
+# and `opposed_flame.T`.
 
-Sc = computeConsumptionSpeed(oppFlame)
+Sc = compute_consumption_speed(opposed_flame)
 
 if "native" in ct.hdf_support():
     output = Path() / "premixed_counterflow_twin_flame.h5"
@@ -127,14 +150,18 @@ else:
     output = Path() / "premixed_counterflow_twin_flame.yaml"
 output.unlink(missing_ok=True)
 
-oppFlame.save(output, name="mix")
+opposed_flame.save(output, name="mix")
 
 print(f"Peak temperature: {T:.1f} K")
 print(f"Strain Rate: {K:.1f} 1/s")
 print(f"Consumption Speed: {Sc * 100:.2f} cm/s")
-oppFlame.save("premixed_counterflow_twin_flame.csv", basis="mole", overwrite=True)
+opposed_flame.save("premixed_counterflow_twin_flame.csv", basis="mole", overwrite=True)
 
-# Generate plots to see results, if user desires
+# %%
+# Plot results
+# ------------
+#
+# Generate plots to see results, if desired.
 if '--plot' in sys.argv:
 
     import matplotlib.pyplot as plt
@@ -143,24 +170,24 @@ if '--plot' in sys.argv:
 
     # Axial Velocity Plot
     plt.subplot(1, 2, 1)
-    plt.plot(oppFlame.grid, oppFlame.velocity, 'r', lw=2)
-    plt.xlim(oppFlame.grid[0], oppFlame.grid[-1])
+    plt.plot(opposed_flame.grid, opposed_flame.velocity, 'r', lw=2)
+    plt.xlim(opposed_flame.grid[0], opposed_flame.grid[-1])
     plt.xlabel('Distance (m)')
     plt.ylabel('Axial Velocity (m/s)')
 
     # Identify the point where the strain rate is calculated
-    plt.plot(oppFlame.grid[strainRatePoint],
-             oppFlame.velocity[strainRatePoint], 'gs')
+    plt.plot(opposed_flame.grid[strain_rate_point],
+             opposed_flame.velocity[strain_rate_point], 'gs')
     plt.annotate('Strain-Rate point',
-                 xy=(oppFlame.grid[strainRatePoint],
-                     oppFlame.velocity[strainRatePoint]),
+                 xy=(opposed_flame.grid[strain_rate_point],
+                     opposed_flame.velocity[strain_rate_point]),
                  xytext=(0.001, 0.1),
                  arrowprops={'arrowstyle': '->'})
 
     # Temperature Plot
     plt.subplot(1, 2, 2)
-    plt.plot(oppFlame.grid, oppFlame.T, 'b', lw=2)
-    plt.xlim(oppFlame.grid[0], oppFlame.grid[-1])
+    plt.plot(opposed_flame.grid, opposed_flame.T, 'b', lw=2)
+    plt.xlim(opposed_flame.grid[0], opposed_flame.grid[-1])
     plt.xlabel('Distance (m)')
     plt.ylabel('Temperature (K)')
 
