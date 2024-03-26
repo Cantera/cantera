1fb7b2927073b1dbe349a646f106a36b3474a453
Ray Speth <speth@mit.edu>
2024-03-26 18:57:27 -0400

    Migrate interactive path diagram example from Jupyter
    Co-authored-by: Sai Krishna <sirumalla.s@husky.neu.edu>

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -14,6 +14,7 @@
 import sys, os, re
 from pathlib import Path
 from sphinx_gallery.sorting import ExplicitOrder
+from sphinx_gallery.scrapers import figure_rst
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -46,6 +47,41 @@ extensions = [
               'sphinx_copybutton',
               ]
 
+
+class GraphvizScraper():
+    """
+    Capture Graphviz objects that are assigned to variables in the global namespace.
+    """
+    def __init__(self):
+        # IDs of graphviz objects that have already been seen and processed
+        self.processed = set()
+
+    def __repr__(self):
+        return 'GraphvizScraper'
+
+    def __call__(self, block, block_vars, gallery_conf):
+        import graphviz
+        # We use a list to collect references to image names
+        image_names = list()
+
+        # The `image_path_iterator` is created by Sphinx-Gallery, it will yield
+        # a path to a file name that adheres to Sphinx-Gallery naming convention.
+        image_path_iterator = block_vars['image_path_iterator']
+
+        # Define a list of our already-created figure objects.
+        for obj in block_vars["example_globals"].values():
+            if isinstance(obj, graphviz.Source) and id(obj) not in self.processed:
+                self.processed.add(id(obj))
+                image_path = Path(next(image_path_iterator)).with_suffix(".svg")
+                obj.format = "svg"
+                obj.render(image_path.with_suffix(""))
+                image_names.append(image_path)
+
+        # Use the `figure_rst` helper function to generate the reST for this
+        # code block's figures.
+        return figure_rst(image_names, gallery_conf['src_dir'])
+
+
 sphinx_gallery_conf = {
     'filename_pattern': '\.py',
     'example_extensions': {'.py', '.cpp', '.h', '.c', '.f', '.f90', '.m'},
@@ -54,6 +90,7 @@ sphinx_gallery_conf = {
     'image_srcset': ["2x"],
     'remove_config_comments': True,
     'ignore_repr_types': r'matplotlib\.(text|axes|legend)',
+    'image_scrapers': ('matplotlib', GraphvizScraper()),
     'examples_dirs': [
        '../samples/python/',
        '../samples/cxx/',
