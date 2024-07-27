7d3ae6e517f8e9e5a6c63a4428913b19e283cb04
Ingmar Schoegl <ischoegl@lsu.edu>
2024-07-27 20:01:30 +0200

    [clib] Remove magic number support for Func1
    Remove func_new and skip deprecation as it was only used by the
    legacy MATLAB toolbox.

--- include/cantera/clib/ctfunc.h
+++ include/cantera/clib/ctfunc.h
@@ -14,26 +14,6 @@
 extern "C" {
 #endif
 
-    const int FourierFuncType = 1;
-    const int PolyFuncType = 2;
-    const int ArrheniusFuncType = 3;
-    const int GaussianFuncType = 4;
-    const int SumFuncType = 20;
-    const int DiffFuncType = 25;
-    const int ProdFuncType = 30;
-    const int RatioFuncType = 40;
-    const int PeriodicFuncType = 50;
-    const int CompositeFuncType = 60;
-    const int TimesConstantFuncType = 70;
-    const int PlusConstantFuncType = 80;
-    const int SinFuncType = 100;
-    const int CosFuncType = 102;
-    const int ExpFuncType = 104;
-    const int PowFuncType = 106;
-    const int ConstFuncType = 110;
-    const int TabulatedFuncType = 120;
-
-    CANTERA_CAPI int func_new(int type, size_t n, size_t lenp, const double* p);
     CANTERA_CAPI int func_new_basic(const char* type, double c);
     CANTERA_CAPI int func_new_advanced(const char* type, size_t lenp, const double* p);
     CANTERA_CAPI int func_new_compound(const char* type, int a, int b);
