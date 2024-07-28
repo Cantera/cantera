622b0f787efeb9ba0abc482bb0ff5e0d705239e0
Ingmar Schoegl <ischoegl@lsu.edu>
2024-07-28 11:58:05 +0200

    [MATLAB] Use new Func1 interfaces

--- interfaces/matlab_experimental/Utility/ctString.m
+++ interfaces/matlab_experimental/Utility/ctString.m
@@ -1,16 +1,24 @@
-function output = ctString(varargin)
+function output = ctString(funcName, varargin)
     % Calls Cantera library functions with string outputs and returns
     % errors if necessary.
 
     err1 = -1;
 
-    funcName = varargin{1};
-    buflen = calllib(ctLib, funcName, varargin{2:end}, 0, '');
+    buflen = calllib(ctLib, funcName, varargin{:}, 0, '');
 
     if buflen > 0
         aa = char(ones(1, buflen));
         ptr = libpointer('cstring', aa);
-        [iok, bb] = calllib(ctLib, funcName, varargin{2:end}, buflen, ptr);
+        nchar = sum(cellfun(@ischar, varargin));
+        if nchar == 0
+            % varargin does not contain char array
+            [iok, bb] = calllib(ctLib, funcName, varargin{:}, buflen, ptr);
+        elseif nchar == 1
+            % varargin contains one char array, which MATLAB returns in second place
+            [iok, ~, bb] = calllib(ctLib, funcName, varargin{:}, buflen, ptr);
+        else
+            error('not implemented - argument list contains more than one char array.')
+        end
         output = bb;
         clear aa bb ptr;
     else
