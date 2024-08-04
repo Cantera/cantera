0cb2a15061ab6fabf26aa51b7d59cfa6bd2faa94
Ingmar Schoegl <ischoegl@lsu.edu>
2024-08-04 14:49:21 -0500

    [Func1] Improve newXyzFunction API and add coverage
    Xyz is 'Sum', 'Diff', 'Prod' and 'Ratio'.

--- src/numerics/Func1.cpp
+++ src/numerics/Func1.cpp
@@ -538,6 +538,9 @@ string PlusConstant1::write(const string& arg) const
 
 double Func1::isProportional(TimesConstant1& other)
 {
+    warn_deprecated(
+        "Func1::isProportional",
+        "Deprecated in Cantera 3.1; replaced by internal function.");
     if (isIdentical(*other.func1_shared())) {
         return other.c();
     }
@@ -546,6 +549,9 @@ double Func1::isProportional(TimesConstant1& other)
 
 double Func1::isProportional(Func1& other)
 {
+    warn_deprecated(
+        "Func1::isProportional",
+        "Deprecated in Cantera 3.1; replaced by internal function.");
     if (isIdentical(other)) {
         return 1.0;
     } else {
@@ -585,6 +591,35 @@ bool isPow(const shared_ptr<Func1>& f)
     return f->type() == "pow";
 }
 
+pair<bool, double> isProportional(
+    const shared_ptr<Func1>&f1, const shared_ptr<Func1>&f2)
+{
+    bool tc1 = isTimesConst(f1);
+    bool tc2 = isTimesConst(f2);
+    if (!tc1 && !tc2) {
+        if (f1->isIdentical(*f2)) {
+            return std::make_pair(true, 1.);
+        }
+        return std::make_pair(false, 0.);
+    }
+    if (!tc1 && tc2) {
+        if (f1->isIdentical(*(f2->func1_shared()))) {
+            return std::make_pair(true, f2->c());
+        }
+        return std::make_pair(false, 0.);
+    }
+    if (tc1 && !tc2) {
+        if (f2->isIdentical(*(f1->func1_shared()))) {
+            return std::make_pair(true, 1. / f1->c());
+        }
+        return std::make_pair(false, 0.);
+    }
+    if (f2->func1_shared()->isIdentical(*(f1->func1_shared()))) {
+        return std::make_pair(true, f2->c() / f1->c());
+    }
+    return std::make_pair(false, 0.);
+}
+
 } // end unnamed namespace
 
 shared_ptr<Func1> newSumFunction(shared_ptr<Func1> f1, shared_ptr<Func1> f2)
@@ -598,8 +633,15 @@ shared_ptr<Func1> newSumFunction(shared_ptr<Func1> f1, shared_ptr<Func1> f2)
     if (isZero(f2)) {
         return f1;
     }
-    double c = f1->isProportional(*f2);
-    if (c != 0.) {
+    if (isConstant(f2)) {
+        return newPlusConstFunction(f1, f2->c());
+    }
+    if (isConstant(f1)) {
+        return newPlusConstFunction(f2, f1->c());
+    }
+    auto prop = isProportional(f1, f2);
+    if (prop.first) {
+        double c = prop.second;
         if (c == -1.) {
             return make_shared<Const1>(0.);
         }
@@ -616,17 +658,19 @@ shared_ptr<Func1> newDiffFunction(shared_ptr<Func1> f1, shared_ptr<Func1> f2)
     if (isZero(f1)) {
         return newTimesConstFunction(f2, -1.);
     }
-
     if (f1->isIdentical(*f2)) {
         return make_shared<Const1>(0.);
     }
-    double c = f1->isProportional(*f2);
-    if (c != 0.) {
+    if (isConstant(f2)) {
+        return newPlusConstFunction(f1, -f2->c());
+    }
+    auto prop = isProportional(f1, f2);
+    if (prop.first) {
+        double c = prop.second;
         if (c == 1.0) {
             return make_shared<Const1>(0.);
-        } else {
-            return newTimesConstFunction(f1, 1. - c);
         }
+        return newTimesConstFunction(f1, 1. - c);
     }
     return make_shared<Diff1>(f1, f2);
 }
@@ -692,8 +736,14 @@ shared_ptr<Func1> newRatioFunction(shared_ptr<Func1> f1, shared_ptr<Func1> f2)
     if (isZero(f1)) {
         return make_shared<Const1>(0.);
     }
+    if (isZero(f2)) {
+        throw CanteraError("newRatioFunction", "Division by zero.");
+    }
     if (f1->isIdentical(*f2)) {
         return make_shared<Const1>(1.);
+    }
+    if (isConstant(f2)) {
+        return newTimesConstFunction(f1, 1. / f2->c());
     }
     if (isPow(f1) && isPow(f2)) {
         return make_shared<Pow1>(f1->c() - f2->c());
