250bc53a752554eb2c01519e0faafdb6f1915967
Bryan Weber <bryan.w.weber@gmail.com>
2024-08-14 13:17:48 -0400

    Bump version to 3.1.0a4

--- interfaces/cython/cantera/ctml2yaml.py
+++ interfaces/cython/cantera/ctml2yaml.py
@@ -2637,7 +2637,7 @@ def convert(
     metadata = BlockMap(
         {
             "generator": "ctml2yaml",
-            "cantera-version": "3.1.0a3",
+            "cantera-version": "3.1.0a4",
             "date": formatdate(localtime=True),
         }
     )
