3f9b592e9f59dc5e49fed4bfe5807f933de7b1e7
Ingmar Schoegl <ischoegl@lsu.edu>
2024-08-15 10:30:39 -0500

    [clib] Add doxygen references to ctfunc.h

--- include/cantera/clib/ctfunc.h
+++ include/cantera/clib/ctfunc.h
@@ -17,22 +17,33 @@
 extern "C" {
 #endif
 
-    CANTERA_CAPI int func_check(const char* type, size_t len, char* buf); //!< @since New in %Cantera 3.1
+    //! @implements checkFunc1
+    //! @since New in %Cantera 3.1
+    CANTERA_CAPI int func_check(const char* type, size_t len, char* buf);
+    //! @implements newFunc1(const string&, double)
     CANTERA_CAPI int func_new_basic(const char* type, double c);
+    //! @implements newFunc1(const string&, const vector<double>&)
     CANTERA_CAPI int func_new_advanced(const char* type, size_t lenp, const double* p);
+    //! @implements newFunc1(const string&, const shared_ptr<Func1>, const shared_ptr<Func1>)
     CANTERA_CAPI int func_new_compound(const char* type, int a, int b);
+    //! @implements newFunc1(const string&, const shared_ptr<Func1>, double)
     CANTERA_CAPI int func_new_modified(const char* type, int a, double c);
-    CANTERA_CAPI int func_new_sum(int a, int b);
-    CANTERA_CAPI int func_new_diff(int a, int b);
-    CANTERA_CAPI int func_new_prod(int a, int b);
-    CANTERA_CAPI int func_new_ratio(int a, int b);
+
+    CANTERA_CAPI int func_new_sum(int a, int b);  //!< @implements newSumFunction
+    CANTERA_CAPI int func_new_diff(int a, int b);  //!< @implements newDiffFunction
+    CANTERA_CAPI int func_new_prod(int a, int b);  //!< @implements newProdFunction
+    CANTERA_CAPI int func_new_ratio(int a, int b);  //!< @implements newRatioFunction
     CANTERA_CAPI int func_del(int i);
+
+    //! @implements Func1::type
     CANTERA_CAPI int func_type(int i, size_t lennm, char* nm);
-    CANTERA_CAPI double func_value(int i, double t);
-    CANTERA_CAPI int func_derivative(int i);
+    CANTERA_CAPI double func_value(int i, double t);  //!< @implements Func1::eval
+    CANTERA_CAPI int func_derivative(int i);  //!< @implements Func1::derivative
     CANTERA_CAPI int func_duplicate(int i);
+    //! @implements Func1::write
     //! @since Changed signature in %Cantera 3.1
     CANTERA_CAPI int func_write(int i, const char* arg, size_t lennm, char* nm);
+
     CANTERA_CAPI int ct_clearFunc();
 
 #ifdef __cplusplus
