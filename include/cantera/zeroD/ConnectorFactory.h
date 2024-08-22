ffb07cd58508db4560761bc7355c11b00cc4a0cf
Ingmar Schoegl <ischoegl@lsu.edu>
2024-08-22 09:16:25 -0500

    [zeroD] Implement ConnectorFactory

---
+++ include/cantera/zeroD/ConnectorFactory.h
@@ -0,0 +1,68 @@
+//! @file ConnectorFactory.h
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at https://cantera.org/license.txt for license and copyright information.
+
+#ifndef CONNECTOR_FACTORY_H
+#define CONNECTOR_FACTORY_H
+
+#include "cantera/base/FactoryBase.h"
+#include "cantera/zeroD/ConnectorNode.h"
+
+namespace Cantera
+{
+
+//! Factory class to create ConnectorNode objects.
+//!
+//! This class is mainly used via the newConnectorNode() function, for example:
+//!
+//! ```cpp
+//!     shared_ptr<ConnectorNode> valve = newConnectorNode("Valve", r0, r1, "my_valve");
+//! ```
+//!
+//! where `r0` and `r1` are reactor objects.
+class ConnectorFactory :
+    public Factory<ConnectorNode, shared_ptr<ReactorBase>, shared_ptr<ReactorBase>, const string&>
+{
+public:
+    static ConnectorFactory* factory();
+
+    void deleteFactory() override;
+
+private:
+    static ConnectorFactory* s_factory;
+    static std::mutex connector_mutex;
+    ConnectorFactory();
+};
+
+//! @defgroup connectorGroup Connectors
+//! %ConnectorNode objects connect zero-dimensional reactors.
+//! ConnectorNode objects should be instantiated via the newConnectorNode() function,
+//! for example:
+//!
+//! ```cpp
+//!     shared_ptr<ConnectorNode> valve = newConnectorNode("Valve", r0, r1, "my_valve");
+//! ```
+//!
+//! where `r0` and `r1` are reactor objects.
+//!
+//! @since New in %Cantera 3.1.
+//!
+//! @ingroup zerodGroup
+//! @{
+
+//! Create a %ConnectorNode object of the specified type
+//! @param model  String specifying reactor type.
+//! @param r0  First reactor.
+//! @param r1  Second reactor.
+//! @param name  Name of the connector.
+//! @since New in %Cantera 3.1.
+shared_ptr<ConnectorNode> newConnectorNode(const string& model,
+                                   shared_ptr<ReactorBase> r0,
+                                   shared_ptr<ReactorBase> r1,
+                                   const string& name="(none)");
+
+//! @}
+}
+
+#endif
