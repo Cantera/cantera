e72030a88cf25ba08c7c97de86dc096152dade2c
Christopher Neal <wandadars@gmail.com>
2024-09-26 21:12:35 -0400

    adding test path to python path so github CI will find the utilities module

--- test/python/conftest.py
+++ test/python/conftest.py
@@ -1,6 +1,10 @@
+import sys
+import os
 import pytest
 
+
 # Loads fixtures defined in utilities.py for use in tests
+sys.path.insert(0,os.path.dirname(os.path.abspath(__file__)))
 pytest_plugins = ["utilities"]
 
 pytest.register_assert_rewrite("pint.testing")
