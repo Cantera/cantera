1de40328e2d9973b8b1e5ba345ff6c698e5139dd
pjsingal <pjs2206@columbia.edu>
2024-10-09 14:56:37 -0400

    changed singal2025 to singal2024

--- samples/python/onedim/flame_speed.py
+++ samples/python/onedim/flame_speed.py
@@ -15,7 +15,7 @@ References:
 
     [2] M. U. Alzueta, I. Salas, H. Hashemi, P. Glarborg, CO-assisted NH3 oxidation, Combust. Flame 257 (2023) 112438.
 
-    [3] P. J. Singal, J. Lee, L. Lei, R. L. Speth, M. P. Burke, Implementation of New Mixture Rules Has a Substantial Impact on Combustion Predictions for H2 and NH3, Proc. Combust. Inst. 41 (2025).
+    [3] P. J. Singal, J. Lee, L. Lei, R. L. Speth, M. P. Burke, Implementation of New Mixture Rules Has a Substantial Impact on Combustion Predictions for H2 and NH3, Proc. Combust. Inst. 40 (2024).
 
 Requires: cantera >= 3.1
 .. tags:: flame speed, kinetics
