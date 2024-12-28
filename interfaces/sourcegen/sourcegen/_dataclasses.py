24cf383ed176cd76eee666bd991c59fa6b1c5630
Ingmar Schoegl <ischoegl@lsu.edu>
2024-12-28 15:14:54 -0700

    [sourcegen] Get doxygen tag information

--- interfaces/sourcegen/sourcegen/_dataclasses.py
+++ interfaces/sourcegen/sourcegen/_dataclasses.py
@@ -16,14 +16,35 @@ class Param:
 
     p_type: str
     name: str = ""
+    description: str = ""
+    direction: str = ""
+    default: Any = None
 
     @classmethod
     def from_str(cls, param: str) -> 'Param':
-        """Generate Param from string parameter"""
+        """Generate Param from parameter string."""
+        param = param.strip()
+        default = None
+        if "=" in param:
+            default = param[param.rfind("=")+1:]
+            param = param[:param.rfind("=")]
         parts = param.strip().rsplit(" ", 1)
         if len(parts) == 2 and parts[0] not in ["const", "virtual", "static"]:
-            return cls(*parts)
+            return cls(*parts, "", "", default)
         return cls(param)
+
+    @classmethod
+    def from_xml(cls, param: str) -> 'Param':
+        """Generate Param from XML string."""
+        param = param.strip()
+        replacements = [(" &amp;", "& "), ("&lt; ", "<"), (" &gt;", ">")]
+        for rep in replacements:
+            param = param.replace(*rep)
+        return cls.from_str(param.strip())
+
+    def short_str(self) -> str:
+        """String representation of the parameter without parameter name."""
+        return self.p_type
 
     def long_str(self) -> str:
         """String representation of the parameter with parameter name."""
@@ -34,10 +55,10 @@ class Param:
 
 @dataclass(frozen=True)
 class ArgList:
-    """Represents a function argument list"""
+    """Represents a function argument list."""
 
-    params: List[Param]
-    spec: str = ""  #: trailing Specification (example: `const`)
+    params: list[Param]  #: List of function parameters
+    spec: str = ""  #: Trailing specification (example: `const`)
 
     @staticmethod
     def _split_arglist(arglist: str) -> Tuple[str, str]:
@@ -59,11 +80,24 @@ class ArgList:
             return cls([], spec)
         return cls([Param.from_str(arg) for arg in arglist.split(",")], spec)
 
+    @classmethod
+    def from_xml(cls, arglist: str) -> 'ArgList':
+        """Generate ArgList from XML string argument list."""
+        arglist, spec = cls._split_arglist(arglist)
+        if not arglist:
+            return cls([], spec)
+        return cls([Param.from_xml(arg) for arg in arglist.split(",")], spec)
+
     def __getitem__(self, k):
         return self.params[k]
 
     def __iter__(self) -> "Iterator[Param]":
         return iter(self.params)
+
+    def short_str(self) -> str:
+        """String representation of the argument list without parameter names."""
+        args = ', '.join([par.short_str() for par in self.params])
+        return f"({args}) {self.spec}".strip()
 
     def long_str(self) -> str:
         """String representation of the argument list with parameter names."""
