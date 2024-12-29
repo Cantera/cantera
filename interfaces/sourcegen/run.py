e620113c6409e7a931928e0b02207c8d2b20aaa9
Ingmar Schoegl <ischoegl@lsu.edu>
2024-12-29 15:34:57 -0700

    [sourcegen] Switch sourcegen utility to argparse

--- interfaces/sourcegen/run.py
+++ interfaces/sourcegen/run.py
@@ -2,6 +2,44 @@
 # at https://cantera.org/license.txt for license and copyright information.
 
 import sys
+import argparse
+import textwrap
+
 import sourcegen
 
-sourcegen.generate_source(*sys.argv[1:])
+def main(argv=None):
+    parser = create_argparser()
+    if argv is None and len(sys.argv) < 2:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args(argv)
+    lang = args.api
+    output = args.output
+    sourcegen.generate_source(lang, output)
+
+def create_argparser():
+    parser = argparse.ArgumentParser(
+        description=(
+            "Experimental source generator for creating Cantera interface code"),
+        epilog=textwrap.dedent(
+            """
+            The **sourcegen** utility is invoked as follows::
+
+                python path/to/sourcegen/run.py --api=csharp --output=.
+
+            where the relative path has to be provided as the utility is not installed.
+            """),
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "--api", default="",
+        help=("Language of generated Cantera interface code. Currently supported "
+              "API options are: 'csharp', 'clib' and 'yaml'."))
+    parser.add_argument(
+        "--output", default="",
+        help="Specifies the OUTPUT folder name.")
+
+    return parser
+
+if __name__ == "__main__":
+    main()
