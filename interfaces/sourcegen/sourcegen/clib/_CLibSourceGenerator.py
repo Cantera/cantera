0db3bc4c048f0143afb399a66b000922022b8aea
Ingmar Schoegl <ischoegl@lsu.edu>
2024-12-29 09:37:32 -0700

    [sourcegen] Annotated Func objects

--- interfaces/sourcegen/sourcegen/clib/_CLibSourceGenerator.py
+++ interfaces/sourcegen/sourcegen/clib/_CLibSourceGenerator.py
@@ -11,9 +11,9 @@ from jinja2 import Environment, BaseLoader
 
 from ._Config import Config
 
-from .._dataclasses import HeaderFile, Recipe, Param, ArgList, Func
+from .._dataclasses import HeaderFile, Param, ArgList, CFunc, Recipe
 from .._SourceGenerator import SourceGenerator
-from .._TagFileParser import TagFileParser, TagDetails, tag_lookup
+from .._TagFileParser import TagFileParser
 
 
 _logger = logging.getLogger()
@@ -28,26 +28,23 @@ class CLibSourceGenerator(SourceGenerator):
         block = "\n * ".join(block).strip() + "\n */"
         return "\n".join([line.rstrip() for line in block.split('\n')])
 
-    def _build_annotation(self, details: TagDetails, ret: Param,
-                          params: list[Param], relates: list=[]) -> str:
-        """Build annotation block."""
-        def param(item: Param):
-            ret = par_template.render(par=item)
-            return f"{ret:<20} {item.description or 'Undocumented.'}"
-
+    def _build_annotation(self, c_func: CFunc) -> str:
+        """Build annotation block via jinja."""
         loader = Environment(loader=BaseLoader)
         par_template = loader.from_string(self._templates["clib-param"])
         template = loader.from_string(self._templates["clib-comment"])
-        implements = ""
-        if details.qualified_name:
-            arglist = ArgList.from_xml(details.arglist)
-            implements = f"{details.qualified_name}{arglist.short_str()}"
 
+        def param(item: Param):
+            ret = par_template.render(par=item)
+            return f"{ret:<20} {item.description}"
+
+        implements = ""
+        if isinstance(c_func.implements, CFunc):
+            implements = c_func.implements.short_declaration()
         block = template.render(
-            briefdescription=details.briefdescription,
-            params=[param(par) for par in params],
-            returns=ret.description, implements=implements,
-            relates=relates)
+            briefdescription=c_func.brief,
+            params=[param(par) for par in c_func.arglist],
+            returns=c_func.returns, implements=implements, relates=c_func.relates)
         return self._javadoc_comment(block)
 
     def _handle_crosswalk(self, what: str, crosswalk: dict, derived: list[str]) -> str:
@@ -67,7 +64,7 @@ class CLibSourceGenerator(SourceGenerator):
         logging.critical(f"Failed crosswalk for handle type {what!r} using {classes}.")
         sys.exit(1)
 
-    def _ret_crosswalk(self, what: str, derived: list[str]) -> tuple[Param, list[Param], list[str]]:
+    def _ret_crosswalk(self, what: str, derived: list[str]) -> tuple[Param, list[Param]]:
         """Crosswalk for return type."""
         what = what.replace("virtual ", "")
         if what in self._config.ret_type_crosswalk:
@@ -80,15 +77,14 @@ class CLibSourceGenerator(SourceGenerator):
                 buffer = [
                     Param("int", "lenBuf", "Length of reserved array.", "in"),
                     Param(ret_type, "charBuf", "Returned string value.", "out")]
-                return returns, buffer, []
+                return returns, buffer
             if ret_type == "void":
                 returns = Param(
-                    "int", "",
-                    "Zero for success or -1 for exception handling.")
-                return returns, [], []
+                    "int", "", "Zero for success or -1 for exception handling.")
+                return returns, []
             if not any([what.startswith("shared_ptr"), ret_type.endswith("[]")]):
                 # direct correspondence
-                return Param(ret_type), [], []
+                return Param(ret_type), []
 
             # all other types require reserved buffers
             what = "vector" if what.startswith("vector") else "array"
@@ -97,7 +93,7 @@ class CLibSourceGenerator(SourceGenerator):
             buffer = [
                 Param("int", "lenBuf", "Length of reserved array.", "in"),
                 Param(ret_type, "valueBuf", f"Returned {what} value.", "out")]
-            return returns, buffer, []
+            return returns, buffer
 
         if "shared_ptr" in what:
             # check for crosswalk with object from cabinets
@@ -106,17 +102,16 @@ class CLibSourceGenerator(SourceGenerator):
             returns = Param(
                 "int", "",
                 f"Handle to stored {handle} object or -1 for exception handling.")
-            return returns, [], [handle]
+            return returns, []
 
         logging.critical(f"Failed crosswalk for return type {what!r}.")
         sys.exit(1)
 
-    def _prop_crosswalk(self, par_list: list[Param]) -> tuple[list[Param], list[str]]:
+    def _prop_crosswalk(self, par_list: list[Param]) -> list[Param]:
         """Crosswalk for argument type."""
         if not par_list:
             return [], []
         params = []
-        cabinets = set()
         for par in par_list:
             what = par.p_type
             if what in self._config.prop_type_crosswalk:
@@ -127,75 +122,59 @@ class CLibSourceGenerator(SourceGenerator):
                 ret_type = self._config.prop_type_crosswalk[what]
                 params.append(Param(ret_type, par.name, par.description, par.direction))
             elif "shared_ptr" in what:
-                handle = self._handle_crosswalk(what, self._config.prop_type_crosswalk, [])
-                cabinets |= {handle}
-                params.append(Param("int", par.name, par.description, par.direction))
+                handle = self._handle_crosswalk(
+                    what, self._config.prop_type_crosswalk, [])
+                description = f"Integer handle to {handle} object. {par.description}"
+                params.append(Param("int", par.name, description, par.direction))
             else:
                 logging.critical(f"Failed crosswalk for argument type {what!r}.")
                 sys.exit(1)
-        return params, list[cabinets]
+        return params
 
-    def build_declaration(self, recipe: Recipe) -> tuple[str, str, list[str]]:
-        """Build strings containing declaration and annotation."""
-        def merge_params(implements, details: TagDetails) -> list[Param]:
+    def clib_header(self, recipe: Recipe) -> tuple[str, str, list[str]]:
+        """Build CLib header from recipe and doxygen annotations."""
+        def merge_params(implements, cxx_func: CFunc) -> list[Param]:
             # If class method, add handle as first parameter
             args_merged = []
             if "::" in implements:
                 what = implements.split("::")[0]
                 args_merged.append(
                     Param("int", "handle", f"Handle to queried {what} object."))
-            # Merge parameters from signature, doxygen info and doxygen details
-            args_used = ArgList.from_xml(details.arglist).params  # from doxygen
+            # Combine signature and annotated C++ function
+            args_annotated = cxx_func.arglist.params
             if "(" in implements:
-                args_short = Func.from_str(implements).arglist[:]  # from recipe
-                args_used = args_used[:len(args_short)]
-            args_annotated = details.parameterlist  # from documentation
-            for arg in args_used:
-                for desc in args_annotated:
-                    if arg.name == desc.name:
-                        args_merged.append(
-                            Param(arg.p_type, arg.name,
-                                  desc.description, desc.direction, arg.default))
-                        break
-                else:
-                    args_merged.append(arg)
-            return args_merged
+                args_short = CFunc.from_str(implements).arglist[:]  # from recipe
+                args_annotated = args_annotated[:len(args_short)]
+            return args_merged + args_annotated
 
         if recipe.implements:
             _logger.info(f"    generating {recipe.name!r} -> {recipe.implements}")
-            tag_info = self._doxygen_tags.tag_info(recipe.implements)
-            details = tag_lookup(tag_info)
+            cxx_func = self._doxygen_tags.cxx_func(recipe.implements, recipe.relates)
 
             # convert XML return type to format suitable for crosswalk
-            ret_type = Param.from_xml(details.type).p_type
-            ret_param, buffer_params, cabinets = self._ret_crosswalk(
-                ret_type, recipe.derived)
-            par_list = merge_params(recipe.implements, details)
-            prop_params, prop_cabinets = self._prop_crosswalk(par_list)
-            cabinets += prop_cabinets
+            ret_param, buffer_params = self._ret_crosswalk(
+                cxx_func.ret_type, recipe.derived)
+            par_list = merge_params(recipe.implements, cxx_func)
+            prop_params = self._prop_crosswalk(par_list)
+            brief = cxx_func.brief
             args = prop_params + buffer_params
 
         elif recipe.what == "destructor":
             _logger.info(f"    generating {recipe.name!r} -> destructor")
             args = [Param("int", "handle", f"Handle to {recipe.base} object.")]
-            details = TagDetails(
-                "", "", "", "", "", "", "", f"Delete {recipe.base} object.", args)
+            brief= f"Delete {recipe.base} object."
+            cxx_func = None
             ret_param = Param(
                 "int", "", "Zero for success and -1 for exception handling.")
-            annotations = f"//! {details.briefdescription}"
             buffer_params = []
-            cabinets = [recipe.base]
 
         else:
             _logger.critical(f"Unable to build declaration for {recipe.name!r} "
                              f"with type {recipe.what!r}.")
             sys.exit(1)
 
-        c_func = Func(ret_param.p_type, recipe.name, ArgList(args))
-        declaration = c_func.declaration()
-        annotations = self._build_annotation(details, ret_param, args, recipe.relates)
-
-        return declaration, annotations, cabinets
+        return CFunc(ret_param.p_type, recipe.name, ArgList(args), brief, cxx_func,
+                     ret_param.description)
 
     def _parse_header(self, header: HeaderFile):
         """Parse header file and generate output."""
@@ -204,9 +183,10 @@ class CLibSourceGenerator(SourceGenerator):
         template = loader.from_string(self._templates["clib-definition"])
         declarations = []
         for recipe in header.recipes:
-            declaration, annotations, _ = self.build_declaration(recipe)
+            c_func = self.clib_header(recipe)
             declarations.append(
-                template.render(declaration=declaration, annotations=annotations))
+                template.render(declaration=c_func.declaration(),
+                                annotations=self._build_annotation(c_func)))
 
         filename = header.output_name(suffix=".h", auto="3")
         guard = f"__{filename.name.upper().replace('.', '_')}__"
