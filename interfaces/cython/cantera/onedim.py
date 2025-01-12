0e1e48a11971e9f2494ef58a9cbbbf59bf9a8cdc
Marina <marinakovaleva0@gmail.com>
2025-01-13 00:41:57 +0900

    Added get_species_reaction_sensitivities function to FlameBase class

--- interfaces/cython/cantera/onedim.py
+++ interfaces/cython/cantera/onedim.py
@@ -169,35 +169,35 @@ class FlameBase(Sim1D):
         """
         super().set_profile(self.flame, component, positions, values)
 
-    def get_species_reaction_sensitivities(self, species: str, ix: int):
+    def get_species_reaction_sensitivities(self, species: str, distance: float):
         r"""
-        Compute the normalized sensitivities of a species
-        :math:`S_u` with respect to the reaction rate constants :math:`k_i`:
-
+        Compute the normalized sensitivities of a species,
+        :math:`X_i` with respect to the reaction rate constants :math:`k_i`:
         .. math::
-            s_i = \frac{k_i}{X} \frac{dX}{dk_i}
+            s_i = \frac{k_i}{X_i} \frac{dX_i}{dk_i}
         :param species:
             Species of interest for sensitivity calculation (i.e "NO")
-        :param ix:
-            grid point index (0 is the first, -1 is the last point in the domain)
+        :param distance:
+            Distance value in the flame grid in m
         """
 
         def g(sim):
-            return self.X[self.gas.species_index(species), ix]
+            return sim.X[sim.gas.species_index(species), ix]
 
-        Nvars = sum(D.n_components * D.n_points for D in self.domains)
+        # get index in the grid at distance
+        ix = np.argmin(np.abs(self.flame.grid - distance))
+        n_vars = sum(dd.n_components * dd.n_points for dd in self.domains)
 
-        # Index of X[ix] in the global solution vector
-        i_Su = self.inlet.n_components + self.flame.component_index(species) + self.domains[1].n_components * ix
-        dgdx = np.zeros(Nvars)
-        dgdx[i_Su] = 1
+        i_X = self.flame.component_index(species) + self.domains[1].n_components * ix
 
-        Su0 = g(self)
+        dgdx = np.zeros(n_vars)
+        dgdx[i_X] = 1
+        X0 = g(self)
 
         def perturb(sim, i, dp):
             sim.gas.set_multiplier(1 + dp, i)
 
-        return self.solve_adjoint(perturb, self.gas.n_reactions, dgdx) / Su0
+        return self.solve_adjoint(perturb, self.gas.n_reactions, dgdx) / X0
 
     @property
     def max_grid_points(self):
@@ -786,12 +786,12 @@ class FreeFlame(FlameBase):
         def g(sim):
             return sim.velocity[0]
 
-        Nvars = sum(D.n_components * D.n_points for D in self.domains)
+        n_vars = sum(dd.n_components * dd.n_points for dd in self.domains)
 
         # Index of u[0] in the global solution vector
         i_Su = self.inlet.n_components + self.flame.component_index('velocity')
 
-        dgdx = np.zeros(Nvars)
+        dgdx = np.zeros(n_vars)
         dgdx[i_Su] = 1
 
         Su0 = g(self)
