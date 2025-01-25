be67f33e5f270a5ca06fc6bf8f7891b205eacfd0
Ingmar Schoegl <ischoegl@lsu.edu>
2025-01-25 08:24:34 -0600

    [sourcegen] Implement default constructors

--- src/equil/MultiPhase.cpp
+++ src/equil/MultiPhase.cpp
@@ -839,11 +839,6 @@ void MultiPhase::updatePhases() const
     }
 }
 
-shared_ptr<MultiPhase> newMultiPhase()
-{
-    return make_shared<MultiPhase>();
-}
-
 std::ostream& operator<<(std::ostream& s, MultiPhase& x)
 {
     x.updatePhases();
