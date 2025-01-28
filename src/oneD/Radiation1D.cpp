0ef8e1158edea82cad94263063a482a5ae41eddf
Christopher Neal <wandadars@gmail.com>
2025-01-28 02:32:14 -0500

    updates to the radiation classes and starting to add documentation

--- src/oneD/Radiation1D.cpp
+++ src/oneD/Radiation1D.cpp
@@ -12,38 +12,25 @@
 namespace Cantera
 {
 
-
-Radiation1D::Radiation1D(ThermoPhase* thermo, double pressure, size_t points,
-                         std::function<double(const double*, size_t)> temperatureFunction,
-                         std::function<double(const double*, size_t, size_t)> moleFractionFunction)
-    : m_thermo(thermo), m_press(pressure), m_points(points),
-    m_T(temperatureFunction), m_X(moleFractionFunction)
+TabularPlanckMean::TabularPlanckMean(ThermoPhase* thermo)
+    : m_thermo(thermo)
 {
     parseRadiationData();
 }
 
-void Radiation1D::setBoundaryEmissivities(double e_left, double e_right)
+void TabularPlanckMean::parseRadiationData()
 {
-    if (e_left < 0 || e_left > 1) {
-        throw CanteraError("Radiation1D::setBoundaryEmissivities",
-            "The left boundary emissivity must be between 0.0 and 1.0!");
-    } else if (e_right < 0 || e_right > 1) {
-        throw CanteraError("Radiation1D::setBoundaryEmissivities",
-            "The right boundary emissivity must be between 0.0 and 1.0!");
-    } else {
-        m_epsilon_left = e_left;
-        m_epsilon_right = e_right;
-    }
-}
-
-void Radiation1D::parseRadiationData() {
     AnyMap radiationPropertiesDB;
-    // Search 'crit-properties.yaml' to find Tc and Pc. Load data if needed.
-    if (radiationPropertiesDB.empty()) {
+
+    try {
         radiationPropertiesDB = AnyMap::fromYamlFile("radiation-properties.yaml");
+    } catch (CanteraError& err) {
+        warn_user("TabularPlanckMean::parseRadiationData",
+            "Failed to load 'radiation-properties.yaml':\n{}"
+            "\nFalling back to default polynomial data for CO2, H2O.", err.what());
     }
 
-    if( radiationPropertiesDB.hasKey("PMAC")) {
+    if(!radiationPropertiesDB.empty() && radiationPropertiesDB.hasKey("PMAC")) {
         auto& data = radiationPropertiesDB["PMAC"].as<AnyMap>();
 
         // Needs to loop over only the species that are in the input yaml data
@@ -92,50 +79,29 @@ void Radiation1D::parseRadiationData() {
                 }
             }
         }
+    }
 
-        // Polynomial coefficients for CO2 and H2O (backwards compatibility)
-        // Check if "CO2" is already in the map, if not, add the polynomial fit data
-        if (!m_PMAC.hasKey("CO2")) {
-            const std::vector<double> c_CO2 = {18.741, -121.310, 273.500, -194.050, 56.310,
-                                                -5.8169};
-            m_PMAC["CO2"]["fit-type"] = "polynomial";
-            m_PMAC["CO2"]["coefficients"] = c_CO2;
-        }
+    // Polynomial coefficients for CO2 and H2O (backwards compatibility)
+    // Check if "CO2" is already in the map, if not, add the polynomial fit data
+    if (!m_PMAC.hasKey("CO2")) {
+        const std::vector<double> c_CO2 = {18.741, -121.310, 273.500, -194.050, 56.310,
+                                            -5.8169};
+        m_PMAC["CO2"]["fit-type"] = "polynomial";
+        m_PMAC["CO2"]["coefficients"] = c_CO2;
+    }
 
-        // Check if "H2O" is already in the map, if not, add the polynomial fit data
-        if (!m_PMAC.hasKey("H2O")) {
-            const std::vector<double> c_H2O = {-0.23093, -1.12390, 9.41530, -2.99880,
-                                                0.51382, -1.86840e-5};
-            m_PMAC["H2O"]["fit-type"] = "polynomial";
-            m_PMAC["H2O"]["coefficients"] = c_H2O;
-        }
+    // Check if "H2O" is already in the map, if not, add the polynomial fit data
+    if (!m_PMAC.hasKey("H2O")) {
+        const std::vector<double> c_H2O = {-0.23093, -1.12390, 9.41530, -2.99880,
+                                            0.51382, -1.86840e-5};
+        m_PMAC["H2O"]["fit-type"] = "polynomial";
+        m_PMAC["H2O"]["coefficients"] = c_H2O;
     }
 }
 
-void Radiation1D::computeRadiation(double* x, size_t jmin, size_t jmax, std::vector<double>& qdotRadiation) {
-    const double k_P_ref = 1.0 * OneAtm;
-    const double StefanBoltz = 5.67e-8;
-
-    double boundary_Rad_left = m_epsilon_left * StefanBoltz * std::pow(m_T(x, 0), 4);
-    double boundary_Rad_right = m_epsilon_right * StefanBoltz * std::pow(m_T(x, m_points - 1), 4);
-
-    for (size_t j = jmin; j < jmax; j++) {
-        double k_P = 0;
-        for (const auto& [sp_name, sp_idx] : m_absorptionSpecies) {
-            const auto& fit_type = m_PMAC[sp_name]["fit-type"].asString();
-            if (fit_type == "table") {
-                k_P += m_press * m_X(x, sp_idx, j) *
-                       interpolateTable(m_PMAC[sp_name]["temperatures"].asVector<double>(), m_PMAC[sp_name]["coefficients"].asVector<double>(), m_T(x, j));
-            } else if (fit_type == "polynomial") {
-                k_P += m_press * m_X(x, sp_idx, j) *
-                       calculatePolynomial(m_PMAC[sp_name]["coefficients"].asVector<double>(), m_T(x, j));
-            }
-        }
-        qdotRadiation[j] = 2 * k_P * (2 * StefanBoltz * std::pow(m_T(x, j), 4) - boundary_Rad_left - boundary_Rad_right);
-    }
-}
-
-double Radiation1D::calculatePolynomial(const std::vector<double>& coefficients, double temperature) {
+double TabularPlanckMean::calculatePolynomial(const std::vector<double>& coefficients,
+                                              double temperature)
+{
     double result = 0.0;
     for (size_t n = 0; n < coefficients.size(); ++n) {
         result += coefficients[n] * std::pow(1000 / temperature, static_cast<double>(n));
@@ -143,18 +109,133 @@ double Radiation1D::calculatePolynomial(const std::vector<double>& coefficients,
     return result / (1.0 * OneAtm);
 }
 
-double Radiation1D::interpolateTable(const std::vector<double>& temperatures, const std::vector<double>& data, double temperature) {
-    size_t index = 0;
-    for (size_t i = 1; i < temperatures.size(); ++i) {
-        if (temperature < temperatures[i]) {
-            index = i - 1;
+double TabularPlanckMean::interpolateTable(const std::vector<double>& temperatures,
+                                           const std::vector<double>& data,
+                                           double temperature)
+{
+    // Handle edge cases first
+    if (temperature <= temperatures.front()) {
+        // alpha = 1.0 / data[0]
+        return 1.0 / data.front();
+    } else if (temperature >= temperatures.back()) {
+        // alpha = 1.0 / data[last]
+        return 1.0 / data.back();
+    }
+
+    // Find the interval [t1, t2] where t1 <= T < t2
+    // so that temperatures[i-1] <= T < temperatures[i]
+    size_t idx = 1;
+    for (; idx < temperatures.size(); ++idx) {
+        if (temperature < temperatures[idx]) {
             break;
         }
     }
-    double t1 = temperatures[index], t2 = temperatures[index + 1];
-    double d1 = data[index], d2 = data[index + 1];
-    return d1 + (d2 - d1) * (temperature - t1) / (t2 - t1);
+
+    // Perform linear interpolation
+    double t1 = temperatures[idx - 1];
+    double t2 = temperatures[idx];
+    double v1 = data[idx - 1];
+    double v2 = data[idx];
+
+    // ln(alpha) = ln(1/v1) + ( ln(1/v2) - ln(1/v1) ) * (T - t1)/(t2 - t1)
+    double frac = (temperature - t1) / (t2 - t1);
+    double lnAlpha = log(1.0 / v1) + (log(1.0 / v2) - log(1.0 / v1)) * frac;
+    return exp(lnAlpha) / (1.0 * OneAtm);
 }
+
+void TabularPlanckMean::getBandProperties(std::vector<double>& kabs,
+                                    std::vector<double>& awts,
+                                    const RadComposition& comp)
+{
+    double k_P = 0;
+
+    // Loop over absorbing species
+    for (const auto& [sp_name, sp_idx] : m_absorptionSpecies) {
+        const auto& fit_type = m_PMAC[sp_name]["fit-type"].asString();
+
+        // Get the species mole fraction from the Composition
+        // If the species doesn't exist in comp.X, error out
+        double x_sp = 0;
+        if (comp.X.find(sp_name) == comp.X.end()) {
+            throw CanteraError("TabularPlanckMean::getBandProperties",
+                "Species '{}' not found in composition data", sp_name);
+        } else {
+            x_sp = comp.X.at(sp_name);
+        }
+
+        if (fit_type == "table") {
+            double kVal = interpolateTable(
+                m_PMAC[sp_name]["temperatures"].asVector<double>(),
+                m_PMAC[sp_name]["coefficients"].asVector<double>(),
+                comp.T);
+            k_P += comp.P * x_sp * kVal;
+        } else if (fit_type == "polynomial") {
+            double kVal = calculatePolynomial(
+                m_PMAC[sp_name]["coefficients"].asVector<double>(),
+                comp.T);
+            k_P += comp.P * x_sp * kVal;
+        }
+    }
+
+    // Store the single-band result
+    kabs.resize(1);
+    awts.resize(1);
+    kabs[0] = k_P;
+    awts[0] = 1.0; // single “band” weighting
+}
+
+
+Radiation1D::Radiation1D(ThermoPhase* thermo, double pressure, size_t points,
+                         std::function<double(const double*, size_t)> temperatureFunction,
+                         std::function<double(const double*, size_t, size_t)> moleFractionFunction,
+                         std::unique_ptr<RadiationPropertyCalculator> props,
+                         std::unique_ptr<RadiationSolver> solver)
+    : m_thermo(thermo), m_press(pressure), m_points(points),
+    m_T(temperatureFunction), m_X(moleFractionFunction),
+    m_props(std::move(props)), m_solver(std::move(solver))
+{
+}
+
+void Radiation1D::setBoundaryEmissivities(double e_left, double e_right)
+{
+    if (e_left < 0 || e_left > 1) {
+        throw CanteraError("Radiation1D::setBoundaryEmissivities",
+            "The left boundary emissivity must be between 0.0 and 1.0!");
+    } else if (e_right < 0 || e_right > 1) {
+        throw CanteraError("Radiation1D::setBoundaryEmissivities",
+            "The right boundary emissivity must be between 0.0 and 1.0!");
+    } else {
+        m_epsilon_left = e_left;
+        m_epsilon_right = e_right;
+    }
+}
+
+
+void Radiation1D::computeRadiation(double* x, size_t jmin, size_t jmax,
+                                   std::vector<double>& qdotRadiation) {
+    const double StefanBoltz = 5.67e-8;
+
+    double boundary_Rad_left = m_epsilon_left * StefanBoltz * std::pow(m_T(x, 0), 4);
+    double boundary_Rad_right = m_epsilon_right * StefanBoltz * std::pow(m_T(x, m_points - 1), 4);
+
+    for (size_t j = jmin; j < jmax; j++) {
+        RadComposition comp;
+        comp.T = m_T(x, j);
+        comp.P = m_press;
+        comp.X = m_thermo->getMoleFractionsByName();
+
+        // Get the band absorption coefficients and weighting factors
+        std::vector<double> kabs, awts;
+        m_props->getBandProperties(kabs, awts, comp);
+
+        // Solve for radiative heat loss
+        qdotRadiation[j] = m_solver->computeHeatLoss(kabs, awts, comp.T,
+                                                     boundary_Rad_left,
+                                                     boundary_Rad_right);
+    }
+}
+
+
 
 
 } // namespace Cantera
