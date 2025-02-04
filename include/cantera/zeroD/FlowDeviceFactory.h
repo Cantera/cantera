f3555d40c87a49ff798db0bb0e4659996268f418
Ingmar Schoegl <ischoegl@lsu.edu>
2025-02-03 21:38:50 -0600

    [zeroD] Deprecate Wall/FlowDevice factories

---
+++ include/cantera/zeroD/FlowDeviceFactory.h
@@ -0,0 +1,14 @@
+//! @file FlowDeviceFactory.h
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at https://cantera.org/license.txt for license and copyright information.
+
+#ifndef FLOWDEVICE_FACTORY_H
+#define FLOWDEVICE_FACTORY_H
+
+#pragma message("warning: FlowDeviceFactory.h is deprecated and will be removed " \
+                "after Cantera 3.2. Use ConnectorFactory.h instead.")
+
+#include "ConnectorFactory.h"
+
+#endif
