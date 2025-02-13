6f653f6fa86122d5c2b01e10dfd3ae75da107144
ssun30 <sun.su@northeastern.edu>
2025-02-12 19:32:20 -0500

    [MATLAB] Added test data folder to test paths

--- test/matlab_experimental/ctTestPath.m
+++ test/matlab_experimental/ctTestPath.m
@@ -21,3 +21,4 @@ cantera_root = getenv('CANTERA_ROOT');
 % Add the Cantera toolbox to the Matlab path
 addpath(genpath([cantera_root, '/interfaces/matlab_experimental']));
 addpath(genpath([cantera_root, '/test/matlab_experimental']));
+addpath(genpath([cantera_root, '/test/data']));
