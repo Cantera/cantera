497153084f94651caa03b07f6cd5aa52eeb6b60b
ssun30 <sun.su@northeastern.edu>
2025-02-25 21:30:37 -0500

    [MATLAB] Added Transport Tests

--- test/matlab_experimental/ctTestTransport.m
+++ test/matlab_experimental/ctTestTransport.m
@@ -5,7 +5,8 @@ classdef ctTestTransport < matlab.unittest.TestCase
     end
 
     properties (SetAccess = immutable)
-        rtol = 1e-6;
+        rtol_min = 1e-8;
+        rtol_max = 1e-2;
         atol = 1e-8;
     end
 
@@ -13,8 +14,6 @@ classdef ctTestTransport < matlab.unittest.TestCase
 
         function testSetUp(self)
             ctTestSetUp
-            copyfile('../data/addReactions_err_test.yaml', ...
-                     './addReactions_err_test.yaml');
         end
 
     end
@@ -22,12 +21,23 @@ classdef ctTestTransport < matlab.unittest.TestCase
     methods (TestClassTeardown)
 
         function testTearDown(self)
-            delete('./addReactions_err_test.yaml');
             ctCleanUp
             ctTestTearDown
         end
 
     end
+
+    methods (TestMethodSetup)
+
+        function createPhase(self)
+            src = 'h2o2.yaml';
+            self.phase = Solution(src);
+            self.phase.TPX = {800, 2*OneAtm, ...
+                              [0.1, 1e-4, 1e-5, 0.2, 2e-4, 0.3, 1e-6, 5e-5, 1e-6, 0.4]};
+        end
+
+    end
+
 
     methods (TestMethodTeardown)
 
@@ -37,199 +47,79 @@ classdef ctTestTransport < matlab.unittest.TestCase
 
     end
 
-    methods
-    
-        function createPhase(self, src, tr)
-            self.phase = Solutoin(src, '', tr);
-            self.phase.X = [0.1, 1e-4, 1e-5, 0.2, 2e-4, 0.3, 1e-6, 5e-5, 1e-6, 0.4];
-            self.phase.TP = {800, 2*OneAtm};
-        end
-    
-    end
-
     methods (Test)
 
         function testScalarProperties(self)
-            self.createPhase('h2o2.yaml', 'default');
-
             self.verifyGreaterThan(self.phase.viscosity, 0.0);
             self.verifyGreaterThan(self.phase.thermalConductivity, 0.0);
         end
 
         function testUnityLewis(self)
-            self.createPhase('h2o2.yaml', 'unity-lewis-number');
+            self.phase.transportModel = 'unity-Lewis-number';
 
             alpha = self.phase.thermalConductivity / (self.phase.D * self.phase.cp);
-            Dkm_prime = phase.mixDiffCoeffs;
+            Dkm_prime = self.phase.mixDiffCoeffs;
 
             self.verifyTrue(all(diff(Dkm_prime) < self.atol));
             self.verifyEqual(Dkm_prime(1), alpha, 'AbsTol', self.atol);
         end
 
         function testMixtureAveraged(self)
-            self.createPhase('h2o2.yaml')
+            self.verifyEqual(self.phase.transportModel, 'mixture-averaged');
+
+            Dkm1 = self.phase.mixDiffCoeffs;
+            Dbin1 = self.phase.binDiffCoeffs;
+
+            self.phase.transportModel = 'multicomponent';
+            Dkm2 = self.phase.mixDiffCoeffs;
+            Dbin2 = self.phase.binDiffCoeffs;
+
+            self.verifyEqual(Dkm1, Dkm2, 'AbsTol', self.atol);
+            self.verifyEqual(Dbin1, Dbin2, 'AbsTol', self.atol);
 
         end
 
-        function testReactionEquations(self)
-            self.verifyEqual(self.phase.nReactions, ...
-                             length(self.phase.reactionEquations));
-            s = strsplit(self.phase.reactionEquation(19), '<=>');
-            r = s{1};
-            p = s{2};
-            self.verifySubstring(r, 'H');
-            self.verifySubstring(r, 'H2O2');
-            self.verifySubstring(p, 'HO2');
-            self.verifySubstring(p, 'H2');
-
+        function testMixDiffCoeffsChange(self)
+            Dkm1 = self.phase.mixDiffCoeffs;
+            self.phase.TP = {self.phase.T + 1, self.phase.P};
+            Dkm2 = self.phase.mixDiffCoeffs;
+            self.verifyTrue(all(Dkm2 > Dkm1));
         end
 
-        function testStoichCoeffs(self)
-            nu_r = full(self.phase.reactantStoichCoeffs);
-            nu_p = full(self.phase.productStoichCoeffs);
+        function testCKMode(self)
+            mu_ct = self.phase.viscosity;
+            cond_ct = self.phase.thermalConductivity;
+            diff_ct = self.phase.binDiffCoeffs;
 
-            function checkReactant(s, i, val)
-                k = self.phase.kineticsSpeciesIndex(s);
-                self.verifyEqual(self.phase.reactantStoichCoeffs(s, i), ...
-                                 val);
-                self.verifyEqual(self.phase.reactantStoichCoeffs(k, i), ...
-                                 val);
-                self.verifyEqual(nu_r(k, i), val);
-            end
+            self.phase.transportModel = 'mixture-averaged-CK';
+            self.verifyEqual(self.phase.transportModel, 'mixture-averaged-CK');
 
-            function checkProduct(s, i, val)
-                k = self.phase.kineticsSpeciesIndex(s);
-                self.verifyEqual(self.phase.productStoichCoeffs(s, i), ...
-                                 val);
-                self.verifyEqual(self.phase.productStoichCoeffs(k, i), ...
-                                 val);
-                self.verifyEqual(nu_p(k, i), val);
-            end
+            mu_ck = self.phase.viscosity;
+            cond_ck = self.phase.thermalConductivity;
+            diff_ck = self.phase.binDiffCoeffs;
 
-            % H + H2O2 <=> HO2 + H2
-            checkReactant('H', 19, 1)
-            checkReactant('H2O2', 19, 1)
-            checkReactant('HO2', 19, 0)
-            checkReactant('H2', 19, 0)
-
-            checkProduct('H', 19, 0)
-            checkProduct('H2O2', 19, 0)
-            checkProduct('HO2', 19, 1)
-            checkProduct('H2', 19, 1)
-
-            % 2 O + M <=> O2 + M
-            checkReactant('O', 1, 2)
-            checkReactant('O2', 1, 0)
-            checkProduct('O', 1, 0)
-            checkProduct('O2', 1, 1)
-
+            self.verifyEqual(mu_ct, mu_ck, 'RelTol', self.rtol_max);
+            self.verifyNotEqual(mu_ct, mu_ck);
+            self.verifyEqual(cond_ct, cond_ck, 'RelTol', self.rtol_max);
+            self.verifyNotEqual(cond_ct, cond_ck);
+            self.verifyEqual(diff_ct, diff_ck, 'RelTol', self.rtol_max);
+            self.verifyNotEqual(diff_ct, diff_ck);
         end
 
-        function testRatesOfProgress(self)
-            forROP = self.phase.forwardRatesOfProgress;
-            revROP = self.phase.reverseRatesOfProgress;
-            netROP = self.phase.netRatesOfProgress;
-
-            l = length(netROP);
-            tol = ones(1, l) .* self.atol;
-
-            self.verifyEqual(l, self.phase.nReactions);
-            self.verifyEqual(forROP - revROP, netROP, 'AbsTol', tol);
-        end
-
-        function testRateConstants(self)
-            kf = self.phase.forwardRateConstants;
-            kr = self.phase.reverseRateConstants;
-            Keq = self.phase.equilibriumConstants;
-
-            l = length(kf);
-            self.verifyEqual(l, self.phase.nReactions);
-
-            ix = find(kr ~= 0);
-            tol = ones(1, l) .* self.rtol;
-            self.verifyEqual(kf(ix) ./ kr(ix), Keq(ix), 'RelTol', tol);
-        end
-
-        function testSpeciesRates(self)
-            nu_p = self.phase.productStoichCoeffs;
-            nu_r = self.phase.reactantStoichCoeffs;
-            forROP = self.phase.forwardRatesOfProgress;
-            revROP = self.phase.reverseRatesOfProgress;
-            cr = full(sum(nu_p .* forROP, 2) + sum(nu_r .* revROP, 2))';
-            de = full(sum(nu_r .* forROP, 2) + sum(nu_p .* revROP, 2))';
-
-            l = length(self.phase.nSpecies);
-            tol = ones(1, l) .* self.rtol;
-
-            self.verifyEqual(self.phase.creationRates, cr, 'RelTol', tol);
-            self.verifyEqual(self.phase.destructionRates, de, 'RelTol', tol);
-            self.verifyEqual(self.phase.netProdRates, cr - de, 'RelTol', tol);
-        end
-
-        function testReactionDeltas(self)
-            H = self.phase.deltaEnthalpy;
-            S = self.phase.deltaEntropy;
-            G = self.phase.deltaGibbs;
-            Hs = self.phase.deltaStandardEnthalpy;
-            Ss = self.phase.deltaStandardEntropy;
-            Gs = self.phase.deltaStandardGibbs;
-            T = self.phase.T;
-
-            l = length(H);
-            tol = ones(1, l) .* self.rtol;
-
-            self.verifyEqual(H - S .* T, G, 'RelTol', tol);
-            self.verifyEqual(Hs - Ss .* T, Gs, 'RelTol', tol);
-        end
-
-        function testEmptyKinetics(self)
+        function testMultiComponent(self)
             try
-                gas = Solution('air-no-reactions.yaml');
-                arr = zeros(1, gas.nSpecies);
-                tol = ones(1, gas.nSpecies) .* self.atol;
-
-                self.verifyEqual(gas.nReactions, 0);
-                self.verifyEqual(gas.creationRates, arr, 'AbsTol', tol);
-                self.verifyEqual(gas.destructionRates, arr, 'AbsTol', tol);
-                self.verifyEqual(gas.netProdRates, arr, 'AbsTol', tol);
-
-                clear gas
-
-            catch ME
-                clear gas
-                rethrow(ME);
-            end
-        end
-
-        function testChemicallyActivated(self)
-            try
-                gas = Solution('chemically-activated-reaction.yaml');
-
-                P = [2026.5, 202650.0, 10132500.0];
-                Rf = [2.851022e-04, 2.775924e+00, 2.481792e+03];
-                xx = [0.01, 0.01, 0.04, 0.10, 0.84];
-
-                for i = 1:length(P)
-                    gas.TPX = {900.0, P(i), xx};
-                    self.verifyEqual(gas.forwardRatesOfProgress(1), Rf(i), 'RelTol', 2.0e-05);
-                end
-
-                clear gas
-
-            catch ME
-                clear gas
-                rethrow(ME);
-            end
-        end
-
-        function testPdepError(self)
-            try
-                gas = Solution('addReactions_err_test.yaml');
+                a = self.phase.multiDiffCoeffs
             catch ME
                 self.verifySubstring(ME.identifier, 'Cantera:ctError');
-                self.verifySubstring(ME.message, 'Invalid rate coefficient');
-                clear gas
+                self.verifySubstring(ME.message, 'NotImplementedError');
             end
+
+            self.verifyEqual(self.phase.thermalDiffCoeffs, ...
+                             zeros(1, self.phase.nSpecies), 'AbsTol', self.atol);
+
+            self.phase.transportModel = 'multicomponent';
+            self.verifyTrue(all(self.phase.multiDiffCoeffs(:) >= 0.0));
+            self.verifyTrue(all(self.phase.thermalDiffCoeffs(:) ~= 0.0));
         end
 
     end
---
+++ test/matlab_experimental/ctTestWaterTransportIAPWS95.m
@@ -0,0 +1,89 @@
+classdef ctTestWaterTransportIAPWS95 < matlab.unittest.TestCase
+
+    properties
+        phase
+    end
+
+    methods (TestClassSetup)
+
+        function testSetUp(self)
+            ctTestSetUp
+            copyfile('../data/thermo-models.yaml', './thermal-models.yaml');
+        end
+
+    end
+
+    methods (TestClassTeardown)
+
+        function testTearDown(self)
+            delete('./thermal-models.yaml');
+            ctCleanUp
+            ctTestTearDown
+        end
+
+    end
+
+    methods (TestMethodSetup)
+
+        function createPhase(self)
+            self.phase = Solution('thermal-models.yaml', 'liquid-water');
+        end
+
+    end
+
+    methods (TestMethodTeardown)
+
+        function deleteSolution(self)
+            clear self.phase;
+        end
+
+    end
+
+    properties (TestParameter)
+        TSet1 = {400, 400, 620, 620};
+        PSet1 = {1e6, 8e6, 1.6e7, 2.8e7};
+        muSet1 = {2.1880e-4, 2.2061e-4, 6.7489e-5, 7.5684e-5};
+        rtolSet1 = {2e-4, 2e-4, 1e-4, 1e-4};
+
+        TSet2 = {660, 660, 660};
+        PSet2 = {2.2e7, 2.54e7, 2.8e7};
+        muSet2 = {2.7129e-5, 3.8212e-5, 5.3159e-5};
+        rtolSet2 = {1e-4, 1e-4, 1e-4};
+
+        TSet3 = {400, 400, 620, 620};
+        PSet3 = {1e6, 8e6, 1.6e7, 2.8e7};
+        kSet3 = {0.68410, 0.68836, 0.45458, 0.49705};
+        rtolSet3 = {1e-4, 1e-4, 1e-4, 1e-4};
+
+        TSet4 = {660, 660, 660};
+        PSet4 = {2.2e7, 2.54e7, 2.8e7};
+        kSet4 = {0.14872, 0.35484, 0.38479};
+        rtolSet4 = {1e-4, 1e-4, 1e-4};
+    end
+
+    methods (Test, ParameterCombination = 'sequential')
+
+        function testViscosityLiquid(self, TSet1, PSet1, muSet1, rtolSet1)
+            self.phase.TP = {TSet1, PSet1};
+            self.verifyEqual(self.phase.viscosity, muSet1, 'RelTol', rtolSet1);
+        end
+
+        function testViscositySuperCritical(self, TSet2, PSet2, muSet2, rtolSet2)
+            self.phase.TP = {TSet2, PSet2};
+            self.verifyEqual(self.phase.viscosity, muSet2, 'RelTol', rtolSet2);
+        end
+
+
+        function testThermalConductivityLiquid(self, TSet3, PSet3, kSet3, rtolSet3)
+            self.phase.TP = {TSet3, PSet3};
+            self.verifyEqual(self.phase.thermalConductivity, kSet3, 'RelTol', rtolSet3);
+        end
+
+        function testThermalConductivitySuperCrit(self, TSet4, PSet4, kSet4, rtolSet4)
+            self.phase.TP = {TSet4, PSet4};
+            self.verifyEqual(self.phase.thermalConductivity, kSet4, 'RelTol', rtolSet4);
+        end
+
+    end
+
+end
