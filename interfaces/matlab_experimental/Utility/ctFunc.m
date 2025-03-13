add23fabd0f5a08a1c5fa4422b2d2f08841679bf
ssun30 <sun.su@northeastern.edu>
2025-03-12 21:55:09 -0400

    [MATLAB] Added missing error code for ctFunc

--- interfaces/matlab_experimental/Utility/ctFunc.m
+++ interfaces/matlab_experimental/Utility/ctFunc.m
@@ -2,7 +2,7 @@ function output = ctFunc(varargin)
     % Calls Cantera library functions with single outputs and returns
     % errors if necessary.
 
-    errorcode = [-1, -999.999, double(intmax('uint64'))];
+    errorcode = [-1, -999, -999.999, double(intmax('uint64'))];
 
     funcName = varargin{1};
     output = calllib(ctLib, funcName, varargin{2:end});
