1db0eeb1140b1512a995b03e50f540ee727ee28d
ssun30 <sun.su@northeastern.edu>
2025-03-24 17:28:58 -0400

    [MATLAB] Added code coverage to CI

---
+++ test/matlab_experimental/matlabCoverage.m
@@ -0,0 +1,18 @@
+import matlab.unittest.TestRunner
+import matlab.unittest.plugins.CodeCoveragePlugin
+import matlab.unittest.plugins.codecoverage.CoberturaFormat
+
+sourceFolder = fullfile(pwd, 'interfaces', 'matlab_experimental');
+testFolder = fullfile(pwd, 'test', 'matlab_experimental');
+
+suite = testsuite(testFolder);
+runner = TestRunner.withTextOutput;
+
+reportFormat = CoberturaFormat(fullfile(testFolder, 'matlabCoverage.xml'));
+coveragePlugin = CodeCoveragePlugin.forFolder(sourceFolder, ...
+                                              'IncludingSubfolders', true, ...
+                                              'Producing', reportFormat);
+runner.addPlugin(coveragePlugin);
+
+results = runner.run(suite);
+disp(results);
