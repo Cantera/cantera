cerfacs/cantera-avbp

Merge differences in src directory
 Committer: pestre <pestre@Dante-MacBookAir-ES.local>

 On branch WIP/Merge_cantera-avbp
 Changes to be committed:
	new file:   src/base/Parser.cpp
	modified:   src/base/application.h
	modified:   src/clib/ctonedim.cpp
	modified:   src/extensions/pythonShim.cpp
	new file:   src/kinetics/CustomKinetics.cpp
	modified:   src/kinetics/Kinetics.cpp
	modified:   src/kinetics/KineticsFactory.cpp
	modified:   src/oneD/Boundary1D.cpp
	modified:   src/oneD/Domain1D.cpp
	modified:   src/oneD/DomainFactory.cpp
	new file:   src/oneD/Flamelet.cpp
	modified:   src/oneD/IonFlow.cpp
	modified:   src/oneD/Sim1D.cpp
	modified:   src/oneD/StFlow.cpp
	modified:   src/oneD/refine.cpp
	new file:   src/transport/AVBPTransport.cpp
	modified:   src/transport/TransportFactory.cpp

src/oneD/Boundary1D.cpp
@@ -36,11 +36,21 @@ void Boundary1D::_init(size_t n)
         Domain1D& r = container().domain(m_index-1);
         if (!r.isConnector()) { // multi-point domain
             m_left_nv = r.nComponents();
-            if (m_left_nv > c_offset_Y) {
-                m_left_nsp = m_left_nv - c_offset_Y;
-            } else {
-                m_left_nsp = 0;
+
+            if (m_type == cFlameletFlow) { 
+                if (m_left_nv > c_offset_Yflamelet) {
+                    m_left_nsp = m_left_nv - c_offset_Yflamelet;
+                } else {
+                    m_left_nsp = 0;
+                }
+            }else{
+                if (m_left_nv > c_offset_Y) {
+                    m_left_nsp = m_left_nv - c_offset_Y;
+                } else {
+                    m_left_nsp = 0;
+                }
             }
+
             m_left_loc = container().start(m_index-1);
             m_left_points = r.nPoints();
             m_flow_left = dynamic_cast<StFlow*>(&r);
@@ -59,10 +69,19 @@ void Boundary1D::_init(size_t n)
         Domain1D& r = container().domain(m_index+1);
         if (!r.isConnector()) { // multi-point domain
             m_right_nv = r.nComponents();
-            if (m_right_nv > c_offset_Y) {
-                m_right_nsp = m_right_nv - c_offset_Y;
-            } else {
-                m_right_nsp = 0;
+
+            if (m_type == cFlameletFlow) {  
+                if (m_right_nv > c_offset_Yflamelet) {
+                    m_right_nsp = m_right_nv - c_offset_Yflamelet;
+                } else {
+                    m_right_nsp = 0;
+                }
+            }else{
+                if (m_right_nv > c_offset_Y) {
+                    m_right_nsp = m_right_nv - c_offset_Y;
+                } else {
+                    m_right_nsp = 0;
+                }
             }
             m_right_loc = container().start(m_index+1);
             m_flow_right = dynamic_cast<StFlow*>(&r);
@@ -140,6 +159,25 @@ void Inlet1D::setMoleFractions(const double* xin)
     }
 }
 
+void Inlet1D::setMassFractions(const std::string& xin)
+{
+    m_xstr = xin;
+    if (m_flow) {
+        m_flow->phase().setMassFractionsByName(xin);
+        m_flow->phase().getMassFractions(m_yin.data());
+        needJacUpdate();
+    }
+}
+
+void Inlet1D::setMassFractions(const double* xin)
+{
+    if (m_flow) {
+        m_flow->phase().setMassFractions(xin);
+        m_flow->phase().getMassFractions(m_yin.data());
+        needJacUpdate();
+    }
+}
+
 void Inlet1D::init()
 {
     _init(0);
@@ -184,57 +222,88 @@ void Inlet1D::eval(size_t jg, double* xg, double* rg,
         double* xb = xg + m_flow->loc();
         double* rb = rg + m_flow->loc();
 
-        // The first flow residual is for u. This, however, is not modified by
-        // the inlet, since this is set within the flow domain from the
-        // continuity equation.
+        if ( m_flow->nEq() == 1 ) { // Solving in mixture fraction space
+	        if (m_flow->doEnergy(0)) {
+                // The third flow residual is for T, where it is set to T(0).  Subtract
+                // the local temperature to hold the flow T to the inlet T.
+                rb[c_offset_Tflamelet] -= m_temp;
+            } else {
+                rb[c_offset_Tflamelet] -= m_flow->T_fixed(0);
+            }
+	        for (size_t k = 0; k < m_nsp; k++) {
+                if (k != m_flow_right->leftExcessSpecies()) {
+                    rb[c_offset_Yflamelet+k] -= m_yin[k];	
+              }
+	        } 
+        } else { // Phisical space
+            // The first flow residual is for u. This, however, is not modified by
+            // the inlet, since this is set within the flow domain from the
+            // continuity equation.
 
-        if (m_flow->doEnergy(0)) {
-            // The third flow residual is for T, where it is set to T(0).  Subtract
-            // the local temperature to hold the flow T to the inlet T.
-            rb[c_offset_T] -= m_temp;
-        } else {
-            rb[c_offset_T] -= m_flow->T_fixed(0);
-        }
+            if (m_flow->doEnergy(0)) {
+                // The third flow residual is for T, where it is set to T(0).  Subtract
+                // the local temperature to hold the flow T to the inlet T.
+                rb[c_offset_T] -= m_temp;
+            } else {
+                rb[c_offset_T] -= m_flow->T_fixed(0);
+            }
 
-        if (m_flow->isFree()) {
-            // if the flow is a freely-propagating flame, mdot is not specified.
-            // Set mdot equal to rho*u, and also set lambda to zero.
-            m_mdot = m_flow->density(0) * xb[c_offset_U];
-            rb[c_offset_L] = xb[c_offset_L];
-        } else if (m_flow->isStrained()) {
-            // The flow domain sets this to -rho*u. Add mdot to specify the mass
-            // flow rate
-            rb[c_offset_L] += m_mdot;
+            if (m_flow->isFree()) {
+                // if the flow is a freely-propagating flame, mdot is not specified.
+                // Set mdot equal to rho*u, and also set lambda to zero.
+                m_mdot = m_flow->density(0) * xb[c_offset_U];
+                rb[c_offset_L] = xb[c_offset_L];
+            } else if (m_flow->isStrained()) {
+                // The flow domain sets this to -rho*u. Add mdot to specify the mass
+                // flow rate
+                rb[c_offset_L] += m_mdot;
 
-            // spreading rate. The flow domain sets this to V(0),
-            // so for finite spreading rate subtract m_V0.
-            rb[c_offset_V] -= m_V0;
-        } else {
-            rb[c_offset_U] = m_flow->density(0) * xb[c_offset_U] - m_mdot;
-            rb[c_offset_L] = xb[c_offset_L];
-        }
+                // spreading rate. The flow domain sets this to V(0),
+                // so for finite spreading rate subtract m_V0.
+                rb[c_offset_V] -= m_V0;
+            } else {
+                rb[c_offset_U] = m_flow->density(0) * xb[c_offset_U] - m_mdot;
+                rb[c_offset_L] = xb[c_offset_L];
+            }
 
-        // add the convective term to the species residual equations
-        for (size_t k = 0; k < m_nsp; k++) {
-            if (k != m_flow_right->leftExcessSpecies()) {
-                rb[c_offset_Y+k] += m_mdot*m_yin[k];
+            // add the convective term to the species residual equations
+            for (size_t k = 0; k < m_nsp; k++) {
+                if (k != m_flow_right->leftExcessSpecies()) {
+                    rb[c_offset_Y+k] += m_mdot*m_yin[k];
+                }
             }
         }
 
     } else {
         // right inlet (should only be used for counter-flow flames)
         // Array elements corresponding to the last point in the flow domain
+
         double* rb = rg + loc() - m_flow->nComponents();
-        rb[c_offset_V] -= m_V0;
-        if (m_flow->doEnergy(m_flow->nPoints() - 1)) {
-            rb[c_offset_T] -= m_temp; // T
-        } else {
-            rb[c_offset_T] -= m_flow->T_fixed(m_flow->nPoints() - 1);
-        }
-        rb[c_offset_U] += m_mdot; // u
-        for (size_t k = 0; k < m_nsp; k++) {
-            if (k != m_flow_left->rightExcessSpecies()) {
-                rb[c_offset_Y+k] += m_mdot * m_yin[k];
+
+        if (m_flow->nEq() == 1 ) { // mixture fraction space
+            if (m_flow->doEnergy(m_flow->nPoints() - 1)) {
+                rb[c_offset_Tflamelet] -= m_temp; // T
+            } else {
+                rb[c_offset_Tflamelet] -= m_flow->T_fixed(m_flow->nPoints() - 1);
+            }
+            for (size_t k = 0; k < m_nsp; k++) {
+                    if (k != m_flow_left->rightExcessSpecies()) {
+                    rb[c_offset_Yflamelet+k] -= m_yin[k];
+                    }
+            }
+        } else { // physical space
+
+            rb[c_offset_V] -= m_V0;
+            if (m_flow->doEnergy(m_flow->nPoints() - 1)) {
+                rb[c_offset_T] -= m_temp; // T
+            } else {
+                rb[c_offset_T] -= m_flow->T_fixed(m_flow->nPoints() - 1);
+            }
+            rb[c_offset_U] += m_mdot; // u
+            for (size_t k = 0; k < m_nsp; k++) {
+                if (k != m_flow_left->rightExcessSpecies()) {
+                    rb[c_offset_Y+k] += m_mdot * m_yin[k];
+                }
             }
         }
     }
@@ -392,10 +461,18 @@ void Outlet1D::eval(size_t jg, double* xg, double* rg, integer* diagg,
     int* db = diag - nc;
 
     size_t last = m_flow_left->nPoints() - 1;
-    if (m_flow_left->doEnergy(last)) {
-        rb[c_offset_T] = xb[c_offset_T] - xb[c_offset_T - nc]; // zero T gradient
+    if (m_flow_left->nEq() == 1) { // mixture fraction space 
+        if (m_flow_left->doEnergy(last)) {
+            rb[c_offset_Tflamelet] = xb[c_offset_Tflamelet] - xb[c_offset_Tflamelet - nc]; // zero T gradient
+        } else {
+            rb[c_offset_Tflamelet] = xb[c_offset_Tflamelet] - m_flow_left->T_fixed(last);
+        }
     } else {
-        rb[c_offset_T] = xb[c_offset_T] - m_flow_left->T_fixed(last);
+        if (m_flow_left->doEnergy(last)) {
+            rb[c_offset_T] = xb[c_offset_T] - xb[c_offset_T - nc]; // zero T gradient
+        } else {
+            rb[c_offset_T] = xb[c_offset_T] - m_flow_left->T_fixed(last);
+        }
     }
     size_t kSkip = c_offset_Y + m_flow_left->rightExcessSpecies();
     for (size_t k = c_offset_Y; k < nc; k++) {

src/oneD/DomainFactory.cpp
@@ -6,6 +6,7 @@
 #include "cantera/oneD/DomainFactory.h"
 #include "cantera/oneD/Boundary1D.h"
 #include "cantera/oneD/StFlow.h"
+#include "cantera/oneD/Flamelet.h"
 #include "cantera/oneD/IonFlow.h"
 #include "cantera/transport/Transport.h"
 
@@ -17,61 +18,71 @@ std::mutex DomainFactory::domain_mutex;
 
 DomainFactory::DomainFactory()
 {
-    reg("inlet", [](shared_ptr<Solution> solution, const string& id) {
+    reg("inlet", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Inlet1D(solution, id);
     });
-    reg("empty", [](shared_ptr<Solution> solution, const string& id) {
+    reg("empty", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Empty1D(solution, id);
     });
-    reg("symmetry-plane", [](shared_ptr<Solution> solution, const string& id) {
+    reg("symmetry-plane", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Symm1D(solution, id);
     });
-    reg("outlet", [](shared_ptr<Solution> solution, const string& id) {
+    reg("outlet", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Outlet1D(solution, id);
     });
-    reg("outlet-reservoir", [](shared_ptr<Solution> solution, const string& id) {
+    reg("outlet-reservoir", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new OutletRes1D(solution, id);
     });
-    reg("surface", [](shared_ptr<Solution> solution, const string& id) {
+    reg("surface", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Surf1D(solution, id);
     });
-    reg("reacting-surface", [](shared_ptr<Solution> solution, const string& id) {
+    reg("reacting-surface", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new ReactingSurf1D(solution, id);
     });
-    reg("gas-flow", [](shared_ptr<Solution> solution, const string& id) {
-        return new StFlow(solution, id);
+    reg("gas-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
+        return new StFlow(solution, id, sections);
     });
-    reg("ion-flow", [](shared_ptr<Solution> solution, const string& id) {
+    reg("ion-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new IonFlow(solution, id);
     });
-    reg("free-flow", [](shared_ptr<Solution> solution, const string& id) {
+    reg("free-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         StFlow* ret;
         if (solution->transport()->transportModel() == "ionized-gas") {
             ret = new IonFlow(solution, id);
         } else {
-            ret = new StFlow(solution, id);
+            ret = new StFlow(solution, id, sections);
         }
         ret->setFreeFlow();
         return ret;
     });
-    reg("axisymmetric-flow", [](shared_ptr<Solution> solution, const string& id) {
+    reg("axisymmetric-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         StFlow* ret;
         if (solution->transport()->transportModel() == "ionized-gas") {
             ret = new IonFlow(solution, id);
         } else {
-            ret = new StFlow(solution, id);
+            ret = new StFlow(solution, id, sections);
         }
         ret->setAxisymmetricFlow();
         return ret;
     });
-    reg("unstrained-flow", [](shared_ptr<Solution> solution, const string& id) {
+    reg("unstrained-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         StFlow* ret;
         if (solution->transport()->transportModel() == "ionized-gas") {
             ret = new IonFlow(solution, id);
         } else {
-            ret = new StFlow(solution, id);
+            ret = new StFlow(solution, id, sections);
         }
         ret->setUnstrainedFlow();
+        return ret;
+    });
+    reg("flamelet-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
+        StFlow* ret;
+        if (solution->transport()->transportModel() == "ionized-gas") {
+            ret = new IonFlow(solution, id);
+        } else {
+            ret = new Flamelet(solution, id, sections); // neq = 1 for flamelet
+        }
+        ret->setFlameletFlow();
         return ret;
     });
 }

Cantera/cantera

 Conflicts:
	src/extensions/pythonShim.cpp
 Author:    pestre <pestre@Dante-MacBookAir-ES.local>
 Date:      Thu Jan 30 12:05:19 2025 +0100
 On branch WIP/merge_CERFACS_devs
 You are currently cherry-picking commit 22c89db5c.
 interactive rebase in progress; onto 7aec821a8
 Last commands done (6 commands done):
    pick 925ed423a Merge differences in samples directory
    pick 68a09c47e Merge differences in src directory
 Next commands to do (8 remaining commands):
    pick f298a7d4e Merge differences in test directory
    pick e118cf6c4 Merge differences for other files
 You are currently rebasing branch 'dev_cantera-avbp' on '7aec821a8'.

src/oneD/Boundary1D.cpp
@@ -35,11 +35,21 @@ void Boundary1D::_init(size_t n)
         Domain1D& r = container().domain(m_index-1);
         if (!r.isConnector()) { // multi-point domain
             m_left_nv = r.nComponents();
-            if (m_left_nv > c_offset_Y) {
-                m_left_nsp = m_left_nv - c_offset_Y;
-            } else {
-                m_left_nsp = 0;
+
+            if (m_type == cFlameletFlow) { 
+                if (m_left_nv > c_offset_Yflamelet) {
+                    m_left_nsp = m_left_nv - c_offset_Yflamelet;
+                } else {
+                    m_left_nsp = 0;
+                }
+            }else{
+                if (m_left_nv > c_offset_Y) {
+                    m_left_nsp = m_left_nv - c_offset_Y;
+                } else {
+                    m_left_nsp = 0;
+                }
             }
+
             m_left_loc = container().start(m_index-1);
             m_left_points = r.nPoints();
             m_flow_left = dynamic_cast<Flow1D*>(&r);
@@ -58,10 +68,19 @@ void Boundary1D::_init(size_t n)
         Domain1D& r = container().domain(m_index+1);
         if (!r.isConnector()) { // multi-point domain
             m_right_nv = r.nComponents();
-            if (m_right_nv > c_offset_Y) {
-                m_right_nsp = m_right_nv - c_offset_Y;
-            } else {
-                m_right_nsp = 0;
+
+            if (m_type == cFlameletFlow) {  
+                if (m_right_nv > c_offset_Yflamelet) {
+                    m_right_nsp = m_right_nv - c_offset_Yflamelet;
+                } else {
+                    m_right_nsp = 0;
+                }
+            }else{
+                if (m_right_nv > c_offset_Y) {
+                    m_right_nsp = m_right_nv - c_offset_Y;
+                } else {
+                    m_right_nsp = 0;
+                }
             }
             m_right_loc = container().start(m_index+1);
             m_flow_right = dynamic_cast<Flow1D*>(&r);
@@ -146,6 +165,25 @@ void Inlet1D::setMoleFractions(const double* xin)
     }
 }
 
+void Inlet1D::setMassFractions(const std::string& xin)
+{
+    m_xstr = xin;
+    if (m_flow) {
+        m_flow->phase().setMassFractionsByName(xin);
+        m_flow->phase().getMassFractions(m_yin.data());
+        needJacUpdate();
+    }
+}
+
+void Inlet1D::setMassFractions(const double* xin)
+{
+    if (m_flow) {
+        m_flow->phase().setMassFractions(xin);
+        m_flow->phase().getMassFractions(m_yin.data());
+        needJacUpdate();
+    }
+}
+
 void Inlet1D::init()
 {
     _init(0);
@@ -191,17 +229,31 @@ void Inlet1D::eval(size_t jg, double* xg, double* rg,
         double* xb = xg + m_flow->loc();
         double* rb = rg + m_flow->loc();
 
-        // The first flow residual is for u. This, however, is not modified by
-        // the inlet, since this is set within the flow domain from the
-        // continuity equation.
+        if ( m_flow->nEq() == 1 ) { // Solving in mixture fraction space
+	        if (m_flow->doEnergy(0)) {
+                // The third flow residual is for T, where it is set to T(0).  Subtract
+                // the local temperature to hold the flow T to the inlet T.
+                rb[c_offset_Tflamelet] -= m_temp;
+            } else {
+                rb[c_offset_Tflamelet] -= m_flow->T_fixed(0);
+            }
+	        for (size_t k = 0; k < m_nsp; k++) {
+                if (k != m_flow_right->leftExcessSpecies()) {
+                    rb[c_offset_Yflamelet+k] -= m_yin[k];	
+              }
+	        } 
+        } else { // Phisical space
+            // The first flow residual is for u. This, however, is not modified by
+            // the inlet, since this is set within the flow domain from the
+            // continuity equation.
 
-        if (m_flow->doEnergy(0)) {
-            // The third flow residual is for T, where it is set to T(0).  Subtract
-            // the local temperature to hold the flow T to the inlet T.
-            rb[c_offset_T] -= m_temp;
-        } else {
-            rb[c_offset_T] -= m_flow->T_fixed(0);
-        }
+            if (m_flow->doEnergy(0)) {
+                // The third flow residual is for T, where it is set to T(0).  Subtract
+                // the local temperature to hold the flow T to the inlet T.
+                rb[c_offset_T] -= m_temp;
+            } else {
+                rb[c_offset_T] -= m_flow->T_fixed(0);
+            }
 
         if (m_flow->isFree()) {
             // if the flow is a freely-propagating flame, mdot is not specified.
@@ -229,39 +281,54 @@ void Inlet1D::eval(size_t jg, double* xg, double* rg,
             rb[c_offset_U] = m_flow->density(0) * xb[c_offset_U] - m_mdot;
         }
 
-        // add the convective term to the species residual equations
-        for (size_t k = 0; k < m_nsp; k++) {
-            if (k != m_flow_right->leftExcessSpecies()) {
-                rb[c_offset_Y+k] += m_mdot*m_yin[k];
+            // add the convective term to the species residual equations
+            for (size_t k = 0; k < m_nsp; k++) {
+                if (k != m_flow_right->leftExcessSpecies()) {
+                    rb[c_offset_Y+k] += m_mdot*m_yin[k];
+                }
             }
         }
 
     } else {
         // right inlet (should only be used for counter-flow flames)
         // Array elements corresponding to the last point in the flow domain
+
         double* rb = rg + loc() - m_flow->nComponents();
         double* xb = xg + loc() - m_flow->nComponents();
         size_t last_index = m_flow->nPoints() - 1;
 
+        if (m_flow->nEq() == 1 ) { // mixture fraction space
+            if (m_flow->doEnergy(m_flow->nPoints() - 1)) {
+                rb[c_offset_Tflamelet] -= m_temp; // T
+            } else {
+                rb[c_offset_Tflamelet] -= m_flow->T_fixed(m_flow->nPoints() - 1);
+            }
+            for (size_t k = 0; k < m_nsp; k++) {
+                    if (k != m_flow_left->rightExcessSpecies()) {
+                    rb[c_offset_Yflamelet+k] -= m_yin[k];
+                    }
+            }
+        } else { // physical space
         rb[c_offset_V] -= m_V0;
-        if (m_flow->doEnergy(m_flow->nPoints() - 1)) {
-            rb[c_offset_T] -= m_temp; // T
-        } else {
-            rb[c_offset_T] -= m_flow->T_fixed(m_flow->nPoints() - 1);
-        }
+            if (m_flow->doEnergy(m_flow->nPoints() - 1)) {
+                rb[c_offset_T] -= m_temp; // T
+            } else {
+                rb[c_offset_T] -= m_flow->T_fixed(m_flow->nPoints() - 1);
+            }
 
-        if (m_flow->twoPointControlEnabled()) { // For point control adjustments
-            // At the right boundary, the mdot is dictated by the velocity at the right
-            // boundary, which comes from the Uo variable. The variable Uo is the
-            // left-moving velocity and has a negative value, so the mass flow has to be
-            // negated to give a positive value when using Uo.
-            m_mdot = -m_flow->density(last_index) * xb[c_offset_Uo];
-        }
-        rb[c_offset_U] += m_mdot;
+            if (m_flow->twoPointControlEnabled()) { // For point control adjustments
+                // At the right boundary, the mdot is dictated by the velocity at the right
+                // boundary, which comes from the Uo variable. The variable Uo is the
+                // left-moving velocity and has a negative value, so the mass flow has to be
+                // negated to give a positive value when using Uo.
+                m_mdot = -m_flow->density(last_index) * xb[c_offset_Uo];
+            }
+            rb[c_offset_U] += m_mdot;
 
-        for (size_t k = 0; k < m_nsp; k++) {
-            if (k != m_flow_left->rightExcessSpecies()) {
-                rb[c_offset_Y+k] += m_mdot * m_yin[k];
+            for (size_t k = 0; k < m_nsp; k++) {
+                if (k != m_flow_left->rightExcessSpecies()) {
+                    rb[c_offset_Y+k] += m_mdot * m_yin[k];
+                }
             }
         }
     }
@@ -418,10 +485,18 @@ void Outlet1D::eval(size_t jg, double* xg, double* rg, integer* diagg,
     int* db = diag - nc;
 
     size_t last = m_flow_left->nPoints() - 1;
-    if (m_flow_left->doEnergy(last)) {
-        rb[c_offset_T] = xb[c_offset_T] - xb[c_offset_T - nc]; // zero T gradient
+    if (m_flow_left->nEq() == 1) { // mixture fraction space 
+        if (m_flow_left->doEnergy(last)) {
+            rb[c_offset_Tflamelet] = xb[c_offset_Tflamelet] - xb[c_offset_Tflamelet - nc]; // zero T gradient
+        } else {
+            rb[c_offset_Tflamelet] = xb[c_offset_Tflamelet] - m_flow_left->T_fixed(last);
+        }
     } else {
-        rb[c_offset_T] = xb[c_offset_T] - m_flow_left->T_fixed(last);
+        if (m_flow_left->doEnergy(last)) {
+            rb[c_offset_T] = xb[c_offset_T] - xb[c_offset_T - nc]; // zero T gradient
+        } else {
+            rb[c_offset_T] = xb[c_offset_T] - m_flow_left->T_fixed(last);
+        }
     }
     size_t kSkip = c_offset_Y + m_flow_left->rightExcessSpecies();
     for (size_t k = c_offset_Y; k < nc; k++) {

src/oneD/DomainFactory.cpp
@@ -6,6 +6,7 @@
 #include "cantera/oneD/DomainFactory.h"
 #include "cantera/oneD/Boundary1D.h"
 #include "cantera/oneD/Flow1D.h"
+#include "cantera/oneD/Flamelet.h"
 #include "cantera/oneD/IonFlow.h"
 #include "cantera/oneD/StFlow.h"
 #include "cantera/transport/Transport.h"
@@ -18,64 +19,74 @@ std::mutex DomainFactory::domain_mutex;
 
 DomainFactory::DomainFactory()
 {
-    reg("inlet", [](shared_ptr<Solution> solution, const string& id) {
+    reg("inlet", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Inlet1D(solution, id);
     });
-    reg("empty", [](shared_ptr<Solution> solution, const string& id) {
+    reg("empty", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Empty1D(solution, id);
     });
-    reg("symmetry-plane", [](shared_ptr<Solution> solution, const string& id) {
+    reg("symmetry-plane", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Symm1D(solution, id);
     });
-    reg("outlet", [](shared_ptr<Solution> solution, const string& id) {
+    reg("outlet", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Outlet1D(solution, id);
     });
-    reg("outlet-reservoir", [](shared_ptr<Solution> solution, const string& id) {
+    reg("outlet-reservoir", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new OutletRes1D(solution, id);
     });
-    reg("surface", [](shared_ptr<Solution> solution, const string& id) {
+    reg("surface", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new Surf1D(solution, id);
     });
-    reg("reacting-surface", [](shared_ptr<Solution> solution, const string& id) {
+    reg("reacting-surface", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new ReactingSurf1D(solution, id);
     });
     reg("gas-flow", [](shared_ptr<Solution> solution, const string& id) {
         return new Flow1D(solution, id);
     });
-    reg("legacy-flow", [](shared_ptr<Solution> solution, const string& id) {
-        return new StFlow(solution, id);
+    reg("legacy-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
+        return new StFlow(solution, id, sections);
     });
-    reg("ion-flow", [](shared_ptr<Solution> solution, const string& id) {
+    reg("ion-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         return new IonFlow(solution, id);
     });
-    reg("free-flow", [](shared_ptr<Solution> solution, const string& id) {
+    reg("free-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         Flow1D* ret;
         if (solution->transport()->transportModel() == "ionized-gas") {
             ret = new IonFlow(solution, id);
         } else {
-            ret = new Flow1D(solution, id);
+            ret = new StFlow(solution, id, sections);
         }
         ret->setFreeFlow();
         return ret;
     });
-    reg("axisymmetric-flow", [](shared_ptr<Solution> solution, const string& id) {
+    reg("axisymmetric-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         Flow1D* ret;
         if (solution->transport()->transportModel() == "ionized-gas") {
             ret = new IonFlow(solution, id);
         } else {
-            ret = new Flow1D(solution, id);
+            ret = new Flow1D(solution, id, sections);
         }
         ret->setAxisymmetricFlow();
         return ret;
     });
-    reg("unstrained-flow", [](shared_ptr<Solution> solution, const string& id) {
+    reg("unstrained-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
         Flow1D* ret;
         if (solution->transport()->transportModel() == "ionized-gas") {
             ret = new IonFlow(solution, id);
         } else {
-            ret = new Flow1D(solution, id);
+            ret = new StFlow(solution, id, sections);
         }
         ret->setUnstrainedFlow();
+        return ret;
+    });
+    reg("flamelet-flow", [](shared_ptr<Solution> solution, const string& id, const size_t& sections = 0) {
+        StFlow* ret;
+        if (solution->transport()->transportModel() == "ionized-gas") {
+            ret = new IonFlow(solution, id);
+        } else {
+            ret = new Flamelet(solution, id, sections); // neq = 1 for flamelet
+        }
+        ret->setFlameletFlow();
         return ret;
     });
 }