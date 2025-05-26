080f93ec7f33964652a91ffb452fd2104355ee2f
Ingmar Schoegl <ischoegl@lsu.edu>
2025-05-26 10:37:37 -0500

    [sourcegen] Use hatchling
    Co-authored-by: Bryan Weber <bryan.w.weber@gmail.com>

---
+++ interfaces/sourcegen/src/sourcegen/__main__.py
@@ -0,0 +1,9 @@
+"""Module entry point for sourcegen."""
+
+# This file is part of Cantera. See License.txt in the top-level directory or
+# at https://cantera.org/license.txt for license and copyright information.
+
+from .api import main
+
+if __name__ == "__main__":
+    main()
