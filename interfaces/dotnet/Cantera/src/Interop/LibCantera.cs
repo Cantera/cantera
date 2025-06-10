b2beba2fbb5bd8d6d1cbc99940ab4215bd5a2c5f
Sammo Gabay <sammo@sammo.ga>
2025-06-10 02:40:47 -0400

    [.NET] Small improvements and clarifications in CSharpSourceGenerator
    * Specify all C to C# mappings explicitly in the crosswalk
    * Add region blocks around groups of related functions in generated LibCantera files

--- interfaces/dotnet/Cantera/src/Interop/LibCantera.cs
+++ interfaces/dotnet/Cantera/src/Interop/LibCantera.cs
@@ -6,6 +6,16 @@ using System.Runtime.InteropServices.Marshalling;
 
 namespace Cantera.Interop;
 
+/// <summary>
+/// Contains P/Invoke signatures for calling the native Cantera library.
+/// </summary>
+/// <remarks>
+/// Each P/Invoke signature represents a single function exported by the
+/// native Cantera library C API. Signatures are marked with LibraryImportAttribute
+/// to facilitate generation of marshalling code at build-time. In addition,
+/// functions that get strings or get or set arrays are marked private and
+/// exposed via wrappers which simplify calling these functions.
+/// </remarks>
 static partial class LibCantera
 {
     const string LibFile = "cantera_shared";
