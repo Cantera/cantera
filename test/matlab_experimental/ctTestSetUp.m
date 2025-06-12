7e463965183cf231d0218c64c5174175ec5eceec
ssun30 <sun.su@northeastern.edu>
2025-06-11 21:39:17 -0400

    [MATLAB] Changed how search paths are set
    Replaced ctRoot with ctPaths, this will allow the
    user to set paths to the shared libraries, header
    files, and toolboxes more freely, instead of
    relying on relative paths to a fixed path for
    a certain conda environment.

--- test/matlab_experimental/ctTestSetUp.m
+++ test/matlab_experimental/ctTestSetUp.m
@@ -1,24 +1,3 @@
-clear all
-
-% Copy library to test folder
-ctTestPath;
-
-if ispc
-    ctName = '/build/lib/cantera_shared.dll';
-elseif ismac
-    ctName = '/build/lib/libcantera_shared.dylib';
-elseif isunix
-    ctName = '/build/lib/libcantera_shared.so';
+function ctTestSetUp()
+    ctLoad();
 end
-% Load Cantera
-if ~libisloaded('libcantera_shared')
-    [~, warnings] = loadlibrary([cantera_root, ctName], ...
-                                [cantera_root, '/include/cantera/clib/ctmatlab.h'], ...
-                                'includepath', [cantera_root, '/include'], ...
-                                'addheader', 'ct', 'addheader', 'ctfunc', ...
-                                'addheader', 'ctmultiphase', 'addheader', ...
-                                'ctonedim', 'addheader', 'ctreactor', ...
-                                'addheader', 'ctrpath', 'addheader', 'ctsurf');
-end
-
-disp('Cantera is loaded for test');
