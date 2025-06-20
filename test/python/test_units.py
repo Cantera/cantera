7b74033b355e3340ef815b3926218f2080da794c
Mark E. Fuller <mark.e.fuller@gmx.de>
2025-06-20 12:39:41 -0400

    Revert "[Python/Test] Work around pint TypeError - pint #1969 resolved"
    This reverts commit dd03a35ca724b06c6f64029c8c31d44437efe87e.

--- test/python/test_units.py
+++ test/python/test_units.py
@@ -1,14 +1,10 @@
+from contextlib import nullcontext
 from dataclasses import dataclass
 from typing import Optional, Tuple, Dict
+import sys
 
 import pytest
-try:
-    pint = pytest.importorskip("pint", "0.17.0")
-except TypeError as e:
-    # The extra exception handling is here because pint is incompatible with
-    # Python 3.13. Once https://github.com/hgrecco/pint/issues/1969 is resolved the
-    # try/except here can be restored to just the importorskip.
-    pytest.skip(f"pint import failed due to {e}", allow_module_level=True)
+pytest.importorskip("pint", "0.17.0")
 import cantera.with_units as ctu
 import cantera as ct
 try:
