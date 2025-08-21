96bd8309291bed9c3fb64941f4fb22ec533ea42e
ssun30 <sun.su@northeastern.edu>
2025-08-20 23:19:34 -0400

    [MATLAB] Changed how ctPaths work
    Now it only uses relative paths to the environment
    where cantera is installed.

--- interfaces/matlab_experimental/Utility/ctLoad.m
+++ interfaces/matlab_experimental/Utility/ctLoad.m
@@ -1,25 +1,37 @@
-function ctLoad()
+function ctLoad(paths)
     % ctLoad
     % Load the Cantera C Library into Memory
+    %
+    %   ctLoad()          uses paths from ctPaths(), default
+    %   ctLoad(paths)     uses explicitly provided paths (only used by unit tests)
 
-    paths = ctPaths();
+    if nargin < 1
+        paths = ctPaths();
+    end
 
     if any(cellfun(@isempty, {paths.libPath, paths.includePath, paths.toolboxPath}))
         error('ctLoad:MissingPath', ...
-              'Library, header, and toolbox paths must be configured with ctPaths(libPath,includePath, toolboxPath).');
+              'Library, header, and toolbox paths must be configured with `ctPaths`');
     end
 
     if ispc
-        libName = 'cantera_shared.dll';
+        pattern = fullfile(paths.libPath, [ctLib, '*.dll']);
     elseif ismac
-        libName = 'libcantera_shared.dylib';
+        pattern = fullfile(paths.libPath, [ctLib, '*.dylib']);
     elseif isunix
-        libName = 'libcantera_shared.so';
+        pattern = fullfile(paths.libPath, [ctLib, '*.so']);
     else
         error('ctLoad:UnsupportedPlatform', 'Operating system not supported.');
     end
 
-    fullLibPath = fullfile(paths.libPath, libName);
+    libFiles = dir(pattern);
+    if isempty(libFiles)
+        error('ctLoad:LibraryNotFound', ...
+              'No matching Cantera library found in %s (pattern: %s)', paths.libPath, pattern);
+    end
+
+    libFile = libFiles(1).name;
+    fullLibPath = fullfile(paths.libPath, libFile);
     fullHeaderPath = fullfile(paths.includePath, 'cantera', 'clib', 'ctmatlab.h');
 
     if ~libisloaded(ctLib)
--- test/matlab_experimental/ctTestTearDown.m
+++ test/matlab_experimental/ctTestTearDown.m
@@ -1,3 +1,15 @@
 function ctTestTearDown()
+    % ctTestTearDown
+    % Unload the Cantera C library and remove test paths from MATLAB.
+
     ctUnload();
+
+    thisFile = mfilename('fullpath');
+    canteraRoot = fileparts(fileparts(fileparts(thisFile)));
+
+    toolboxPath = fullfile(canteraRoot, 'interfaces', 'matlab_experimental');
+    testPath    = fullfile(canteraRoot, 'test');
+
+    rmpath(genpath(toolboxPath));
+    rmpath(genpath(testPath));
 end
