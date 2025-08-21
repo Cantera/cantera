5e5d170ef13e5801dae3b224f305d9a653252b6e
ssun30 <sun.su@northeastern.edu>
2025-08-20 23:20:24 -0400

    [MATLAB] Incorporated sample tests into CI
    The test_examples script can still be used to
    run all examples by the user, but it's not
    necessary for the unit test suite.

---
+++ test/matlab_experimental/ctTestSamples.m
@@ -0,0 +1,48 @@
+classdef ctTestSamples < ctTestCase
+
+    methods (TestMethodSetup)
+
+        function addSampleFolder(self)
+            thisFile = mfilename('fullpath');
+            canteraRoot = fileparts(fileparts(fileparts(thisFile)));
+            samplePath = fullfile(canteraRoot, 'samples', 'matlab_experimental');
+            addpath(samplePath);
+        end
+
+    end
+
+    methods (TestMethodTeardown)
+
+        function removeSampleFolder(self)
+            close all
+            thisFile = mfilename('fullpath');
+            canteraRoot = fileparts(fileparts(fileparts(thisFile)));
+            samplePath = fullfile(canteraRoot, 'samples', 'matlab_experimental');
+            rmpath(samplePath);
+        end
+
+    end
+
+    properties (TestParameter)
+            ExampleScript = {
+                'equil', 'isentropic', 'reactor1', 'reactor2', 'surf_reactor', ...
+                'periodic_cstr', 'plug_flow_reactor', 'lithium_ion_battery', ...
+                'rankine', 'prandtl1', 'prandtl2', 'catcomb','ignite', 'diamond_cvd', ...
+                % 'flame1', 'flame2', 'diff_flame', 'ignite_hp', 'ignite_uv'
+            };
+    end
+
+    methods (Test, ParameterCombination = 'sequential')
+
+        function testExample(self, ExampleScript)
+            try
+                feval(ExampleScript);
+            catch ME
+                self.assertFail(sprintf('Example %s failed: %s', ...
+                                ExampleScript, ME.message));
+            end
+        end
+
+    end
+
+end
