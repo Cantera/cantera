72d21e1c7fbf6e325bf2a58d013c744aaf7480ad
Ray Speth <speth@mit.edu>
2025-09-07 21:24:00 -0400

    Bump version to 3.2.0a3

--- interfaces/cython/cantera/ck2yaml.py
+++ interfaces/cython/cantera/ck2yaml.py
@@ -2036,7 +2036,7 @@ class Parser:
             metadata = BlockMap([
                 ("generator", "ck2yaml"),
                 ("input-files", FlowList(self.files)),
-                ("cantera-version", "3.2.0a2"),
+                ("cantera-version", "3.2.0a3"),
                 ("date", formatdate(localtime=True)),
             ])
             if desc.strip():
