cf225480fca38a77f60ba1bbbaf2c55ef6c783d8
ssun30 <sun.su@northeastern.edu>
2025-09-25 09:05:23 -0400

    [MATLAB] Reworked the scripts to build the Cantera MATLAB interface

--- interfaces/matlab_experimental/cantera/ctBuildInterface.m
+++ interfaces/matlab_experimental/cantera/ctBuildInterface.m
@@ -1,31 +1,13 @@
-function ctBuildInterface(mode)
-    % Mode determines how the paths are set. By default it's set to 'auto'
-    % and the environment variable CANTERA_ROOT is used to determine the
-    % other paths. This mode is used when the user compiled Cantera from
-    % source, so that all the necessary files are already in their correct
-    % locations. If the user obtained the MATLAB toolbox from FileExchange
-    % and the header files and compiled library files are downloaded by the
-    % MLTBX setup script, the 'FileExchange' mode is used.
+function ctBuildInterface()
+    % Builds the Cantera MATLAB C++ Interface from the Cantera CLib
+    % header files and the compiled Cantera library.
+    % The environment variable CANTERA_ROOT is used to obtain the paths.
 
-    if nargin < 1
-        mode = 'auto';
-    end
-
-    if strcmp(mode, 'auto')
-        canteraRoot = string(getenv('CANTERA_ROOT'));
-        includeDir = canteraRoot + "/include";
-        headerDir = includeDir + "/cantera/clib";
-        ctLibDir = canteraRoot + "/build/lib";
-        outputDir = canteraRoot + "/interfaces/matlab_experimental/cantera";
-    elseif strcmp(mode, 'FileExchange')
-        canteraRoot = string(pwd);
-        includeDir = canteraRoot + "/include";
-        headerDir = includeDir;
-        ctLibDir = canteraRoot + "/lib";
-        outputDir = canteraRoot + "/matlab_experimental/cantera";
-    else
-        error('Invalid mode. Use "auto" or "FileExchange".');
-    end
+    canteraRoot = string(getenv('CANTERA_ROOT'));
+    includeDir = canteraRoot + "/include";
+    headerDir = includeDir + "/cantera/clib";
+    ctLibDir = canteraRoot + "/build/lib";
+    outputDir = canteraRoot + "/interfaces/matlab_experimental/cantera";
 
     % Display the paths (Optional step, for debugging)
     disp("Paths used in the build process:");
@@ -38,9 +20,8 @@ function ctBuildInterface(mode)
     ctGenerateLibraryDefinitions(includeDir, headerDir, ctLibDir, outputDir);
     ctEditLibraryDefinitions(outputDir);
 
-    % Print out the library definitions
+    % Build C++ Interface
     libDef = feval("definectMatlab");
-
     build(libDef);
     addpath(fullfile(libDef.OutputFolder,libDef.InterfaceName));
     libDef.copyRuntimeDependencies(Verbose=true);
