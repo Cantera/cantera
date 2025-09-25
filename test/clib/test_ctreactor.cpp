4a880002230454861e1e59ff2c4e768065ae21ab
Ray Speth <speth@mit.edu>
2025-09-24 21:04:59 -0400

    [Test] Add test of reactor surfaces for new CLib

--- test/clib/test_ctreactor.cpp
+++ test/clib/test_ctreactor.cpp
@@ -102,3 +102,39 @@ TEST(ctreactor, reactor_clone)
     EXPECT_DOUBLE_EQ(thermo_temperature(thermo), T);
     EXPECT_GT(thermo_temperature(contents_thermo), T);
 }
+
+TEST(ctreactor, surface)
+{
+    vector<double> Tref = {
+        1050.000, 1050.517, 1051.107, 1051.744, 1052.443, 1053.220, 1054.102, 1055.124,
+        1056.344, 1057.849, 1059.791, 1062.453, 1066.443, 1073.424, 1091.018, 2918.287,
+        2918.287, 2918.287, 2918.287, 2918.287};
+
+    double T = 1050;
+    double P = 5 * 101325;
+    string X = "CH4:1.0, O2:2.0, N2:7.52";
+
+    int32_t surf = sol_newInterface("ptcombust.yaml", "Pt_surf", 0, 0);
+    int32_t gas = sol_adjacent(surf, 0);
+    int32_t gas_thermo = sol_thermo(gas);
+    thermo_setMoleFractionsByName(gas_thermo, X.c_str());
+    thermo_setTemperature(gas_thermo, T);
+    thermo_setPressure(gas_thermo, P);
+
+    int32_t reactor = reactor_new("IdealGasReactor", gas, 1, "test");
+    vector<int32_t> adjacent = {reactor};
+    int32_t rsurf = reactor_newSurface(surf, 1, adjacent.data(), 1, "surf");
+    ASSERT_GE(rsurf, 0);
+    int32_t net = reactornet_new(1, adjacent.data());
+    ASSERT_GE(net, 0);
+
+    double t = 0.0;
+    int count = 0;
+    while (t < 0.1) {
+        ASSERT_NEAR(reactor_temperature(reactor), Tref[count], 1e-2);
+        t = reactornet_time(net) + 5e-3;
+        int32_t status = reactornet_advance(net, t);
+        ASSERT_EQ(status, 0);
+        count++;
+    }
+}
