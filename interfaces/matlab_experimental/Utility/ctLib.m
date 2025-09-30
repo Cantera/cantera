77cf43fbf206550dc53290fbdfa8b8b94cdb10fc
ssun30 <sun.su@northeastern.edu>
2025-09-29 22:47:46 -0400

    [MATLAB] MATLAB interface loads from the correct path for MATLAB-related libraries depending on the CPU architecture

--- interfaces/matlab_experimental/Utility/ctLib.m
+++ interfaces/matlab_experimental/Utility/ctLib.m
@@ -3,7 +3,29 @@ function str = ctLib(libDir)
     if ispc
         str = libDir + "/cantera_shared.dll";
     elseif ismac
-        str = libDir + "/libcantera_shared.dylib";
+        if ~isempty(dir(fullfile(libDir, 'libcantera.dylib')))
+            baseName = 'libcantera';
+        elseif ~isempty(dir(fullfile(libDir, 'libcantera_shared.dylib')))
+            baseName = 'libcantera_shared';
+        else
+            error('No matching shared libraries found in %s', libDir);
+        end
+        files = dir(fullfile(libDir, [baseName, '*.dylib']));
+        versioned = {};
+        for k = 1:numel(files)
+            name = files(k).name;
+            tokens = regexp(name, [baseName,'\.([0-9]+(\.[0-9]+)*)\.dylib$'], 'tokens', 'once');
+            if ~isempty(tokens)
+                versioned{end+1} = name;
+            end
+        end
+        if isempty(versioned)
+            warning('No versioned library found, falling back to unversioned');
+            str = libDir + baseName + ".dylib";
+        else
+            [~, idx] = max(cellfun(@(s) sscanf(s, [baseName, '.%f.dylib']), versioned));
+            str = libDir + "/" + versioned{idx};
+        end
     elseif isunix
         if ~isempty(dir(fullfile(libDir, 'libcantera.so*')))
             baseName = 'libcantera';
