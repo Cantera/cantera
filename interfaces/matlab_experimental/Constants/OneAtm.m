ba596b15d2e22ea47d7ce677d6a1cd953eae01bd
ssun30 <sun.su@northeastern.edu>
2025-10-02 20:01:28 -0400

    [MATLAB] Added physical constants from the ct standard class instead of hardcoded values.

--- interfaces/matlab_experimental/Constants/OneAtm.m
+++ interfaces/matlab_experimental/Constants/OneAtm.m
@@ -6,5 +6,5 @@ function p = OneAtm
     % :return:
     %     One atmosphere in Pascals.
 
-    p = 101325.0;
+    p = ctFunc('ct_OneAtm');
 end
---
+++ interfaces/matlab_experimental/Constants/permeability_0.m
@@ -0,0 +1,10 @@
+function r = permeability_0
+    % Get the vacuum permeability in N/A^2. ::
+    %
+    %     >> r = permeability_0
+    %
+    % :return:
+    %     The vacuum permeability in N/A^2.
+
+    r = ctFunc('ct_permeability_0');
+end
