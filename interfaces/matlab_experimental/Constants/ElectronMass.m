77bc27cc51221d6e5be4fc14c0e8ac0444d61d44
ssun30 <sun.su@northeastern.edu>
2025-10-02 20:01:28 -0400

    [MATLAB] Added physical constants from the ct standard class instead of hardcoded values.

---
+++ interfaces/matlab_experimental/Constants/ElectronMass.m
@@ -0,0 +1,10 @@
+function r = ElectronMass
+    % Get the electron mass in kg. ::
+    %
+    %     >> r = ElectronMass
+    %
+    % :return:
+    %     The electron mass in kg.
+
+    r = ctFunc('ct_ElectronMass');
+end
