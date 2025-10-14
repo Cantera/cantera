1fc23139f49abc11a816a002b1050619815265eb
ssun30 <sun.su@northeastern.edu>
2025-10-14 16:14:18 -0400

    [CI] Switch to Generated CLib for MATLAB tests MATLAB Coverage will now skip Slow tests

--- test/matlab_experimental/runMatlabInterfaceTests.m
+++ test/matlab_experimental/runMatlabInterfaceTests.m
@@ -4,6 +4,8 @@ thisFile = mfilename('fullpath');
 testFolder = fileparts(thisFile);
 addpath(genpath(testFolder));
 suite = testsuite(testFolder);
+% Sample tests are tagged as 'Slow' due to the extra headroom
+% required when Cantera is loaded in 'outofprocess' mode.
 isSlow = cellfun(@(tags) any(strcmp(tags, 'Slow')), {suite.Tags});
 suite = suite(~isSlow);
 
