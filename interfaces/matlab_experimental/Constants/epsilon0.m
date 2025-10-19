e4895a507007365aee4314550e469a78730e9234
Ingmar Schoegl <ischoegl@lsu.edu>
2025-10-19 13:14:27 -0500

    [sourcegen] Avoid snake_case in API

--- interfaces/matlab_experimental/Constants/epsilon0.m
+++ interfaces/matlab_experimental/Constants/epsilon0.m
@@ -1,10 +1,10 @@
-function r = epsilon_0
+function r = epsilon0
     % Get the vacuum permittivity in C^2/N/m^2. ::
     %
-    %     >> r = epsilon_0
+    %     >> r = epsilon0
     %
     % :return:
     %     The vacuum permittivity in C^2/N/m^2.
 
-    r = ctFunc('ct_epsilon_0');
+    r = ctFunc('ct_epsilon0');
 end
