f46e7fcc8e94a4672b1730b8af97c5e8c99b2a7e
Ray Speth <speth@mit.edu>
2025-10-25 13:08:18 -0400

    [Examples] Show timing info for 1D solver stats

--- samples/python/onedim/diffusion_flame.py
+++ samples/python/onedim/diffusion_flame.py
@@ -76,7 +76,7 @@ f.save(output)
 # %%
 # write the velocity, temperature, and mole fractions to a CSV file
 f.save('diffusion_flame.csv', basis="mole", overwrite=True)
-f.show_stats(0)
+f.show_stats()
 no_rad = f.to_array()
 
 # %%
