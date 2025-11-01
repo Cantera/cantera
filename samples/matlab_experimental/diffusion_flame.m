8f415f123ca1e7bf3fd28aa55a203ca8aabfdc11
Ingmar Schoegl <ischoegl@lsu.edu>
2025-11-01 12:47:59 -0500

    [MATLAB] Rename diff_flame.m to diffusion_flame.m
    Also, update and simplify.

--- samples/matlab_experimental/diffusion_flame.m
+++ samples/matlab_experimental/diffusion_flame.m
@@ -1,32 +1,31 @@
 %% Opposed-flow diffusion flame
 %
-% This example uses the ``CounterFlowDiffusionFlame`` function to solve an
+% This example uses the ``CounterFlowDiffusionFlame`` object to solve an
 % opposed-flow diffusion flame for Ethane in Air. This example is the same
 % as the :doc:`diffusion_flame.py <../python/onedim/diffusion_flame>`
 % example without radiation.
 %
 % Requires: cantera >= 3.2.0
 %
-% .. tags:: Matlab, combustion, 1D flow, diffusion flame, plotting
+% .. tags:: Matlab, combustion, 1D flow, strained flame, diffusion flame, plotting
 
 %%
-% Initialization
+% Problem Definition
+% ------------------
 
-tic % total running time of the script
-help diff_flame
+tic  % total running time of the script
+help diffusion_flame
 
-runtime = cputime; % Record the starting time
+runtime = cputime;  % Record the starting time
 
 %%
-% Parameter values of inlet streams
+% **Set parameter values**
 
-p = OneAtm; % Pressure
-tin = 300.0; % Inlet temperature
-mdot_o = 0.72; % Air mass flux, kg/m^2/s
-mdot_f = 0.24; % Fuel mass flux, kg/m^2/s
-transport = 'mixture-averaged'; % Transport model
-% NOTE: Transport model needed if mechanism file does not have transport
-% properties.
+p = OneAtm;  % Pressure
+tin = 300.0;  % Inlet temperature
+mdot_o = 0.72;  % Air mass flux, kg/m^2/s
+mdot_f = 0.24;  % Fuel mass flux, kg/m^2/s
+transport = 'mixture-averaged';  % Transport model
 
 %%
 % Set-up initial grid, loglevel, tolerances. Enable/Disable grid refinement
@@ -34,49 +33,44 @@ transport = 'mixture-averaged'; % Transport model
 width = 0.02;
 nz = 11;
 
-tol_ss = {1.0e-5, 1.0e-9}; % {rtol atol} for steady-state problem
-tol_ts = {1.0e-3, 1.0e-9}; % {rtol atol} for time stepping
-logLevel = 1; % Amount of diagnostic output (0 to 5)
-refineGrid = 1; % 1 to enable refinement, 0 to disable
+tol_ss = {1.0e-5, 1.0e-9};  % {rtol atol} for steady-state problem
+tol_ts = {1.0e-3, 1.0e-9};  % {rtol atol} for time stepping
+logLevel = 1;  % Amount of diagnostic output (0 to 5)
+refineGrid = 1;  % 1 to enable refinement, 0 to disable
 
 %%
-% Create the gas objects for the fuel and oxidizer streams
+% **Create the gas objects for the fuel and oxidizer streams**
 %
 % These objects will be used to evaluate all thermodynamic, kinetic, and
 % transport properties.
 
 fuel = Solution('gri30.yaml', 'gri30', transport);
 ox = Solution('gri30.yaml', 'gri30', transport);
-oxcomp = 'O2:0.21, N2:0.78'; % Air composition
-fuelcomp = 'C2H6:1'; % Fuel composition
+oxcomp = 'O2:0.21, N2:0.78';  % Air composition
+fuelcomp = 'C2H6:1';  % Fuel composition
+
 % Set each gas mixture state with the corresponding composition.
 fuel.TPX = {tin, p, fuelcomp};
 ox.TPX = {tin, p, oxcomp};
 
 %%
-% Set-up the flow object
+% **Set up the flow object**
 %
 % For this problem, the ``AxisymmetricFlow`` model is needed. Set the state of
 % the flow as the fuel gas object. This is arbitrary and is only used to
 % initialize the flow object. Set the grid to the initial grid defined
 % prior, same for the tolerances.
 
-f = AxisymmetricFlow(fuel, 'flow');
-f.P = p;
-f.setupUniformGrid(nz, width, 0.0);
-f.setSteadyTolerances(tol_ss{:}, 'default');
-f.setTransientTolerances(tol_ts{:}, 'default');
+flow = AxisymmetricFlow(fuel, 'flow');
+flow.P = p;
+flow.setupUniformGrid(nz, width, 0.0);
+flow.setSteadyTolerances(tol_ss{:}, 'default');
+flow.setTransientTolerances(tol_ts{:}, 'default');
 
 %%
-% Create the fuel and oxidizer inlet steams
+% **Create the fuel and oxidizer inlet steams**
 %
 % Specify the temperature, mass flux, and composition correspondingly.
-
-% Set the oxidizer inlet.
-inlet_o = Inlet(ox, 'air_inlet');
-inlet_o.T = tin;
-inlet_o.massFlux = mdot_o;
-inlet_o.X = oxcomp;
 
 % Set the fuel inlet.
 inlet_f = Inlet(fuel, 'fuel_inlet');
@@ -84,8 +78,14 @@ inlet_f.T = tin;
 inlet_f.massFlux = mdot_f;
 inlet_f.X = fuelcomp;
 
+% Set the oxidizer inlet.
+inlet_o = Inlet(ox, 'air_inlet');
+inlet_o.T = tin;
+inlet_o.massFlux = mdot_o;
+inlet_o.X = oxcomp;
+
 %%
-% Create the flame object
+% **Create the flame object**
 %
 % Once the inlets have been created, they can be assembled
 % to create the flame object. Function ``CounterFlorDiffusionFlame``
@@ -94,10 +94,13 @@ inlet_f.X = fuelcomp;
 % object, oxidizer inlet object, fuel gas object, oxidizer gas object, and
 % the name of the oxidizer species as in character format.
 
-fl = CounterFlowDiffusionFlame(inlet_f, f, inlet_o, fuel, ox, 'O2');
+fl = CounterFlowDiffusionFlame(inlet_f, flow, inlet_o);
 
 %%
-% Solve with fixed temperature profile
+% Solution
+% --------
+%
+% **Solve with fixed temperature profile**
 %
 % Grid refinement is turned off for this process in this example.
 % To turn grid refinement on, change 0 to 1 for last input is solve function.
@@ -105,7 +108,7 @@ fl = CounterFlowDiffusionFlame(inlet_f, f, inlet_o, fuel, ox, 'O2');
 fl.solve(logLevel, 0);
 
 %%
-% Enable the energy equation
+% **Enable the energy equation**
 %
 % The energy equation will now be solved to compute the temperature profile.
 % We also tighten the grid refinement criteria to get an accurate final
@@ -113,9 +116,12 @@ fl.solve(logLevel, 0);
 % on cantera.org in the Matlab User's Guide and can be accessed by
 % ``help setRefineCriteria``.
 
-f.energyEnabled = true;
-f.setRefineCriteria(200.0, 0.1, 0.2);
+flow.energyEnabled = true;
+flow.setRefineCriteria(200.0, 0.1, 0.2);
 fl.solve(logLevel, refineGrid);
+
+%%
+% **Show statistics and display results**
 
 %%
 % Show statistics of solution and elapsed time
@@ -124,43 +130,36 @@ fl.writeStats;
 elapsed = cputime - runtime;
 e = sprintf('Elapsed CPU time: %10.4g', elapsed);
 disp(e);
+toc
 
 %%
-% Plot results
 % Make a single plot showing temperature and mass fraction of select
 % species along axial distance from fuel inlet to air inlet.
 
 figure(1);
 subplot(2, 3, 1);
-plotSolution(f, 'T');
-title('Temperature [K]');
+plotSolution(f, 'T', 'Temperature [K]');
 subplot(2, 3, 2);
-plotSolution(f, 'velocity');
-title('Axial Velocity [m/s]');
+plotSolution(f, 'velocity', 'Axial Velocity [m/s]');
 subplot(2, 3, 3);
-plotSolution(f, 'O2');
-title('O2 Mass Fraction');
+plotSolution(f, 'O2', 'O2 Mass Fraction');
 subplot(2, 3, 4);
-plotSolution(f, 'H2O');
-title('H2O Mass Fraction');
+plotSolution(f, 'H2O', 'H2O Mass Fraction');
 subplot(2, 3, 5);
-plotSolution(f, 'C2H6');
-title('C2H6 Mass Fraction');
+plotSolution(f, 'C2H6', 'C2H6 Mass Fraction');
 subplot(2, 3, 6);
-plotSolution(f, 'CO2');
-title('CO2 Mass Fraction');
-
-toc
+plotSolution(f, 'CO2', 'CO2 Mass Fraction');
 
 %%
 % Plotting Utility
 % ----------------
 
-function plotSolution(domain, component)
+function plotSolution(domain, component, titleStr)
     % Utility for plotting a specific solution component
     z = domain.grid;
     x = domain.values(component);
     plot(z, x);
     xlabel('z (m)');
     ylabel(component);
+    title(titleStr)
 end
