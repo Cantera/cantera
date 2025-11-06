d96ff2217c323f31a2be2e49c6afe0ef235c550e
Ray Speth <speth@mit.edu>
2025-11-06 10:46:59 -0500

    [Doc] Add links from Matlab to C++ Doxygen docs

--- interfaces/matlab_experimental/Base/CarbonDioxide.m
+++ interfaces/matlab_experimental/Base/CarbonDioxide.m
@@ -12,7 +12,7 @@ function c = CarbonDioxide()
     % computational equations for forty substances.* Stanford: Stanford
     % University, 1979. Print.
     %
-    % For more details, see classes Cantera::PureFluid and tpx::CarbonDioxide in the
+    % For more details, see classes :ct:`PureFluidPhase` and :ct:`CarbonDioxide` in the
     % Cantera C++ source code documentation.
     %
     % :return:
--- interfaces/matlab_experimental/Base/Heptane.m
+++ interfaces/matlab_experimental/Base/Heptane.m
@@ -12,7 +12,7 @@ function h = Heptane
     % computational equations for forty substances.* Stanford: Stanford
     % University, 1979. Print.
     %
-    % For more details, see classes Cantera::PureFluid and tpx::Heptane in the
+    % For more details, see classes :ct:`PureFluidPhase` and :ct:`Heptane` in the
     % Cantera C++ source code documentation.
     %
     % :return:
