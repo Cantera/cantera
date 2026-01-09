Cantera/cantera

commit c0e0831ef04935887585382b20d2736e13f298e1

[Equil] Remove unused "inert species" option from VCS solver

src/equil/vcs_solve_TP.cpp
@@ -89,13 +89,6 @@ int VCS_SOLVE::vcs_solve_TP(int print_lvl, int printDetails, int maxit)
         plogf("%5d PHASES\n", m_numPhases);
         plogf(" PRESSURE%22.8g %3s\n", m_pressurePA, "Pa ");
         plogf(" TEMPERATURE%19.3f K\n", m_temperature);
-        vcs_VolPhase* Vphase = m_VolPhaseList[0].get();
-        if (Vphase->nSpecies() > 0) {
-            plogf(" PHASE1 INERTS%17.3f\n", TPhInertMoles[0]);
-        }
-        if (m_numPhases > 1) {
-            plogf(" PHASE2 INERTS%17.3f\n", TPhInertMoles[1]);
-        }
         plogf("\n ELEMENTAL ABUNDANCES             CORRECT");
         plogf("          FROM ESTIMATE           Type\n\n");
         for (size_t i = 0; i < m_nelem; ++i) {
@@ -2745,11 +2738,6 @@ void VCS_SOLVE::vcs_dfe(const int stateCalc,
 
     // Might as well recalculate the phase mole vector and compare to the stored
     // one. They should be correct.
-    double* tPhInertMoles = &TPhInertMoles[0];
-    for (size_t iph = 0; iph < m_numPhases; iph++) {
-        tlogMoles[iph] = tPhInertMoles[iph];
-
-    }
     for (size_t kspec = 0; kspec < m_nsp; kspec++) {
         if (m_speciesUnknownType[kspec] != VCS_SPECIES_TYPE_INTERFACIALVOLTAGE) {
             size_t iph = m_phaseID[kspec];
@@ -2945,7 +2933,7 @@ double VCS_SOLVE::l2normdg(double dgLocal[]) const
 double VCS_SOLVE::vcs_tmoles()
 {
     for (size_t i = 0; i < m_numPhases; i++) {
-        m_tPhaseMoles_old[i] = TPhInertMoles[i];
+        m_tPhaseMoles_old[i] = 0.0;
     }
     for (size_t i = 0; i < m_nsp; i++) {
         if (m_speciesUnknownType[i] == VCS_SPECIES_TYPE_MOLNUM) {
@@ -2969,7 +2957,7 @@ double VCS_SOLVE::vcs_tmoles()
 void VCS_SOLVE::check_tmoles() const
 {
     for (size_t i = 0; i < m_numPhases; i++) {
-        double m_tPhaseMoles_old_a = TPhInertMoles[i];
+        double m_tPhaseMoles_old_a = 0.0;
 
         for (size_t k = 0; k < m_nsp; k++) {
             if (m_speciesUnknownType[k] == VCS_SPECIES_TYPE_MOLNUM && m_phaseID[k] == i) {

include/cantera/equil/vcs_VolPhase.h
@@ -91,11 +91,9 @@ public:
      * @param numSpecies  Number of species in the phase
      * @param numElem     Number of elements in the phase
      * @param phaseName   String name for the phase
-     * @param molesInert  kmoles of inert in the phase (defaults to zero)
      */
     void resize(const size_t phaseNum, const size_t numSpecies,
-                const size_t numElem, const char* const phaseName,
-                const double molesInert = 0.0);
+                const size_t numElem, const char* const phaseName);
 
     void elemResize(const size_t numElemConstraints);
 
@@ -363,10 +361,9 @@ public:
      *   that an alternate method is used to calculate the total phase
      *   concentrations.
      * - VCS_PHASE_EXIST_YES = 2 : Does exist currently
-     * - VCS_PHASE_EXIST_ALWAYS = 3: Always exists because it contains inerts
-     *   which can't exist in any other phase. Or, the phase exists always
-     *   because it consists of a single species, which is identified with the
-     *   voltage, for example, it's an electron metal phase.
+     * - VCS_PHASE_EXIST_ALWAYS = 3: Always exists because it consists of a single
+     *   species that is identified with the voltage, for example, it's an electron
+     *   metal phase.
      */
     int exists() const;
 
@@ -399,16 +396,6 @@ public:
      * @param spGlobalIndex  Global species index (across all phases)
      */
     void setSpGlobalIndexVCS(const size_t spIndex, const size_t spGlobalIndex);
-
-    //! Sets the total moles of inert in the phase
-    /*!
-     * @param tMolesInert Value of the total kmols of inert species in the
-     *     phase.
-     */
-    void setTotalMolesInert(const double tMolesInert);
-
-    //! Returns the value of the total kmol of inert in the phase
-    double totalMolesInert() const;
 
     //! Returns the global index of the local element index for the phase
     size_t elemGlobalIndex(const size_t e) const;
@@ -626,9 +613,6 @@ public:
     string PhaseName;
 
 private:
-    //!  Total moles of inert in the phase
-    double m_totalMolesInert = 0.0;
-
     //! Boolean indicating whether the phase is an ideal solution
     //! and therefore its molar-based activity coefficients are
     //! uniformly equal to one.

include/cantera/equil/vcs_solve.h
@@ -717,17 +717,12 @@ public:
 
     //! Calculate the total dimensionless Gibbs free energy
     /*!
-     * Inert species are handled as if they had a standard free energy of
-     * zero. Note, for this algorithm this function should be monotonically
-     * decreasing.
+     * Note, for this algorithm this function should be monotonically decreasing.
      */
     double vcs_Total_Gibbs(double* w, double* fe, double* tPhMoles);
 
     //! Calculate the total dimensionless Gibbs free energy of a single phase
     /*!
-     * Inert species are handled as if they had a standard free energy of
-     * zero and if they obeyed ideal solution/gas theory.
-     *
      * @param iphase   ID of the phase
      * @param w        Species mole number vector for all species
      * @param fe       vector of partial molar free energies of all of the
@@ -1233,8 +1228,7 @@ public:
 
     //! Total number of kmoles in all phases
     /*!
-     * This number includes the inerts.
-     *            -> Don't use this except for scaling purposes
+     * Don't use this except for scaling purposes
      */
     double m_totalMolNum = 0.0;
 
@@ -1272,13 +1266,6 @@ public:
 
     //! Pressure
     double m_pressurePA;
-
-    //! Total kmoles of inert to add to each phase
-    /*!
-     * TPhInertMoles[iph] = Total kmoles of inert to add to each phase
-     * length = number of phases
-     */
-    vector<double> TPhInertMoles;
 
     //! Tolerance requirement for major species
     double m_tolmaj= 1e-8;

src/equil/vcs_VolPhase.cpp
@@ -30,11 +30,9 @@ vcs_VolPhase::~vcs_VolPhase()
 }
 
 void vcs_VolPhase::resize(const size_t phaseNum, const size_t nspecies,
-                          const size_t numElem, const char* const phaseName,
-                          const double molesInert)
+                          const size_t numElem, const char* const phaseName)
 {
     AssertThrowMsg(nspecies > 0, "vcs_VolPhase::resize", "nspecies Error");
-    setTotalMolesInert(molesInert);
     m_phi = 0.0;
     m_phiVarIndex = npos;
 
@@ -227,14 +225,6 @@ void vcs_VolPhase::setMoleFractionsState(const double totalMoles,
     }
     double fractotal = 1.0;
     v_totalMoles = totalMoles;
-    if (m_totalMolesInert > 0.0) {
-        if (m_totalMolesInert > v_totalMoles) {
-            throw CanteraError("vcs_VolPhase::setMolesFractionsState",
-                 "inerts greater than total: {} {}",
-                 v_totalMoles, m_totalMolesInert);
-        }
-        fractotal = 1.0 - m_totalMolesInert/v_totalMoles;
-    }
     double sum = 0.0;
     for (size_t k = 0; k < m_numSpecies; k++) {
         Xmol_[k] = moleFractions[k];
@@ -255,7 +245,7 @@ void vcs_VolPhase::setMoleFractionsState(const double totalMoles,
 void vcs_VolPhase::setMolesFromVCS(const int stateCalc,
                                    const double* molesSpeciesVCS)
 {
-    v_totalMoles = m_totalMolesInert;
+    v_totalMoles = 0.0;
 
     if (molesSpeciesVCS == 0) {
         AssertThrowMsg(m_owningSolverObject, "vcs_VolPhase::setMolesFromVCS",
@@ -317,9 +307,6 @@ void vcs_VolPhase::setMolesFromVCS(const int stateCalc,
         }
     }
     _updateMoleFractionDependencies();
-    if (m_totalMolesInert > 0.0) {
-        m_existence = VCS_PHASE_EXIST_ALWAYS;
-    }
 
     // If stateCalc is old and the total moles is positive, then we have a valid
     // state. If the phase went away, it would be a valid starting point for
@@ -454,15 +441,6 @@ double vcs_VolPhase::_updateVolPM() const
         m_totalVol += PartialMolarVol[k] * Xmol_[k];
     }
     m_totalVol *= v_totalMoles;
-
-    if (m_totalMolesInert > 0.0) {
-        if (m_gasPhase) {
-            double volI = m_totalMolesInert * GasConstant * Temp_ / Pres_;
-            m_totalVol += volI;
-        } else {
-            throw CanteraError("vcs_VolPhase::_updateVolPM", "unknown situation");
-        }
-    }
     m_UpToDate_VolPM = true;
     return m_totalVol;
 }
@@ -610,21 +588,13 @@ const vector<double>& vcs_VolPhase::creationMoleNumbers(
 void vcs_VolPhase::setTotalMoles(const double totalMols)
 {
     v_totalMoles = totalMols;
-    if (m_totalMolesInert > 0.0) {
+    if (m_singleSpecies && (m_phiVarIndex == 0)) {
         m_existence = VCS_PHASE_EXIST_ALWAYS;
-        AssertThrowMsg(totalMols >= m_totalMolesInert,
-                       "vcs_VolPhase::setTotalMoles",
-                       "totalMoles less than inert moles: {} {}",
-                       totalMols, m_totalMolesInert);
     } else {
-        if (m_singleSpecies && (m_phiVarIndex == 0)) {
-            m_existence = VCS_PHASE_EXIST_ALWAYS;
+        if (totalMols > 0.0) {
+            m_existence = VCS_PHASE_EXIST_YES;
         } else {
-            if (totalMols > 0.0) {
-                m_existence = VCS_PHASE_EXIST_YES;
-            } else {
-                m_existence = VCS_PHASE_EXIST_NO;
-            }
+            m_existence = VCS_PHASE_EXIST_NO;
         }
     }
 }
@@ -679,11 +649,9 @@ void vcs_VolPhase::setExistence(const int existence)
             throw CanteraError("vcs_VolPhase::setExistence",
                                "setting false existence for phase with moles");
         }
-    } else if (m_totalMolesInert == 0.0) {
-        if (v_totalMoles == 0.0 && (!m_singleSpecies || m_phiVarIndex != 0)) {
-            throw CanteraError("vcs_VolPhase::setExistence",
-                    "setting true existence for phase with no moles");
-        }
+    } else if (v_totalMoles == 0.0 && (!m_singleSpecies || m_phiVarIndex != 0)) {
+        throw CanteraError("vcs_VolPhase::setExistence",
+                "setting true existence for phase with no moles");
     }
     if (m_singleSpecies && m_phiVarIndex == 0 && (existence == VCS_PHASE_EXIST_NO || existence == VCS_PHASE_EXIST_ZEROEDPHASE)) {
         throw CanteraError("vcs_VolPhase::setExistence",
@@ -704,36 +672,6 @@ void vcs_VolPhase::setSpGlobalIndexVCS(const size_t spIndex,
     if (spGlobalIndex >= m_numElemConstraints) {
         creationGlobalRxnNumbers_[spIndex] = spGlobalIndex - m_numElemConstraints;
     }
-}
-
-void vcs_VolPhase::setTotalMolesInert(const double tMolesInert)
-{
-    if (m_totalMolesInert != tMolesInert) {
-        m_UpToDate = false;
-        m_UpToDate_AC = false;
-        m_UpToDate_VolStar = false;
-        m_UpToDate_VolPM = false;
-        m_UpToDate_GStar = false;
-        m_UpToDate_G0 = false;
-        v_totalMoles += (tMolesInert - m_totalMolesInert);
-        m_totalMolesInert = tMolesInert;
-    }
-    if (m_totalMolesInert > 0.0) {
-        m_existence = VCS_PHASE_EXIST_ALWAYS;
-    } else if (m_singleSpecies && (m_phiVarIndex == 0)) {
-        m_existence = VCS_PHASE_EXIST_ALWAYS;
-    } else {
-        if (v_totalMoles > 0.0) {
-            m_existence = VCS_PHASE_EXIST_YES;
-        } else {
-            m_existence = VCS_PHASE_EXIST_NO;
-        }
-    }
-}
-
-double vcs_VolPhase::totalMolesInert() const
-{
-    return m_totalMolesInert;
 }
 
 size_t vcs_VolPhase::elemGlobalIndex(const size_t e) const

src/equil/vcs_solve.cpp
@@ -94,7 +94,6 @@ VCS_SOLVE::VCS_SOLVE(MultiPhase* mphase, int printLvl) :
     m_deltaPhaseMoles.resize(m_numPhases, 0.0);
     m_TmpPhase.resize(m_numPhases, 0.0);
     m_TmpPhase2.resize(m_numPhases, 0.0);
-    TPhInertMoles.resize(m_numPhases, 0.0);
 
     // ind[] is an index variable that keep track of solution vector rotations.
     m_speciesMapIndex.resize(m_nsp, 0);
@@ -166,10 +165,9 @@ VCS_SOLVE::VCS_SOLVE(MultiPhase* mphase, int printLvl) :
         //    ->PhaseNum = phase number in the thermo problem
         //    ->GasPhase = Boolean indicating whether it is a gas phase
         //    ->NumSpecies = number of species in the phase
-        //    ->TMolesInert = Inerts in the phase = 0.0 for cantera
         //    ->PhaseName  = Name of the phase
         vcs_VolPhase* VolPhase = m_VolPhaseList[iphase].get();
-        VolPhase->resize(iphase, nSpPhase, nelem, phaseName.c_str(), 0.0);
+        VolPhase->resize(iphase, nSpPhase, nelem, phaseName.c_str());
         VolPhase->m_gasPhase = gasPhase;
 
         // Tell the vcs_VolPhase pointer about cantera
@@ -360,15 +358,14 @@ VCS_SOLVE::VCS_SOLVE(MultiPhase* mphase, int printLvl) :
         // Printout of the Phase structure information
         writeline('-', 80, true, true);
         plogf("             Information about phases\n");
-        plogf("  PhaseName    PhaseNum SingSpec GasPhase EqnState NumSpec");
-        plogf("  TMolesInert       Tmoles(kmol)\n");
+        plogf("  PhaseName      PhaseNum SingSpec   EqnState        NumSpec     "
+              "Tmoles(kmol)\n");
 
         for (size_t iphase = 0; iphase < m_numPhases; iphase++) {
             vcs_VolPhase* VolPhase = m_VolPhaseList[iphase].get();
-            plogf("%16s %5d %5d %8d %16s %8d %16e ", VolPhase->PhaseName.c_str(),
+            plogf("%16s %8d %8d %16s %8d ", VolPhase->PhaseName.c_str(),
                   VolPhase->VP_ID_, VolPhase->m_singleSpecies,
-                  VolPhase->m_gasPhase, VolPhase->eos_name(),
-                  VolPhase->nSpecies(), VolPhase->totalMolesInert());
+                  VolPhase->eos_name(), VolPhase->nSpecies());
             plogf("%16e\n", VolPhase->totalMoles());
         }
 
@@ -378,12 +375,6 @@ VCS_SOLVE::VCS_SOLVE(MultiPhase* mphase, int printLvl) :
         writeline('=', 20);
         writeline('=', 80);
         plogf("\n");
-    }
-
-    // TPhInertMoles[] -> must be copied over here
-    for (size_t iph = 0; iph < m_numPhases; iph++) {
-        vcs_VolPhase* Vphase = m_VolPhaseList[iph].get();
-        TPhInertMoles[iph] = Vphase->totalMolesInert();
     }
 
     // m_speciesIndexVector[] is an index variable that keep track of solution
@@ -1841,8 +1832,6 @@ void VCS_SOLVE::vcs_CalcLnActCoeffJac(const double* const moleSpeciesVCS)
 
 int VCS_SOLVE::vcs_report(int iconv)
 {
-    bool inertYes = false;
-
     // SORT DEPENDENT SPECIES IN DECREASING ORDER OF MOLES
     vector<pair<double, size_t>> x_order;
     for (size_t i = 0; i < m_nsp; i++) {
@@ -1906,19 +1895,6 @@ int VCS_SOLVE::vcs_report(int iconv)
             throw CanteraError("VCS_SOLVE::vcs_report", "we have a problem");
         }
         plogf("\n");
-    }
-    for (size_t i = 0; i < m_numPhases; i++) {
-        if (TPhInertMoles[i] > 0.0) {
-            inertYes = true;
-            if (i == 0) {
-                plogf(" Inert Gas Species        ");
-            } else {
-                plogf(" Inert Species in phase %16s ",
-                      m_VolPhaseList[i]->PhaseName);
-            }
-            plogf("%14.7E     %14.7E    %12.4E\n", TPhInertMoles[i],
-                  TPhInertMoles[i] / m_tPhaseMoles_old[i], 0.0);
-        }
     }
     if (m_numSpeciesRdc != m_nsp) {
         plogf("\n SPECIES WITH LESS THAN 1.0E-32 KMOLES:\n\n");
@@ -2030,15 +2006,10 @@ int VCS_SOLVE::vcs_report(int iconv)
 
     // GLOBAL SATISFACTION INFORMATION
 
-    // Calculate the total dimensionless Gibbs Free Energy. Inert species are
-    // handled as if they had a standard free energy of zero
+    // Calculate the total dimensionless Gibbs Free Energy.
     double g = vcs_Total_Gibbs(&m_molNumSpecies_old[0], &m_feSpecies_old[0],
                                &m_tPhaseMoles_old[0]);
     plogf("\n\tTotal Dimensionless Gibbs Free Energy = G/RT = %15.7E\n", g);
-    if (inertYes) {
-        plogf("\t\t(Inert species have standard free energy of zero)\n");
-    }
-
     plogf("\nElemental Abundances (kmol): ");
     plogf("         Actual                    Target         Type      ElActive\n");
     for (size_t i = 0; i < m_nelem; ++i) {
@@ -2786,17 +2757,6 @@ double VCS_SOLVE::vcs_Total_Gibbs(double* molesSp, double* chemPot,
 {
     double g = 0.0;
 
-    for (size_t iph = 0; iph < m_numPhases; iph++) {
-        vcs_VolPhase* Vphase = m_VolPhaseList[iph].get();
-        if ((TPhInertMoles[iph] > 0.0) && (tPhMoles[iph] > 0.0)) {
-            g += TPhInertMoles[iph] *
-                 log(TPhInertMoles[iph] / tPhMoles[iph]);
-            if (Vphase->m_gasPhase) {
-                g += TPhInertMoles[iph] * log(m_pressurePA/(1.01325E5));
-            }
-        }
-    }
-
     for (size_t kspec = 0; kspec < m_numSpeciesRdc; ++kspec) {
         if (m_speciesUnknownType[kspec] != VCS_SPECIES_TYPE_INTERFACIALVOLTAGE) {
             g += molesSp[kspec] * chemPot[kspec];
@@ -2810,23 +2770,11 @@ double VCS_SOLVE::vcs_GibbsPhase(size_t iphase, const double* const w,
                                  const double* const fe)
 {
     double g = 0.0;
-    double phaseMols = 0.0;
     for (size_t kspec = 0; kspec < m_numSpeciesRdc; ++kspec) {
         if (m_phaseID[kspec] == iphase && m_speciesUnknownType[kspec] != VCS_SPECIES_TYPE_INTERFACIALVOLTAGE) {
             g += w[kspec] * fe[kspec];
-            phaseMols += w[kspec];
         }
     }
-
-    if (TPhInertMoles[iphase] > 0.0) {
-        phaseMols += TPhInertMoles[iphase];
-        g += TPhInertMoles[iphase] * log(TPhInertMoles[iphase] / phaseMols);
-        vcs_VolPhase* Vphase = m_VolPhaseList[iphase].get();
-        if (Vphase->m_gasPhase) {
-            g += TPhInertMoles[iphase] * log(m_pressurePA/1.01325E5);
-        }
-    }
-
     return g;
 }
 
@@ -2900,15 +2848,14 @@ void VCS_SOLVE::vcs_prob_specifyFully()
         // Printout of the Phase structure information
         writeline('-', 80, true, true);
         plogf("             Information about phases\n");
-        plogf("  PhaseName    PhaseNum SingSpec GasPhase EqnState NumSpec");
-        plogf("  TMolesInert       Tmoles(kmol)\n");
+        plogf("  PhaseName      PhaseNum SingSpec   EqnState        NumSpec     "
+              "Tmoles(kmol)\n");
 
         for (size_t iphase = 0; iphase < m_numPhases; iphase++) {
             vcs_VolPhase* VolPhase = m_VolPhaseList[iphase].get();
-            plogf("%16s %5d %5d %8d %16s %8d %16e ", VolPhase->PhaseName.c_str(),
+            plogf("%16s %8d %8d %16s %8d ", VolPhase->PhaseName.c_str(),
                   VolPhase->VP_ID_, VolPhase->m_singleSpecies,
-                  VolPhase->m_gasPhase, VolPhase->eos_name(),
-                  VolPhase->nSpecies(), VolPhase->totalMolesInert());
+                  VolPhase->eos_name(), VolPhase->nSpecies());
             plogf("%16e\n", VolPhase->totalMoles());
         }
 
@@ -2995,9 +2942,7 @@ void VCS_SOLVE::vcs_inest(double* const aw, double* const sa, double* const sm,
     vcs_tmoles();
 
     // m_tPhaseMoles_new[] will consist of just the component moles
-    for (size_t iph = 0; iph < m_numPhases; iph++) {
-        m_tPhaseMoles_new[iph] = TPhInertMoles[iph] + 1.0E-20;
-    }
+    m_tPhaseMoles_new.assign(m_numPhases, 1.0e-20);
     for (size_t kspec = 0; kspec < m_numComponents; ++kspec) {
         if (m_speciesUnknownType[kspec] == VCS_SPECIES_TYPE_MOLNUM) {
             m_tPhaseMoles_new[m_phaseID[kspec]] += m_molNumSpecies_old[kspec];
@@ -3205,10 +3150,7 @@ void VCS_SOLVE::vcs_SSPhase()
     for (size_t iph = 0; iph < m_numPhases; iph++) {
         vcs_VolPhase* Vphase = m_VolPhaseList[iph].get();
         Vphase->m_singleSpecies = false;
-        if (TPhInertMoles[iph] > 0.0) {
-            Vphase->setExistence(2);
-        }
-        if (numPhSpecies[iph] <= 1 && TPhInertMoles[iph] == 0.0) {
+        if (numPhSpecies[iph] <= 1) {
             Vphase->m_singleSpecies = true;
         }
     }
@@ -3325,16 +3267,15 @@ void VCS_SOLVE::prob_report(int print_lvl)
         // Printout of the Phase structure information
         writeline('-', 80, true, true);
         plogf("             Information about phases\n");
-        plogf("  PhaseName    PhaseNum SingSpec  GasPhase   "
-              " EqnState    NumSpec");
-        plogf("  TMolesInert      TKmoles\n");
+        plogf("  PhaseName      PhaseNum SingSpec   EqnState        NumSpec     "
+              "Tmoles(kmol)\n");
 
         for (size_t iphase = 0; iphase < m_numPhases; iphase++) {
             vcs_VolPhase* Vphase = m_VolPhaseList[iphase].get();
-            plogf("%16s %5d %5d %8d ", Vphase->PhaseName,
-                  Vphase->VP_ID_, Vphase->m_singleSpecies, Vphase->m_gasPhase);
-            plogf("%16s %8d %16e ", Vphase->eos_name(),
-                  Vphase->nSpecies(), Vphase->totalMolesInert());
+            plogf("%16s %8d %8d ", Vphase->PhaseName,
+                  Vphase->VP_ID_, Vphase->m_singleSpecies);
+            plogf("%16s %8d ", Vphase->eos_name(),
+                  Vphase->nSpecies());
             if (m_doEstimateEquil >= 0) {
                 plogf("%16e\n", Vphase->totalMoles());
             } else {

commit d056a0bcbe2051f50bee053e93d0ddb04a50497b

[Equil] Fix uninitialzed variable used in debug assertion
Fix a regression introduced in c0e0831ef. This caused some tests
to fail when compiling with assertions enabled.

src/equil/vcs_solve_TP.cpp
@@ -2688,6 +2688,7 @@ void VCS_SOLVE::vcs_dfe(const int stateCalc,
         writelogendl();
     }
 
+    m_TmpPhase.assign(m_numPhases, 0.0);
     double* tlogMoles = &m_TmpPhase[0];
 
     // Might as well recalculate the phase mole vector and compare to the stored