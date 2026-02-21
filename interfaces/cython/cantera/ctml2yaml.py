59407c60124dcd08b192681591b52bbcfcbf32ef
Ray Speth <speth@mit.edu>
2026-02-20 22:26:42 -0500

    Bump version to 3.2.1b1

--- interfaces/cython/cantera/ctml2yaml.py
+++ interfaces/cython/cantera/ctml2yaml.py
@@ -2736,7 +2736,7 @@ def convert(
     metadata: CommentedMap = BlockMap(
         {
             "generator": "ctml2yaml",
-            "cantera-version": "3.2.0",
+            "cantera-version": "3.2.1b1",
             "date": formatdate(localtime=True),
         }
     )
