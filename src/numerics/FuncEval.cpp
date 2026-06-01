5c370e30369b0c3d0710f886aaa3b50659d99038
Ray Speth <speth@mit.edu>
2026-05-31 23:50:37 -0400

    [Doc] Fix missing/incorrect file Doxygen comments

--- src/numerics/FuncEval.cpp
+++ src/numerics/FuncEval.cpp
@@ -1,3 +1,8 @@
+//! @file FuncEval.cpp
+
+// This file is part of Cantera. See License.txt in the top-level directory or
+// at https://cantera.org/license.txt for license and copyright information.
+
 #include "cantera/numerics/FuncEval.h"
 #include <sstream>
 
