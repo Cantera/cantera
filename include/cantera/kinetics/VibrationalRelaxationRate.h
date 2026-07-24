77902ad36feb3c30410f37a057b93f2f210b910a
Gaetanosaure <gaetan.bizot@orange.fr>
2026-07-24 10:58:22 +0200

    corrected the exponents on K in documentation of VibrationalRelaxationRate.h

--- include/cantera/kinetics/VibrationalRelaxationRate.h
+++ include/cantera/kinetics/VibrationalRelaxationRate.h
@@ -50,7 +50,7 @@ struct DetailedVibData : public ReactionData
  *     + E T^{-z}
  * \right)
  * @f]
- * where `T` is the gas temperature in K.
+ * where `T` is the gas temperature in Kelvin (K).
  *
  * The `constant` model relaxes the vibrational species with a constant rate 
  * coefficient. It could just as well be an Arrhenius rate, but the constant 
@@ -85,9 +85,9 @@ struct DetailedVibData : public ReactionData
  * - `A` uses standard Cantera rate coefficient units. Its units depend on the
  *   reaction order and are converted by `ArrheniusBase`.
  * - `b`, `B`, `m`, `z` are dimensionless.
- * - `C` is interpreted as K^(1/3), assuming `T` is in K.
- * - `D` is interpreted as K^m, assuming `T` is in K.
- * - `E` is interpreted as K^z, assuming `T` is in K.
+ * - `C` is interpreted as @f$ K^{-1/3} @f$, assuming `T` is in K.
+ * - `D` is interpreted as @f$ K^{m} @f$, assuming `T` is in K.
+ * - `E` is interpreted as @f$ K^{z} @f$, assuming `T` is in K.
  *
  * The coefficients `B`, `C`, `D`, `E`, `m`, `z` are read as
  * raw floating-point values. They are not converted by Cantera's unit system.
